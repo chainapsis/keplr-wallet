502740f14d2613b99b676a178532b4a9d9297670
yjh <yunjh1994@gmail.com>
2019-08-30 21:56:24 +0900

    Add address component

---
+++ src/components/address/index.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { ToolTip } from "../tooltip";
+
+export interface AddressProps {
+  maxCharacters: number;
+  children: string;
+  tooltipFontSize?: string;
+  tooltipAddress?: string;
+}
+
+export class Address extends React.Component<AddressProps> {
+  render() {
+    const { tooltipFontSize, tooltipAddress, children } = this.props;
+
+    return (
+      <ToolTip
+        options={{ placement: "top" }}
+        tooltip={
+          <div
+            className="address-tooltip"
+            style={{ fontSize: tooltipFontSize }}
+          >
+            {tooltipAddress ? tooltipAddress : children}
+          </div>
+        }
+      >
+        {this.shortenAddress(children)}
+      </ToolTip>
+    );
+  }
+
+  shortenAddress(bech32: string): string {
+    let { maxCharacters } = this.props;
+
+    if (maxCharacters >= bech32.length) {
+      return bech32;
+    }
+
+    const i = bech32.indexOf("1");
+    const prefix = bech32.slice(0, i);
+    const address = bech32.slice(i + 1);
+
+    maxCharacters -= prefix.length;
+    maxCharacters -= 3; // For "..."
+    maxCharacters -= 1; // For "1"
+
+    if (maxCharacters <= 0) {
+      return "";
+    }
+
+    const mid = Math.floor(address.length / 2);
+    let former = address.slice(0, mid);
+    let latter = address.slice(mid);
+
+    while (maxCharacters < former.length + latter.length) {
+      if ((former.length + latter.length) % 2 === 1 && former.length > 0) {
+        former = former.slice(0, former.length - 1);
+      } else {
+        latter = latter.slice(1);
+      }
+    }
+
+    return prefix + "1" + former + "..." + latter;
+  }
+}
