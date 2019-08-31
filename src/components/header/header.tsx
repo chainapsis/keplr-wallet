ea6eae5e609f1effbd9051f78e509996512c63fd
yjh <yunjh1994@gmail.com>
2019-08-31 14:45:37 +0900

    Fix header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,18 +5,19 @@ import "./style";
 
 export interface HeaderProps {
   fixed?: boolean;
-  left: React.ReactNode;
+  left?: React.ReactNode;
+  right?: React.ReactNode;
 }
 
 export class Header extends React.Component<HeaderProps> {
   render() {
-    const { fixed, left } = this.props;
+    const { fixed, left, right, children } = this.props;
 
     return (
       <div className={classnames(["header", { fixed }])}>
         <div className="header-menu-left">{left}</div>
-        <div className="header-section">section</div>
-        <div className="header-menu-right">right</div>
+        <div className="header-section">{children}</div>
+        <div className="header-menu-right">{right}</div>
       </div>
     );
   }
