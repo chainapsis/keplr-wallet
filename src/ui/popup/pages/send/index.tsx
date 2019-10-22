8bb020cdce4cda1d1fded4f11a75fbf3fb78ad2d
Thunnini <yunjh1994@gmail.com>
2019-10-23 02:23:31 +0900

    Improve send page

--- src/ui/popup/pages/send/index.tsx
+++ src/ui/popup/pages/send/index.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, useState } from "react";
-import { Form, Input, Label } from "../../../components/form";
+import { Input } from "../../../components/form";
 import { RouteComponentProps } from "react-router-dom";
 import { useStore } from "../../stores";
 
@@ -28,105 +28,147 @@ import { Coin } from "@everett-protocol/cosmosjs/common/coin";
 import { Int } from "@everett-protocol/cosmosjs/common/int";
 
 import bigInteger from "big-integer";
+import useForm from "react-hook-form";
+import { observer } from "mobx-react";
+import { Button } from "../../../components/button";
+
+interface FormData {
+  recipient: string;
+  amount: string;
+}
 
 // Don't need to be observer
-export const SendPage: FunctionComponent<RouteComponentProps> = ({
-  history
-}) => {
-  const { chainStore, accountStore } = useStore();
+export const SendPage: FunctionComponent<RouteComponentProps> = observer(
+  ({ history }) => {
+    const { register, handleSubmit, errors } = useForm<FormData>({
+      defaultValues: {
+        recipient: "",
+        amount: ""
+      }
+    });
 
-  const [recipient, setRecipient] = useState("");
-  const [amount, setAmount] = useState("");
+    const { chainStore, accountStore } = useStore();
+    const [loading, setLoading] = useState(false);
 
-  return (
-    <HeaderLayout
-      showChainName
-      canChangeChainInfo={false}
-      onBackButton={() => {
-        history.goBack();
-      }}
-    >
-      <Form
-        onSubmit={async e => {
-          e.preventDefault();
-
-          const cosmosjs = new Api<Rest>(
-            {
-              chainId: chainStore.chainInfo.chainId,
-              walletProvider: new PopupWalletProvider({
-                onRequestSignature: (index: string) => {
-                  history.push(`/sign/${index}`);
-                }
-              }),
-              rpc: chainStore.chainInfo.rpc,
-              // No need.
-              rest: "",
-              disableGlobalBech32Config: true
-            },
-            {
-              txEncoder: defaultTxEncoder,
-              txBuilder: stdTxBuilder,
-              restFactory: (context: Context) => {
-                return new GaiaRest(context);
-              },
-              queryAccount: (
-                context: Context,
-                address: string | Uint8Array
-              ): Promise<Account> => {
-                return queryAccount(
-                  context.get("bech32Config"),
-                  context.get("rpcInstance"),
-                  address
-                );
-              },
-              bech32Config: chainStore.chainInfo.bech32Config,
-              bip44: chainStore.chainInfo.bip44,
-              registerCodec: (codec: Codec) => {
-                CmnCdc.registerCodec(codec);
-                Crypto.registerCodec(codec);
-                // XXX: If cosmos-sdk/MsgSend has disambiguation bytes, it will not work
-                Bank.registerCodec(codec);
-              }
-            }
-          );
-
-          await cosmosjs.enable();
-
-          // eslint-disable-next-line react-hooks/rules-of-hooks
-          useBech32Config(chainStore.chainInfo.bech32Config, () => {
-            const msg = new MsgSend(
-              AccAddress.fromBech32(accountStore.bech32Address),
-              AccAddress.fromBech32(recipient),
-              [Coin.parse(amount)]
-            );
-            cosmosjs.sendMsgs([msg], {
-              gas: bigInteger(60000),
-              memo: "test",
-              fee: new Coin("uatom", new Int("111"))
-            });
-          });
+    return (
+      <HeaderLayout
+        showChainName
+        canChangeChainInfo={false}
+        onBackButton={() => {
+          history.goBack();
         }}
       >
-        <Label>Recipient</Label>
-        <Input
-          type="text"
-          required
-          value={recipient}
-          onChange={e => {
-            setRecipient(e.target.value);
-          }}
-        />
-        <Label>Amount</Label>
-        <Input
-          type="text"
-          required
-          value={amount}
-          onChange={e => {
-            setAmount(e.target.value);
-          }}
-        />
-        <Input type="submit" value="Submit" />
-      </Form>
-    </HeaderLayout>
-  );
-};
+        <form
+          onSubmit={handleSubmit(async data => {
+            setLoading(true);
+
+            const cosmosjs = new Api<Rest>(
+              {
+                chainId: chainStore.chainInfo.chainId,
+                walletProvider: new PopupWalletProvider({
+                  onRequestSignature: (index: string) => {
+                    history.push(`/sign/${index}`);
+                  }
+                }),
+                rpc: chainStore.chainInfo.rpc,
+                // No need.
+                rest: "",
+                disableGlobalBech32Config: true
+              },
+              {
+                txEncoder: defaultTxEncoder,
+                txBuilder: stdTxBuilder,
+                restFactory: (context: Context) => {
+                  return new GaiaRest(context);
+                },
+                queryAccount: (
+                  context: Context,
+                  address: string | Uint8Array
+                ): Promise<Account> => {
+                  return queryAccount(
+                    context.get("bech32Config"),
+                    context.get("rpcInstance"),
+                    address
+                  );
+                },
+                bech32Config: chainStore.chainInfo.bech32Config,
+                bip44: chainStore.chainInfo.bip44,
+                registerCodec: (codec: Codec) => {
+                  CmnCdc.registerCodec(codec);
+                  Crypto.registerCodec(codec);
+                  // XXX: If cosmos-sdk/MsgSend has disambiguation bytes, it will not work
+                  Bank.registerCodec(codec);
+                }
+              }
+            );
+
+            await cosmosjs.enable();
+
+            // eslint-disable-next-line react-hooks/rules-of-hooks
+            useBech32Config(chainStore.chainInfo.bech32Config, () => {
+              const msg = new MsgSend(
+                AccAddress.fromBech32(accountStore.bech32Address),
+                AccAddress.fromBech32(data.recipient),
+                [Coin.parse(data.amount)]
+              );
+              cosmosjs.sendMsgs([msg], {
+                gas: bigInteger(60000),
+                memo: "test",
+                fee: new Coin("uatom", new Int("111"))
+              });
+            });
+          })}
+        >
+          <Input
+            type="text"
+            label="Recipient"
+            name="recipient"
+            error={errors.recipient && errors.recipient.message}
+            ref={register({
+              required: "Recipient is required",
+              validate: value => {
+                // This is not react hook.
+                // eslint-disable-next-line react-hooks/rules-of-hooks
+                return useBech32Config(
+                  chainStore.chainInfo.bech32Config,
+                  () => {
+                    try {
+                      AccAddress.fromBech32(value);
+                    } catch (e) {
+                      return "Invalid address";
+                    }
+                  }
+                );
+              }
+            })}
+          />
+          <Input
+            type="text"
+            label="Amount"
+            name="amount"
+            error={errors.amount && errors.amount.message}
+            ref={register({
+              required: "Amount is required",
+              validate: value => {
+                try {
+                  Coin.parse(value);
+                } catch (e) {
+                  return "Invalid amount";
+                }
+              }
+            })}
+          />
+          <Button
+            type="submit"
+            color="primary"
+            size="medium"
+            fullwidth
+            loading={loading}
+          >
+            Send
+          </Button>
+        </form>
+      </HeaderLayout>
+    );
+  }
+);
