0485cf93d2765a685fd6ca6f09dcd3b290376a98
Thunnini <yunjh1994@gmail.com>
2019-10-23 02:11:09 +0900

    Improve button

--- src/ui/components/button/button.tsx
+++ src/ui/components/button/button.tsx
@@ -10,12 +10,16 @@ import {
 import { Link } from "react-router-dom";
 
 export interface ButtonProps extends ButtonHTMLAttributes<any> {
+  className?: string;
   size?: Size;
   color?: Color;
+  /**
+   * If "to" prop is defined, button will be Link for react-router.
+   */
   to?: string;
   href?: string;
   target?: string;
-  fullWidth?: boolean;
+  fullwidth?: boolean;
   outline?: boolean;
   inverted?: boolean;
   rounded?: boolean;
@@ -31,7 +35,7 @@ export const Button: FunctionComponent<ButtonProps> = props => {
     to,
     href,
     target,
-    fullWidth,
+    fullwidth,
     outline,
     inverted,
     rounded,
@@ -40,40 +44,55 @@ export const Button: FunctionComponent<ButtonProps> = props => {
     loading
   } = props;
 
+  const attributes = { ...props };
+  delete attributes.size;
+  delete attributes.color;
+  delete attributes.to;
+  delete attributes.href;
+  delete attributes.target;
+  // This is necessary because undefined boolean attributes make error.
+  delete attributes.fullwidth;
+  delete attributes.outline;
+  delete attributes.inverted;
+  delete attributes.rounded;
+  delete attributes.focused;
+  delete attributes.active;
+  delete attributes.loading;
+  delete attributes.children;
+
+  const className = classnames(props.className, [
+    "button",
+    getSizeClass(size),
+    getColorClass(color),
+    { "is-fullwidth": fullwidth },
+    { "is-outline": outline },
+    { "is-inverted": inverted },
+    { "is-rounded": rounded },
+    { "is-focused": focused },
+    { "is-active": active },
+    { "is-loading": loading }
+  ]);
+
   return (
     <>
       {to ? (
         <Link
-          {...props}
-          className={classnames(props.className, [
-            "button",
-            getSizeClass(size),
-            getColorClass(color),
-            { "is-fullwidth": fullWidth },
-            { "is-outline": outline },
-            { "is-inverted": inverted },
-            { "is-rounded": rounded },
-            { "is-focused": focused },
-            { "is-active": active },
-            { "is-loading": loading }
-          ])}
+          {...attributes}
+          className={className}
           to={to}
           target={target}
           href={href}
         >
           {props.children}
         </Link>
-      ) : (
-        <a
-          {...props}
-          className={classnames(props.className, [
-            "button",
-            getSizeClass(size),
-            getColorClass(color)
-          ])}
-        >
+      ) : href ? (
+        <a {...attributes} className={className} href={href} target={target}>
           {props.children}
         </a>
+      ) : (
+        <button {...attributes} className={className}>
+          {props.children}
+        </button>
       )}
     </>
   );
