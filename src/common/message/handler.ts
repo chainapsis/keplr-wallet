fdaabb224954d49cf1836702d3f6425246928caa
Thunnini <yunjh1994@gmail.com>
2019-11-13 21:55:16 +0900

    Add type `internal handler` for routing logic with type check

--- src/common/message/handler.ts
+++ src/common/message/handler.ts
@@ -1,3 +1,8 @@
 import { Message } from "./message";
 
 export type Handler = (msg: Message<unknown>) => any;
+export type InternalHandler<M extends Message<unknown>> = (
+  msg: M
+) =>
+  | (M extends Message<infer R> ? R : never)
+  | (Promise<M extends Message<infer R> ? R : never>);
