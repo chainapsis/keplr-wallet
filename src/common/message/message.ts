b90c5159ac468074735abbd0991c9a52f5177db2
Thunnini <yunjh1994@gmail.com>
2019-11-13 12:52:44 +0900

    Add generic type of Result to message

--- src/common/message/message.ts
+++ src/common/message/message.ts
@@ -1,18 +1,28 @@
-/*
- This messaging system is influenced by cosmos-sdk.
- The messages are processed in the following order:
- "deserialze message -> validate basic -> handler by routing".
- This deserializing system has weak polymorphism feature.
- Message would be converted to object according to their class and registered type.
- But, nested class is not supported. Message's fields should be primitive types
- and array should not include class.
+/**
+ * This messaging system is influenced by cosmos-sdk.
+ * The messages are processed in the following order:
+ * "deserialze message -> approve external -> validate basic -> handler by routing".
+ * This deserializing system has weak polymorphism feature.
+ * Message would be converted to object according to their class and registered type.
+ * But, nested class is not supported. Non primitivie types or array that includes non primitive types in message's fields
+ * can't be decoded to their type properly. In this case, user should set thier prototype manually.
  */
-export abstract class Message {
+export abstract class Message<R> {
+  /**
+   * It is needed to infer the type of result from messaging in order to use message with easy and safe type checking.
+   * However, typescript doesn't infer the type of result if generic R is not used in structure due to its structural typing system.
+   * So, we need to use generic R though there is no need to use generic R in structure.
+   * This is just dummy field for generic R, and actually it is never used.
+   */
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  private resultType: R;
+
   abstract validateBasic(): void;
   abstract route(): string;
   abstract type(): string;
-  /*
-   Ask for approval if message is sent externally.
+  /**
+   * Ask for approval if message is sent externally.
    */
   approveExternal(sender: chrome.runtime.MessageSender): boolean {
     if (!sender.url) {
