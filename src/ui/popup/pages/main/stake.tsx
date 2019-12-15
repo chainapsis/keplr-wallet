27dc3d79df9d6b96fa4548685b65769ec5a78930
Thunnini <yunjh1994@gmail.com>
2019-12-15 22:05:34 +0900

    Make the stake section in main page to be able to send withdraw rewards msg

--- src/ui/popup/pages/main/stake.tsx
+++ src/ui/popup/pages/main/stake.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useCallback, useState } from "react";
 
 import { Button } from "../../../components/button";
 
@@ -10,16 +10,137 @@ import { observer } from "mobx-react";
 import styleStake from "./stake.module.scss";
 import classnames from "classnames";
 import { Dec } from "@everett-protocol/cosmosjs/common/decimal";
-import { getCurrencyFromDenom } from "../../../../common/currency";
+import { getCurrency, getCurrencyFromDenom } from "../../../../common/currency";
 import { Currency } from "../../../../chain-info";
+import { Msg } from "@everett-protocol/cosmosjs/core/tx";
+import { MsgWithdrawDelegatorReward } from "@everett-protocol/cosmosjs/x/distribution";
+import {
+  AccAddress,
+  useBech32Config,
+  ValAddress
+} from "@everett-protocol/cosmosjs/common/address";
+import { useCosmosJS } from "../../../hooks";
+import { PopupWalletProvider } from "../../wallet-provider";
+import { TxBuilderConfig } from "@everett-protocol/cosmosjs/core/txBuilder";
+import bigInteger from "big-integer";
+import { Coin } from "@everett-protocol/cosmosjs/common/coin";
+import { Int } from "@everett-protocol/cosmosjs/common/int";
+import { CoinUtils } from "../../../../common/coin-utils";
+import { sendMessage } from "../../../../common/message";
+import {
+  ApproveSignMsg,
+  ApproveTxBuilderConfigMsg,
+  GetRequestedTxBuilderConfigMsg
+} from "../../../../background/keyring";
+import { BACKGROUND_PORT } from "../../../../common/message/constant";
+import { useNotification } from "../../../components/notification";
 
 export const StakeView: FunctionComponent = observer(() => {
   const { chainStore, accountStore } = useStore();
+
+  const [walletProvider] = useState(
+    // Skip the approving for withdrawing rewards.
+    new PopupWalletProvider(
+      {
+        onRequestTxBuilderConfig: async (chainId: string) => {
+          const msg = GetRequestedTxBuilderConfigMsg.create(chainId);
+          const result = await sendMessage(BACKGROUND_PORT, msg);
+          await sendMessage(
+            BACKGROUND_PORT,
+            ApproveTxBuilderConfigMsg.create({
+              chainId,
+              ...result.config
+            })
+          );
+        }
+      },
+      {
+        onRequestSignature: async (index: string) => {
+          await sendMessage(BACKGROUND_PORT, ApproveSignMsg.create(index));
+        }
+      }
+    )
+  );
+  const cosmosJS = useCosmosJS(chainStore.chainInfo, walletProvider);
+
+  const notification = useNotification();
 
   const reward = useReward(
     chainStore.chainInfo.rest,
     accountStore.bech32Address
   );
+
+  const withdrawAllRewards = useCallback(() => {
+    if (reward.rewards.length > 0 && accountStore.bech32Address) {
+      const msgs: Msg[] = [];
+
+      for (const r of reward.rewards) {
+        // This is not react hooks.
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        useBech32Config(chainStore.chainInfo.bech32Config, () => {
+          const msg = new MsgWithdrawDelegatorReward(
+            AccAddress.fromBech32(accountStore.bech32Address),
+            ValAddress.fromBech32(r.validator_address)
+          );
+
+          msgs.push(msg);
+        });
+      }
+
+      if (msgs.length > 0) {
+        if (cosmosJS.sendMsgs) {
+          const config: TxBuilderConfig = {
+            gas: bigInteger(110000 * msgs.length),
+            memo: "",
+            fee: new Coin(
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              getCurrency(
+                chainStore.chainInfo.nativeCurrency
+              )!.coinMinimalDenom,
+              new Int("1000")
+            )
+          };
+
+          cosmosJS.sendMsgs(
+            msgs,
+            config,
+            () => {
+              notification.push({
+                placement: "top-center",
+                type: "success",
+                duration: 2,
+                content: "Reward withdrawn!",
+                canDelete: true,
+                transition: {
+                  duration: 0.25
+                }
+              });
+            },
+            e => {
+              notification.push({
+                placement: "top-center",
+                type: "warning",
+                duration: 2,
+                content: `Fail to withdraw rewards: ${e.message}`,
+                canDelete: true,
+                transition: {
+                  duration: 0.25
+                }
+              });
+            },
+            "commit"
+          );
+        }
+      }
+    }
+  }, [
+    reward.rewards,
+    accountStore.bech32Address,
+    chainStore.chainInfo.bech32Config,
+    chainStore.chainInfo.nativeCurrency,
+    cosmosJS,
+    notification
+  ]);
 
   let isRewardExist = false;
   let rewardCurrency: Currency | undefined;
@@ -38,14 +159,29 @@ export const StakeView: FunctionComponent = observer(() => {
             <div className={styleStake.vertical}>
               <div className={styleStake.title}>Pending Staking Reward</div>
               <div className={styleStake.content}>
-                {`${new Dec(reward.totalReward[0].amount).toString(3)} ${
+                {`${CoinUtils.shrinkDecimals(
+                  new Dec(reward.totalReward[0].amount).truncate(),
+                  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                  rewardCurrency!.coinDecimals,
+                  0,
+                  3
+                )} ${
                   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                   rewardCurrency!.coinDenom
                 }`}
               </div>
             </div>
             <div style={{ flex: 1 }} />
-            <Button color="primary">Claim</Button>
+            <Button
+              color="primary"
+              disabled={cosmosJS == null || cosmosJS.sendMsgs == null}
+              loading={cosmosJS.loading}
+              onClick={() => {
+                withdrawAllRewards();
+              }}
+            >
+              Claim
+            </Button>
           </div>
           <hr className={styleStake.hr} />
         </>
