fd6ff52fc408a1afd90c204dc68e099c666545a5
Thunnini <yunjh1994@gmail.com>
2019-12-27 16:16:59 +0900

    Add withdraw all rewards button to stake page in web

--- src/ui/web/sections/cosmos/stake/index.tsx
+++ src/ui/web/sections/cosmos/stake/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useCallback } from "react";
 
 import { Card } from "../../../components/card";
 
@@ -15,6 +15,20 @@ import { TopValidators } from "./top-validators";
 import { Delegations } from "./delegations";
 import { useUnbondingInfos } from "../../../../hooks/use-unbonding-info";
 import { useReward } from "../../../../hooks/use-reward";
+import { Button } from "../../../../components/button";
+import { Msg } from "@everett-protocol/cosmosjs/core/tx";
+import { Dec } from "@everett-protocol/cosmosjs/common/decimal";
+import { Int } from "@everett-protocol/cosmosjs/common/int";
+import {
+  AccAddress,
+  useBech32Config,
+  ValAddress
+} from "@everett-protocol/cosmosjs/common/address";
+import { MsgWithdrawDelegatorReward } from "@everett-protocol/cosmosjs/x/distribution";
+import { TxBuilderConfig } from "@everett-protocol/cosmosjs/core/txBuilder";
+import bigInteger from "big-integer";
+import { Coin } from "@everett-protocol/cosmosjs/common/coin";
+import { useNotification } from "../../../../components/notification";
 
 export const StakeSection: FunctionComponent = observer(() => {
   const { chainStore } = useStore();
@@ -43,11 +57,108 @@ export const StakeSection: FunctionComponent = observer(() => {
     chainStore.chainInfo.nativeCurrency
   ) as Currency;
 
+  const notification = useNotification();
+
+  const withdrawAllRewards = useCallback(() => {
+    if (rewards.length > 0 && cosmosJS.addresses.length > 0) {
+      const msgs: Msg[] = [];
+
+      for (const r of rewards) {
+        let rewardExist = false;
+        if (r.reward) {
+          for (const reward of r.reward) {
+            const dec = new Dec(reward.amount);
+            if (dec.truncate().gt(new Int(0))) {
+              rewardExist = true;
+              break;
+            }
+          }
+        }
+
+        if (rewardExist) {
+          // This is not react hooks.
+          // eslint-disable-next-line react-hooks/rules-of-hooks
+          useBech32Config(chainStore.chainInfo.bech32Config, () => {
+            const msg = new MsgWithdrawDelegatorReward(
+              AccAddress.fromBech32(cosmosJS.addresses[0]),
+              ValAddress.fromBech32(r.validator_address)
+            );
+
+            msgs.push(msg);
+          });
+        }
+      }
+
+      if (msgs.length > 0) {
+        if (cosmosJS.sendMsgs) {
+          const config: TxBuilderConfig = {
+            gas: bigInteger(140000 * msgs.length),
+            memo: "",
+            fee: new Coin(nativeCurrency.coinMinimalDenom, new Int("1000"))
+          };
+
+          cosmosJS.sendMsgs(
+            msgs,
+            config,
+            () => {
+              notification.push({
+                type: "success",
+                content: "Tx succeeds",
+                duration: 5,
+                canDelete: true,
+                placement: "top-right",
+                transition: {
+                  duration: 0.25
+                }
+              });
+            },
+            (e: Error) => {
+              notification.push({
+                type: "danger",
+                content: e.toString(),
+                duration: 5,
+                canDelete: true,
+                placement: "top-right",
+                transition: {
+                  duration: 0.25
+                }
+              });
+            },
+            "commit"
+          );
+        }
+      }
+    }
+  }, [
+    chainStore.chainInfo.bech32Config,
+    cosmosJS,
+    nativeCurrency.coinMinimalDenom,
+    notification,
+    rewards
+  ]);
+
+  const cosmosJSInited = cosmosJS.sendMsgs && cosmosJS.addresses.length > 0;
+
   return (
     <div className={style.container}>
       {delegateInfos.length > 0 ? (
         <Card>
-          <div className={style.title}>My validators</div>
+          <div className={style.title}>
+            My validators
+            <div style={{ float: "right" }}>
+              <Button
+                className={style.button}
+                color="primary"
+                loading={cosmosJS.loading}
+                disabled={!cosmosJSInited}
+                onClick={() => {
+                  withdrawAllRewards();
+                }}
+              >
+                CLAIM ALL REWARD
+              </Button>
+            </div>
+          </div>
           <Delegations
             validators={validator.validators}
             delegations={delegateInfos}
