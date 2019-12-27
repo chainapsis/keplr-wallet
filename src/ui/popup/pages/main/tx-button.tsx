f4e980d9d18687312d6ecf0ea50378add6597926
Thunnini <yunjh1994@gmail.com>
2019-12-27 22:40:11 +0900

    Add copy address button to deposit modal

--- src/ui/popup/pages/main/tx-button.tsx
+++ src/ui/popup/pages/main/tx-button.tsx
@@ -1,14 +1,22 @@
-import React, { FunctionComponent, useEffect, useRef, useState } from "react";
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState
+} from "react";
 
 import styleTxButton from "./tx-button.module.scss";
 
 import { Button } from "../../../components/button";
+import { Address } from "../../../components/address";
 
 import { observer } from "mobx-react";
 
 import { useStore } from "../../stores";
 
 import Modal from "react-modal";
+import { useNotification } from "../../../components/notification";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const QrCode = require("qrcode");
@@ -24,13 +32,47 @@ const DepositModal: FunctionComponent<{
     }
   }, [bech32Address]);
 
+  const notification = useNotification();
+
+  const copyAddress = useCallback(async () => {
+    await navigator.clipboard.writeText(bech32Address);
+    // TODO: Show success tooltip.
+    notification.push({
+      placement: "top-center",
+      type: "success",
+      duration: 2,
+      content: "Address copied!",
+      canDelete: true,
+      transition: {
+        duration: 0.25
+      }
+    });
+  }, [notification, bech32Address]);
+
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "250px" }}>
+    <div
+      style={{ display: "flex", flexDirection: "column", height: "250px" }}
+      className={styleTxButton.depositModal}
+    >
       <div style={{ flex: 1 }} />
       <div style={{ display: "flex" }}>
         <div style={{ flex: 1 }} />
         <canvas id="qrcode" ref={qrCodeRef} />
         <div style={{ flex: 1 }} />
+      </div>
+      <div
+        className={styleTxButton.address}
+        onClick={() => {
+          copyAddress();
+        }}
+      >
+        <Address
+          maxCharacters={28}
+          lineBreakBeforePrefix={false}
+          tooltipFontSize="12px"
+        >
+          {bech32Address}
+        </Address>
       </div>
       <div style={{ flex: 1 }} />
     </div>
@@ -45,7 +87,7 @@ export const TxButtonView: FunctionComponent = observer(() => {
   return (
     <div className={styleTxButton.containerTxButton}>
       <Modal
-        style={{ content: { width: "300px", minWidth: "300px" } }}
+        style={{ content: { width: "330px", minWidth: "330px" } }}
         isOpen={isDepositOpen}
         onRequestClose={() => {
           setIsDepositOpen(false);
