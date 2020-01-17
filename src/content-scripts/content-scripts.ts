406ff6c006a15f6fac9849edcfb9bc9de513efc6
Thunnini <yunjh1994@gmail.com>
2020-01-17 19:47:57 +0900

    Make the content scripts to handle both chrome api and other web extension api

--- src/content-scripts/content-scripts.ts
+++ src/content-scripts/content-scripts.ts
@@ -5,7 +5,11 @@ listenAndProxyMessages();
 const container = document.head || document.documentElement;
 const scriptElement = document.createElement("script");
 
-scriptElement.src = chrome.runtime.getURL("injectedScript.bundle.js");
+if (typeof chrome === "undefined") {
+  scriptElement.src = browser.runtime.getURL("injectedScript.bundle.js");
+} else {
+  scriptElement.src = chrome.runtime.getURL("injectedScript.bundle.js");
+}
 scriptElement.type = "text/javascript";
 container.insertBefore(scriptElement, container.children[0]);
 scriptElement.remove();
