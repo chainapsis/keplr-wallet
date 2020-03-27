338ac9b5960ad1431efe046d532f051863302f30
Thunnini <yunjh1994@gmail.com>
2020-03-27 17:22:16 +0900

    Improve the intl provider

--- src/ui/popup/language.tsx
+++ src/ui/popup/language.tsx
@@ -1,4 +1,9 @@
-import React, { FunctionComponent } from "react";
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useState
+} from "react";
 import { IntlProvider } from "react-intl";
 
 import MessagesEn from "./languages/en.json";
@@ -9,25 +14,60 @@ const messages: { [lang: string]: Record<string, string> } = {
   ko: MessagesKo
 };
 
-function getMessages(): { language: string; messages: Record<string, string> } {
-  const language = navigator.language.split(/[-_]/)[0]; // language without region code
-
-  if (!messages[language]) {
-    return { language: "en", messages: messages["en"] };
-  }
-
-  return {
-    language,
-    messages: Object.assign(messages["en"], messages[language])
-  };
+function getMessages(language: string): Record<string, string> {
+  return Object.assign(MessagesEn, messages[language]);
 }
 
+function initLanguage(): string {
+  const language =
+    localStorage.getItem("language") || navigator.language.split(/[-_]/)[0]; // language without region code
+
+  if (!messages[language]) {
+    return "en";
+  }
+
+  return language;
+}
+
+interface Language {
+  language: string;
+  setLanguage: (language: string) => void;
+}
+
+const LanguageContext = React.createContext<Language | null>(null);
+
+export const useLanguage = (): Language => {
+  const lang = React.useContext(LanguageContext);
+  if (!lang) {
+    throw new Error("You have forgot to use intl provider");
+  }
+  return lang;
+};
+
 export const AppIntlProvider: FunctionComponent = props => {
-  const { language, messages } = getMessages();
+  const [language, setLanguage] = useState(initLanguage());
+  const [messages, setMessages] = useState(getMessages(language));
+
+  useEffect(() => {
+    document.body.setAttribute("data-lang", language);
+  }, [language]);
+
+  useEffect(() => {
+    setMessages(getMessages(language));
+  }, [language]);
+
+  const setLanguageCallback = useCallback((language: string) => {
+    localStorage.setItem("language", language);
+    setLanguage(language);
+  }, []);
 
   return (
-    <IntlProvider locale={language} messages={messages}>
-      {props.children}
-    </IntlProvider>
+    <LanguageContext.Provider
+      value={{ language: language, setLanguage: setLanguageCallback }}
+    >
+      <IntlProvider locale={language} messages={messages}>
+        {props.children}
+      </IntlProvider>
+    </LanguageContext.Provider>
   );
 };
