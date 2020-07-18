e67e5e9fe7f7aafbb05bd344d8b7e1746bc3dbcf
Thunnini <yunjh1994@gmail.com>
2020-07-18 23:59:37 +0900

    Redesign buttons in the register page

--- src/ui/popup/pages/register/intro.tsx
+++ src/ui/popup/pages/register/intro.tsx
@@ -2,36 +2,27 @@ import React, { FunctionComponent } from "react";
 
 import styleIntro from "./intro.module.scss";
 
-import { FormattedMessage, useIntl } from "react-intl";
+import { FormattedMessage } from "react-intl";
+import { Button } from "reactstrap";
 
 export const IntroInPage: FunctionComponent<{
   onRequestNewAccount: () => void;
   onRequestRecoverAccount: () => void;
 }> = props => {
-  const intl = useIntl();
-
   return (
-    <div>
-      <BigButton
-        icon="fa-plus"
-        title={intl.formatMessage({
-          id: "register.intro.button.new-account.title"
-        })}
-        content={intl.formatMessage({
-          id: "register.intro.button.new-account.content"
-        })}
-        onClick={props.onRequestNewAccount}
-      />
-      <BigButton
-        icon="fa-download"
-        title={intl.formatMessage({
-          id: "register.intro.button.import-account.title"
-        })}
-        content={intl.formatMessage({
-          id: "register.intro.button.import-account.content"
-        })}
+    <div className={styleIntro.innerContainer}>
+      <Button color="primary" outline block onClick={props.onRequestNewAccount}>
+        <FormattedMessage id="register.intro.button.new-account.title" />
+      </Button>
+      <Button
+        color="primary"
+        outline
+        block
+        style={{ marginLeft: 0 }}
         onClick={props.onRequestRecoverAccount}
-      />
+      >
+        <FormattedMessage id="register.intro.button.import-account.title" />
+      </Button>
       <div className={styleIntro.subContent}>
         <FormattedMessage
           id="register.intro.sub-content"
@@ -40,28 +31,6 @@ export const IntroInPage: FunctionComponent<{
           }}
         />
       </div>
-    </div>
-  );
-};
-
-const BigButton: FunctionComponent<{
-  icon: string;
-  title: string;
-  content: string;
-  onClick: () => void;
-}> = ({ icon, title, content, onClick }) => {
-  return (
-    <div className={styleIntro.bigButton} onClick={onClick}>
-      <span className={`icon is-medium ${styleIntro.icon}`}>
-        <i className={`fas fa-2x ${icon}`} />
-      </span>
-      <div className={styleIntro.description}>
-        <div className={styleIntro.title}>{title}</div>
-        <div className={styleIntro.content}>{content}</div>
-      </div>
-      <span className={`icon is-small ${styleIntro.arrow}`}>
-        <i className="fas fa-angle-right" />
-      </span>
     </div>
   );
 };
