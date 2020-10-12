8cc6d8e9fc6c3337be1191c47e27d8bd1178a7dc
Thunnini <yunjh1994@gmail.com>
2020-10-12 22:58:03 +0900

    Change the design of the card that shows the asset and the staked asset

--- src/ui/popup/pages/main/asset.tsx
+++ src/ui/popup/pages/main/asset.tsx
@@ -6,12 +6,155 @@ import { observer } from "mobx-react";
 import { useStore } from "../../stores";
 import styleAsset from "./asset.module.scss";
 import { CoinUtils } from "../../../../common/coin-utils";
-import { getFiatCurrencyFromLanguage } from "../../../../common/currency";
+import {
+  Currency,
+  FiatCurrency,
+  getFiatCurrencyFromLanguage
+} from "../../../../common/currency";
 
-import { FormattedMessage } from "react-intl";
-import { ToolTip } from "../../../components/tooltip";
 import { useLanguage } from "../../language";
 import { DecUtils } from "../../../../common/dec-utils";
+
+import { Doughnut } from "react-chartjs-2";
+import { Int } from "@chainapsis/cosmosjs/common/int";
+import { Price } from "../../stores/price";
+
+export const AssetStakedChartView: FunctionComponent<{
+  fiat: Price | undefined;
+  fiatCurrency: FiatCurrency;
+  stakeCurrency: Currency;
+  available: Int;
+  staked: Int;
+}> = ({ fiat, fiatCurrency, stakeCurrency, available, staked }) => {
+  const hasCoinGeckoId = stakeCurrency.coinGeckoId != null;
+
+  const availableDec = new Dec(available, stakeCurrency.coinDecimals);
+  const stakedDec = new Dec(staked, stakeCurrency.coinDecimals);
+  const totalDec = availableDec.add(stakedDec);
+
+  // If fiat value is fetched, show the value that is multiplied with amount and fiat value.
+  // If not, just show the amount of asset.
+  const data: number[] = [
+    fiat && !fiat.value.equals(new Dec(0))
+      ? parseFloat(availableDec.mul(fiat.value).toString())
+      : parseFloat(availableDec.toString()),
+    fiat && !fiat.value.equals(new Dec(0))
+      ? parseFloat(stakedDec.mul(fiat.value).toString())
+      : parseFloat(stakedDec.toString())
+  ];
+
+  return (
+    <React.Fragment>
+      <div style={{ position: "relative" }}>
+        <Doughnut
+          data={{
+            datasets: [
+              {
+                data,
+                backgroundColor: ["#5e72e4", "#11cdef"]
+              }
+            ],
+
+            labels: ["Available", "Staked"]
+          }}
+          options={{
+            rotation: 0.5 * Math.PI,
+            cutoutPercentage: 85,
+            legend: {
+              display: false
+            }
+          }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            textAlign: "center",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)"
+          }}
+        >
+          <div
+            style={{
+              fontSize: "13px",
+              color: "#32325d"
+            }}
+          >
+            Total Balance
+          </div>
+          <div
+            style={{
+              fontSize: "20px",
+              color: "#32325d"
+            }}
+          >
+            {fiat && !fiat.value.equals(new Dec(0))
+              ? fiatCurrency.symbol +
+                DecUtils.trim(
+                  fiatCurrency.parse(
+                    parseFloat(fiat.value.mul(totalDec).toString())
+                  )
+                )
+              : hasCoinGeckoId
+              ? "?"
+              : `${CoinUtils.shrinkDecimals(
+                  available.add(staked),
+                  stakeCurrency.coinDecimals,
+                  0,
+                  3
+                )} ${stakeCurrency.coinDenom.toUpperCase()}`}
+          </div>
+        </div>
+      </div>
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between"
+          }}
+        >
+          <div style={{ color: "#5e72e4", letterSpacing: "-0.3px" }}>
+            <span className="badge-dot badge badge-secondary">
+              <i className="bg-primary" />
+            </span>
+            Available
+          </div>
+          <div style={{ minWidth: "12px" }} />
+          <div
+            style={{ color: "#525f7f", letterSpacing: "-0.3px" }}
+          >{`${CoinUtils.shrinkDecimals(
+            available,
+            stakeCurrency.coinDecimals,
+            0,
+            4
+          )} ${stakeCurrency.coinDenom.toUpperCase()}`}</div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between"
+          }}
+        >
+          <div style={{ color: "#11cdef", letterSpacing: "-0.3px" }}>
+            <span className="badge-dot badge badge-secondary">
+              <i className="bg-info" />
+            </span>
+            Staked
+          </div>
+          <div style={{ minWidth: "12px" }} />
+          <div
+            style={{ color: "#525f7f", letterSpacing: "-0.3px" }}
+          >{`${CoinUtils.shrinkDecimals(
+            staked,
+            stakeCurrency.coinDecimals,
+            0,
+            4
+          )} ${stakeCurrency.coinDenom.toUpperCase()}`}</div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export const AssetView: FunctionComponent = observer(() => {
   const { chainStore, accountStore, priceStore } = useStore();
@@ -39,16 +182,18 @@ export const AssetView: FunctionComponent = observer(() => {
 
   const stakeCurrency = chainStore.chainInfo.stakeCurrency;
 
-  const coinAmount = CoinUtils.amountOf(
+  const availableAmount = CoinUtils.amountOf(
     accountStore.assets,
     stakeCurrency.coinMinimalDenom
   );
 
-  const hasCoinGeckoId = stakeCurrency.coinGeckoId != null;
+  const stakedAmount = accountStore.stakedAsset
+    ? accountStore.stakedAsset.amount
+    : new Int(0);
 
   return (
     <div className={styleAsset.containerAsset}>
-      <div className={styleAsset.title}>
+      {/*<div className={styleAsset.title}>
         <FormattedMessage id="main.account.message.available-balance" />
       </div>
       <div className={styleAsset.fiat}>
@@ -66,9 +211,15 @@ export const AssetView: FunctionComponent = observer(() => {
           : hasCoinGeckoId
           ? "?"
           : "-"}
-      </div>
-      {/* TODO: Show the information that account is fetching. */}
-      <div className={styleAsset.amount}>
+      </div>*/}
+      <AssetStakedChartView
+        fiat={fiat}
+        fiatCurrency={fiatCurrency}
+        stakeCurrency={stakeCurrency}
+        available={availableAmount}
+        staked={stakedAmount}
+      />
+      {/*<div className={styleAsset.amount}>
         <div>
           {!(accountStore.assets.length === 0)
             ? CoinUtils.shrinkDecimals(
@@ -99,7 +250,7 @@ export const AssetView: FunctionComponent = observer(() => {
             </ToolTip>
           ) : null}
         </div>
-      </div>
+      </div>*/}
     </div>
   );
 });
