62a6e578f2ae087b90d2211c0edd28b5bdfced79
Thunnini <yunjh1994@gmail.com>
2020-12-04 18:54:59 +0900

    `Decrypt` method on `KeplrEnigmaUtils` returns the empty Uint8Array if `ciphertext` is empty

--- src/content-scripts/inject/enigma-utils.ts
+++ src/content-scripts/inject/enigma-utils.ts
@@ -39,6 +39,10 @@ export class KeplrEnigmaUtils {
     ciphertext: Uint8Array,
     nonce: Uint8Array
   ): Promise<Uint8Array> => {
+    if (!ciphertext || ciphertext.length === 0) {
+      return new Uint8Array();
+    }
+
     return Buffer.from(
       await sendMessage(
         BACKGROUND_PORT,
