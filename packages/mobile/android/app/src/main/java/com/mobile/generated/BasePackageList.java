b49a86303e783702116f6f91113a0a1bfa3e0894
Thunnini <yunjh1994@gmail.com>
2021-02-28 22:13:56 +0900

    Update the gradle version and add the generated file for android build

---
+++ packages/mobile/android/app/src/main/java/com/mobile/generated/BasePackageList.java
@@ -0,0 +1,16 @@
+package com.mobile.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.permissions.PermissionsPackage()
+    );
+  }
+}
