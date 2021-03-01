22eb9a88eff65b993a9207ce17f33aad17d827cf
Thunnini <yunjh1994@gmail.com>
2021-03-01 19:38:31 +0900

    Remove unused messages

--- packages/background/src/tx/init.ts
+++ packages/background/src/tx/init.ts
@@ -1,17 +1,11 @@
 import { Router } from "@keplr-wallet/router";
-import {
-  RequestBackgroundTxMsg,
-  RequestBackgroundTxWithResultMsg,
-  SendTxMsg,
-} from "./messages";
+import { SendTxMsg } from "./messages";
 import { ROUTE } from "./constants";
 import { getHandler } from "./handler";
 import { BackgroundTxService } from "./service";
 
 export function init(router: Router, service: BackgroundTxService): void {
   router.registerMessage(SendTxMsg);
-  router.registerMessage(RequestBackgroundTxMsg);
-  router.registerMessage(RequestBackgroundTxWithResultMsg);
 
   router.addHandler(ROUTE, getHandler(service));
 }
