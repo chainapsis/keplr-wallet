2b96a59b9e92a9b248f3eacd00aaea65570401eb
Thunnini <yunjh1994@gmail.com>
2021-03-10 00:08:44 +0900

    Add the open graph meta tags to the docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,22 @@ module.exports = {
   base: process.env.VUEPRESS_BASE || "/",
   head: [
     ["link", { rel: "icon", type: "image/svg+xml", href: "/favicon-svg.svg" }],
+    ["meta", { property: "og:type", content: "website" }],
+    ["meta", { property: "og:url", content: "https://docs.keplr.app" }],
+    ["meta", { property: "og:title", content: "Documentation | Keplr Wallet" }],
+    [
+      "meta",
+      {
+        property: "og:description",
+        content:
+          "Keplr is a non-custodial blockchain wallets for webpages that allow users to interact with blockchain applications.",
+      },
+    ],
+    [
+      "meta",
+      { property: "og:image", content: "https://docs.keplr.app/og-image.jpg" },
+    ],
+    ["meta", { name: "twitter:card", content: "summary_large_image" }],
   ],
   themeConfig: {
     custom: true,
