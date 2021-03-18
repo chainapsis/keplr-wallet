eef16c74129a658690b64de61485a8649380300d
wseungjin <bose1021@naver.com>
2021-03-18 13:35:55 +0900

    add : add warning view for udpate password

---
+++ packages/extension/src/pages/setting/keyring/change/warning-view.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from "react";
+
+import styleWarningView from "./warning-view.module.scss";
+import { FormattedMessage } from "react-intl";
+
+export const WarningView: FunctionComponent = () => {
+  return (
+    <div className={styleWarningView.innerContainer}>
+      <img
+        className={styleWarningView.imgLock}
+        src={require("../../../../public/assets/img/icons8-lock.svg")}
+        alt="lock"
+      />
+      <p>
+        <FormattedMessage id="setting.keyring.change.password.warning" />
+      </p>
+    </div>
+  );
+};
