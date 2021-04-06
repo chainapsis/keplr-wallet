f7076fa0ad108fa5ae6b8f5ed271b7adfced4f57
Thunnini <yunjh1994@gmail.com>
2021-04-06 15:17:12 +0900

    Fix the `MainActivity.java` to handle the react-native-gesture and screens

--- packages/mobile/android/app/src/main/java/com/mobile/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/mobile/MainActivity.java
@@ -1,6 +1,11 @@
 package com.mobile;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +16,20 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "mobile";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
