00ee92c3eceb121075e103305c8f1658bd759aa5
wseungjin <bose1021@naver.com>
2021-04-09 16:49:28 +0900

    feat : add Address Component

---
+++ packages/mobile/src/components/address/index.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent } from "react";
+
+import { Text } from "react-native-elements";
+import { View } from "react-native";
+import { Bech32Address } from "@keplr-wallet/cosmos";
+
+export interface AddressProps {
+  maxCharacters: number;
+  address: string;
+}
+
+export const Address: FunctionComponent<AddressProps> = ({
+  maxCharacters,
+  address,
+}) => {
+  return (
+    <View
+      style={{
+        borderRadius: 100,
+        borderWidth: 1,
+        borderColor: "#e6e6e6",
+        paddingHorizontal: 16,
+      }}
+    >
+      <Text>{Bech32Address.shortenAddress(address, maxCharacters)}</Text>
+    </View>
+  );
+};
