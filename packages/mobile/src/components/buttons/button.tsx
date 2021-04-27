42014488a28d88941912d2c3d87852628cf59628
wseungjin <bose1021@naver.com>
2021-04-27 18:08:13 +0900

    refactor : use the infered props type `RNButton`

--- packages/mobile/src/components/buttons/button.tsx
+++ packages/mobile/src/components/buttons/button.tsx
@@ -1,10 +1,5 @@
 import React, { FunctionComponent } from "react";
-import {
-  StyleProp,
-  TextStyle,
-  ViewStyle,
-  GestureResponderEvent,
-} from "react-native";
+import { StyleProp, TextStyle, ViewStyle } from "react-native";
 import { Button as RNButton } from "react-native-elements";
 import {
   bw1,
@@ -18,40 +13,36 @@ import {
   bgcPrimary200,
 } from "../../styles";
 
-type DefalutButtonProps = {
-  containerStyle?: StyleProp<ViewStyle>[];
-  buttonStyle?: StyleProp<ViewStyle>[];
-  titleStyle?: StyleProp<TextStyle>[];
-  disabledStyle?: StyleProp<ViewStyle>[];
-  disabledTitleStyle?: StyleProp<TextStyle>[];
-  disabled?: boolean;
-  loading?: boolean;
-  title: string;
-  onPress?: (event: GestureResponderEvent) => void;
-};
+export const Button: FunctionComponent<
+  React.ComponentProps<typeof RNButton> & {
+    containerStyle?: StyleProp<ViewStyle>[];
+    buttonStyle?: StyleProp<ViewStyle>[];
+    titleStyle?: StyleProp<TextStyle>[];
+    disabledStyle?: StyleProp<ViewStyle>[];
+    disabledTitleStyle?: StyleProp<TextStyle>[];
+  }
+> = (props) => {
+  const attributes = { ...props };
+  delete attributes.containerStyle;
+  delete attributes.buttonStyle;
+  delete attributes.titleStyle;
+  delete attributes.disabledStyle;
+  delete attributes.disabledTitleStyle;
 
-export const Button: FunctionComponent<DefalutButtonProps> = ({
-  containerStyle = [],
-  titleStyle = [],
-  buttonStyle = [],
-  disabledStyle = [],
-  disabledTitleStyle = [],
-  title,
-  disabled,
-  loading,
-  onPress,
-}) => {
+  const containerStyle = props.containerStyle ?? [];
+  const buttonStyle = props.buttonStyle ?? [];
+  const titleStyle = props.titleStyle ?? [];
+  const disabledStyle = props.disabledStyle ?? [];
+  const disabledTitleStyle = props.disabledTitleStyle ?? [];
+
   return (
     <RNButton
+      {...attributes}
       containerStyle={sf([flex1, ...containerStyle])}
       buttonStyle={sf([bw1, br1, px4, py3, ...buttonStyle])}
       titleStyle={sf([buttonFont1, ...titleStyle])}
-      title={title}
-      disabled={disabled}
       disabledStyle={sf([bgcPrimary200, ...disabledStyle])}
       disabledTitleStyle={sf([fcWhite, ...disabledTitleStyle])}
-      onPress={onPress}
-      loading={loading}
     />
   );
 };
