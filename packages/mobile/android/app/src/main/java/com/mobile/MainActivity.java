96b08055f8747cb2a01c0d66a6642dc873aea4ef
wseungjin <bose1021@naver.com>
2021-05-07 13:29:52 +0900

    chore : add setting firebase android

--- packages/mobile/android/app/src/main/java/com/mobile/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/mobile/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mobile;
+package com.chainapsis.keplr;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- packages/mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -1,6 +1,6 @@
-package com.mobile;
+package com.chainapsis.keplr;
 
-import com.mobile.generated.BasePackageList;
+import com.chainapsis.keplr.generated.BasePackageList;
 
 import android.app.Application;
 import android.content.Context;
@@ -77,7 +77,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.mobile.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.chainapsis.keplr.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
