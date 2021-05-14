e382a0c135097ec1741a3f22df79af5e82329db1
wseungjin <bose1021@naver.com>
2021-05-14 15:42:01 +0900

    feat: add un,re,delegate scr to stakeScreenStack

--- packages/mobile/src/screens/stake/index.tsx
+++ packages/mobile/src/screens/stake/index.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/display-name */
 import React, { FunctionComponent } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { StakeScreen } from "./stake-screen";
-import { ValidatorDetailsScreen } from "./validator-details-screen";
-import { StakedDetailsScreen } from "./staked-details-screen";
+import { ValidatorListScreen } from "./validator-list";
+import { ValidatorDetailsScreen } from "./validator-details";
+import { StakedListScreen } from "./staked-list";
+import { DelegateScreen } from "./delegate";
 import { GradientBackground } from "../../components/svg";
+import { UndelegateScreen } from "./undelegate";
+import { RedelegateScreen, RedelegateValidatorScreen } from "./redelegate";
 
 const StakeStack = createStackNavigator();
 
@@ -16,14 +19,21 @@ export const StakeStackScreen: FunctionComponent = () => {
         headerBackTitleVisible: false,
       }}
     >
-      <StakeStack.Screen name="Stake" component={StakeScreen} />
+      <StakeStack.Screen
+        name="Validator List"
+        component={ValidatorListScreen}
+      />
       <StakeStack.Screen
         name="Validator Details"
         component={ValidatorDetailsScreen}
       />
+      <StakeStack.Screen name="Staked List" component={StakedListScreen} />
+      <StakeStack.Screen name="Delegate" component={DelegateScreen} />
+      <StakeStack.Screen name="Undelegate" component={UndelegateScreen} />
+      <StakeStack.Screen name="Redelegate" component={RedelegateScreen} />
       <StakeStack.Screen
-        name="Staking Details"
-        component={StakedDetailsScreen}
+        name="Redelegate Validator"
+        component={RedelegateValidatorScreen}
       />
     </StakeStack.Navigator>
   );
