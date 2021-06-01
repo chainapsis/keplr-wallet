efff30f60a0e861fa923ea2654e84f952f2f2112
wseungjin <bose1021@naver.com>
2021-06-01 19:04:50 +0900

    refactor : remove Home stack from main stack

--- packages/mobile/src/screens/home/index.tsx
+++ packages/mobile/src/screens/home/index.tsx
@@ -3,36 +3,18 @@ import React, { FunctionComponent, useLayoutEffect, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import { Button as RNButton, Card } from "react-native-elements";
 import { SafeAreaPage } from "../../components/page";
-import { createStackNavigator } from "@react-navigation/stack";
 import { DrawerActions, useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/Feather";
 import { AccountView } from "./account";
 import { AssetView } from "./asset";
 import { TxButtonView } from "./tx-button";
-import { GradientBackground } from "../../components/svg";
 import { StakingRewardsView } from "./staking-rewards";
 import { TokensView } from "./tokens";
 import { TotalStakedView } from "./total-staked";
 import { GovernanceView } from "./governance";
-import { sf, fcHigh, h3 } from "../../styles";
 import { useStore } from "../../stores";
 
-const HomeStack = createStackNavigator();
-
-export const HomeStackScreen: FunctionComponent = () => {
-  return (
-    <HomeStack.Navigator
-      screenOptions={{
-        headerBackground: () => <GradientBackground />,
-        headerTitleStyle: sf([h3, fcHigh]),
-      }}
-    >
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-    </HomeStack.Navigator>
-  );
-};
-
-const HomeScreen: FunctionComponent = observer(() => {
+export const HomeScreen: FunctionComponent = observer(() => {
   const navigation = useNavigation();
   const { chainStore } = useStore();
 
