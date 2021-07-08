5c6b819ffdaf54687b475ab4840d61a4f003a7f5
Thunnini <yunjh1994@gmail.com>
2021-07-08 19:39:42 +0900

    Use the custom logic if BigInt is not supported

--- packages/unit/src/coin-utils.ts
+++ packages/unit/src/coin-utils.ts
@@ -134,12 +134,35 @@ export class CoinUtils {
     const fractionStr = fraction.toString(decimals).replace("0.", "");
 
     const integerStr = locale
-      ? // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        BigInt(integer.toString()).toLocaleString("en-US")
+      ? CoinUtils.integerStringToUSLocaleString(integer.toString())
       : integer.toString();
 
     return integerStr + (fractionStr.length > 0 ? "." : "") + fractionStr;
+  }
+
+  /**
+   * Change the non-locale integer string to locale string.
+   * Only support en-US format.
+   * This method uses the BigInt if the environment supports the BigInt.
+   * @param numberStr
+   */
+  static integerStringToUSLocaleString(numberStr: string): string {
+    if (numberStr.indexOf(".") >= 0) {
+      throw new Error(`${numberStr} is not integer`);
+    }
+
+    if (typeof BigInt !== "undefined") {
+      return BigInt(numberStr).toLocaleString("en-US");
+    }
+
+    const integer = numberStr;
+
+    const chunks: string[] = [];
+    for (let i = integer.length; i > 0; i -= 3) {
+      chunks.push(integer.slice(Math.max(0, i - 3), i));
+    }
+
+    return chunks.reverse().join(",");
   }
 
   static coinToTrimmedString(
