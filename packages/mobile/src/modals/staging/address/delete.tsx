50a1d4d12ee8b39a2982455e050c36995a6c2b21
Thunnini <yunjh1994@gmail.com>
2021-08-26 17:03:02 +0900

    Make the delete address book modal open in the center

--- packages/mobile/src/modals/staging/address/delete.tsx
+++ packages/mobile/src/modals/staging/address/delete.tsx
@@ -10,60 +10,66 @@ export const AddressDeleteModal: FunctionComponent<{
   close: () => void;
   addressBookConfig: AddressBookConfig;
   addressIndex: number;
-}> = registerModal(({ close, addressBookConfig, addressIndex }) => {
-  const style = useStyle();
+}> = registerModal(
+  ({ close, addressBookConfig, addressIndex }) => {
+    const style = useStyle();
 
-  return (
-    <View style={style.flatten(["padding-page"])}>
-      <View
-        style={style.flatten([
-          "border-radius-8",
-          "overflow-hidden",
-          "background-color-white",
-          "padding-x-20",
-          "padding-y-28",
-          "items-center",
-        ])}
-      >
-        <Text
+    return (
+      <View style={style.flatten(["padding-page"])}>
+        <View
           style={style.flatten([
-            "h3",
-            "color-text-black-medium",
-            "margin-bottom-8",
+            "border-radius-8",
+            "overflow-hidden",
+            "background-color-white",
+            "padding-x-20",
+            "padding-y-28",
+            "items-center",
           ])}
         >
-          Delete Address
-        </Text>
-        <Text
-          style={style.flatten([
-            "body2",
-            "color-text-black-medium",
-            "margin-bottom-16",
-            "text-center",
-          ])}
-        >
-          Are you sure you want to delete this address?
-        </Text>
-        <View style={style.flatten(["flex-row"])}>
-          <Button
-            containerStyle={style.flatten(["flex-1"])}
-            text="Cancel"
-            mode="outline"
-            onPress={() => {
-              close();
-            }}
-          />
-          <View style={style.flatten(["width-12"])} />
-          <Button
-            containerStyle={style.flatten(["flex-1"])}
-            text="Delete"
-            onPress={() => {
-              addressBookConfig.removeAddressBook(addressIndex);
-              close();
-            }}
-          />
+          <Text
+            style={style.flatten([
+              "h3",
+              "color-text-black-medium",
+              "margin-bottom-8",
+            ])}
+          >
+            Delete Address
+          </Text>
+          <Text
+            style={style.flatten([
+              "body2",
+              "color-text-black-medium",
+              "margin-bottom-16",
+              "text-center",
+            ])}
+          >
+            Are you sure you want to delete this address?
+          </Text>
+          <View style={style.flatten(["flex-row"])}>
+            <Button
+              containerStyle={style.flatten(["flex-1"])}
+              text="Cancel"
+              mode="outline"
+              onPress={() => {
+                close();
+              }}
+            />
+            <View style={style.flatten(["width-12"])} />
+            <Button
+              containerStyle={style.flatten(["flex-1"])}
+              text="Delete"
+              onPress={() => {
+                addressBookConfig.removeAddressBook(addressIndex);
+                close();
+              }}
+            />
+          </View>
         </View>
       </View>
-    </View>
-  );
-});
+    );
+  },
+  {
+    align: "center",
+    transitionVelocity: 3000,
+  }
+);
