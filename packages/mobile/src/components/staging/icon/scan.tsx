62cc22d97a927be6ac2b69f36e1697cc895dad97
Thunnini <yunjh1994@gmail.com>
2021-08-30 16:38:28 +0900

    Minor fix

--- packages/mobile/src/components/staging/icon/scan.tsx
+++ packages/mobile/src/components/staging/icon/scan.tsx
@@ -11,7 +11,7 @@ export const ScanIcon: FunctionComponent<{
         stroke={color}
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth="2.5"
+        strokeWidth="2"
         d="M18.375 24.5h3.063a3.063 3.063 0 003.062-3.063v-3.062m0-8.75V6.562A3.063 3.063 0 0021.437 3.5h-3.062m-8.75 21H6.562A3.063 3.063 0 013.5 21.437v-3.062m0-8.75V6.562A3.062 3.062 0 016.563 3.5h3.062"
       />
       <Rect
@@ -20,7 +20,7 @@ export const ScanIcon: FunctionComponent<{
         x="8.167"
         y="8.167"
         stroke={color}
-        strokeWidth="2.5"
+        strokeWidth="2"
         rx="1"
       />
     </Svg>
