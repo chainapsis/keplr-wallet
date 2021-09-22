604cf41be53cac38507cfb41b11d2d766c6b9dd4
Thunnini <yunjh1994@gmail.com>
2021-09-22 10:20:00 +0900

    Add "@keplr-wallet/wc-qrcode-modal"

---
+++ packages/wc-qrcode-modal/src/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { IQRCodeModalOptions } from "@walletconnect/types";
+import { Modal, ModalUIOptions } from "./modal";
+
+export class KeplrQRCodeModalV1 {
+  constructor(protected readonly uiOptions?: ModalUIOptions) {}
+
+  open(uri: string, cb: any, qrcodeModalOptions?: IQRCodeModalOptions) {
+    console.log(uri, cb, qrcodeModalOptions);
+    const wrapper = document.createElement("div");
+    wrapper.setAttribute("id", "keplr-qrcode-modal-v1");
+    document.body.appendChild(wrapper);
+
+    ReactDom.render(
+      <Modal
+        uri={uri}
+        close={() => {
+          this.close();
+          cb();
+        }}
+        uiOptions={this.uiOptions}
+      />,
+      wrapper
+    );
+  }
+
+  close() {
+    const wrapper = document.getElementById("keplr-qrcode-modal-v1");
+    if (wrapper) {
+      document.body.removeChild(wrapper);
+    }
+  }
+}
