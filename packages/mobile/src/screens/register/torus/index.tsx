03c5a15487502a2c63cff8067173f21143573d55
Thunnini <yunjh1994@gmail.com>
2021-09-23 19:29:06 +0900

    Change the flow of registering torus key

--- packages/mobile/src/screens/register/torus/index.tsx
+++ packages/mobile/src/screens/register/torus/index.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
-import { RouteProp, useRoute } from "@react-navigation/native";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import { RegisterConfig } from "@keplr-wallet/hooks";
 import { useStyle } from "../../../styles";
 import { useSmartNavigation } from "../../../navigation";
@@ -14,12 +14,161 @@ import { Buffer } from "buffer/";
 import NodeDetailManager from "@toruslabs/fetch-node-details";
 import Torus from "@toruslabs/torus.js";
 import { useStore } from "../../../stores";
+import { useLoadingScreen } from "../../../providers/loading-screen";
 
 interface FormData {
   name: string;
   password: string;
   confirmPassword: string;
 }
+
+const useTorusGoogleSignIn = (): {
+  privateKey: Uint8Array | undefined;
+  email: string | undefined;
+} => {
+  const [privateKey, setPrivateKey] = useState<Uint8Array | undefined>();
+  const [email, setEmail] = useState<string | undefined>();
+
+  const loadingScreen = useLoadingScreen();
+  const naviagtion = useNavigation();
+
+  useEffect(() => {
+    loadingScreen.setIsLoading(true);
+
+    (async () => {
+      try {
+        const nonce: string = Math.floor(Math.random() * 10000).toString();
+        const state = encodeURIComponent(
+          Buffer.from(
+            JSON.stringify({
+              instanceId: nonce,
+              redirectToOpener: false,
+            })
+          ).toString("base64")
+        );
+
+        const finalUrl = new URL(
+          "https://accounts.google.com/o/oauth2/v2/auth"
+        );
+        finalUrl.searchParams.append("response_type", "token id_token");
+        finalUrl.searchParams.append(
+          "client_id",
+          "413984222848-8r7u4ip9i6htppalo6jopu5qbktto6mi.apps.googleusercontent.com"
+        );
+        finalUrl.searchParams.append("state", state);
+        finalUrl.searchParams.append("scope", "profile email openid");
+        finalUrl.searchParams.append("nonce", nonce);
+        finalUrl.searchParams.append("prompt", "consent select_account");
+        finalUrl.searchParams.append(
+          "redirect_uri",
+          "https://oauth.keplr.app/google.html"
+        );
+
+        const result = await WebBrowser.openAuthSessionAsync(
+          finalUrl.href,
+          "app.keplr.oauth://"
+        );
+        if (result.type !== "success") {
+          throw new Error("Failed to get the oauth");
+        }
+
+        if (!result.url.startsWith("app.keplr.oauth://google#")) {
+          throw new Error("Invalid redirection");
+        }
+
+        const redirectedUrl = new URL(result.url);
+        const paramsString = redirectedUrl.hash;
+        const searchParams = new URLSearchParams(
+          paramsString.startsWith("#") ? paramsString.slice(1) : paramsString
+        );
+        if (state !== searchParams.get("state")) {
+          throw new Error("State doesn't match");
+        }
+        const idToken = searchParams.get("id_token");
+        const accessToken = searchParams.get("access_token");
+
+        const userResponse = await fetch(
+          "https://www.googleapis.com/userinfo/v2/me",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${accessToken || idToken}`,
+            },
+          }
+        );
+
+        if (userResponse.ok) {
+          const userInfo: {
+            picture: string;
+            email: string;
+            name: string;
+          } = await userResponse.json();
+
+          const { email } = userInfo;
+
+          const nodeDetailManager = new NodeDetailManager({
+            network: "mainnet",
+            proxyAddress: "0x638646503746d5456209e33a2ff5e3226d698bea",
+          });
+          const {
+            torusNodeEndpoints,
+            torusNodePub,
+            torusIndexes,
+          } = await nodeDetailManager.getNodeDetails();
+
+          const torus = new Torus({
+            enableLogging: __DEV__,
+            metadataHost: "https://metadata.tor.us",
+            allowHost: "https://signer.tor.us/api/allow",
+          });
+
+          const response = await torus.getPublicAddress(
+            torusNodeEndpoints,
+            torusNodePub,
+            {
+              verifier: "chainapsis-google",
+              verifierId: email.toLowerCase(),
+            },
+            true
+          );
+          const data = await torus.retrieveShares(
+            torusNodeEndpoints,
+            torusIndexes,
+            "chainapsis-google",
+            {
+              verifier_id: email.toLowerCase(),
+            },
+            (idToken || accessToken) as string
+          );
+          if (typeof response === "string")
+            throw new Error("must use extended pub key");
+          if (
+            data.ethAddress.toLowerCase() !== response.address.toLowerCase()
+          ) {
+            throw new Error("data ethAddress does not match response address");
+          }
+
+          setPrivateKey(Buffer.from(data.privKey.toString(), "hex"));
+          setEmail(email);
+        } else {
+          throw new Error("Failed to fetch user data");
+        }
+      } catch (e) {
+        console.log(e);
+        naviagtion.goBack();
+      } finally {
+        loadingScreen.setIsLoading(false);
+      }
+    })();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    privateKey,
+    email,
+  };
+};
 
 export const TorusSignInScreen: FunctionComponent = observer(() => {
   const route = useRoute<
@@ -43,6 +192,8 @@ export const TorusSignInScreen: FunctionComponent = observer(() => {
   const registerConfig: RegisterConfig = route.params.registerConfig;
   const [mode] = useState(registerConfig.mode);
 
+  const { privateKey, email } = useTorusGoogleSignIn();
+
   const {
     control,
     handleSubmit,
@@ -54,144 +205,38 @@ export const TorusSignInScreen: FunctionComponent = observer(() => {
   const [isCreating, setIsCreating] = useState(false);
 
   const submit = handleSubmit(async () => {
+    if (!privateKey || !email) {
+      return;
+    }
+
     setIsCreating(true);
 
     try {
-      const nonce: string = Math.floor(Math.random() * 10000).toString();
-      const state = encodeURIComponent(
-        Buffer.from(
-          JSON.stringify({
-            instanceId: nonce,
-            redirectToOpener: false,
-          })
-        ).toString("base64")
+      await registerConfig.createPrivateKey(
+        getValues("name"),
+        privateKey,
+        getValues("password"),
+        email
       );
+      analyticsStore.setUserId();
+      analyticsStore.setUserProperties({
+        registerType: "google",
+      });
+      analyticsStore.logEvent("OAuth sign in finished", {
+        accountType: "privateKey",
+      });
 
-      const finalUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth");
-      finalUrl.searchParams.append("response_type", "token id_token");
-      finalUrl.searchParams.append(
-        "client_id",
-        "413984222848-8r7u4ip9i6htppalo6jopu5qbktto6mi.apps.googleusercontent.com"
-      );
-      finalUrl.searchParams.append("state", state);
-      finalUrl.searchParams.append("scope", "profile email openid");
-      finalUrl.searchParams.append("nonce", nonce);
-      finalUrl.searchParams.append("prompt", "consent select_account");
-      finalUrl.searchParams.append(
-        "redirect_uri",
-        "https://oauth.keplr.app/google.html"
-      );
-
-      const result = await WebBrowser.openAuthSessionAsync(
-        finalUrl.href,
-        "app.keplr.oauth://"
-      );
-      if (result.type !== "success") {
-        throw new Error("Failed to get the oauth");
-      }
-
-      if (!result.url.startsWith("app.keplr.oauth://google#")) {
-        throw new Error("Invalid redirection");
-      }
-
-      const redirectedUrl = new URL(result.url);
-      const paramsString = redirectedUrl.hash;
-      const searchParams = new URLSearchParams(
-        paramsString.startsWith("#") ? paramsString.slice(1) : paramsString
-      );
-      if (state !== searchParams.get("state")) {
-        throw new Error("State doesn't match");
-      }
-      const idToken = searchParams.get("id_token");
-      const accessToken = searchParams.get("access_token");
-
-      const userResponse = await fetch(
-        "https://www.googleapis.com/userinfo/v2/me",
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${accessToken || idToken}`,
-          },
-        }
-      );
-
-      if (userResponse.ok) {
-        const userInfo: {
-          picture: string;
-          email: string;
-          name: string;
-        } = await userResponse.json();
-
-        const { email } = userInfo;
-
-        const nodeDetailManager = new NodeDetailManager({
-          network: "mainnet",
-          proxyAddress: "0x638646503746d5456209e33a2ff5e3226d698bea",
-        });
-        const {
-          torusNodeEndpoints,
-          torusNodePub,
-          torusIndexes,
-        } = await nodeDetailManager.getNodeDetails();
-
-        const torus = new Torus({
-          enableLogging: __DEV__,
-          metadataHost: "https://metadata.tor.us",
-          allowHost: "https://signer.tor.us/api/allow",
-        });
-
-        const response = await torus.getPublicAddress(
-          torusNodeEndpoints,
-          torusNodePub,
+      smartNavigation.reset({
+        index: 0,
+        routes: [
           {
-            verifier: "chainapsis-google",
-            verifierId: email.toLowerCase(),
-          },
-          true
-        );
-        const data = await torus.retrieveShares(
-          torusNodeEndpoints,
-          torusIndexes,
-          "chainapsis-google",
-          {
-            verifier_id: email.toLowerCase(),
-          },
-          (idToken || accessToken) as string
-        );
-        if (typeof response === "string")
-          throw new Error("must use extended pub key");
-        if (data.ethAddress.toLowerCase() !== response.address.toLowerCase()) {
-          throw new Error("data ethAddress does not match response address");
-        }
-
-        await registerConfig.createPrivateKey(
-          getValues("name"),
-          Buffer.from(data.privKey.toString(), "hex"),
-          getValues("password"),
-          email
-        );
-        analyticsStore.setUserId();
-        analyticsStore.setUserProperties({
-          registerType: "google",
-        });
-        analyticsStore.logEvent("OAuth sign in finished", {
-          accountType: "privateKey",
-        });
-
-        smartNavigation.reset({
-          index: 0,
-          routes: [
-            {
-              name: "Register.End",
-              params: {
-                password: getValues("password"),
-              },
+            name: "Register.End",
+            params: {
+              password: getValues("password"),
             },
-          ],
-        });
-      } else {
-        throw userResponse;
-      }
+          },
+        ],
+      });
     } catch (e) {
       console.log(e);
       setIsCreating(false);
@@ -301,7 +346,13 @@ export const TorusSignInScreen: FunctionComponent = observer(() => {
         </React.Fragment>
       ) : null}
       <View style={style.flatten(["flex-1"])} />
-      <Button text="Next" size="large" loading={isCreating} onPress={submit} />
+      <Button
+        text="Next"
+        size="large"
+        loading={isCreating}
+        onPress={submit}
+        disabled={!privateKey || !email}
+      />
       {/* Mock element for bottom padding */}
       <View style={style.flatten(["height-12"])} />
     </PageWithScrollView>
