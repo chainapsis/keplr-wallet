f3a0e5aef7d5f86027cb44dedd8c89a874bbd00e
Thunnini <yunjh1994@gmail.com>
2021-09-28 21:11:10 +0900

    Fix a bug that the root store can be made multiple times

--- packages/mobile/src/stores/index.tsx
+++ packages/mobile/src/stores/index.tsx
@@ -4,8 +4,23 @@ import { createRootStore, RootStore } from "./root";
 
 const storeContext = React.createContext<RootStore | null>(null);
 
+/*
+ I don't know why react native works that way.
+ But, react native clears all view and create new views when window size changes of the app exits by back button on android.
+ So, rootStore can be made multiple times if it handles on the provider.
+ To prevent this problem, handle the root store statically.
+ */
+let rootStore: RootStore | undefined;
+
 export const StoreProvider: FunctionComponent = ({ children }) => {
-  const [stores] = useState(() => createRootStore());
+  const [stores] = useState(() => {
+    if (rootStore) {
+      return rootStore;
+    }
+
+    rootStore = createRootStore();
+    return rootStore;
+  });
 
   return (
     <storeContext.Provider value={stores}>{children}</storeContext.Provider>
