02f3d1ca08bb9aff90ac11dfa9420021ebc89fa7
Thunnini <yunjh1994@gmail.com>
2021-10-23 02:58:17 +0900

    Make the content scripts not import the background package #164

--- packages/extension/src/content-scripts/events.ts
+++ packages/extension/src/content-scripts/events.ts
@@ -1,21 +1,46 @@
-import {
-  interactionForegroundInit,
-  InteractionForegroundService,
-} from "@keplr-wallet/background";
-import { Router } from "@keplr-wallet/router";
+import { Message, Router } from "@keplr-wallet/router";
+
+class PushEventDataMsg extends Message<void> {
+  public static type() {
+    return "push-event-data";
+  }
+
+  constructor(
+    public readonly data: {
+      type: string;
+      data: unknown;
+    }
+  ) {
+    super();
+  }
+
+  validateBasic(): void {
+    if (!this.data.type) {
+      throw new Error("Type should not be empty");
+    }
+  }
+
+  route(): string {
+    return "interaction-foreground";
+  }
+
+  type(): string {
+    return PushEventDataMsg.type();
+  }
+}
 
 export function initEvents(router: Router) {
-  interactionForegroundInit(
-    router,
-    new InteractionForegroundService({
-      onInteractionDataReceived: (): void => {
-        // noop
-      },
-      onEventDataReceived: (data): void => {
-        if (data.type === "keystore-changed") {
+  router.registerMessage(PushEventDataMsg);
+
+  router.addHandler("interaction-foreground", (_, msg) => {
+    switch (msg.constructor) {
+      case PushEventDataMsg:
+        if ((msg as PushEventDataMsg).data.type === "keystore-changed") {
           window.dispatchEvent(new Event("keplr_keystorechange"));
         }
-      },
-    })
-  );
+        return;
+      default:
+        throw new Error("Unknown msg type");
+    }
+  });
 }
