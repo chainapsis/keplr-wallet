3af0605fa668c6fedaf4ef9bfc83bc20a450f8e6
Thunnini <yunjh1994@gmail.com>
2021-11-25 23:56:48 +0900

    Integrate the logic that updates the chain info with the logic that checks the updatable property

--- packages/background/src/updater/service.ts
+++ packages/background/src/updater/service.ts
@@ -60,136 +60,46 @@ export class ChainUpdaterService {
     // If chain id is not fomatted as {chainID}-{version},
     // there is no way to deal with the updated chain id.
     if (!ChainIdHelper.hasChainVersion(chainInfo.chainId)) {
-      return chainInfo.chainId;
+      return;
     }
 
-    const instance = Axios.create({
-      baseURL: chainInfo.rpc,
-    });
+    const updates = await ChainUpdaterService.checkChainUpdate(chainInfo);
 
-    // Get the status to get the chain id.
-    const result = await instance.get<{
-      result: {
-        node_info: {
-          network: string;
-        };
-      };
-    }>("/status");
+    if (updates.explicit || updates.slient) {
+      const currentVersion = ChainIdHelper.parse(chainInfo.chainId);
 
-    const currentVersion = ChainIdHelper.parse(chainInfo.chainId);
-    const fetchedChainId = result.data.result.node_info.network;
-    const fetchedVersion = ChainIdHelper.parse(fetchedChainId);
+      if (updates.chainId) {
+        const fetchedChainId = updates.chainId;
+        const fetchedVersion = ChainIdHelper.parse(fetchedChainId);
 
-    if (
-      currentVersion.identifier === fetchedVersion.identifier &&
-      currentVersion.version < fetchedVersion.version
-    ) {
-      await this.saveChainProperty(currentVersion.identifier, {
-        chainId: fetchedChainId,
-      });
-    }
+        if (
+          currentVersion.identifier === fetchedVersion.identifier &&
+          currentVersion.version < fetchedVersion.version
+        ) {
+          await this.saveChainProperty(currentVersion.identifier, {
+            chainId: fetchedChainId,
+          });
+        }
+      }
 
-    let staragteUpdate = false;
-    try {
-      if (!chainInfo.features || !chainInfo.features.includes("stargate")) {
-        const restInstance = Axios.create({
-          baseURL: chainInfo.rest,
-        });
-
-        // If the chain doesn't have the stargate feature,
-        // but it can use the GRPC HTTP Gateway,
-        // assume that it can support the stargate and try to update the features.
-        await restInstance.get("/cosmos/base/tendermint/v1beta1/node_info");
-
+      if (updates.features && updates.features.length > 0) {
         const savedChainProperty = await this.getUpdatedChainProperty(
           chainInfo.chainId
         );
 
+        const updateFeatures = savedChainProperty.features ?? [];
+
+        for (const feature of updates.features) {
+          if (updateFeatures.indexOf(feature) < 0) {
+            updateFeatures.push(feature);
+          }
+        }
+
         await this.saveChainProperty(currentVersion.identifier, {
-          features: (savedChainProperty.features ?? []).concat(["stargate"]),
+          features: updateFeatures,
         });
-        staragteUpdate = true;
       }
-    } catch {}
-
-    try {
-      if (
-        (!chainInfo.features || !chainInfo.features.includes("ibc-transfer")) &&
-        (staragteUpdate ||
-          (chainInfo.features && chainInfo.features.includes("stargate")))
-      ) {
-        const restInstance = Axios.create({
-          baseURL: chainInfo.rest,
-        });
-
-        // If the chain doesn't have the ibc transfer feature,
-        // try to fetch the params of ibc transfer module.
-        // assume that it can support the ibc transfer if the params return true, and try to update the features.
-        const result = await restInstance.get<{
-          params: {
-            receive_enabled: boolean;
-            send_enabled: boolean;
-          };
-        }>("/ibc/applications/transfer/v1beta1/params");
-        if (
-          result.data.params.receive_enabled &&
-          result.data.params.send_enabled
-        ) {
-          const savedChainProperty = await this.getUpdatedChainProperty(
-            chainInfo.chainId
-          );
-
-          await this.saveChainProperty(currentVersion.identifier, {
-            features: (savedChainProperty.features ?? []).concat([
-              "ibc-transfer",
-            ]),
-          });
-        }
-      }
-    } catch {}
-
-    try {
-      if (
-        (!chainInfo.features ||
-          !chainInfo.features.includes("no-legacy-stdTx")) &&
-        (staragteUpdate ||
-          (chainInfo.features && chainInfo.features.includes("stargate")))
-      ) {
-        const restInstance = Axios.create({
-          baseURL: chainInfo.rest,
-        });
-
-        // The chain with above cosmos-sdk@v0.44.0 can't send the legacy stdTx,
-        // Assume that it can't send the legacy stdTx if the POST /txs responses "not implemented".
-        const result = await restInstance.post<
-          | {
-              code: 12;
-              message: "Not Implemented";
-              details: [];
-            }
-          | any
-        >("/txs", undefined, {
-          validateStatus: (status) => {
-            return (status >= 200 && status < 300) || status === 501;
-          },
-        });
-        if (
-          result.status === 501 &&
-          result.data.code === 12 &&
-          result.data.message === "Not Implemented"
-        ) {
-          const savedChainProperty = await this.getUpdatedChainProperty(
-            chainInfo.chainId
-          );
-
-          await this.saveChainProperty(currentVersion.identifier, {
-            features: (savedChainProperty.features ?? []).concat([
-              "no-legacy-stdTx",
-            ]),
-          });
-        }
-      }
-    } catch {}
+    }
   }
 
   private async getUpdatedChainProperty(
@@ -232,6 +142,9 @@ export class ChainUpdaterService {
   ): Promise<{
     explicit: boolean;
     slient: boolean;
+
+    chainId?: string;
+    features?: string[];
   }> {
     const chainId = chainInfo.chainId;
 
@@ -350,9 +263,23 @@ export class ChainUpdaterService {
       }
     } catch {}
 
+    const features: string[] = [];
+    if (staragteUpdate) {
+      features.push("stargate");
+    }
+    if (ibcTransferUpdate) {
+      features.push("ibc-transfer");
+    }
+    if (noLegacyStdTxUpdate) {
+      features.push("no-legacy-stdTx");
+    }
+
     return {
       explicit: version.version < fetchedVersion.version,
       slient: staragteUpdate || ibcTransferUpdate || noLegacyStdTxUpdate,
+
+      chainId: resultChainId,
+      features,
     };
   }
 }
