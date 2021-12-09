e047762854008d955616c5a7fdbd5ac6a369f9e7
Thunnini <yunjh1994@gmail.com>
2021-12-10 00:48:07 +0900

    Add ADR-36 related functions #207

--- packages/crypto/src/key.ts
+++ packages/crypto/src/key.ts
@@ -1,9 +1,17 @@
-import EC from "elliptic";
+import { ec } from "elliptic";
 import CryptoJS from "crypto-js";
 
 import { Buffer } from "buffer/";
 
 export class PrivKeySecp256k1 {
+  static generateRandomKey(): PrivKeySecp256k1 {
+    const secp256k1 = new ec("secp256k1");
+
+    return new PrivKeySecp256k1(
+      Buffer.from(secp256k1.genKeyPair().getPrivate().toArray())
+    );
+  }
+
   constructor(protected readonly privKey: Uint8Array) {}
 
   toBytes(): Uint8Array {
@@ -11,7 +19,7 @@ export class PrivKeySecp256k1 {
   }
 
   getPubKey(): PubKeySecp256k1 {
-    const secp256k1 = new EC.ec("secp256k1");
+    const secp256k1 = new ec("secp256k1");
 
     const key = secp256k1.keyFromPrivate(this.privKey);
 
@@ -21,7 +29,7 @@ export class PrivKeySecp256k1 {
   }
 
   sign(msg: Uint8Array): Uint8Array {
-    const secp256k1 = new EC.ec("secp256k1");
+    const secp256k1 = new ec("secp256k1");
     const key = secp256k1.keyFromPrivate(this.privKey);
 
     const hash = CryptoJS.SHA256(
@@ -52,5 +60,48 @@ export class PubKeySecp256k1 {
     hash = CryptoJS.RIPEMD160(CryptoJS.enc.Hex.parse(hash)).toString();
 
     return new Uint8Array(Buffer.from(hash, "hex"));
+  }
+
+  toKeyPair(): ec.KeyPair {
+    const secp256k1 = new ec("secp256k1");
+
+    return secp256k1.keyFromPublic(
+      Buffer.from(this.pubKey).toString("hex"),
+      "hex"
+    );
+  }
+
+  verify(msg: Uint8Array, signature: Uint8Array): boolean {
+    const hash = CryptoJS.SHA256(
+      CryptoJS.lib.WordArray.create(msg as any)
+    ).toString();
+
+    const secp256k1 = new ec("secp256k1");
+
+    let r = signature.slice(0, 32);
+    let s = signature.slice(32);
+    const rIsNegative = r[0] >= 0x80;
+    const sIsNegative = s[0] >= 0x80;
+    if (rIsNegative) {
+      r = new Uint8Array([0, ...r]);
+    }
+    if (sIsNegative) {
+      s = new Uint8Array([0, ...s]);
+    }
+
+    // Der encoding
+    const derData = new Uint8Array([
+      0x02,
+      r.length,
+      ...r,
+      0x02,
+      s.length,
+      ...s,
+    ]);
+    return secp256k1.verify(
+      Buffer.from(hash, "hex"),
+      new Uint8Array([0x30, derData.length, ...derData]),
+      this.toKeyPair()
+    );
   }
 }
