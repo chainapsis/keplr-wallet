4f5a3cd16ad4b216404d3d767348d71accb02b0e
Thunnini <yunjh1994@gmail.com>
2022-01-03 23:48:56 +0900

    Remove the usages of `precision` for the units

--- packages/stores/src/query/cosmos/governance/proposal.ts
+++ packages/stores/src/query/cosmos/governance/proposal.ts
@@ -131,18 +131,18 @@ export class ObservableQueryProposal extends ObservableChainQuery<ProposalTally>
     if (this.proposalStatus !== ProposalStatus.VOTING_PERIOD) {
       return {
         yes: new IntPretty(new Int(this.raw.final_tally_result.yes))
-          .precision(stakeCurrency.coinDecimals)
+          .moveDecimalPointLeft(stakeCurrency.coinDecimals)
           .maxDecimals(stakeCurrency.coinDecimals),
         no: new IntPretty(new Int(this.raw.final_tally_result.no))
-          .precision(stakeCurrency.coinDecimals)
+          .moveDecimalPointLeft(stakeCurrency.coinDecimals)
           .maxDecimals(stakeCurrency.coinDecimals),
         abstain: new IntPretty(new Int(this.raw.final_tally_result.abstain))
-          .precision(stakeCurrency.coinDecimals)
+          .moveDecimalPointLeft(stakeCurrency.coinDecimals)
           .maxDecimals(stakeCurrency.coinDecimals),
         noWithVeto: new IntPretty(
           new Int(this.raw.final_tally_result.no_with_veto)
         )
-          .precision(stakeCurrency.coinDecimals)
+          .moveDecimalPointLeft(stakeCurrency.coinDecimals)
           .maxDecimals(stakeCurrency.coinDecimals),
       };
     }
@@ -151,35 +151,35 @@ export class ObservableQueryProposal extends ObservableChainQuery<ProposalTally>
       return {
         yes: new IntPretty(new Int(0))
           .ready(false)
-          .precision(stakeCurrency.coinDecimals)
+          .moveDecimalPointLeft(stakeCurrency.coinDecimals)
           .maxDecimals(stakeCurrency.coinDecimals),
         no: new IntPretty(new Int(0))
           .ready(false)
-          .precision(stakeCurrency.coinDecimals)
+          .moveDecimalPointLeft(stakeCurrency.coinDecimals)
           .maxDecimals(stakeCurrency.coinDecimals),
         abstain: new IntPretty(new Int(0))
           .ready(false)
-          .precision(stakeCurrency.coinDecimals)
+          .moveDecimalPointLeft(stakeCurrency.coinDecimals)
           .maxDecimals(stakeCurrency.coinDecimals),
         noWithVeto: new IntPretty(new Int(0))
           .ready(false)
-          .precision(stakeCurrency.coinDecimals)
+          .moveDecimalPointLeft(stakeCurrency.coinDecimals)
           .maxDecimals(stakeCurrency.coinDecimals),
       };
     }
 
     return {
       yes: new IntPretty(new Int(this.response.data.result.yes))
-        .precision(stakeCurrency.coinDecimals)
+        .moveDecimalPointLeft(stakeCurrency.coinDecimals)
         .maxDecimals(stakeCurrency.coinDecimals),
       no: new IntPretty(new Int(this.response.data.result.no))
-        .precision(stakeCurrency.coinDecimals)
+        .moveDecimalPointLeft(stakeCurrency.coinDecimals)
         .maxDecimals(stakeCurrency.coinDecimals),
       abstain: new IntPretty(new Int(this.response.data.result.abstain))
-        .precision(stakeCurrency.coinDecimals)
+        .moveDecimalPointLeft(stakeCurrency.coinDecimals)
         .maxDecimals(stakeCurrency.coinDecimals),
       noWithVeto: new IntPretty(new Int(this.response.data.result.no_with_veto))
-        .precision(stakeCurrency.coinDecimals)
+        .moveDecimalPointLeft(stakeCurrency.coinDecimals)
         .maxDecimals(stakeCurrency.coinDecimals),
     };
   }
