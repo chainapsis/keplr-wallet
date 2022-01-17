d3e91b727dd32852cfbfa1e8370dbab57c3c3676
Thunnini <yunjh1994@gmail.com>
2022-01-18 01:16:41 +0900

    Show the error if exists on the ledger modal

--- packages/mobile/src/modals/ledger/index.tsx
+++ packages/mobile/src/modals/ledger/index.tsx
@@ -14,6 +14,34 @@ import { Ledger, LedgerInitErrorOn } from "@keplr-wallet/background";
 import { getLastUsedLedgerDeviceId } from "../../utils/ledger";
 import { RectButton } from "../../components/rect-button";
 import { useUnmount } from "../../hooks";
+import Svg, { Path } from "react-native-svg";
+
+const AlertIcon: FunctionComponent<{
+  size: number;
+  color: string;
+}> = ({ size, color }) => {
+  return (
+    <Svg width={size} height={size} fill="none" viewBox="0 0 100 100">
+      <Path
+        stroke={color}
+        strokeMiterlimit="10"
+        strokeWidth="6.25"
+        d="M87.5 50c0-20.703-16.797-37.5-37.5-37.5S12.5 29.297 12.5 50 29.297 87.5 50 87.5 87.5 70.703 87.5 50z"
+      />
+      <Path
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="4.167"
+        d="M48.62 30.329l1.12 23.828 1.12-23.818a1.12 1.12 0 00-1.131-1.172v0a1.12 1.12 0 00-1.11 1.162v0z"
+      />
+      <Path
+        fill={color}
+        d="M49.74 69.754a3.906 3.906 0 110-7.812 3.906 3.906 0 010 7.812z"
+      />
+    </Svg>
+  );
+};
 
 export const LedgerGranterModal: FunctionComponent<{
   isOpen: boolean;
@@ -78,6 +106,7 @@ export const LedgerGranterModal: FunctionComponent<{
         name: string;
       }[]
     >([]);
+    const [errorOnListen, setErrorOnListen] = useState<string | undefined>();
 
     useEffect(() => {
       let unsubscriber: (() => void) | undefined;
@@ -124,8 +153,19 @@ export const LedgerGranterModal: FunctionComponent<{
                 }
               }
             },
-            error: (e: Error) => {
-              console.log(e);
+            error: (e?: Error | any) => {
+              if (!e) {
+                setErrorOnListen("Unknown error");
+              } else {
+                if ("message" in e && typeof e.message === "string") {
+                  setErrorOnListen(e.message);
+                } else if ("toString" in e) {
+                  setErrorOnListen(e.toString());
+                } else {
+                  setErrorOnListen("Unknown error");
+                }
+              }
+              setIsFinding(false);
             },
           }).unsubscribe;
         })();
@@ -168,12 +208,35 @@ export const LedgerGranterModal: FunctionComponent<{
       >
         {isAvailable ? (
           <React.Fragment>
-            <Text style={style.flatten(["subtitle3", "color-text-black-high"])}>
-              1. Open the Cosmos app on your Ledger device
-            </Text>
-            <Text style={style.flatten(["subtitle3", "color-text-black-high"])}>
-              2. Select the hardware wallet you’d like to pair
-            </Text>
+            {errorOnListen ? (
+              <View style={style.flatten(["items-center"])}>
+                <AlertIcon size={100} color={style.get("color-danger").color} />
+                <Text style={style.flatten(["h4", "color-danger"])}>Error</Text>
+                <Text
+                  style={style.flatten([
+                    "subtitle3",
+                    "color-text-black-medium",
+                    "margin-top-16",
+                  ])}
+                >
+                  {errorOnListen}
+                </Text>
+              </View>
+            ) : (
+              <React.Fragment>
+                <Text
+                  style={style.flatten(["subtitle3", "color-text-black-high"])}
+                >
+                  1. Open the Cosmos app on your Ledger device
+                </Text>
+                <Text
+                  style={style.flatten(["subtitle3", "color-text-black-high"])}
+                >
+                  2. Select the hardware wallet you’d like to pair
+                </Text>
+              </React.Fragment>
+            )}
+
             {devices.map((device) => {
               return (
                 <LedgerNanoBLESelector
