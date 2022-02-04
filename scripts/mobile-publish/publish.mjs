19fff7ee07f80a4217d0c860b7196eaccab6b937
Thunnini <yunjh1994@gmail.com>
2022-02-05 03:01:57 +0900

    Minor fix

--- scripts/mobile-publish/publish.mjs
+++ scripts/mobile-publish/publish.mjs
@@ -4,51 +4,55 @@ import "zx/globals";
 import semver from "semver";
 
 (async () => {
-  const targetPlatform = process.argv.find(
-    (arg) => arg === "android" || arg === "ios"
-  );
-  if (!targetPlatform) {
-    throw new Error("Please provide target platform");
-  }
-
-  console.log(`Deploy to ${targetPlatform}`);
-
-  const versions = (await $`git tag --points-at HEAD`).stdout
-    .split(/\s/)
-    .map((v) => v.trim())
-    .filter((v) => !!v);
-
-  for (const version of versions) {
-    if (!version.startsWith("mobile/v")) {
-      continue;
+  try {
+    const targetPlatform = process.argv.find(
+      (arg) => arg === "android" || arg === "ios"
+    );
+    if (!targetPlatform) {
+      throw new Error("Please provide target platform");
     }
 
-    const semantic = semver.parse(version.replace("mobile/v", ""));
+    console.log(`Deploy to ${targetPlatform}`);
 
-    if (semantic) {
-      if (semantic.prerelease.length > 0) {
-        if (semantic.prerelease[0] !== "codepush") {
-          throw new Error(`Invalid version: ${semantic.raw}`);
+    const versions = (await $`git tag --points-at HEAD`).stdout
+      .split(/\s/)
+      .map((v) => v.trim())
+      .filter((v) => !!v);
+
+    for (const version of versions) {
+      if (!version.startsWith("mobile/v")) {
+        continue;
+      }
+
+      const semantic = semver.parse(version.replace("mobile/v", ""));
+
+      if (semantic) {
+        if (semantic.prerelease.length > 0) {
+          if (semantic.prerelease[0] !== "codepush") {
+            throw new Error(`Invalid version: ${semantic.raw}`);
+          }
+        }
+
+        const isCodepush =
+          semantic.prerelease.length === 2 &&
+          semantic.prerelease[0] === "codepush";
+
+        console.log(`Found ${semantic.raw}. IsCodepush: ${isCodepush}`);
+
+        const directory =
+          targetPlatform === "android"
+            ? `${__dirname}/../../packages/mobile/android/fastlane`
+            : `${__dirname}/../../packages/mobile/ios/fastlane`;
+
+        if (isCodepush) {
+          await $`cd ${directory} && bundle exec fastlane deploy_codepush version:${semantic.raw}`;
+        } else {
+          await $`cd ${directory} && bundle exec fastlane deploy version:${semantic.raw}`;
         }
       }
-
-      const isCodepush =
-        semantic.prerelease.length === 2 &&
-        semantic.prerelease[0] === "codepush";
-
-      console.log(`Found ${semantic.raw}. IsCodepush: ${isCodepush}`);
-
-      const directory =
-        targetPlatform === "android"
-          ? `${__dirname}/../../packages/mobile/android/fastlane`
-          : `${__dirname}/../../packages/mobile/ios/fastlane`;
-
-      if (isCodepush) {
-        await $`cd ${directory} && bundle exec fastlane deploy_codepush version:${semantic.raw}`;
-      } else {
-        await $`cd ${directory} && bundle exec fastlane deploy version:${semantic.raw}`;
-      }
     }
+  } catch (e) {
+    throw e;
   }
 })();
 
--- scripts/mobile-publish/repository-dispatch.mjs
+++ scripts/mobile-publish/repository-dispatch.mjs
@@ -5,49 +5,53 @@ import semver from "semver";
 import { Octokit } from "@octokit/core";
 
 (async () => {
-  const versions = (await $`git tag --points-at HEAD`).stdout
-    .split(/\s/)
-    .map((v) => v.trim())
-    .filter((v) => !!v);
+  try {
+    const versions = (await $`git tag --points-at HEAD`).stdout
+      .split(/\s/)
+      .map((v) => v.trim())
+      .filter((v) => !!v);
 
-  let mobileTagFounded = false;
+    let mobileTagFounded = false;
 
-  for (const version of versions) {
-    if (!version.startsWith("mobile/v")) {
-      continue;
+    for (const version of versions) {
+      if (!version.startsWith("mobile/v")) {
+        continue;
+      }
+
+      const semantic = semver.parse(version.replace("mobile/v", ""));
+
+      if (semantic) {
+        mobileTagFounded = true;
+        break;
+      }
     }
 
-    const semantic = semver.parse(version.replace("mobile/v", ""));
+    if (mobileTagFounded) {
+      const authKey = process.env["MOBILE_PUBLISH_AUTH_KEY"];
+      if (!authKey) {
+        throw new Error("MOBILE_PUBLISH_AUTH_KEY is not provided");
+      }
+      const owner = process.env["MOBILE_PUBLISH_OWNER"];
+      if (!owner) {
+        throw new Error("MOBILE_PUBLISH_OWNER is not provided");
+      }
+      const repo = process.env["MOBILE_PUBLISH_REPO"];
+      if (!repo) {
+        throw new Error("MOBILE_PUBLISH_REPO is not provided");
+      }
 
-    if (semantic) {
-      mobileTagFounded = true;
-      break;
-    }
-  }
+      const octokit = new Octokit({ auth: authKey });
 
-  if (mobileTagFounded) {
-    const authKey = process.env["MOBILE_PUBLISH_AUTH_KEY"];
-    if (!authKey) {
-      throw new Error("MOBILE_PUBLISH_AUTH_KEY is not provided");
+      await octokit.request(`POST /repos/${owner}/${repo}/dispatches`, {
+        event_type: "mobile_publish",
+        client_payload: {
+          ref: process.env["GITHUB_REF"],
+          sha: process.env["GITHUB_SHA"],
+        },
+      });
     }
-    const owner = process.env["MOBILE_PUBLISH_OWNER"];
-    if (!owner) {
-      throw new Error("MOBILE_PUBLISH_OWNER is not provided");
-    }
-    const repo = process.env["MOBILE_PUBLISH_REPO"];
-    if (!repo) {
-      throw new Error("MOBILE_PUBLISH_REPO is not provided");
-    }
-
-    const octokit = new Octokit({ auth: authKey });
-
-    await octokit.request(`POST /repos/${owner}/${repo}/dispatches`, {
-      event_type: "mobile_publish",
-      client_payload: {
-        ref: process.env["GITHUB_REF"],
-        sha: process.env["GITHUB_SHA"],
-      },
-    });
+  } catch (e) {
+    throw e;
   }
 })();
 
