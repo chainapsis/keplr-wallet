11428fe7c94ff3cb74ae3acc7f8ce815ce9ee277
Bryan White <bryanchriswhite@gmail.com>
2022-03-01 16:43:48 +0100

    fix: add missing config var

--- packages/extension/src/config.ui.var.ts
+++ packages/extension/src/config.ui.var.ts
@@ -1,6 +1,7 @@
 import { IntlMessages } from "./languages";
 import { RegisterOption } from "@keplr-wallet/hooks";
 
+export const AMPLITUDE_API_KEY = "";
 export const ETHEREUM_ENDPOINT =
   "https://mainnet.infura.io/v3/eeb00e81cdb2410098d5a270eff9b341";
 
