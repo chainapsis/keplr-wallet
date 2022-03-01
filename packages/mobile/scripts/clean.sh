592c327f483ae7000c7de796ccf257a9bd066cad
Bryan White <bryanchriswhite@gmail.com>
2022-03-01 16:44:47 +0100

    chore: skip cleanup steps in mobile package if cli tools aren't installed

--- packages/mobile/scripts/clean.sh
+++ packages/mobile/scripts/clean.sh
@@ -4,12 +4,21 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd "$DIR/../ios"
-xcodebuild clean
+
+if [ "$(which xcodebuild 2>/dev/null)" == "" ]; then
+  echo "xcodebuild not found - skipping xcodebuild clean..."
+else
+  xcodebuild clean
+fi
 
 rm -rf "$DIR/../ios/Pods"
 
 cd "$DIR/../android"
-./gradlew clean
+if [ "$(which gradlew 2>/dev/null)" == "" ]; then
+  echo "gradlew not found - skipping gradlew clean..."
+else
+  ./gradlew clean
+fi
 
 rm -rf "$DIR/../android/.gradle"
 rm -rf "$DIR/../android/build"
