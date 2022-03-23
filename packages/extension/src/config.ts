1ad5947537da3e4d5316eef2fe586029faa343e9
Thunnini <yunjh1994@gmail.com>
2022-03-23 15:56:16 +0900

    Change gas price step for osmosis

--- packages/extension/src/config.ts
+++ packages/extension/src/config.ts
@@ -193,8 +193,8 @@ export const EmbedChainInfos: ChainInfo[] = [
     ],
     gasPriceStep: {
       low: 0,
-      average: 0,
-      high: 0.025,
+      average: 0.025,
+      high: 0.04,
     },
     features: ["stargate", "ibc-transfer", "no-legacy-stdTx", "ibc-go"],
   },
