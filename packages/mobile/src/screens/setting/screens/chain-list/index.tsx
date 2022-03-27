a8e50c97e9f355b83ada5dffe1199ec180519e77
Thunnini <yunjh1994@gmail.com>
2022-03-28 08:43:30 +0900

    WIP

---
+++ packages/mobile/src/screens/setting/screens/chain-list/index.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from "react";
+import { observer } from "mobx-react-lite";
+import { PageWithView } from "../../../../components/page";
+
+export const SettingChainListScreen: FunctionComponent = observer(() => {
+  return <PageWithView></PageWithView>;
+});
