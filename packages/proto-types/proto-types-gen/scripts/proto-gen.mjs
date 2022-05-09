0342e83c0acc4c9e3c035d33b5f0883ab3dadfc5
Thunnini <yunjh1994@gmail.com>
2022-05-09 21:38:23 +0900

    Reorder of inputs for proto-types and re-build proto-types package

--- packages/proto-types/proto-types-gen/scripts/proto-gen.mjs
+++ packages/proto-types/proto-types-gen/scripts/proto-gen.mjs
@@ -91,13 +91,13 @@ function setOutputHash(root, hash) {
     const thirdPartyProtoPath = path.join(baseDirPath, "third_party/proto");
 
     const inputs = [
+      "cosmos/authz/v1beta1/tx.proto",
       "cosmos/base/v1beta1/coin.proto",
       "cosmos/bank/v1beta1/bank.proto",
       "cosmos/bank/v1beta1/tx.proto",
       "cosmos/bank/v1beta1/authz.proto",
       "cosmos/staking/v1beta1/tx.proto",
       "cosmos/staking/v1beta1/authz.proto",
-      "cosmos/authz/v1beta1/tx.proto",
       "cosmos/gov/v1beta1/gov.proto",
       "cosmos/gov/v1beta1/tx.proto",
       "cosmos/distribution/v1beta1/tx.proto",
