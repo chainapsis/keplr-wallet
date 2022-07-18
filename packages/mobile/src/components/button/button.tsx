6e1734b4c3b15ba821e2b65d165aaac3f2d1a737
Thunnini <yunjh1994@gmail.com>
2022-07-18 21:10:43 +0900

    Update color sets for button component

--- packages/mobile/src/components/button/button.tsx
+++ packages/mobile/src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ReactElement, useState } from "react";
+import React, { FunctionComponent, ReactElement } from "react";
 import { useStyle } from "../../styles";
 import { Text, StyleSheet, TextStyle, View, ViewStyle } from "react-native";
 import { LoadingSpinner } from "../spinner";
@@ -40,32 +40,49 @@ export const Button: FunctionComponent<{
 }) => {
   const style = useStyle();
 
-  const [isPressed, setIsPressed] = useState(false);
-
   const backgroundColorDefinitions: string[] = (() => {
     const baseColor = color === "primary" ? "blue" : "red";
 
     switch (mode) {
       case "fill":
         if (disabled) {
-          return ["background-color-gray-50", "dark:background-color-gray-400"];
+          if (color === "primary") {
+            return [
+              "background-color-gray-50",
+              "dark:background-color-platinum-400",
+            ];
+          } else {
+            return [
+              "background-color-red-100",
+              "dark:background-color-red-700",
+            ];
+          }
         } else {
-          return [
-            `background-color-${baseColor}-400`,
-            `dark:background-color-${baseColor}-300`,
-          ];
+          return [`background-color-${baseColor}-400`];
         }
       case "light":
         if (disabled) {
-          return ["background-color-gray-50", "dark:background-color-gray-400"];
+          if (color === "primary") {
+            return [
+              "background-color-gray-50",
+              "dark:background-color-platinum-500",
+            ];
+          } else {
+            return [
+              "background-color-red-100",
+              "dark:background-color-red-700",
+            ];
+          }
         } else {
           return [
             `background-color-${baseColor}-100`,
-            `dark:background-color-${baseColor}-600`,
+            color === "primary"
+              ? "dark:background-color-platinum-500"
+              : `dark:background-color-${baseColor}-600`,
           ];
         }
       case "outline":
-        return ["background-color-white", "dark:background-color-black"];
+        return ["background-color-transparent"];
       default:
         return ["background-color-transparent"];
     }
@@ -88,33 +105,48 @@ export const Button: FunctionComponent<{
     switch (mode) {
       case "fill":
         if (disabled) {
-          return ["color-gray-300", "dark:color-gray-100"];
+          if (color === "primary") {
+            return ["color-gray-300", "dark:color-platinum-200"];
+          } else {
+            return [`color-${baseColor}-200`, `dark:color-${baseColor}-500`];
+          }
         }
 
-        return ["color-white"];
+        if (color === "primary") {
+          return ["color-white", "dark:color-blue-100"];
+        } else {
+          return ["color-white"];
+        }
       case "light":
         if (disabled) {
-          return ["color-gray-300", "dark:color-gray-100"];
+          if (color === "primary") {
+            return ["color-gray-300", "dark:color-platinum-300"];
+          } else {
+            return [`color-${baseColor}-200`, `dark:color-${baseColor}-500`];
+          }
         }
-        if (isPressed) {
-          return [`color-${baseColor}-500`, `color-${baseColor}-200`];
-        }
-        return [`color-${baseColor}-400`, `dark:color-${baseColor}-100`];
+
+        return [
+          `color-${baseColor}-400`,
+          color === "primary"
+            ? "dark:color-platinum-50"
+            : `dark:color-${baseColor}-50`,
+        ];
       case "outline":
         if (disabled) {
           return [`color-${baseColor}-200`, `dark:color-${baseColor}-600`];
         }
-        if (isPressed) {
-          return [`color-${baseColor}-300`, `dark:color-${baseColor}-600`];
-        }
+
         return [`color-${baseColor}-400`];
       case "text":
         if (disabled) {
-          return [`color-${baseColor}-200`];
+          if (color === "primary") {
+            return ["color-gray-200", "dark:color-platinum-300"];
+          } else {
+            return [`color-${baseColor}-200`, `dark:color-${baseColor}-600`];
+          }
         }
-        if (isPressed) {
-          return [`color-${baseColor}-500`];
-        }
+
         return [`color-${baseColor}-400`, `dark:color-${baseColor}-300`];
     }
   })();
@@ -168,16 +200,8 @@ export const Button: FunctionComponent<{
         `dark:border-color-${baseColor}-600`,
       ];
     }
-    if (isPressed) {
-      return [
-        `border-color-${baseColor}-300`,
-        `dark:border-color-${baseColor}-600`,
-      ];
-    }
-    return [
-      `border-color-${baseColor}-400`,
-      `dark:border-color-${baseColor}-600`,
-    ];
+
+    return [`border-color-${baseColor}-400`];
   })();
 
   return (
@@ -210,7 +234,6 @@ export const Button: FunctionComponent<{
           buttonStyle,
         ])}
         onPress={onPress}
-        onActiveStateChange={(active) => setIsPressed(active)}
         enabled={!loading && !disabled}
         rippleColor={rippleColor}
         underlayColor={underlayColor}
