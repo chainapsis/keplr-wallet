1dab2d15edf7784f1222001c4054b4dc4d94d9a7
Jonas Keinholz <git@inyono.dev>
2022-07-22 00:09:52 +0200

    obi-wallet(mobile): get FlatList to work, yarn format

--- obi-wallet/apps/mobile/src/app/screens/home/components/background.tsx
+++ obi-wallet/apps/mobile/src/app/screens/home/components/background.tsx
@@ -1,14 +1,35 @@
 import React from "react";
-import { SafeAreaView, View, Text, Image, Button, Pressable } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  Button,
+  Pressable,
+} from "react-native";
 
-const Background = ({style}) => {
-    return <View style={{ justifyContent: 'space-between', backgroundColor: '#1E1E1E', height: '100%', ...style }}>
-
-            <View style={{ flex: 1, position: 'relative' }}>
-            {/* <FontAwesomeIcon icon={faChevronLeft} style={{ color: '#7B87A8', marginLeft: 5, marginTop: 5 }} /> */}
-            <Image source={require('../assets/backgroundblue.png')} style={{ alignSelf: 'flex-end' }} />
-            <Image source={require('../assets/backgroundpink.png')} style={{ position: 'absolute', zIndex: -1, left: 0 }} />
-            </View>
-               </View >
-}
+const Background = ({ style }) => {
+  return (
+    <View
+      style={{
+        justifyContent: "space-between",
+        backgroundColor: "#1E1E1E",
+        height: "100%",
+        ...style,
+      }}
+    >
+      <View style={{ flex: 1, position: "relative" }}>
+        {/* <FontAwesomeIcon icon={faChevronLeft} style={{ color: '#7B87A8', marginLeft: 5, marginTop: 5 }} /> */}
+        <Image
+          source={require("../assets/backgroundblue.png")}
+          style={{ alignSelf: "flex-end" }}
+        />
+        <Image
+          source={require("../assets/backgroundpink.png")}
+          style={{ position: "absolute", zIndex: -1, left: 0 }}
+        />
+      </View>
+    </View>
+  );
+};
 export default Background;
--- obi-wallet/apps/mobile/src/app/screens/onboarding/index.tsx
+++ obi-wallet/apps/mobile/src/app/screens/onboarding/index.tsx
@@ -1,6 +1,13 @@
 import { Home } from "@obi-wallet/common";
 import React from "react";
-import { SafeAreaView, View, Text, Image, Button, Pressable } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  Button,
+  Pressable,
+} from "react-native";
 
 import { useNavigation } from "../../stack";
 import { useStore } from "../../stores";
@@ -10,23 +17,67 @@ export function Onboarding() {
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={{justifyContent:'space-between', backgroundColor:'#1E1E1E', height:'100%'}}>
-        <View style={{ flex:6, position:'relative'}}>
-            <Image source={require('./assets/backgroundblue.png')} />
-            <Image source={require('./assets/backgroundpink.png')} style={{position:'absolute', zIndex:-1}}/>
+    <SafeAreaView
+      style={{
+        justifyContent: "space-between",
+        backgroundColor: "#1E1E1E",
+        height: "100%",
+      }}
+    >
+      <View style={{ flex: 6, position: "relative" }}>
+        <Image source={require("./assets/backgroundblue.png")} />
+        <Image
+          source={require("./assets/backgroundpink.png")}
+          style={{ position: "absolute", zIndex: -1 }}
+        />
+      </View>
+      <View
+        style={{
+          flex: 4,
+          paddingRight: 20,
+          paddingLeft: 20,
+          justifyContent: "space-between",
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Image source={require("./assets/loop.png")} />
+          <Text
+            style={{
+              color: "#F6F5FF",
+              fontSize: 32,
+              fontWeight: "600",
+              marginTop: 32,
+            }}
+          >
+            Welcome to Loop
+          </Text>
+          <Text
+            style={{
+              color: "#999CB6",
+              fontSize: 16,
+              fontWeight: "400",
+              marginTop: 12,
+            }}
+          >
+            Loop, powered by Obi, is the world’s most powerful wallet for web3{" "}
+          </Text>
         </View>
-        <View  style={{flex:4, paddingRight:20, paddingLeft:20, justifyContent: "space-between"}}>
-            <View style={{flex:1}}>
-                <Image source={require('./assets/loop.png')}/>
-                <Text style={{color:'#F6F5FF', fontSize:32, fontWeight:'600', marginTop:32}}>Welcome to Loop</Text>
-                <Text style={{color:'#999CB6', fontSize:16, fontWeight:'400', marginTop:12}}>Loop, powered by Obi, is the world’s most powerful wallet for web3 </Text>
-            </View>
-            <Pressable style={{backgroundColor:'#59D6E6', width:335, height:56, borderRadius:12, justifyContent: 'center', alignItems:'center', marginBottom:48}}>
-                <Text style={{color:'#040317', fontWeight:'bold', fontSize:16}}>Get Started >></Text>
-            </Pressable>
-        </View>
+        <Pressable
+          style={{
+            backgroundColor: "#59D6E6",
+            width: 335,
+            height: 56,
+            borderRadius: 12,
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 48,
+          }}
+        >
+          <Text style={{ color: "#040317", fontWeight: "bold", fontSize: 16 }}>
+            Get Started {">>"}
+          </Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
-
-
