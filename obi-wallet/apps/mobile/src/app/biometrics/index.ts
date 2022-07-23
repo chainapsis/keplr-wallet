843d4921422b4091bbbe30b7c09faedbbdb89477
Jonas Keinholz <git@inyono.dev>
2022-07-24 00:42:59 +0200

    Revert "obi-wallet(mobile): use react-native-device-crypto instead of react-native-biometrics"
    This reverts commit bc2be83a3ab3d77007d1feb70db706327a21ec3e.

--- obi-wallet/apps/mobile/src/app/biometrics/index.ts
+++ obi-wallet/apps/mobile/src/app/biometrics/index.ts
@@ -1,29 +1,19 @@
-import ReactNativeDeviceCrypto, {
-  AccessLevel,
-  BiometryParams,
-} from "react-native-device-crypto";
+import ReactNativeBiometrics from "react-native-biometrics";
 
-const BIOMETRICS_KEY = "obi-wallet-biometrics";
+export const biometrics = new ReactNativeBiometrics({
+  allowDeviceCredentials: false,
+});
 
 export async function isBiometricsAvailable() {
-  return ReactNativeDeviceCrypto.isBiometryEnrolled();
+  const { available } = await biometrics.isSensorAvailable();
+  return available;
 }
 
-export async function getPublicKey() {
-  return ReactNativeDeviceCrypto.getOrCreateAsymmetricKey(BIOMETRICS_KEY, {
-    accessLevel: AccessLevel.AUTHENTICATION_REQUIRED,
-    invalidateOnNewBiometry: false,
-  });
-}
-
-export async function createBiometricSignature({
-  payload,
-  biometryParams,
-}: {
-  payload: string;
-  biometryParams: BiometryParams;
-}) {
-  // Ensure that the key pair is available
-  await getPublicKey();
-  return ReactNativeDeviceCrypto.sign(BIOMETRICS_KEY, payload, biometryParams);
+export async function createBiometricSignature(
+  ...payload: Parameters<typeof biometrics.createSignature>
+) {
+  const { keysExist } = await biometrics.biometricKeysExist();
+  // Create keys only if we don't have them already
+  if (!keysExist) await biometrics.createKeys();
+  return await biometrics.createSignature(...payload);
 }
