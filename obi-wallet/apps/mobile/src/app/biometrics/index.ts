bc2be83a3ab3d77007d1feb70db706327a21ec3e
Jonas Keinholz <git@inyono.dev>
2022-07-23 23:33:05 +0200

    obi-wallet(mobile): use react-native-device-crypto instead of react-native-biometrics

--- obi-wallet/apps/mobile/src/app/biometrics/index.ts
+++ obi-wallet/apps/mobile/src/app/biometrics/index.ts
@@ -1,19 +1,29 @@
-import ReactNativeBiometrics from "react-native-biometrics";
+import ReactNativeDeviceCrypto, {
+  AccessLevel,
+  BiometryParams,
+} from "react-native-device-crypto";
 
-export const biometrics = new ReactNativeBiometrics({
-  allowDeviceCredentials: false,
-});
+const BIOMETRICS_KEY = "obi-wallet-biometrics";
 
 export async function isBiometricsAvailable() {
-  const { available } = await biometrics.isSensorAvailable();
-  return available;
+  return ReactNativeDeviceCrypto.isBiometryEnrolled();
 }
 
-export async function createBiometricSignature(
-  ...payload: Parameters<typeof biometrics.createSignature>
-) {
-  const { keysExist } = await biometrics.biometricKeysExist();
-  // Create keys only if we don't have them already
-  if (!keysExist) await biometrics.createKeys();
-  return await biometrics.createSignature(...payload);
+export async function getPublicKey() {
+  return ReactNativeDeviceCrypto.getOrCreateAsymmetricKey(BIOMETRICS_KEY, {
+    accessLevel: AccessLevel.AUTHENTICATION_REQUIRED,
+    invalidateOnNewBiometry: false,
+  });
+}
+
+export async function createBiometricSignature({
+  payload,
+  biometryParams,
+}: {
+  payload: string;
+  biometryParams: BiometryParams;
+}) {
+  // Ensure that the key pair is available
+  await getPublicKey();
+  return ReactNativeDeviceCrypto.sign(BIOMETRICS_KEY, payload, biometryParams);
 }
