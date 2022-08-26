6956fbd8d6753892960d8fc2c295ae2ea7db5cb9
Jonas Keinholz <git@inyono.dev>
2022-08-26 23:27:43 +0200

    obi-wallet(mobile): build & load injected provider

--- obi-wallet/apps/mobile/src/main.tsx
+++ obi-wallet/apps/mobile/src/main.tsx
@@ -1,5 +1,6 @@
 import "./shim";
-import { AppRegistry } from "react-native";
+import { AppRegistry, Platform } from "react-native";
+import RNFS from "react-native-fs";
 
 import { App } from "./app";
 import { initBackground } from "./background";
@@ -7,3 +8,13 @@ import "react-native-gesture-handler";
 initBackground();
 
 AppRegistry.registerComponent("Mobile", () => App);
+
+if (Platform.OS === "ios") {
+  RNFS.readFile(`${RNFS.MainBundlePath}/index.js`).then((res) =>
+    console.log("I", res)
+  );
+}
+
+if (Platform.OS === "android") {
+  RNFS.readFileAssets("index.js").then((res) => console.log("A", res));
+}
