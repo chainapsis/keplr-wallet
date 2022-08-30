291270877dcaf859a73065cd447fd7ff7a2f7e6e
BlockJayn <blockjayn@gmail.com>
2022-08-30 08:44:43 +0200

    Fixed Icon-Button for Android + hitSlop added

--- obi-wallet/apps/mobile/src/app/button/icon-button.tsx
+++ obi-wallet/apps/mobile/src/app/button/icon-button.tsx
@@ -1,16 +1,13 @@
-import {
-  Platform,
-  TouchableNativeFeedback,
-  TouchableOpacity,
-  TouchableWithoutFeedbackProps,
-} from "react-native";
+import { TouchableOpacity, TouchableWithoutFeedbackProps } from "react-native";
 
 export type IconButtonProps = TouchableWithoutFeedbackProps;
 
 export function IconButton(props: IconButtonProps) {
-  if (Platform.OS === "ios") {
-    return <TouchableOpacity {...props} />;
-  } else {
-    return <TouchableNativeFeedback {...props} />;
-  }
+  return (
+    // icon-button doesnt work on Android, Android seems to work fine with "TouchableOpacity" in this case, but not with "TouchableNativeFeedback", Deleted Differenciation between OS (if/else), added a hitSlop for a nicer user experience and cleaned up the imports
+    <TouchableOpacity
+      {...props}
+      hitSlop={{ top: 15, bottom: 15, left: 15, right: 15 }}
+    />
+  );
 }
