f2027a7c5c2167441f079a9ee87a9a9c2e5cb6c2
Jonas Keinholz <git@inyono.dev>
2022-08-31 10:02:47 +0200

    format comment

--- obi-wallet/apps/mobile/src/app/button/icon-button.tsx
+++ obi-wallet/apps/mobile/src/app/button/icon-button.tsx
@@ -4,7 +4,8 @@ export type IconButtonProps = TouchableWithoutFeedbackProps;
 
 export function IconButton(props: IconButtonProps) {
   return (
-    // icon-button doesnt work on Android, Android seems to work fine with "TouchableOpacity" in this case, but not with "TouchableNativeFeedback", Deleted Differenciation between OS (if/else), added a hitSlop for a nicer user experience and cleaned up the imports
+    // icon-button doesn't work on Android, Android seems to work fine with
+    // `TouchableOpacity` in this case, but not with `TouchableNativeFeedback`.
     <TouchableOpacity
       {...props}
       hitSlop={{ top: 15, bottom: 15, left: 15, right: 15 }}
