ee6803fd8f6409e2b027478089d101ddb33cfa2c
Jonas Keinholz <git@inyono.dev>
2022-08-31 22:32:00 +0200

    obi-wallet(injected-provider): add injected keplr code

--- obi-wallet/libs/injected-provider/src/index.ts
+++ obi-wallet/libs/injected-provider/src/index.ts
@@ -1,2 +1,13 @@
-// @ts-expect-error
-window.ReactNativeWebView.postMessage(JSON.stringify(window.location));
+import { RNInjectedKeplr } from "./injected-provider";
+import { init } from "./init";
+
+// TODO: Set the Keplr version properly
+const keplr = new RNInjectedKeplr("0.10.10", "mobile-web");
+
+init(
+  keplr,
+  (chainId: string) => keplr.getOfflineSigner(chainId),
+  (chainId: string) => keplr.getOfflineSignerOnlyAmino(chainId),
+  (chainId: string) => keplr.getOfflineSignerAuto(chainId),
+  (chainId: string) => keplr.getEnigmaUtils(chainId)
+);
