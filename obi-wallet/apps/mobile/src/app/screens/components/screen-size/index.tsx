e58711c5a4045307ec7029b433100bb7d2cebf3a
BlockJayn <blockjayn@gmail.com>
2022-09-06 23:55:05 +0200

    Revert responsive funct; update screen with figma

---
+++ obi-wallet/apps/mobile/src/app/screens/components/screen-size/index.tsx
@@ -0,0 +1,7 @@
+import { Dimensions } from "react-native";
+
+export const { width: screenWidth, height: screenHeight } =
+  Dimensions.get("window");
+
+export const smallDeviceMaxWidth = 375;
+export const smallDeviceMaxHeight = 667;
