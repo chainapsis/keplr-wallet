ba8cfc066038f8cb17a60a654bd1a67b6bef8ea7
Jonas Keinholz <git@inyono.dev>
2022-09-09 23:09:55 +0200

    obi-wallet(mobile): handle APP_ENV

--- obi-wallet/apps/mobile/src/background/sentry.ts
+++ obi-wallet/apps/mobile/src/background/sentry.ts
@@ -1,11 +1,10 @@
 import * as Sentry from "@sentry/react-native";
-import { SENTRY_DSN } from "react-native-dotenv";
+import { APP_ENV, SENTRY_DSN } from "react-native-dotenv";
 
 export function initSentry() {
   Sentry.init({
     dsn: SENTRY_DSN,
-    // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-    // We recommend adjusting this value in production.
-    tracesSampleRate: 1.0,
+    tracesSampleRate: __DEV__ ? 1.0 : 0.5,
+    environment: APP_ENV,
   });
 }
