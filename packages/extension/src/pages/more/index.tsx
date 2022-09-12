99733ba5c03e0af850304e5798cac3661e61c84b
imvinay84 <vsvinaysingh82@gmail.com>
2022-09-12 16:15:00 +0530

    fix UI issues

--- packages/extension/src/pages/more/index.tsx
+++ packages/extension/src/pages/more/index.tsx
@@ -1,11 +1,11 @@
 import React, { FunctionComponent } from "react";
 import { useHistory } from "react-router";
 import { HeaderLayout } from "../../layouts";
-import bellIcon from "../../public/assets/icon/bell.png";
 import { Card, CardBody } from "reactstrap";
 import { IBCTransferView } from "../main/ibc-transfer";
 import classnames from "classnames";
 import style from "./style.module.scss";
+import { Menu } from "../main/menu";
 
 export const MorePage: FunctionComponent = () => {
   const history = useHistory();
@@ -14,9 +14,7 @@ export const MorePage: FunctionComponent = () => {
     <HeaderLayout
       showChainName={true}
       canChangeChainInfo={true}
-      onBackButton={() => {
-        history.goBack();
-      }}
+      menuRenderer={<Menu />}
       rightRenderer={
         <div
           style={{
@@ -27,16 +25,16 @@ export const MorePage: FunctionComponent = () => {
             paddingRight: "20px",
           }}
         >
-          <img
-            src={bellIcon}
-            alt="notification"
+          <div
             style={{ width: "16px", cursor: "pointer" }}
             onClick={(e) => {
               e.preventDefault();
 
               history.push("/setting/set-keyring");
             }}
-          />
+          >
+            <i className="fa fa-user" aria-hidden="true"></i>
+          </div>
         </div>
       }
     >
