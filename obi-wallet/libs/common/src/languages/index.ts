b25662358fa0745d3a27d38dd84d8d6f434fcf6e
Jonas Keinholz <git@inyono.dev>
2022-09-16 02:35:21 +0200

    obi-wallet(mobile): remove any

--- obi-wallet/libs/common/src/languages/index.ts
+++ obi-wallet/libs/common/src/languages/index.ts
@@ -2,8 +2,8 @@ import MessagesDe from "./de.json";
 import MessagesEn from "./en.json";
 import MessagesEs from "./es.json";
 
-export type IntlMessage = Record<string, any>;
-export type IntlMessages = { [lang: string]: Record<string, any> };
+export type IntlMessage = Record<string, string>;
+export type IntlMessages = { [lang: string]: Record<string, string> };
 
 export const messages: IntlMessages = {
   en: MessagesEn,
