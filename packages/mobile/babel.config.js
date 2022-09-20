c525e8f5b62c1f493d6bd1a06c2e80f9866dee7a
Thunnini <yunjh1994@gmail.com>
2022-09-21 00:16:54 +0900

    Bump "react-native-reanimated"

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -4,5 +4,6 @@ module.exports = {
     ["@babel/plugin-transform-flow-strip-types"],
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["react-native-reanimated/plugin"],
   ],
 };
