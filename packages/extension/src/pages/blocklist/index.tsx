5af32360159e6804ae21f6e9e9f731e021b2d8a1
PINOT <pinot.kim@kakao.com>
2022-09-21 21:10:19 +0900

    Update layout

--- packages/extension/src/pages/blocklist/index.tsx
+++ packages/extension/src/pages/blocklist/index.tsx
@@ -1,6 +1,6 @@
 import { BACKGROUND_PORT } from "@keplr-wallet/router";
 import { InExtensionMessageRequester } from "@keplr-wallet/router-extension";
-import React, { FunctionComponent, useMemo } from "react";
+import React, { FunctionComponent } from "react";
 import { URLTempAllowMsg } from "@keplr-wallet/background";
 import ReactDOM from "react-dom";
 import style from "./style.module.scss";
@@ -20,13 +20,22 @@ export const BlocklistPage: FunctionComponent = () => {
   return (
     <div className={style.container}>
       <div className={style.inner}>
-        <h1>SECURITY ALERT</h1>
-        <p>
-          Keplr has detected that this domain has been flagged as a phishing
-          site. To protect the safety of your assets, we recommend you exit this
-          website immediately.
-        </p>
-        <button onClick={handleMove}>Continue to {origin} (unsafe)</button>
+        <img
+          className={style.image}
+          src={require("../../public/assets/img/blocklist.svg")}
+          alt=""
+        />
+        <div>
+          <h1 className={style.title}>SECURITY ALERT</h1>
+          <p className={style.description}>
+            Keplr has detected that this domain has been flagged as a phishing
+            site. To protect the safety of your assets, we recommend you exit
+            this website immediately.
+          </p>
+          <button className={style.link} onClick={handleMove}>
+            Continue to {origin} (unsafe)
+          </button>
+        </div>
       </div>
     </div>
   );
