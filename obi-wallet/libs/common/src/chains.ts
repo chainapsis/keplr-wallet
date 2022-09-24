7ae9cfa61e86a07cea2f2c220fa7d2c97c3e1ca5
ntropian <pete@deviantsfactions.com>
2022-09-24 17:28:47 -0400

    hotfix update code id

--- obi-wallet/libs/common/src/chains.ts
+++ obi-wallet/libs/common/src/chains.ts
@@ -19,7 +19,7 @@ export const chains = {
     chainId: "juno-1" as const,
     label: "Juno",
     prefix: "juno",
-    currentCodeId: 1057,
+    currentCodeId: 1060,
     rpc: "https://rpc-juno.itastakers.com/",
     denom: "ujuno",
     startingUsdDebt: "0",
