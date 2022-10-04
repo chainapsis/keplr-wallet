e6ec47eff05c43ce9877c2b3624f3fa41bc9b145
Thunnini <yunjh1994@gmail.com>
2022-10-04 22:27:07 +0900

    Remove the usage of "_signTypedData" method because ethermint's non-standard format EIP712 tx can't pass validation of ethersjs

--- packages/background/src/keyring/keyring.ts
+++ packages/background/src/keyring/keyring.ts
@@ -18,6 +18,7 @@ import { Wallet } from "@ethersproject/wallet";
 import * as BytesUtils from "@ethersproject/bytes";
 import { computeAddress } from "@ethersproject/transactions";
 import { EIP712MessageValidator } from "./eip712";
+import { _TypedDataEncoder } from "@ethersproject/hash";
 
 export enum KeyRingStatus {
   NOTLOADED,
@@ -871,25 +872,60 @@ export class KeyRing {
         const data = await EIP712MessageValidator.validateAsync(
           JSON.parse(Buffer.from(message).toString())
         );
-        const signature = await ethWallet._signTypedData(
-          data.domain,
-          // Seems that there is no way to set primary type and the first type becomes primary type.
-          (() => {
-            const types = { ...data.types };
-            delete types["EIP712Domain"];
-            const primary = types[data.primaryType];
-            if (!primary) {
-              throw new Error(`No matched primary type: ${data.primaryType}`);
-            }
-            delete types[data.primaryType];
-            return {
-              [data.primaryType]: primary,
-              ...types,
-            };
-          })(),
-          data.message
+        // Since ethermint eip712 tx uses non-standard format, it cannot pass validation of ethersjs.
+        // Therefore, it should be handled at a slightly lower level.
+        const signature = await ethWallet._signingKey().signDigest(
+          Hash.keccak256(
+            Buffer.concat([
+              // eth separator
+              Buffer.from("19", "hex"),
+              // Version: 1
+              Buffer.from("01", "hex"),
+              Buffer.from(
+                _TypedDataEncoder
+                  .hashStruct(
+                    "EIP712Domain",
+                    { EIP712Domain: data.types.EIP712Domain },
+                    data.domain
+                  )
+                  .replace("0x", ""),
+                "hex"
+              ),
+              Buffer.from(
+                _TypedDataEncoder
+                  .from(
+                    // Seems that there is no way to set primary type and the first type becomes primary type.
+                    (() => {
+                      const types = { ...data.types };
+                      delete types["EIP712Domain"];
+                      const primary = types[data.primaryType];
+                      if (!primary) {
+                        throw new Error(
+                          `No matched primary type: ${data.primaryType}`
+                        );
+                      }
+                      delete types[data.primaryType];
+                      return {
+                        [data.primaryType]: primary,
+                        ...types,
+                      };
+                    })()
+                  )
+                  .hash(data.message)
+                  .replace("0x", ""),
+                "hex"
+              ),
+            ])
+          )
         );
-        return BytesUtils.arrayify(signature);
+        return Buffer.concat([
+          Buffer.from(signature.r.replace("0x", ""), "hex"),
+          Buffer.from(signature.s.replace("0x", ""), "hex"),
+          // The metamask doesn't seem to consider the chain id in this case... (maybe bug on metamask?)
+          signature.recoveryParam
+            ? Buffer.from("1c", "hex")
+            : Buffer.from("1b", "hex"),
+        ]);
       }
       default:
         throw new Error(`Unknown sign type: ${type}`);
