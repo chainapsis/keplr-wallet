571e324134b827b4d2fac9ec986af3fcda99ced8
Austin Chandra <austinchandra@berkeley.edu>
2022-10-04 13:53:45 -0700

    Implement query for ERC20 balance; rename Metadata to ContractData

--- packages/stores-etc/src/erc20/query/index.ts
+++ packages/stores-etc/src/erc20/query/index.ts
@@ -3,6 +3,7 @@ import { KVStore } from "@keplr-wallet/common";
 import Axios from "axios";
 import { Interface } from "@ethersproject/abi";
 import { computed, makeObservable } from "mobx";
+import { BigNumber } from "@ethersproject/bignumber";
 
 const erc20MetadataInterface: Interface = new Interface([
   {
@@ -41,6 +42,25 @@ const erc20MetadataInterface: Interface = new Interface([
       {
         name: "",
         type: "uint8",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "address",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
       },
     ],
     payable: false,
@@ -160,12 +180,70 @@ export class ObservableQueryERC20MetadataDecimals extends ObservableJsonRPCQuery
   }
 }
 
-export class ObservableQueryERC20MetadataInner {
+export class ObservableQueryERC20ContractBalance extends ObservableJsonRPCQuery<string> {
+  constructor(
+    kvStore: KVStore,
+    ethereumURL: string,
+    contractAddress: string,
+    userAddress: string
+  ) {
+    const instance = Axios.create({
+      ...{
+        baseURL: ethereumURL,
+      },
+    });
+
+    let messageData: string;
+    try {
+      messageData = erc20MetadataInterface.encodeFunctionData("balanceOf", [
+        userAddress,
+      ]);
+    } catch (e) {
+      messageData = "";
+    }
+
+    super(kvStore, instance, "", "eth_call", [
+      {
+        to: contractAddress,
+        data: messageData,
+      },
+      "latest",
+    ]);
+
+    makeObservable(this);
+  }
+
+  @computed
+  get balance(): string | undefined {
+    if (!this.response) {
+      return undefined;
+    }
+
+    try {
+      const balance = erc20MetadataInterface.decodeFunctionResult(
+        "balanceOf",
+        this.response.data
+      )[0] as BigNumber;
+      return balance.toString();
+    } catch (e) {
+      console.log(e);
+    }
+    return undefined;
+  }
+}
+
+export class ObservableQueryERC20ContractDataInner {
   protected readonly _queryName: ObservableQueryERC20MetadataName;
   protected readonly _querySymbol: ObservableQueryERC20MetadataSymbol;
   protected readonly _queryDecimals: ObservableQueryERC20MetadataDecimals;
+  protected readonly _queryBalance: ObservableQueryERC20ContractBalance;
 
-  constructor(kvStore: KVStore, ethereumURL: string, contractAddress: string) {
+  constructor(
+    kvStore: KVStore,
+    ethereumURL: string,
+    contractAddress: string,
+    userAddress: string
+  ) {
     this._queryName = new ObservableQueryERC20MetadataName(
       kvStore,
       ethereumURL,
@@ -182,6 +260,13 @@ export class ObservableQueryERC20MetadataInner {
       kvStore,
       ethereumURL,
       contractAddress
+    );
+
+    this._queryBalance = new ObservableQueryERC20ContractBalance(
+      kvStore,
+      ethereumURL,
+      contractAddress,
+      userAddress
     );
   }
 
@@ -204,6 +289,10 @@ export class ObservableQueryERC20MetadataInner {
   get decimals(): number | undefined {
     return this._queryDecimals.decimals;
   }
+
+  get balance(): string | undefined {
+    return this._queryBalance.balance;
+  }
 }
 
 /**
@@ -211,21 +300,23 @@ export class ObservableQueryERC20MetadataInner {
  * This is on temporal stage to implement currency registrar for gravity bridge and axelar network.
  * It is not possible to handle multiple networks on Ethereum at the same time.
  */
-export class ObservableQueryERC20Metadata extends HasMapStore<ObservableQueryERC20MetadataInner> {
+export class ObservableQueryERC20ContractData extends HasMapStore<ObservableQueryERC20ContractDataInner> {
   constructor(
     protected readonly kvStore: KVStore,
-    protected readonly ethereumURL: string
+    protected readonly ethereumURL: string,
+    protected readonly userAddress?: string
   ) {
     super((contractAddress) => {
-      return new ObservableQueryERC20MetadataInner(
+      return new ObservableQueryERC20ContractDataInner(
         this.kvStore,
         this.ethereumURL,
-        contractAddress
+        contractAddress,
+        userAddress ?? ""
       );
     });
   }
 
-  get(contractAddress: string): ObservableQueryERC20MetadataInner {
+  get(contractAddress: string): ObservableQueryERC20ContractDataInner {
     return super.get(contractAddress);
   }
 }
