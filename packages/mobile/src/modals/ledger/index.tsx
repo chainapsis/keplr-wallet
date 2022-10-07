0f4614ae0dac5225f4695a372ebb7e68b70fa6d3
Thunnini <yunjh1994@gmail.com>
2022-10-07 19:30:48 +0900

    Minor fix

--- packages/mobile/src/modals/ledger/index.tsx
+++ packages/mobile/src/modals/ledger/index.tsx
@@ -18,7 +18,7 @@ import TransportBLE, {
   bleManager,
 } from "@ledgerhq/react-native-hw-transport-ble";
 import { LoadingSpinner } from "../../components/spinner";
-import { Ledger, LedgerInitErrorOn } from "@keplr-wallet/background";
+import { Ledger, LedgerApp, LedgerInitErrorOn } from "@keplr-wallet/background";
 import { getLastUsedLedgerDeviceId } from "../../utils/ledger";
 import { RectButton } from "../../components/rect-button";
 import { useUnmount } from "../../hooks";
@@ -365,7 +365,11 @@ const LedgerNanoBLESelector: FunctionComponent<{
 
     try {
       setIsConnecting(true);
-      const ledger = await Ledger.init(() => TransportBLE.open(deviceId));
+      const ledger = await Ledger.init(
+        () => TransportBLE.open(deviceId),
+        undefined,
+        LedgerApp.Cosmos
+      );
       await ledger.close();
 
       return true;
@@ -410,11 +414,6 @@ const LedgerNanoBLESelector: FunctionComponent<{
         {!isConnecting && initErrorOn === LedgerInitErrorOn.App ? (
           <Text style={style.flatten(["subtitle3", "color-text-low"])}>
             Please open Cosmos App
-          </Text>
-        ) : null}
-        {!isConnecting && initErrorOn === LedgerInitErrorOn.Support ? (
-          <Text style={style.flatten(["subtitle3", "color-text-low"])}>
-            This app or coinType is unsupported
           </Text>
         ) : null}
         {!isConnecting && initErrorOn === LedgerInitErrorOn.Unknown ? (
