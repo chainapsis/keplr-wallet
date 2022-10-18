bd9dff657e422e04e1911ef7c6891ccc7a7e899b
Thunnini <yunjh1994@gmail.com>
2022-10-18 22:13:48 +0900

    Freeze a injected keplr on external context

--- packages/extension/src/content-scripts/inject/injected-script.ts
+++ packages/extension/src/content-scripts/inject/injected-script.ts
@@ -1,14 +1,7 @@
 import { InjectedKeplr } from "@keplr-wallet/provider";
-import { init } from "./init";
+import { injectKeplrToWindow } from "@keplr-wallet/provider";
 
 import manifest from "../../manifest.json";
 
 const keplr = new InjectedKeplr(manifest.version, "extension");
-
-init(
-  keplr,
-  (chainId: string) => keplr.getOfflineSigner(chainId),
-  (chainId: string) => keplr.getOfflineSignerOnlyAmino(chainId),
-  (chainId: string) => keplr.getOfflineSignerAuto(chainId),
-  (chainId: string) => keplr.getEnigmaUtils(chainId)
-);
+injectKeplrToWindow(keplr);
