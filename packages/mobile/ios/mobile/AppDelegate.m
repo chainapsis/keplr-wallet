7adea2b6452b890b2f9fc3bea08e15d66588fc90
Thunnini <yunjh1994@gmail.com>
2022-10-21 01:34:11 +0900

    Minor bump react-native

--- packages/mobile/ios/mobile/AppDelegate.m
+++ packages/mobile/ios/mobile/AppDelegate.m
@@ -61,7 +61,11 @@ static void InitializeFlipper(UIApplication *application) {
                                                    moduleName:@"mobile"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  if (@available(iOS 13.0, *)) {
+    rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+    rootView.backgroundColor = [UIColor whiteColor];
+  }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
