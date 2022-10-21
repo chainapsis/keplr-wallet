c7399ba33bdaaf22561010e9903048616afc70c9
HeesungB <hezo25@naver.com>
2022-10-21 14:52:40 +0900

    Rollback checkChainUpdate function

--- packages/background/src/updater/service.ts
+++ packages/background/src/updater/service.ts
@@ -193,4 +193,179 @@ export class ChainUpdaterService {
 
     return await this.chainsService.getChainInfos();
   }
+
+  /**
+   * Returns wether the chain has been changed.
+   * Currently, only check the chain id has been changed.
+   * @param chainInfo Chain information.
+   */
+  public static async checkChainUpdate(
+    chainInfo: Readonly<ChainInfo>
+  ): Promise<{
+    explicit: boolean;
+    slient: boolean;
+
+    chainId?: string;
+    features?: string[];
+  }> {
+    const chainId = chainInfo.chainId;
+
+    // If chain id is not fomatted as {chainID}-{version},
+    // there is no way to deal with the updated chain id.
+    if (!ChainIdHelper.hasChainVersion(chainId)) {
+      return {
+        explicit: false,
+        slient: false,
+      };
+    }
+
+    const instance = Axios.create({
+      baseURL: chainInfo.rpc,
+    });
+
+    // Get the status to get the chain id.
+    const result = await instance.get<{
+      result: {
+        node_info: {
+          network: string;
+        };
+      };
+    }>("/status");
+
+    const resultChainId = result.data.result.node_info.network;
+
+    const version = ChainIdHelper.parse(chainId);
+    const fetchedVersion = ChainIdHelper.parse(resultChainId);
+
+    // TODO: Should throw an error?
+    if (version.identifier !== fetchedVersion.identifier) {
+      return {
+        explicit: false,
+        slient: false,
+      };
+    }
+
+    const restInstance = Axios.create({
+      baseURL: chainInfo.rest,
+    });
+
+    let ibcGoUpdates = false;
+    try {
+      if (!chainInfo.features || !chainInfo.features.includes("ibc-go")) {
+        // If the chain uses the ibc-go module separated from the cosmos-sdk,
+        // we need to check it because the REST API is different.
+        const result = await restInstance.get<{
+          params: {
+            receive_enabled: boolean;
+            send_enabled: boolean;
+          };
+        }>("/ibc/apps/transfer/v1/params");
+
+        if (result.status === 200) {
+          ibcGoUpdates = true;
+        }
+      }
+    } catch {}
+
+    let ibcTransferUpdate = false;
+    try {
+      if (!chainInfo.features || !chainInfo.features.includes("ibc-transfer")) {
+        const isIBCGo =
+          ibcGoUpdates ||
+          (chainInfo.features && chainInfo.features.includes("ibc-go"));
+
+        // If the chain doesn't have the ibc transfer feature,
+        // try to fetch the params of ibc transfer module.
+        // assume that it can support the ibc transfer if the params return true, and try to update the features.
+        const result = await restInstance.get<{
+          params: {
+            receive_enabled: boolean;
+            send_enabled: boolean;
+          };
+        }>(
+          isIBCGo
+            ? "/ibc/apps/transfer/v1/params"
+            : "/ibc/applications/transfer/v1beta1/params"
+        );
+        if (
+          result.data.params.receive_enabled &&
+          result.data.params.send_enabled
+        ) {
+          ibcTransferUpdate = true;
+        }
+      }
+    } catch {}
+
+    let wasmd24Update = false;
+    try {
+      if (
+        chainInfo.features?.includes("cosmwasm") &&
+        !chainInfo.features.includes("wasmd_0.24+")
+      ) {
+        // It is difficult to decide which contract address to test on each chain.
+        // So it simply sends a query that fails unconditionally.
+        // However, if 400 bad request instead of 501 occurs, the url itself exists.
+        // In this case, it is assumed that wasmd 0.24+ version.
+        const result = await restInstance.get(
+          "/cosmwasm/wasm/v1/contract/test/smart/test",
+          {
+            validateStatus: (status) => {
+              return status === 400 || status === 501;
+            },
+          }
+        );
+        if (result.status === 400) {
+          wasmd24Update = true;
+        }
+      }
+    } catch {}
+
+    let querySpendableBalances = false;
+    try {
+      if (
+        !chainInfo.features ||
+        !chainInfo.features.includes(
+          "query:/cosmos/bank/v1beta1/spendable_balances"
+        )
+      ) {
+        // It is difficult to decide which account to test on each chain.
+        // So it simply sends a query that fails unconditionally.
+        // However, if 400 bad request instead of 501 occurs, the url itself exists.
+        // In this case, it is assumed that we can query /cosmos/bank/v1beta1/spendable_balances/{account}
+        const result = await restInstance.get(
+          "/cosmos/bank/v1beta1/spendable_balances/test",
+          {
+            validateStatus: (status) => {
+              return status === 400 || status === 501;
+            },
+          }
+        );
+        if (result.status === 400) {
+          querySpendableBalances = true;
+        }
+      }
+    } catch {}
+
+    const features: string[] = [];
+    if (ibcGoUpdates) {
+      features.push("ibc-go");
+    }
+    if (ibcTransferUpdate) {
+      features.push("ibc-transfer");
+    }
+    if (wasmd24Update) {
+      features.push("wasmd_0.24+");
+    }
+    if (querySpendableBalances) {
+      features.push("query:/cosmos/bank/v1beta1/spendable_balances");
+    }
+
+    return {
+      explicit: version.version < fetchedVersion.version,
+      slient: features.length > 0,
+
+      chainId: resultChainId,
+      features,
+    };
+  }
 }
