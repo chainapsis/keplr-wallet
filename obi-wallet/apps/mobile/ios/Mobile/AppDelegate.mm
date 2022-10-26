66dacdcc9f67a07df783a50de1e5b05a848b12a1
Jonas Keinholz <git@inyono.dev>
2022-10-26 23:15:16 +0200

    obi-wallet(mobile): remove unused code

--- obi-wallet/apps/mobile/ios/Mobile/AppDelegate.mm
+++ obi-wallet/apps/mobile/ios/Mobile/AppDelegate.mm
@@ -48,13 +48,6 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
   NSDictionary *initProps = [self prepareInitialProps];
   UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Mobile", initProps);
-  /*
-  if (@available(iOS 13.0, *)) {
-    rootView.backgroundColor = [UIColor systemBackgroundColor];
-  } else {
-    rootView.backgroundColor = [UIColor whiteColor];
-  }
-  */
   rootView.backgroundColor = [UIColor colorWithRed:0.06 green:0.06 blue:0.12 alpha:1.0];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
