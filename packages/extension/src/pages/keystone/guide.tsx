d3082bcbfccd4d1ebd606e159c231bb25809eed4
Renfeng Shi <renfeng@keyst.one>
2022-10-28 18:36:46 +0800

    keystone import key ui

--- packages/extension/src/pages/keystone/guide.tsx
+++ packages/extension/src/pages/keystone/guide.tsx
@@ -1,13 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "reactstrap";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperClass, { Navigation, Pagination } from "swiper";
+import "swiper/swiper-bundle.min.css";
 import style from "./style.module.scss";
 
 export function Guide({ onScan }: { onScan(): void }) {
+  const [stepIndex, setStepIndex] = useState(0);
+  const onActiveIndexChange = (swiper: SwiperClass) => {
+    setStepIndex(swiper.activeIndex);
+  };
+
   return (
     <div className={style.page}>
-      <div>Connect</div>
-      <Button color="primary" block onClick={onScan}>
-        Scan the QR code
+      <div className={style.steps}>
+        <div className={style.title}>Tutorial</div>
+        <Swiper
+          navigation={{ prevEl: "#swiper-prev", nextEl: "#swiper-next" }}
+          pagination={{ clickable: true, el: "#swiper-pagination" }}
+          modules={[Navigation, Pagination]}
+          onActiveIndexChange={onActiveIndexChange}
+        >
+          {[1, 2, 3, 4].map((e: number) => (
+            <SwiperSlide className={style.item} key={e}>
+              <img
+                src={require(`../../public/assets/img/keystone/tutorial${e}.png`)}
+                width="285"
+              />
+            </SwiperSlide>
+          ))}
+          <div id="swiper-prev" className={style.prev}>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              stroke="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10.6665 13.3333L5.6469 7.99992L10.6665 2.66659"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+          <div id="swiper-next" className={style.next}>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              stroke="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5.3335 2.66675L10.3531 8.00008L5.3335 13.3334"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+        </Swiper>
+        <div id="swiper-pagination" className={style.pagination} />
+        {stepIndex === 0 && (
+          <div className={style["swiper-title"]}>
+            Select the <em>Cosmos</em> option in the “Software Wallet” menu
+            available in your Keystone
+          </div>
+        )}
+        {stepIndex === 1 && (
+          <div className={style["swiper-title"]}>
+            Tap the icon “ <em>…</em> ” on the top right corner
+          </div>
+        )}
+        {stepIndex === 2 && (
+          <div className={style["swiper-title"]}>
+            Select <em>Connect Software Wallet</em>
+          </div>
+        )}
+        {stepIndex === 3 && (
+          <div className={style["swiper-title"]}>
+            Click <em>Scan</em> button below and scan the QR code on the
+            Keystone device
+          </div>
+        )}
+        <div className={style.more}>
+          <a href="/" target="_blank">
+            More Details
+          </a>
+        </div>
+      </div>
+      <Button
+        color={stepIndex === 3 ? "primary" : "secondary"}
+        block
+        onClick={onScan}
+      >
+        {stepIndex === 3 ? "Scan the QR code" : "Skip to scan"}
       </Button>
     </div>
   );
--- packages/extension/src/pages/keystone/scan.tsx
+++ packages/extension/src/pages/keystone/scan.tsx
@@ -1,41 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./style.module.scss";
 import { AnimatedQRScanner, Purpose } from "@keystonehq/animated-qr";
 import { UR } from "@keplr-wallet/stores";
+import { Loading } from "./loading";
 
 export interface Props {
   onChange(ur: UR): void;
 }
 
 export function Scan({ onChange }: Props) {
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+  let timer: NodeJS.Timeout | undefined;
+  const onVideoLoaded = (isLoaded: boolean) => {
+    // the first trigger is too early
+    if (!timer) {
+      timer = setTimeout(() => {
+        setIsVideoLoaded(isLoaded);
+      }, 2000);
+    } else {
+      clearTimeout(timer);
+      setIsVideoLoaded(isLoaded);
+    }
+  };
   return (
-    <div className={style.page}>
-      <div>Scan the QR code displayed on your Keystone Device</div>
-      <AnimatedQRScanner
-        purpose={Purpose.COSMOS_SYNC}
-        handleScan={onChange}
-        handleError={console.error}
-        options={{
-          width: 200,
-        }}
-      />
-      <a
-        href="#done"
-        onClick={(e) => {
-          e.preventDefault();
-          onChange({
-            type: "A",
-            cbor:
-              "02bda203ca44c955f1db94bb0d34ef072cebeb27f5bc7b13656bb2881301d017a6",
-          });
-        }}
-      >
-        Done for debug
-      </a>
-      <p>
-        Position the QR code in front of your camera. The screen is blurred but
-        this will not affect the scan.
-      </p>
+    <div className={`${style.page} ${style.center}`}>
+      <div>
+        <div className={style.title}>Scan the QR Code</div>
+        <div className={style.subtitle}>
+          Scan the QR code displayed on your Keystone Device
+        </div>
+        <div className={style.scanner}>
+          {/* {!isVideoLoaded && (
+            <img src={require("../../public/assets/svg/scanner.svg")} />
+          )} */}
+          <img src={require("../../public/assets/svg/scanner.svg")} />
+          <AnimatedQRScanner
+            purpose={Purpose.COSMOS_SYNC}
+            handleScan={onChange}
+            handleError={console.error}
+            videoLoaded={onVideoLoaded}
+            options={{
+              width: 248,
+              height: 248,
+            }}
+          />
+        </div>
+        <p className={style["help-text"]}>
+          Position the QR code in front of your camera. The screen is blurred
+          but this will not affect the scan.
+        </p>
+        <a
+          href="#done"
+          style={{ position: "absolute" }}
+          onClick={(e) => {
+            e.preventDefault();
+            onChange({
+              type: "A",
+              cbor:
+                "02bda203ca44c955f1db94bb0d34ef072cebeb27f5bc7b13656bb2881301d017a6",
+            });
+          }}
+        >
+          Done for debug
+        </a>
+      </div>
+      {isConnecting && <Loading title="Connecting" />}
     </div>
   );
 }
