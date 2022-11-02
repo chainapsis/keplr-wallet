a311f49db787c9982c085ba6988954cb57c7ee4e
HeesungB <hezo25@naver.com>
2022-11-02 16:21:18 +0900

    Fix logic that regist chain info

--- packages/stores/src/core/interaction/chain-suggest.ts
+++ packages/stores/src/core/interaction/chain-suggest.ts
@@ -2,10 +2,15 @@ import { InteractionStore } from "./interaction";
 import { ChainInfo } from "@keplr-wallet/types";
 import { SuggestChainInfoMsg } from "@keplr-wallet/background";
 import { flow, makeObservable, observable } from "mobx";
+import { ChainIdHelper } from "@keplr-wallet/cosmos";
+import Axios from "axios";
 
 export class ChainSuggestStore {
   @observable
   protected _isLoading: boolean = false;
+
+  @observable
+  communityChainInfo: ChainInfo | undefined;
 
   constructor(protected readonly interactionStore: InteractionStore) {
     makeObservable(this);
@@ -22,13 +27,39 @@ export class ChainSuggestStore {
   }
 
   @flow
+  *getCommunityChainInfo() {
+    this._isLoading = true;
+
+    if (this.waitingSuggestedChainInfo) {
+      try {
+        const chainIdentifier = ChainIdHelper.parse(
+          this.waitingSuggestedChainInfo.data.chainId
+        ).identifier;
+        const chainInfoResponse = yield Axios.get<ChainInfo>(
+          `https://raw.githubusercontent.com/danielkim89/cicd-test/main/cosmos/${chainIdentifier}.json`
+        );
+
+        const chainInfo: ChainInfo = chainInfoResponse.data;
+        this.communityChainInfo = chainInfo;
+      } finally {
+        this._isLoading = false;
+      }
+    }
+  }
+
+  @flow
   *approve() {
     this._isLoading = true;
 
     try {
       const data = this.waitingSuggestedChainInfo;
+
       if (data) {
-        yield this.interactionStore.approve(data.type, data.id, {});
+        yield this.interactionStore.approve(
+          data.type,
+          data.id,
+          this.communityChainInfo ?? data.data
+        );
       }
     } finally {
       this._isLoading = false;
