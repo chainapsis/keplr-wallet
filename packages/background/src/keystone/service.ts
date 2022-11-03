20b640349244019c696269eb9253d4e62c55ea0d
Renfeng Shi <renfeng@keyst.one>
2022-11-03 13:56:00 +0800

    center when window resize

--- packages/background/src/keystone/service.ts
+++ packages/background/src/keystone/service.ts
@@ -1,13 +1,8 @@
-import {
-  // APP_PORT,
-  Env,
-  KeplrError,
-} from "@keplr-wallet/router";
+import { Env, KeplrError } from "@keplr-wallet/router";
 import { BIP44HDPath } from "../keyring";
 import { KVStore } from "@keplr-wallet/common";
 import { InteractionService } from "../interaction";
 import { Buffer } from "buffer/";
-// import { EthSignType } from "@keplr-wallet/types";
 import { publicKeyConvert } from "secp256k1";
 
 export const TYPE_KEYSTONE_GET_PUBKEY = "keystone-get-pubkey";
@@ -63,21 +58,4 @@ export class KeystoneService {
       };
     });
   }
-
-  // async sign(
-  //   env: Env,
-  //   bip44HDPath: BIP44HDPath,
-  //   expectedPubKey: Uint8Array,
-  //   message: Uint8Array
-  // ): Promise<Uint8Array> {}
-
-  // async signEthereum(
-  //   env: Env,
-  //   type: EthSignType,
-  //   bip44HDPath: BIP44HDPath,
-  //   expectedPubKey: Uint8Array,
-  //   message: Uint8Array
-  // ): Promise<Uint8Array> {}
-
-  // async useKeystone<T>(env: Env, type: string, data: any): Promise<T> {}
 }
