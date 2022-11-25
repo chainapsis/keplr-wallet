eae0f171008dfdd38f04d502951e3b6798d9cc53
Thunnini <yunjh1994@gmail.com>
2022-11-25 16:34:56 +0900

    Fix a mistake

--- packages/background/src/updater/service.ts
+++ packages/background/src/updater/service.ts
@@ -34,7 +34,7 @@ export class ChainUpdaterService {
 
     if (updatedChainInfo) {
       chainInfo = {
-        ...chainInfo,
+        ...updatedChainInfo,
         walletUrlForStaking:
           updatedChainInfo.walletUrlForStaking || origin.walletUrlForStaking,
         features: (() => {
