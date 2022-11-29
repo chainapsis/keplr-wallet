dacf7b3ab26ff565610e97b94dee6e61b76c429b
Thunnini <yunjh1994@gmail.com>
2022-11-29 22:37:52 +0900

    Set user id for analytics

--- packages/extension/src/stores/index.tsx
+++ packages/extension/src/stores/index.tsx
@@ -1,11 +1,28 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 
 import { createRootStore, RootStore } from "./root";
+import { getKeplrFromWindow } from "@keplr-wallet/stores";
+import { KeplrCoreTypes } from "@keplr-wallet/provider/build/core-types";
+import { Keplr } from "@keplr-wallet/types";
 
 const storeContext = React.createContext<RootStore | null>(null);
 
 export const StoreProvider: FunctionComponent = ({ children }) => {
   const [stores] = useState(() => createRootStore());
+
+  useEffect(() => {
+    getKeplrFromWindow().then(
+      (keplr: (Keplr & Partial<KeplrCoreTypes>) | undefined) => {
+        // Remember that `KeplrCoreTypes` is only usable on privileged env.
+        // Definitely, extension is privileged env. So, we can use `privileged env`.
+        if (keplr && keplr.__core__getAnalyticsId) {
+          keplr.__core__getAnalyticsId().then((id) => {
+            stores.analyticsStore.setUserId(id);
+          });
+        }
+      }
+    );
+  }, [stores.analyticsStore]);
 
   return (
     <storeContext.Provider value={stores}>{children}</storeContext.Provider>
