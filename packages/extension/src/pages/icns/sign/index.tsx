73701a4317f09f7d5ef462bfc827c7219fb9ca4a
Thunnini <yunjh1994@gmail.com>
2022-12-13 13:37:17 +0900

    WIP: ICNSAdr36SignPage

--- packages/extension/src/pages/icns/sign/index.tsx
+++ packages/extension/src/pages/icns/sign/index.tsx
@@ -1,24 +1,89 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useLayoutEffect } from "react";
 import { observer } from "mobx-react-lite";
 import { Button } from "reactstrap";
 import { useStore } from "../../../stores";
+import { EmptyLayout } from "../../../layouts/empty-layout";
+import style from "./style.module.scss";
+import { Bech32Address } from "@keplr-wallet/cosmos";
 
 export const ICNSAdr36SignPage: FunctionComponent = observer(() => {
+  useLayoutEffect(() => {
+    // XXX: Temporal solution for fitting the popup window.
+    //      Even though this is noy proper way to adjust style,
+    //      it is safe because this page only can be open on popup.
+    document.documentElement.style.height = "100%";
+    document.body.style.height = "100%";
+    const app = document.getElementById("app");
+    if (app) {
+      app.style.height = "100%";
+    }
+  }, []);
+
   const { icnsInteractionStore } = useStore();
 
   return (
-    <Button
-      onClick={(e) => {
-        e.preventDefault();
+    <EmptyLayout style={{ height: "100%" }}>
+      <div className={style.container}>
+        <div style={{ flex: 1, overflow: "scroll", borderRadius: "14px" }}>
+          {icnsInteractionStore.waitingData?.data.accountInfos.map(
+            (accountInfo, i) => {
+              return (
+                <div key={i} className={style.nameContainer}>
+                  <div
+                    className={style.name}
+                  >{`${icnsInteractionStore.waitingData?.data.username}.${accountInfo.bech32Prefix}`}</div>
+                  <div className={style.address}>
+                    {Bech32Address.shortenAddress(
+                      accountInfo.bech32Address,
+                      30
+                    )}
+                  </div>
+                </div>
+              );
+            }
+          )}
+        </div>
+        <div className={style.buttons}>
+          <Button
+            className={style.button}
+            color="danger"
+            outline={true}
+            block={true}
+            onClick={(e) => {
+              e.preventDefault();
 
-        if (icnsInteractionStore.waitingData) {
-          icnsInteractionStore.approve(icnsInteractionStore.waitingData.id);
-        }
+              if (icnsInteractionStore.waitingData) {
+                icnsInteractionStore.reject(
+                  icnsInteractionStore.waitingData.id
+                );
+              }
 
-        window.close();
-      }}
-    >
-      tes
-    </Button>
+              window.close();
+            }}
+          >
+            Reject
+          </Button>
+          <Button
+            className={style.button}
+            color="primary"
+            block={true}
+            disabled={!icnsInteractionStore.waitingData}
+            onClick={(e) => {
+              e.preventDefault();
+
+              if (icnsInteractionStore.waitingData) {
+                icnsInteractionStore.approve(
+                  icnsInteractionStore.waitingData.id
+                );
+              }
+
+              window.close();
+            }}
+          >
+            Approve
+          </Button>
+        </div>
+      </div>
+    </EmptyLayout>
   );
 });
