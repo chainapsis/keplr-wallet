6349f5e3e67460270677309c1b9dd4adfe12e23a
Thunnini <yunjh1994@gmail.com>
2023-01-11 00:47:22 +0800

    Improve styling of markdown

--- packages/mobile/src/screens/governance/internal/markdown.tsx
+++ packages/mobile/src/screens/governance/internal/markdown.tsx
@@ -19,6 +19,7 @@ export const MemoizedHtmlRender: FunctionComponent<{
       }}
       contentWidth={contentWidth}
       baseStyle={style.flatten([
+        "body3",
         "color-text-middle",
         "dark:color-platinum-100",
         "margin-0",
@@ -31,15 +32,16 @@ export const MemoizedHtmlRender: FunctionComponent<{
         p: ParagraphRenderer,
       }}
       tagsStyles={{
-        h1: style.flatten(["margin-top-8", "margin-bottom-4"]),
-        h2: style.flatten(["margin-top-8", "margin-bottom-4"]),
-        h3: style.flatten(["margin-top-8", "margin-bottom-4"]),
-        h4: style.flatten(["margin-top-8", "margin-bottom-4"]),
-        h5: style.flatten(["margin-top-8", "margin-bottom-4"]),
-        h6: style.flatten(["margin-top-8", "margin-bottom-4"]),
+        h1: style.flatten(["h2", "margin-top-8", "margin-bottom-4"]),
+        h2: style.flatten(["h3", "margin-top-8", "margin-bottom-4"]),
+        h3: style.flatten(["h4", "margin-top-8", "margin-bottom-4"]),
+        h4: style.flatten(["h5", "margin-top-8", "margin-bottom-4"]),
+        h5: style.flatten(["h6", "margin-top-8", "margin-bottom-4"]),
+        h6: style.flatten(["h7", "margin-top-8", "margin-bottom-4"]),
         p: style.flatten(["margin-top-4", "margin-bottom-4"]),
         ul: style.flatten(["margin-top-4", "margin-bottom-4"]),
-        li: style.flatten(["margin-bottom-8"]),
+        li: style.flatten(["margin-bottom-4"]),
+        strong: style.flatten(["color-text-high", "dark:color-platinum-50"]),
       }}
     />
   );
