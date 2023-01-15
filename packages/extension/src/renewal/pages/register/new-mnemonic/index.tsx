13186118748de18f7b8dfbcbedb8a75687d15935
Thunnini <yunjh1994@gmail.com>
2023-01-15 16:42:16 +0800

    WIP

--- packages/extension/src/renewal/pages/register/new-mnemonic/index.tsx
+++ packages/extension/src/renewal/pages/register/new-mnemonic/index.tsx
@@ -1,11 +1,14 @@
-import React, { FunctionComponent, useMemo } from "react";
+import React, { FunctionComponent, useMemo, useState } from "react";
 import {
   RegisterSceneBox,
   RegisterSceneBoxHeader,
 } from "../components/register-scene-box";
 import { Stack } from "../../../components/stack";
 import { Button } from "../../../components/button";
-import { useSceneTransition } from "../../../components/transition";
+import {
+  useSceneTransition,
+  VerticalResizeTransition,
+} from "../../../components/transition";
 import { Column, Columns } from "../../../components/column";
 import { TextInput } from "../../../components/input";
 import { XAxis } from "../../../components/axis";
@@ -16,11 +19,13 @@ import { Bleed } from "../../../components/bleed";
 export const NewMnemonicScene: FunctionComponent = () => {
   const sceneTransition = useSceneTransition();
 
-  const threeWords: string[][] = useMemo(() => {
-    const words = "bag edit veteran phrase catch seat injury fade pact tomorrow pact charge".split(
+  const [words, setWords] = useState(
+    "bag edit veteran phrase catch seat injury fade pact tomorrow pact charge".split(
       " "
-    );
+    )
+  );
 
+  const threeWords: string[][] = useMemo(() => {
     let temp: string[] = [];
     const r: string[][] = [];
     for (const word of words) {
@@ -36,43 +41,59 @@ export const NewMnemonicScene: FunctionComponent = () => {
     }
 
     return r;
-  }, []);
+  }, [words]);
 
   return (
     <RegisterSceneBox>
       <RegisterSceneBoxHeader>New mnemonic</RegisterSceneBoxHeader>
       <Stack>
-        <Bleed left="1rem">
-          <Stack gutter="0.75rem">
-            {threeWords.map((words, i) => {
-              return (
-                <Columns key={i} sum={3}>
-                  {words.map((word, j) => {
-                    return (
-                      <Column key={j} weight={1}>
-                        <XAxis alignY="center">
-                          <Styles.IndexText>{i * 3 + j + 1}.</Styles.IndexText>
-                          <TextInput
-                            value={word}
-                            readOnly={true}
-                            removeBottomMargin={true}
-                          />
-                        </XAxis>
-                      </Column>
-                    );
-                  })}
-                </Columns>
-              );
-            })}
-          </Stack>
-        </Bleed>
-        <Gutter size="1rem" />
+        <VerticalResizeTransition>
+          <Bleed left="1rem">
+            <Stack gutter="0.75rem">
+              {threeWords.map((words, i) => {
+                return (
+                  <Columns key={i} sum={3}>
+                    {words.map((word, j) => {
+                      return (
+                        <Column key={j} weight={1}>
+                          <XAxis alignY="center">
+                            <Styles.IndexText>
+                              {i * 3 + j + 1}.
+                            </Styles.IndexText>
+                            <TextInput
+                              value={word}
+                              readOnly={true}
+                              removeBottomMargin={true}
+                            />
+                          </XAxis>
+                        </Column>
+                      );
+                    })}
+                  </Columns>
+                );
+              })}
+            </Stack>
+          </Bleed>
+          <Gutter size="1rem" />
+        </VerticalResizeTransition>
         <Button
           mode="light"
           size="small"
           text="Set BIP Path"
           onClick={() => {
-            alert("TODO");
+            if (words.length === 12) {
+              setWords(
+                "bag edit veteran phrase catch seat injury fade pact tomorrow pact charge bag edit veteran phrase catch seat injury fade pact tomorrow pact charge".split(
+                  " "
+                )
+              );
+            } else {
+              setWords(
+                "bag edit veteran phrase catch seat injury fade pact tomorrow pact charge".split(
+                  " "
+                )
+              );
+            }
           }}
         />
         <Gutter size="1rem" />
