3133b48d759f484f98887c8b16dd16158c0e1707
HeesungB <hezo25@naver.com>
2023-01-27 17:26:55 +0900

    Fix grants logic, Add tooltip

--- packages/extension/src/pages/main/authz.tsx
+++ packages/extension/src/pages/main/authz.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useMemo } from "react";
 import styleAuthZ from "./authz.module.scss";
 import classnames from "classnames";
 import { FormattedDate, FormattedMessage } from "react-intl";
@@ -8,106 +8,120 @@ import { AuthZ } from "@keplr-wallet/stores";
 import { useHistory } from "react-router";
 import { Bech32Address } from "@keplr-wallet/cosmos";
 import { Buffer } from "buffer/";
+import { ToolTip } from "../../components/tooltip";
 
 interface Props {
   grants: AuthZ.Grant[];
 }
 
-interface GrantFilter {
-  name: string;
-  filter: (grant: AuthZ.Grant) => boolean;
-}
+type grantListType = Record<string, AuthZ.Grant[]>;
 
 export const AuthZView: FunctionComponent<Props> = (props) => {
   const { grants } = props;
   const history = useHistory();
 
-  const isSendAuthorization = (grant: AuthZ.Grant) =>
-    grant.authorization["@type"] === "/cosmos.bank.v1beta1.SendAuthorization";
+  const grantList = useMemo(() => {
+    const tempGrantList: grantListType = {
+      Send: [],
+      Delegate: [],
+      Redelegate: [],
+      Undelegate: [],
+      "Withdraw Reward": [],
+      Vote: [],
+      Deposit: [],
+      Custom: [],
+    };
 
-  const isGenericAuthorization = (grant: AuthZ.Grant) =>
-    grant.authorization["@type"] ===
-    "/cosmos.authz.v1beta1.GenericAuthorization";
+    const isSendAuthorization = (grant: AuthZ.Grant) =>
+      grant.authorization["@type"] === "/cosmos.bank.v1beta1.SendAuthorization";
 
-  const isStakeAuthorization = (grant: AuthZ.Grant) =>
-    grant.authorization["@type"] ===
-    "/cosmos.staking.v1beta1.StakeAuthorization";
+    const isGenericAuthorization = (grant: AuthZ.Grant) =>
+      grant.authorization["@type"] ===
+      "/cosmos.authz.v1beta1.GenericAuthorization";
 
-  const grantFilters: GrantFilter[] = [
-    {
-      name: "Send",
-      filter: (grant: AuthZ.Grant) =>
+    const isStakeAuthorization = (grant: AuthZ.Grant) =>
+      grant.authorization["@type"] ===
+      "/cosmos.staking.v1beta1.StakeAuthorization";
+
+    grants.forEach((grant) => {
+      if (
         isSendAuthorization(grant) ||
         (isGenericAuthorization(grant) &&
           (grant.authorization as AuthZ.GenericAuthorization).msg ===
-            "/cosmos.bank.v1beta1.MsgSend"),
-    },
-    {
-      name: "Delegate",
-      filter: (grant: AuthZ.Grant) =>
+            "/cosmos.bank.v1beta1.MsgSend")
+      ) {
+        tempGrantList.Send.push(grant);
+        return;
+      }
+
+      if (
         (isStakeAuthorization(grant) &&
           (grant.authorization as AuthZ.StakeAuthorization)
             .authorization_type === "AUTHORIZATION_TYPE_DELEGATE") ||
         (isGenericAuthorization(grant) &&
           (grant.authorization as AuthZ.GenericAuthorization).msg ===
-            "/cosmos.staking.v1beta1.MsgDelegate"),
-    },
-    {
-      name: "Redelegate",
-      filter: (grant: AuthZ.Grant) =>
+            "/cosmos.staking.v1beta1.MsgDelegate")
+      ) {
+        tempGrantList.Delegate.push(grant);
+        return;
+      }
+
+      if (
         (isStakeAuthorization(grant) &&
           (grant.authorization as AuthZ.StakeAuthorization)
             .authorization_type === "AUTHORIZATION_TYPE_REDELEGATE") ||
         (isGenericAuthorization(grant) &&
           (grant.authorization as AuthZ.GenericAuthorization).msg ===
-            "/cosmos.staking.v1beta1.MsgBeginRedelegate"),
-    },
-    {
-      name: "Undelegate",
-      filter: (grant: AuthZ.Grant) =>
+            "/cosmos.staking.v1beta1.MsgBeginRedelegate")
+      ) {
+        tempGrantList.Redelegate.push(grant);
+        return;
+      }
+
+      if (
         (isStakeAuthorization(grant) &&
           (grant.authorization as AuthZ.StakeAuthorization)
             .authorization_type === "AUTHORIZATION_TYPE_UNDELEGATE") ||
         (isGenericAuthorization(grant) &&
           (grant.authorization as AuthZ.GenericAuthorization).msg ===
-            "/cosmos.staking.v1beta1.MsgUndelegate"),
-    },
-    {
-      name: "Withdraw Reward",
-      filter: (grant: AuthZ.Grant) =>
+            "/cosmos.staking.v1beta1.MsgUndelegate")
+      ) {
+        tempGrantList.Undelegate.push(grant);
+        return;
+      }
+
+      if (
         isGenericAuthorization(grant) &&
         (grant.authorization as AuthZ.GenericAuthorization).msg ===
-          "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
-    },
-    {
-      name: "Vote",
-      filter: (grant: AuthZ.Grant) =>
+          "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward"
+      ) {
+        tempGrantList["Withdraw Reward"].push(grant);
+        return;
+      }
+
+      if (
         isGenericAuthorization(grant) &&
         (grant.authorization as AuthZ.GenericAuthorization).msg ===
-          "/cosmos.gov.v1beta1.MsgVote",
-    },
-    {
-      name: "Deposit",
-      filter: (grant: AuthZ.Grant) =>
+          "/cosmos.gov.v1beta1.MsgVote"
+      ) {
+        tempGrantList.Vote.push(grant);
+        return;
+      }
+
+      if (
         isGenericAuthorization(grant) &&
         (grant.authorization as AuthZ.GenericAuthorization).msg ===
-          "/cosmos.gov.v1beta1.MsgDeposit",
-    },
-    {
-      name: "Custom",
-      filter: (grant: AuthZ.Grant) =>
-        isGenericAuthorization(grant) &&
-        ![
-          "/cosmos.bank.v1beta1.MsgSend",
-          "/cosmos.staking.v1beta1.MsgDelegate",
-          "/cosmos.staking.v1beta1.MsgBeginRedelegate",
-          "/cosmos.staking.v1beta1.MsgUndelegate",
-          "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
-          "/cosmos.gov.v1beta1.MsgDeposit",
-          "/cosmos.gov.v1beta1.MsgVote",
-        ].includes((grant.authorization as AuthZ.GenericAuthorization).msg),
-    },
-  ];
+          "/cosmos.gov.v1beta1.MsgDeposit"
+      ) {
+        tempGrantList.Deposit.push(grant);
+        return;
+      }
+
+      tempGrantList.Custom.push(grant);
+    });
+
+    return tempGrantList;
+  }, [grants]);
 
   const onClickAuthZItem = (title: string, grant: AuthZ.Grant) => {
     history.push({
@@ -132,54 +146,66 @@ export const AuthZView: FunctionComponent<Props> = (props) => {
           <FormattedMessage id="main.authz.title" />
         </p>
 
-        {grantFilters.map((grantFilter) => {
-          const filteredGrant = grants.filter(grantFilter.filter);
-
-          const grantsItems = filteredGrant.map((grant, index) => {
-            return (
-              <div
-                className={styleAuthZ.item}
-                key={index}
-                onClick={() => onClickAuthZItem(grantFilter.name, grant)}
-              >
-                <div className={styleAuthZ.title}>
-                  <FormattedMessage id="main.authz.grant.grantee.information" />
-                  <span className={styleAuthZ.bold}>
-                    {Bech32Address.shortenAddress(grant.grantee, 20)}
-                  </span>
-                </div>
-                {grant.expiration ? (
-                  <div className={styleAuthZ.expiration}>
-                    <FormattedMessage id="main.authz.grant.expiration.information" />
-                    {new Date() < new Date(grant.expiration) ? (
-                      <FormattedDate
-                        value={grant.expiration}
-                        year="numeric"
-                        month="2-digit"
-                        day="2-digit"
-                        hour="2-digit"
-                        minute="2-digit"
-                        hour12={false}
-                      />
+        {grantList
+          ? Object.entries(grantList).map(([title, grants]) => {
+              const grantsItems = grants.map((grant, index) => {
+                return (
+                  <div
+                    className={styleAuthZ.item}
+                    key={index}
+                    onClick={() => onClickAuthZItem(title, grant)}
+                  >
+                    <div className={styleAuthZ.title}>
+                      <FormattedMessage id="main.authz.grant.grantee.information" />
+                      <ToolTip
+                        trigger="hover"
+                        options={{ placement: "bottom" }}
+                        tooltip={
+                          <div className={styleAuthZ.tooltip}>
+                            {grant.grantee}
+                          </div>
+                        }
+                      >
+                        <span className={styleAuthZ.bold}>
+                          {Bech32Address.shortenAddress(grant.grantee, 20)}
+                        </span>
+                      </ToolTip>
+                    </div>
+                    {grant.expiration ? (
+                      <div className={styleAuthZ.expiration}>
+                        <FormattedMessage id="main.authz.grant.expiration.information" />
+                        {new Date() < new Date(grant.expiration) ? (
+                          <FormattedDate
+                            value={grant.expiration}
+                            year="numeric"
+                            month="2-digit"
+                            day="2-digit"
+                            hour="2-digit"
+                            minute="2-digit"
+                            hour12={false}
+                          />
+                        ) : (
+                          <FormattedMessage id="main.authz.grant.expiration.expired" />
+                        )}
+                      </div>
                     ) : (
-                      <FormattedMessage id="main.authz.grant.expiration.expired" />
+                      <FormattedMessage id="main.authz.grant.expiration.infinite" />
                     )}
                   </div>
-                ) : null}
-              </div>
-            );
-          });
+                );
+              });
 
-          return grantsItems.length > 0 ? (
-            <Collapse
-              key={grantFilter.name}
-              title={grantFilter.name}
-              count={`${grantsItems.length}`}
-            >
-              <div className={styleAuthZ.grants}>{grantsItems}</div>
-            </Collapse>
-          ) : null;
-        })}
+              return grantsItems.length > 0 ? (
+                <Collapse
+                  key={title}
+                  title={title}
+                  count={`${grantsItems.length}`}
+                >
+                  <div className={styleAuthZ.grants}>{grantsItems}</div>
+                </Collapse>
+              ) : null;
+            })
+          : null}
       </div>
     </div>
   );
