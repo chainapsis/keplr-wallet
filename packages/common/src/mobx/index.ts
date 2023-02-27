b6d232ad980ccdcbbdf10af638001348da11cae6
Thunnini <yunjh1994@gmail.com>
2023-02-27 19:45:53 +0900

    Move `DebounceActionTimer` under "common" package

--- packages/common/src/mobx/index.ts
+++ packages/common/src/mobx/index.ts
@@ -1,25 +1,2 @@
-// Copied from the mobx-state-tree repository.
-/**
- * @experimental
- * experimental api - might change on minor/patch releases
- *
- * Convert a promise to a generator yielding that promise
- * This is intended to allow for usage of `yield*` in async actions to
- * retain the promise return type.
- *
- * Example:
- * ```ts
- * function getDataAsync(input: string): Promise<number> { ... }
- *
- * const someModel.actions(self => ({
- *   someAction: flow(function*() {
- *     // value is typed as number
- *     const value = yield* toGenerator(getDataAsync("input value"));
- *     ...
- *   })
- * }))
- * ```
- */
-export function* toGenerator<R>(p: Promise<R>) {
-  return (yield p) as R;
-}
+export * from "./etc";
+export * from "./debounce";
