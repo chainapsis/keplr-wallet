6fc41cfb0fed92321d68b838ef07a7f0ebc5753b
HeesungB <hezo25@naver.com>
2023-03-16 21:49:36 +0900

    Add claim tokens expand view

--- packages/extension/src/pages/main/components/claim-all/index.tsx
+++ packages/extension/src/pages/main/components/claim-all/index.tsx
@@ -1,10 +1,16 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { Column, Columns } from "../../../../components/column";
 import { Button } from "../../../../components/button";
 import { Stack } from "../../../../components/stack";
 import { Box } from "../../../../components/box";
+import { CoinPretty } from "@keplr-wallet/unit";
+import { VerticalCollapseTransition } from "../../../../components/transition/vertical-collapse";
 
-export const ClaimAll: FunctionComponent = () => {
+export const ClaimAll: FunctionComponent<{ tokens: CoinPretty[] }> = ({
+  tokens,
+}) => {
+  const [isExpanded, setIsExpanded] = useState(true);
+
   return (
     <Box paddingX="0.5rem">
       <Columns sum={1} alignY="center">
@@ -16,6 +22,32 @@ export const ClaimAll: FunctionComponent = () => {
         </Column>
         <Button text="Claim All" />
       </Columns>
+
+      <Button text="claim tokens" onClick={() => setIsExpanded(!isExpanded)} />
+
+      <VerticalCollapseTransition collapsed={isExpanded}>
+        {tokens.map((token) => {
+          return (
+            <Columns sum={1} key={token.currency.coinMinimalDenom}>
+              {token.currency.coinImageUrl && (
+                <img
+                  width="32px"
+                  height="32px"
+                  src={token.currency.coinImageUrl}
+                />
+              )}
+              <Column weight={1}>
+                <Stack>
+                  <Box>{token.currency.coinDenom}</Box>
+                  <Box>{token.hideDenom(true).toString()}</Box>
+                </Stack>
+              </Column>
+
+              <Button text="Claim" />
+            </Columns>
+          );
+        })}
+      </VerticalCollapseTransition>
     </Box>
   );
 };
