8e09aca4b21748d663a86a182e68d5972cdd43ac
Thunnini <yunjh1994@gmail.com>
2023-03-20 15:43:08 +0900

    Simplify query test slightly

--- packages/stores/src/common/query/query.spec.ts
+++ packages/stores/src/common/query/query.spec.ts
@@ -1,16 +1,24 @@
 import { ObservableQuery, QueryOptions } from "./query";
 import { QuerySharedContext } from "./context";
 import { MemoryKVStore } from "@keplr-wallet/common";
-import { autorun } from "mobx";
+import { autorun, makeObservable, observable, runInAction } from "mobx";
 
 export class MockObservableQuery extends ObservableQuery<string> {
-  protected count: number = 0;
+  @observable.ref
+  protected resolver:
+    | {
+        resolve: (res: { headers: any; data: string }) => void;
+        reject: (e: Error) => void;
+      }
+    | undefined = undefined;
 
   constructor(
     sharedContext: QuerySharedContext,
     options: Partial<QueryOptions> = {}
   ) {
     super(sharedContext, "https://noop.org", "/nop", options);
+
+    makeObservable(this);
   }
 
   changeURL(url: string) {
@@ -18,13 +26,64 @@ export class MockObservableQuery extends ObservableQuery<string> {
   }
 
   protected override async fetchResponse(
-    _: AbortController
+    abortController: AbortController
   ): Promise<{ headers: any; data: string }> {
-    this.count++;
-    return {
-      headers: {},
-      data: (this.count - 1).toString(),
+    abortController.signal.onabort = () => {
+      if (this.resolver) {
+        runInAction(() => {
+          this.resolver = undefined;
+        });
+      }
     };
+
+    return new Promise((resolve, reject) => {
+      runInAction(() => {
+        this.resolver = {
+          resolve,
+          reject,
+        };
+      });
+    });
+  }
+
+  waitBeforeFetchResponse() {
+    return new Promise<void>((resolve) => {
+      const disposal = autorun(
+        () => {
+          if (this.resolver) {
+            resolve();
+            if (disposal) {
+              disposal();
+            }
+          }
+        },
+        {
+          delay: 10,
+        }
+      );
+    });
+  }
+
+  resolveResponse(res: { headers: any; data: string }) {
+    if (!this.resolver) {
+      throw new Error("Resolver is not set");
+    }
+
+    this.resolver.resolve(res);
+    runInAction(() => {
+      this.resolver = undefined;
+    });
+  }
+
+  rejectResponse(e: Error) {
+    if (!this.resolver) {
+      throw new Error("Resolver is not set");
+    }
+
+    this.resolver.reject(e);
+    runInAction(() => {
+      this.resolver = undefined;
+    });
   }
 }
 
@@ -32,7 +91,7 @@ describe("Test Query store", () => {
   it("test basic query", async () => {
     const store = new MockObservableQuery(
       new QuerySharedContext(new MemoryKVStore("test"), {
-        responseDebounceMs: 0,
+        responseDebounceMs: 10,
       })
     );
 
@@ -40,7 +99,7 @@ describe("Test Query store", () => {
     expect(store.isStarted).toBe(false);
     expect(store.isFetching).toBe(false);
 
-    autorun(() => {
+    const disposal = autorun(() => {
       if (store.response && store.response.data !== "0") {
         throw new Error();
       }
@@ -50,10 +109,23 @@ describe("Test Query store", () => {
     expect(store.isStarted).toBe(true);
     expect(store.isFetching).toBe(true);
 
+    await store.waitBeforeFetchResponse();
+    store.resolveResponse({
+      headers: {},
+      data: "0",
+    });
     await store.waitResponse();
 
     expect(store.isObserved).toBe(true);
     expect(store.isStarted).toBe(true);
+    expect(store.isFetching).toBe(false);
+
+    expect(store.response?.data).toBe("0");
+
+    disposal();
+
+    expect(store.isObserved).toBe(false);
+    expect(store.isStarted).toBe(false);
     expect(store.isFetching).toBe(false);
 
     expect(store.response?.data).toBe("0");
@@ -62,7 +134,7 @@ describe("Test Query store", () => {
   it("test basic waitResponse()", async () => {
     const store = new MockObservableQuery(
       new QuerySharedContext(new MemoryKVStore("test"), {
-        responseDebounceMs: 0,
+        responseDebounceMs: 10,
       })
     );
 
@@ -70,6 +142,50 @@ describe("Test Query store", () => {
     expect(store.isStarted).toBe(false);
     expect(store.isFetching).toBe(false);
 
+    store.waitBeforeFetchResponse().then(() => {
+      store.resolveResponse({
+        headers: {},
+        data: "0",
+      });
+    });
+    await store.waitResponse();
+
+    expect(store.isObserved).toBe(false);
+    expect(store.isStarted).toBe(false);
+    expect(store.isFetching).toBe(false);
+
+    expect(store.response?.data).toBe("0");
+  });
+
+  it("test basic waitResponse() with observation disposed", async () => {
+    const store = new MockObservableQuery(
+      new QuerySharedContext(new MemoryKVStore("test"), {
+        responseDebounceMs: 10,
+      })
+    );
+
+    const disposal = autorun(() => {
+      if (store.response) {
+        throw new Error();
+      }
+    });
+
+    expect(store.isObserved).toBe(true);
+    expect(store.isStarted).toBe(true);
+    expect(store.isFetching).toBe(true);
+
+    setTimeout(() => {
+      disposal();
+
+      setTimeout(() => {
+        store.waitBeforeFetchResponse().then(() => {
+          store.resolveResponse({
+            headers: {},
+            data: "0",
+          });
+        });
+      }, 100);
+    }, 100);
     await store.waitResponse();
 
     expect(store.isObserved).toBe(false);
@@ -82,7 +198,7 @@ describe("Test Query store", () => {
   it("test basic waitFreshResponse()", async () => {
     const store = new MockObservableQuery(
       new QuerySharedContext(new MemoryKVStore("test"), {
-        responseDebounceMs: 0,
+        responseDebounceMs: 10,
       })
     );
 
@@ -90,6 +206,50 @@ describe("Test Query store", () => {
     expect(store.isStarted).toBe(false);
     expect(store.isFetching).toBe(false);
 
+    store.waitBeforeFetchResponse().then(() => {
+      store.resolveResponse({
+        headers: {},
+        data: "0",
+      });
+    });
+    await store.waitFreshResponse();
+
+    expect(store.isObserved).toBe(false);
+    expect(store.isStarted).toBe(false);
+    expect(store.isFetching).toBe(false);
+
+    expect(store.response?.data).toBe("0");
+  });
+
+  it("test basic waitFreshResponse() with observation disposed", async () => {
+    const store = new MockObservableQuery(
+      new QuerySharedContext(new MemoryKVStore("test"), {
+        responseDebounceMs: 10,
+      })
+    );
+
+    const disposal = autorun(() => {
+      if (store.response) {
+        throw new Error();
+      }
+    });
+
+    expect(store.isObserved).toBe(true);
+    expect(store.isStarted).toBe(true);
+    expect(store.isFetching).toBe(true);
+
+    setTimeout(() => {
+      disposal();
+
+      setTimeout(() => {
+        store.waitBeforeFetchResponse().then(() => {
+          store.resolveResponse({
+            headers: {},
+            data: "0",
+          });
+        });
+      }, 100);
+    }, 100);
     await store.waitFreshResponse();
 
     expect(store.isObserved).toBe(false);
