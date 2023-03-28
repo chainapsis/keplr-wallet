93d13481fd420f0e668b1fb4d9c93449745247d2
HeesungB <hezo25@naver.com>
2023-03-28 21:03:53 +0900

    Fix claim all layout

--- packages/extension/src/pages/main/components/claim-all/index.tsx
+++ packages/extension/src/pages/main/components/claim-all/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "../../../../components/button";
 import { Stack } from "../../../../components/stack";
 import { Box } from "../../../../components/box";
 import { VerticalCollapseTransition } from "../../../../components/transition/vertical-collapse";
-import { Body2, Subtitle2 } from "../../../../components/typography";
+import { Body2, Subtitle2, Subtitle3 } from "../../../../components/typography";
 import { ColorPalette } from "../../../../styles";
 import { ViewToken } from "../../index";
 import styled from "styled-components";
@@ -13,8 +13,19 @@ import { ArrowDownIcon, ArrowUpIcon } from "../../../../components/icon";
 const Styles = {
   Container: styled.div`
     background-color: ${ColorPalette["gray-600"]};
-    padding: 0.75rem 1rem 0 1rem;
+    padding: 0.75rem 0 0 0;
     border-radius: 0.375rem;
+  `,
+  ExpandButton: styled(Box)`
+    :hover {
+      background-color: ${ColorPalette["gray-500"]};
+      opacity: 0.5;
+    }
+
+    :active {
+      background-color: ${ColorPalette["gray-500"]};
+      opacity: 0.2;
+    }
   `,
 };
 
@@ -25,21 +36,23 @@ export const ClaimAll: FunctionComponent<{ viewTokens: ViewToken[] }> = ({
 
   return (
     <Styles.Container>
-      <Columns sum={1} alignY="center">
-        <Column weight={1}>
-          <Stack gutter="0.5rem">
-            <Body2 style={{ color: ColorPalette["gray-300"] }}>
-              Pending Staking Reward
-            </Body2>
-            <Subtitle2 style={{ color: ColorPalette["gray-10"] }}>
-              $ 1.50
-            </Subtitle2>
-          </Stack>
-        </Column>
-        <Button text="Claim All" size="small" />
-      </Columns>
+      <Box paddingX="1rem">
+        <Columns sum={1} alignY="center">
+          <Column weight={1}>
+            <Stack gutter="0.5rem">
+              <Body2 style={{ color: ColorPalette["gray-300"] }}>
+                Pending Staking Reward
+              </Body2>
+              <Subtitle2 style={{ color: ColorPalette["gray-10"] }}>
+                $ 1.50
+              </Subtitle2>
+            </Stack>
+          </Column>
+          <Button text="Claim All" size="small" />
+        </Columns>
+      </Box>
 
-      <Box
+      <Styles.ExpandButton
         paddingX="0.125rem"
         alignX="center"
         onClick={() => setIsExpanded(!isExpanded)}
@@ -49,34 +62,48 @@ export const ClaimAll: FunctionComponent<{ viewTokens: ViewToken[] }> = ({
         ) : (
           <ArrowUpIcon width="1.25rem" height="1.25rem" />
         )}
-      </Box>
+      </Styles.ExpandButton>
 
       <VerticalCollapseTransition collapsed={isExpanded}>
         {viewTokens.map((viewToken) => {
           return (
-            <Columns
-              sum={1}
+            <ClaimTokenItem
+              viewToken={viewToken}
               key={`${viewToken.chainInfo.chainId}-${viewToken.token.currency.coinMinimalDenom}`}
-            >
-              {viewToken.token.currency.coinImageUrl && (
-                <img
-                  width="32px"
-                  height="32px"
-                  src={viewToken.token.currency.coinImageUrl}
-                />
-              )}
-              <Column weight={1}>
-                <Stack>
-                  <Box>{viewToken.token.currency.coinDenom}</Box>
-                  <Box>{viewToken.token.hideDenom(true).toString()}</Box>
-                </Stack>
-              </Column>
-
-              <Button text="Claim" />
-            </Columns>
+            />
           );
         })}
       </VerticalCollapseTransition>
     </Styles.Container>
+  );
+};
+
+const ClaimTokenItem: FunctionComponent<{ viewToken: ViewToken }> = ({
+  viewToken,
+}) => {
+  return (
+    <Box paddingY="0.5rem">
+      <Columns sum={1} gutter="0.75rem" alignY="center">
+        {viewToken.token.currency.coinImageUrl && (
+          <img
+            width="32px"
+            height="32px"
+            src={viewToken.token.currency.coinImageUrl}
+          />
+        )}
+        <Column weight={1}>
+          <Stack gutter="0.375rem">
+            <Subtitle3 style={{ color: ColorPalette["gray-300"] }}>
+              {viewToken.token.currency.coinDenom}
+            </Subtitle3>
+            <Subtitle2 style={{ color: ColorPalette["gray-10"] }}>
+              {viewToken.token.hideDenom(true).toString()}
+            </Subtitle2>
+          </Stack>
+        </Column>
+
+        <Button text="Claim" size="small" color="secondary" />
+      </Columns>
+    </Box>
   );
 };
