1af004e8edac170bf8260ff694e54bdcdc6943c4
HeesungB <hezo25@naver.com>
2023-03-28 21:02:09 +0900

    Add toggle component

---
+++ packages/extension/src/components/icon/check-toggle.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent } from "react";
+import { IconProps } from "./types";
+
+export const CheckToggleIcon: FunctionComponent<IconProps> = ({
+  width = 24,
+  height = 24,
+}) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.00003 16.17L4.83003 12L3.41003 13.41L9.00003 19L21 6.99997L19.59 5.58997L9.00003 16.17Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
