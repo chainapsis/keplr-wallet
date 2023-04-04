a3aec9c8847807152677d6f0f10cf2bebbcf0e52
Pratap Mahey <pratap.mahey@fetch.ai>
2023-04-04 12:24:07 +0530

    chore: modify package release scripts

--- scripts/package-release.py
+++ scripts/package-release.py
@@ -38,6 +38,7 @@ def main():
     # collect information
     version = _current_version()
     output_filename = os.path.join(RELEASES_ROOT, f'wallet-{version[1:]}.zip')
+    firefox_output_filename = os.path.join(RELEASES_ROOT, f'firefox-wallet-{version[1:]}.zip')
 
     # user feedback
     print(HEADING)
@@ -59,7 +60,8 @@ def main():
     cmd = ['zip', '-r', output_filename, 'prod/']
     subprocess.check_call(cmd, cwd=EXTENSION_ROOT)
 
-    cmd = ['zip', '-r', f"firefox-{output_filename}", '.']
+    # package up firefox extenson
+    cmd = ['zip', '-r', firefox_output_filename, '.']
     subprocess.check_call(cmd, cwd= os.path.join(EXTENSION_ROOT, 'prod'))
 
 if __name__ == '__main__':
