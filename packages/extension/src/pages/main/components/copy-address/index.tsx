af45dd2d269ece8d910a873b3a239798159c2008
HeesungB <hezo25@naver.com>
2023-04-05 21:38:18 +0900

    Add copy address component

---
+++ packages/extension/src/pages/main/components/copy-address/index.tsx
@@ -0,0 +1,48 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+import { ColorPalette } from "../../../../styles";
+import { Caption1 } from "../../../../components/typography";
+import { CopyIcon } from "../../../../components/icon";
+import { Columns } from "../../../../components/column";
+
+const Styles = {
+  Container: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.125rem;
+
+    width: 7.5rem;
+    padding: 0.5rem;
+
+    background-color: ${ColorPalette["gray-600"]};
+    border-radius: 16rem;
+
+    cursor: pointer;
+
+    :hover {
+      background-color: ${ColorPalette["gray-500"]};
+    }
+
+    svg {
+      color: ${ColorPalette["gray-300"]};
+    }
+
+    user-select: none;
+  `,
+};
+
+export const CopyAddress: FunctionComponent<{ onClick: () => void }> = ({
+  onClick,
+}) => {
+  return (
+    <Columns sum={1} alignY="center" columnAlign="center">
+      <Styles.Container onClick={onClick}>
+        <Caption1 style={{ color: ColorPalette["gray-300"] }}>
+          Copy Address
+        </Caption1>
+        <CopyIcon width="1rem" height="1rem" />
+      </Styles.Container>
+    </Columns>
+  );
+};
