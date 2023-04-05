e07efd6769e0486b137c19fe73d4319581a9ca5c
HeesungB <hezo25@naver.com>
2023-04-05 21:36:59 +0900

    Add general modal component

---
+++ packages/extension/src/components/modal/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { ModalProps } from "./types";
+
+export const Styles = {
+  Container: styled.div<Pick<ModalProps, "isOpen" | "yAlign">>`
+    display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+    flex-direction: row;
+    justify-content: center;
+    align-items: ${({ yAlign }) => {
+      switch (yAlign) {
+        case "top":
+          return "flex-start";
+        case "center":
+          return "center";
+        default:
+          return "flex-end";
+      }
+    }};
+    position: fixed;
+    inset: 0;
+
+    background-color: rgba(2, 2, 2, 0.56);
+    z-index: 100;
+  `,
+  Children: styled.div<Pick<ModalProps, "height">>`
+    display: flex;
+
+    width: 100%;
+    height: ${({ height }) => height || "auto"};
+
+    max-width: 360px;
+    max-height: 100vh;
+  `,
+};
---
+++ packages/extension/src/components/modal/types.ts
@@ -0,0 +1,5 @@
+export interface ModalProps {
+  isOpen: boolean;
+  yAlign?: "top" | "center" | "bottom";
+  height?: string;
+}
