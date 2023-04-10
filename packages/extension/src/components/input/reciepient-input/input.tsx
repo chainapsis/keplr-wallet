c81ff052f6f82d19a6cc7359b1c6cf8f87e97164
HeesungB <hezo25@naver.com>
2023-04-11 00:09:44 +0900

    Fix reciepient input layout

--- packages/extension/src/components/input/reciepient-input/input.tsx
+++ packages/extension/src/components/input/reciepient-input/input.tsx
@@ -2,26 +2,46 @@ import React, { FunctionComponent } from "react";
 import { TextInput } from "../text-input";
 import { observer } from "mobx-react-lite";
 import { IRecipientConfig } from "@keplr-wallet/hooks";
+import { ProfileIcon } from "../../icon";
+import { Box } from "../../box";
+import { ColorPalette } from "../../../styles";
+import { Modal } from "../../modal";
+import { AddressListModal } from "../../../pages/send/amount/address-list-modal";
 
 export const RecipientInput: FunctionComponent<{
   recipientConfig: IRecipientConfig;
 }> = observer(({ recipientConfig }) => {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
   return (
-    <TextInput
-      label="Recipient"
-      onChange={(e) => {
-        e.preventDefault();
-        recipientConfig.setValue(e.target.value);
-      }}
-      value={recipientConfig.value}
-      error={(() => {
-        const uiProperties = recipientConfig.uiProperties;
-
-        const err = uiProperties.error || uiProperties.warning;
-        if (err) {
-          return err.message || err.toString();
+    <Box>
+      <TextInput
+        label="Address"
+        onChange={(e) => {
+          e.preventDefault();
+          recipientConfig.setValue(e.target.value);
+        }}
+        right={
+          <Box
+            style={{ color: ColorPalette["gray-50"] }}
+            onClick={() => setIsModalOpen(true)}
+          >
+            <ProfileIcon width="1rem" height="1rem" />
+          </Box>
         }
-      })()}
-    />
+        value={recipientConfig.value}
+        error={(() => {
+          const uiProperties = recipientConfig.uiProperties;
+
+          const err = uiProperties.error || uiProperties.warning;
+          if (err) {
+            return err.message || err.toString();
+          }
+        })()}
+      />
+
+      <Modal isOpen={isModalOpen}>
+        <AddressListModal setIsOpen={setIsModalOpen} />
+      </Modal>
+    </Box>
   );
 });
