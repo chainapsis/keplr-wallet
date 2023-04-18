e6170edaaaa0902ea260404c97293663f91f17a5
Thunnini <yunjh1994@gmail.com>
2023-04-18 21:55:50 +0900

    Change gradient color

--- packages/extension/src/pages/main/components/chart/index.tsx
+++ packages/extension/src/pages/main/components/chart/index.tsx
@@ -116,8 +116,8 @@ export const DualChart: FunctionComponent<{
       >
         <defs>
           <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" stopColor="#05a" />
-            <stop offset="100%" stopColor="#0a5" />
+            <stop offset="0%" stopColor="#00C2FF" />
+            <stop offset="100%" stopColor="#DB00FF" />
           </linearGradient>
         </defs>
 
