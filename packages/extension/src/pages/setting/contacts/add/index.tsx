b281b49d4b6c35768038e3e70412b193452c70a4
HeesungB <hezo25@naver.com>
2023-04-19 12:03:08 +0800

    Add contact add logic

--- packages/extension/src/pages/setting/contacts/add/index.tsx
+++ packages/extension/src/pages/setting/contacts/add/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import styled from "styled-components";
 import { Stack } from "../../../../components/stack";
@@ -7,6 +7,12 @@ import { HeaderLayout } from "../../../../layouts/header";
 import { TextInput } from "../../../../components/input";
 import { Button } from "../../../../components/button";
 import { useSearchParams } from "react-router-dom";
+import { useStore } from "../../../../stores";
+import { useAddressBookConfig } from "@keplr-wallet/hooks";
+import { ExtensionKVStore } from "@keplr-wallet/common";
+import { useNavigate } from "react-router";
+import { ColorPalette } from "../../../../styles";
+import { useForm } from "react-hook-form";
 
 const Styles = {
   Container: styled(Stack)`
@@ -14,42 +20,113 @@ const Styles = {
   `,
   BottomButton: styled.div`
     padding: 0.75rem;
+
+    height: 4.75rem;
+
+    background-color: ${ColorPalette["gray-700"]};
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
   `,
 };
 
+interface FormData {
+  name: string;
+  address: string;
+  memo: string | null;
+}
+
 export const SettingContactsAdd: FunctionComponent = observer(() => {
+  const { chainStore } = useStore();
+  const navigate = useNavigate();
+
   const [searchParams] = useSearchParams();
+  const chainId = searchParams.get("chainId");
+  const index = searchParams.get("index");
   const isAdd =
-    searchParams.get("name") === null && searchParams.get("address") === null;
+    searchParams.get("name") === null &&
+    searchParams.get("address") === null &&
+    index === null;
+
+  if (chainId === null) {
+    navigate(-1);
+    return null;
+  }
+
+  const { handleSubmit, register, setFocus } = useForm<FormData>({
+    defaultValues: {
+      name: searchParams.get("name") ?? "",
+      address: searchParams.get("address") ?? "",
+      memo: searchParams.get("memo"),
+    },
+  });
+
+  console.log(searchParams);
+
+  useEffect(() => {
+    setFocus("name");
+  }, [setFocus]);
+
+  const addressBookConfig = useAddressBookConfig(
+    new ExtensionKVStore("address-book"),
+    chainStore,
+    chainId,
+    {
+      setRecipient: (): void => {
+        // noop
+      },
+      setMemo: (): void => {
+        // noop
+      },
+    }
+  );
 
   return (
     <HeaderLayout
       title={`${isAdd ? "Add" : "Edit"} Address`}
       left={<BackButton />}
-      bottom={
-        <Styles.BottomButton>
-          <Button text="Confirm" color="secondary" size="large" />
-        </Styles.BottomButton>
-      }
     >
-      <form>
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          if (isAdd) {
+            await addressBookConfig.addAddressBook({
+              name: data.name,
+              address: data.address,
+              memo: data.memo ?? "",
+            });
+          } else {
+            if (index) {
+              await addressBookConfig.editAddressBookAt(Number(index), {
+                name: data.name,
+                address: data.address,
+                memo: data.memo ?? "",
+              });
+            }
+          }
+          navigate(-1);
+        })}
+      >
         <Styles.Container gutter="1rem">
           <TextInput
             label="Name"
-            value={searchParams.get("name") ?? ""}
-            onChange={() => {}}
+            {...register("name", {
+              required: true,
+            })}
           />
           <TextInput
             label="Address"
-            value={searchParams.get("address") ?? ""}
-            onChange={() => {}}
+            {...register("address", {
+              required: true,
+            })}
           />
-          <TextInput
-            label="Memo(Option)"
-            value={searchParams.get("memo") ?? ""}
-            onChange={() => {}}
-          />
+          <TextInput label="Memo(Option)" {...register("memo")} />
         </Styles.Container>
+
+        <Styles.BottomButton>
+          <Button text="Confirm" color="secondary" size="large" />
+        </Styles.BottomButton>
       </form>
     </HeaderLayout>
   );
