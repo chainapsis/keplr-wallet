c4fb64dab7ff0072bffe99ffbbbccb83a2d17e8c
Thunnini <yunjh1994@gmail.com>
2023-04-24 14:01:31 +0900

    Disable send button if no balances

--- packages/extension/src/pages/main/components/buttons/index.tsx
+++ packages/extension/src/pages/main/components/buttons/index.tsx
@@ -1,12 +1,22 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useMemo } from "react";
 import { Column, Columns } from "../../../../components/column";
 import { Button } from "../../../../components/button";
 import { Box } from "../../../../components/box";
 import { Gutter } from "../../../../components/gutter";
 import { useNavigate } from "react-router";
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../../../stores";
+import { Dec } from "@keplr-wallet/unit";
 
-export const Buttons: FunctionComponent = () => {
+export const Buttons: FunctionComponent = observer(() => {
+  const { hugeQueriesStore } = useStore();
+
   const navigate = useNavigate();
+
+  const balances = hugeQueriesStore.getAllBalances(true);
+  const hasBalance = useMemo(() => {
+    return balances.find((bal) => bal.token.toDec().gt(new Dec(0))) != null;
+  }, [balances]);
 
   return (
     <Box>
@@ -16,9 +26,13 @@ export const Buttons: FunctionComponent = () => {
         </Column>
         <Gutter size="0.5rem" />
         <Column weight={1}>
-          <Button text="Send" onClick={() => navigate("/send/select-asset")} />
+          <Button
+            text="Send"
+            disabled={!hasBalance}
+            onClick={() => navigate("/send/select-asset")}
+          />
         </Column>
       </Columns>
     </Box>
   );
-};
+});
