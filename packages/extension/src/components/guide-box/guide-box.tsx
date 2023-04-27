358d20d87ac5984a4a024e056545a0a5b80e17ea
HeesungB <hezo25@naver.com>
2023-04-27 15:49:38 +0900

    Fix guide box component style

--- packages/extension/src/components/guide-box/guide-box.tsx
+++ packages/extension/src/components/guide-box/guide-box.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react";
 import { GuideBoxProps } from "./types";
 import { Styles } from "./styles";
-import { Columns } from "../column";
+import { Column, Columns } from "../column";
 import { InformationIcon } from "../icon";
 import { Box } from "../box";
 
@@ -15,7 +15,9 @@ export const GuideBox: FunctionComponent<GuideBoxProps> = ({
     <Styles.Container gutter="0.5rem" color={color}>
       <Columns sum={1} alignY="center" gutter="0.375rem">
         <InformationIcon width="1.25rem" height="1.25rem" />
-        <Styles.Title color={color}>{title}</Styles.Title>
+        <Column weight={1}>
+          <Styles.Title color={color}>{title}</Styles.Title>
+        </Column>
       </Columns>
       <Styles.Paragraph color={color}>{paragraph}</Styles.Paragraph>
       {bottom ? <Box>{bottom}</Box> : null}
