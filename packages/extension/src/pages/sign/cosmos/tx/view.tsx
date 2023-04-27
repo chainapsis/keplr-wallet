8bc47292d7e81481e1b71e3960e64616042b2dff
Thunnini <yunjh1994@gmail.com>
2023-04-27 20:42:42 +0900

    Split signing logic to view for each interaction data

--- packages/extension/src/pages/sign/cosmos/tx/view.tsx
+++ packages/extension/src/pages/sign/cosmos/tx/view.tsx
@@ -1,13 +1,16 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
+import { SignInteractionStore } from "@keplr-wallet/stores";
+import { Box } from "../../../../components/box";
+import { Column, Columns } from "../../../../components/column";
+import { XAxis } from "../../../../components/axis";
+import { H5 } from "../../../../components/typography";
+import { ColorPalette } from "../../../../styles";
+import { ViewDataButton } from "../../components/view-data-button";
+import { MessageItem } from "../../components/message-item";
+import { Stack } from "../../../../components/stack";
+import { MemoInput } from "../../../../components/input/memo-input";
+import { FeeControl } from "../../../../components/input/fee-control";
 import { observer } from "mobx-react-lite";
-import { Stack } from "../../../components/stack";
-import { BackButton } from "../../../layouts/header/components";
-import { HeaderLayout } from "../../../layouts/header";
-import { ColorPalette } from "../../../styles";
-import { Box } from "../../../components/box";
-import { useStore } from "../../../stores";
-import { useInteractionInfo } from "../../../hooks";
-import { MessageItem } from "../components/message-item";
 import {
   useFeeConfig,
   useMemoConfig,
@@ -17,32 +20,33 @@ import {
   useTxConfigsValidate,
   useZeroAllowedGasConfig,
 } from "@keplr-wallet/hooks";
+import { useStore } from "../../../../stores";
 import { unescapeHTML } from "@keplr-wallet/common";
-import { MemoInput } from "../../../components/input/memo-input";
-import { Column, Columns } from "../../../components/column";
-import { XAxis } from "../../../components/axis";
-import { H5 } from "../../../components/typography";
 import { CoinPretty, Int } from "@keplr-wallet/unit";
-import { FeeControl } from "../../../components/input/fee-control";
-import { ViewDataButton } from "../components/view-data-button";
+import { BackButton } from "../../../../layouts/header/components";
+import { HeaderLayout } from "../../../../layouts/header";
+import { useInteractionInfo } from "../../../../hooks";
 
-export const SignCosmosTxPage: FunctionComponent = observer(() => {
-  const { chainStore, signInteractionStore, queriesStore } = useStore();
+/**
+ * 서명을 처리할때 웹페이지에서 연속적으로 서명을 요청했을 수 있고
+ * 그러면 서명할 데이터에 대해서 FIFO 순으로 순차적으로 UI에서 표시하고 처리해준다.
+ * 하지만 문제는 tx관련된 hook들은 구현의 간단함을 위해서 한 컴포넌트 라이프사이클에서
+ * 하나의 tx에 대해서만 처리하고 이후 다른 tx가 필요하다고 다시 초기화하거나 할 수 없도록 되어있다.
+ * 이 문제 때문에 각 서명 데이터에 대해서 처리되고 나면 그 컴포넌트는 unmount되고
+ * 같은 컴포넌트가 새롭게 mount되어야 한다.
+ * 그렇기 때문에 처리 로직이 완전히 이 컴포넌트로 분리되어있고
+ * 이 컴포넌트를 호출하는 쪽에서 "key" prop을 통해서 위의 요구사항을 꼭 만족시켜야한다.
+ * 또한 prop으로 받는 "interactionData"는 절대로 불변해야한다.
+ */
+export const CosmosTxView: FunctionComponent<{
+  interactionData: NonNullable<SignInteractionStore["waitingData"]>;
+}> = observer(({ interactionData }) => {
+  const { chainStore, queriesStore, signInteractionStore } = useStore();
 
   const [isViewData, setIsViewData] = useState(false);
 
-  const chainId = (() => {
-    if (signInteractionStore.waitingData?.data) {
-      return signInteractionStore.waitingData.data.chainId;
-    }
-    return chainStore.chainInfos[0].chainId;
-  })();
-  const signer = (() => {
-    if (signInteractionStore.waitingData?.data) {
-      return signInteractionStore.waitingData.data.signer;
-    }
-    return "";
-  })();
+  const chainId = interactionData.data.chainId;
+  const signer = interactionData.data.signer;
 
   const senderConfig = useSenderConfig(chainStore, chainId, signer);
   // There are services that sometimes use invalid tx to sign arbitrary data on the sign page.
@@ -63,58 +67,56 @@ export const SignCosmosTxPage: FunctionComponent = observer(() => {
   amountConfig.setSignDocHelper(signDocHelper);
 
   useEffect(() => {
-    if (signInteractionStore.waitingData) {
-      const data = signInteractionStore.waitingData;
-      if (data.data.chainId !== data.data.signDocWrapper.chainId) {
-        // Validate the requested chain id and the chain id in the sign doc are same.
-        throw new Error("Chain id unmatched");
-      }
-      signDocHelper.setSignDocWrapper(data.data.signDocWrapper);
-      gasConfig.setValue(data.data.signDocWrapper.gas);
-      let memo = data.data.signDocWrapper.memo;
-      if (data.data.signDocWrapper.mode === "amino") {
-        // For amino-json sign doc, the memo is escaped by default behavior of golang's json marshaller.
-        // For normal users, show the escaped characters with unescaped form.
-        // Make sure that the actual sign doc's memo should be escaped.
-        // In this logic, memo should be escaped from account store or background's request signing function.
-        memo = unescapeHTML(memo);
-      }
-      memoConfig.setValue(memo);
-      if (
-        data.data.signOptions.preferNoSetFee &&
-        data.data.signDocWrapper.fees[0]
-      ) {
-        feeConfig.setFee(
-          data.data.signDocWrapper.fees.map((fee) => {
-            const currency = chainStore
-              .getChain(data.data.chainId)
-              .forceFindCurrency(fee.denom);
-            return new CoinPretty(currency, new Int(fee.amount));
-          })
-        );
-      }
-      amountConfig.setDisableBalanceCheck(
-        !!data.data.signOptions.disableBalanceCheck
-      );
-      feeConfig.setDisableBalanceCheck(
-        !!data.data.signOptions.disableBalanceCheck
-      );
-      // TODO
-      // if (
-      //   data.data.signDocWrapper.granter &&
-      //   data.data.signDocWrapper.granter !== data.data.signer
-      // ) {
-      //   feeConfig.setDisableBalanceCheck(true);
-      // }
+    const data = interactionData;
+    if (data.data.chainId !== data.data.signDocWrapper.chainId) {
+      // Validate the requested chain id and the chain id in the sign doc are same.
+      throw new Error("Chain id unmatched");
     }
+    signDocHelper.setSignDocWrapper(data.data.signDocWrapper);
+    gasConfig.setValue(data.data.signDocWrapper.gas);
+    let memo = data.data.signDocWrapper.memo;
+    if (data.data.signDocWrapper.mode === "amino") {
+      // For amino-json sign doc, the memo is escaped by default behavior of golang's json marshaller.
+      // For normal users, show the escaped characters with unescaped form.
+      // Make sure that the actual sign doc's memo should be escaped.
+      // In this logic, memo should be escaped from account store or background's request signing function.
+      memo = unescapeHTML(memo);
+    }
+    memoConfig.setValue(memo);
+    if (
+      data.data.signOptions.preferNoSetFee &&
+      data.data.signDocWrapper.fees[0]
+    ) {
+      feeConfig.setFee(
+        data.data.signDocWrapper.fees.map((fee) => {
+          const currency = chainStore
+            .getChain(data.data.chainId)
+            .forceFindCurrency(fee.denom);
+          return new CoinPretty(currency, new Int(fee.amount));
+        })
+      );
+    }
+    amountConfig.setDisableBalanceCheck(
+      !!data.data.signOptions.disableBalanceCheck
+    );
+    feeConfig.setDisableBalanceCheck(
+      !!data.data.signOptions.disableBalanceCheck
+    );
+    // TODO
+    // if (
+    //   data.data.signDocWrapper.granter &&
+    //   data.data.signDocWrapper.granter !== data.data.signer
+    // ) {
+    //   feeConfig.setDisableBalanceCheck(true);
+    // }
   }, [
     amountConfig,
     chainStore,
-    gasConfig,
-    memoConfig,
     feeConfig,
+    gasConfig,
+    interactionData,
+    memoConfig,
     signDocHelper,
-    signInteractionStore.waitingData,
   ]);
 
   const msgs = signDocHelper.signDocWrapper
@@ -123,10 +125,6 @@ export const SignCosmosTxPage: FunctionComponent = observer(() => {
       : signDocHelper.signDocWrapper.protoSignDoc.txMsgs
     : [];
 
-  const interactionInfo = useInteractionInfo(() => {
-    signInteractionStore.rejectAll();
-  });
-
   const txConfigsValidate = useTxConfigsValidate({
     senderConfig,
     gasConfig,
@@ -134,6 +132,8 @@ export const SignCosmosTxPage: FunctionComponent = observer(() => {
     feeConfig,
     memoConfig,
   });
+
+  const interactionInfo = useInteractionInfo();
 
   return (
     <HeaderLayout
@@ -145,16 +145,15 @@ export const SignCosmosTxPage: FunctionComponent = observer(() => {
         color: "primary",
         size: "large",
         disabled:
-          signInteractionStore.waitingData == null ||
-          txConfigsValidate.interactionBlocked,
+          txConfigsValidate.interactionBlocked || !signDocHelper.signDocWrapper,
         isLoading: signInteractionStore.isObsoleteInteraction(
-          signInteractionStore.waitingData?.id
+          interactionData.id
         ),
         onClick: async () => {
-          if (signInteractionStore.waitingData) {
+          if (signDocHelper.signDocWrapper) {
             await signInteractionStore.approveWithProceedNext(
-              signInteractionStore.waitingData.id,
-              signInteractionStore.waitingData.data.signDocWrapper,
+              interactionData.id,
+              signDocHelper.signDocWrapper,
               (proceedNext) => {
                 if (!proceedNext) {
                   if (
@@ -257,7 +256,7 @@ export const SignCosmosTxPage: FunctionComponent = observer(() => {
             senderConfig={senderConfig}
             gasConfig={gasConfig}
             disableAutomaticFeeSet={
-              signInteractionStore.waitingData?.data.signOptions.preferNoSetFee
+              interactionData.data.signOptions.preferNoSetFee
             }
           />
         </Stack>
