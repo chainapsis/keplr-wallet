6cd9a82367a86666bac42207395a2ed887e8e49f
HeesungB <hezo25@naver.com>
2023-04-28 17:07:31 +0900

    Add register typogarphy

---
+++ packages/extension/src/pages/register/components/typography/index.ts
@@ -0,0 +1,5 @@
+export * from "./base";
+export * from "./register-h1";
+export * from "./register-h2";
+export * from "./register-h3";
+export * from "./register-h4";
--- packages/extension/src/styles/global.ts
+++ packages/extension/src/styles/global.ts
@@ -9,6 +9,8 @@ import InterSemiBold from "../public/assets/font/Inter-SemiBold.ttf";
 import InterBold from "../public/assets/font/Inter-Bold.ttf";
 import InterExtraBold from "../public/assets/font/Inter-ExtraBold.ttf";
 import InterBlack from "../public/assets/font/Inter-Black.ttf";
+import HafferSemiBold from "../public/assets/font/HafferSQXH-SemiBold.ttf";
+import HafferMedium from "../public/assets/font/HafferSQXH-Medium.ttf";
 import { ColorPalette } from "./colors";
 
 export const GlobalStyle = createGlobalStyle`
@@ -66,6 +68,18 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Inter';
     font-weight: 900;
     src: url(${InterBlack}) format("truetype");
+  }
+
+  @font-face {
+    font-family: 'Haffer';
+    font-weight: 600;
+    src: url(${HafferSemiBold}) format("truetype");
+  }
+
+  @font-face {
+    font-family: 'Haffer';
+    font-weight: 500;
+    src: url(${HafferMedium}) format("truetype");
   }
   
   html, body {
