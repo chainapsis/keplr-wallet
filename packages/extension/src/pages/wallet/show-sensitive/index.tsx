30fff4c9a9da2f970b2b62132259dcae057c49c1
Thunnini <yunjh1994@gmail.com>
2023-05-03 21:34:51 +0900

    Improve `WalletShowSensitivePage`

--- packages/extension/src/pages/wallet/show-sensitive/index.tsx
+++ packages/extension/src/pages/wallet/show-sensitive/index.tsx
@@ -1,9 +1,7 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
-import styled from "styled-components";
-import { Stack } from "../../../components/stack";
 import { ColorPalette } from "../../../styles";
-import { Subtitle2, Subtitle3 } from "../../../components/typography";
+import { Subtitle3 } from "../../../components/typography";
 import { HeaderLayout } from "../../../layouts/header";
 import { BackButton } from "../../../layouts/header/components";
 import { TextInput } from "../../../components/input";
@@ -11,33 +9,7 @@ import { useForm } from "react-hook-form";
 import { useStore } from "../../../stores";
 import { useSearchParams } from "react-router-dom";
 import { Box } from "../../../components/box";
-import { YAxis } from "../../../components/axis";
 import { Gutter } from "../../../components/gutter";
-
-const Styles = {
-  Container: styled(Stack)`
-    height: 100%;
-
-    padding: 0.75rem;
-  `,
-  Flex1: styled.div`
-    flex: 1;
-  `,
-  Paragraph: styled(Subtitle3)`
-    text-align: center;
-    color: ${ColorPalette["gray-200"]};
-    padding: 0 0.5rem;
-  `,
-  RecoveryPhrase: styled(Subtitle2)`
-    color: ${ColorPalette["gray-50"]};
-    background-color: ${ColorPalette["gray-600"]};
-
-    padding: 1.25rem 1.875rem;
-    border-radius: 0.5rem;
-
-    text-align: center;
-  `,
-};
 
 interface FormData {
   password: string;
@@ -89,7 +61,15 @@ export const WalletShowSensitivePage: FunctionComponent = observer(() => {
               size: "large",
               type: "submit",
             }
-          : undefined
+          : {
+              color: "secondary",
+              text: "Close",
+              size: "large",
+              type: "button",
+              onClick: () => {
+                window.close();
+              },
+            }
       }
       onSubmit={
         sensitive === ""
@@ -113,36 +93,50 @@ export const WalletShowSensitivePage: FunctionComponent = observer(() => {
           : undefined
       }
     >
-      {sensitive === "" ? (
-        <Styles.Container>
-          <Gutter size="5.875rem" />
+      <Box
+        padding="0.75rem"
+        paddingTop="0.5rem"
+        paddingBottom="0"
+        height="100%"
+      >
+        {sensitive === "" ? (
+          <React.Fragment>
+            <Box alignX="center" alignY="center" style={{ flex: 1 }}>
+              <Box
+                width="8.5rem"
+                height="8.5rem"
+                backgroundColor={ColorPalette["gray-200"]}
+              />
 
-          <YAxis alignX="center">
-            <Box
-              width="8.5rem"
-              height="8.5rem"
-              backgroundColor={ColorPalette["gray-200"]}
+              <Gutter size="2rem" />
+
+              <Subtitle3 color={ColorPalette["gray-200"]}>
+                Please type in your password to proceed
+              </Subtitle3>
+            </Box>
+            <TextInput
+              label="Password"
+              type="password"
+              error={errors.password && errors.password.message}
+              {...register("password", { required: true })}
             />
-          </YAxis>
-
-          <Gutter size="2rem" />
-
-          <Styles.Paragraph>
-            Please type in your password to proceed
-          </Styles.Paragraph>
-
-          <Styles.Flex1 />
-
-          <TextInput
-            label="Password"
-            type="password"
-            error={errors.password && errors.password.message}
-            {...register("password", { required: true })}
-          />
-        </Styles.Container>
-      ) : (
-        <Styles.RecoveryPhrase>{sensitive}</Styles.RecoveryPhrase>
-      )}
+          </React.Fragment>
+        ) : (
+          <Box
+            paddingX="1.75rem"
+            paddingY="1.25rem"
+            backgroundColor={ColorPalette["gray-600"]}
+            borderRadius="0.5rem"
+            minHeight="10.25rem"
+            style={{
+              textAlign: "center",
+              lineBreak: sensitive.trim().includes(" ") ? "auto" : "anywhere",
+            }}
+          >
+            <Subtitle3 color={ColorPalette["gray-50"]}>{sensitive}</Subtitle3>
+          </Box>
+        )}
+      </Box>
     </HeaderLayout>
   );
 });
