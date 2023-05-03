28529362d3a492d6c7ff1a78ac359a6785e21b30
Thunnini <yunjh1994@gmail.com>
2023-05-03 22:26:34 +0900

    WIP

--- packages/extension/src/pages/main/components/internal-link-view/index.tsx
+++ packages/extension/src/pages/main/components/internal-link-view/index.tsx
@@ -1,8 +1,14 @@
 import styled from "styled-components";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useLayoutEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { ColorPalette } from "../../../../styles";
 import { Button2 } from "../../../../components/typography";
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../../../stores";
+import { ChainImageFallback } from "../../../../components/image";
+import { Bleed } from "../../../../components/bleed";
+import { Gutter } from "../../../../components/gutter";
+import { ChainInfo } from "@keplr-wallet/types";
 
 const Styles = {
   Container: styled.div`
@@ -16,7 +22,7 @@ const Styles = {
     border-radius: 0.375rem;
   `,
   TextButton: styled(Button2)`
-    width: 100%;
+    flex: 1;
 
     display: flex;
     align-items: center;
@@ -30,6 +36,8 @@ const Styles = {
     cursor: pointer;
     user-select: none;
 
+    white-space: nowrap;
+
     :hover {
       background-color: ${ColorPalette["gray-500"]};
     }
@@ -41,18 +49,102 @@ const Styles = {
   `,
 };
 
-export const InternalLinkView: FunctionComponent = () => {
+export const InternalLinkView: FunctionComponent = observer(() => {
+  const { chainStore } = useStore();
+
   const navigate = useNavigate();
+
+  const [imgs, setImgs] = useState<ChainInfo[]>([]);
+
+  useLayoutEffect(() => {
+    const chainInfos = chainStore.chainInfos
+      .filter((chainInfo) => chainInfo.embedded.embedded)
+      .filter(
+        (chainInfo) =>
+          chainInfo.chainSymbolImageUrl != null &&
+          chainInfo.chainSymbolImageUrl.length > 0
+      )
+      .map((chainInfo) => chainInfo.embedded);
+
+    setImgs(shuffle(chainInfos.slice()));
+
+    // Should be called once. This is intentional.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Styles.Container>
-      <Styles.TextButton onClick={() => navigate("/setting/chain/list")}>
-        Manage Chain List
+      <Styles.TextButton
+        onClick={(e) => {
+          e.preventDefault();
+
+          // TODO
+        }}
+      >
+        <Gutter size="0.5rem" />
+        {imgs.slice(0, 3).map((chainInfo, i) => {
+          return (
+            <Bleed key={chainInfo.chainId} left={i === 0 ? "0" : "0.5rem"}>
+              <ChainImageFallback
+                src={chainInfo.chainSymbolImageUrl}
+                alt={chainInfo.chainName}
+                style={{
+                  width: "1.25rem",
+                  height: "1.25rem",
+                }}
+              />
+            </Bleed>
+          );
+        })}
+        <Gutter size="0.15rem" />
+        Manage Chains
+        <Gutter size="0.15rem" />
+        <ManageChainListIcon size="1.125rem" />
+        <Gutter size="0.5rem" />
       </Styles.TextButton>
 
       <Styles.Divider />
 
       <Styles.TextButton>Go to Dashboard</Styles.TextButton>
     </Styles.Container>
+  );
+});
+
+function shuffle(array: ChainInfo[]) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+const ManageChainListIcon: FunctionComponent<{
+  size: string;
+}> = ({ size }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M18.75 12.75h1.5a.75.75 0 000-1.5h-1.5a.75.75 0 000 1.5zM12 6a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 6zM12 18a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 18zM3.75 6.75h1.5a.75.75 0 100-1.5h-1.5a.75.75 0 000 1.5zM5.25 18.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5zM3 12a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 013 12zM9 3.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zM12.75 12a2.25 2.25 0 114.5 0 2.25 2.25 0 01-4.5 0zM9 15.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
+      />
+    </svg>
   );
 };
