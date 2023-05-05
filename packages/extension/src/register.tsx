8f43de1b06cc420324469c5db9f969b721100881
Thunnini <yunjh1994@gmail.com>
2023-05-05 20:03:54 +0900

    Minro fix

--- packages/extension/src/register.tsx
+++ packages/extension/src/register.tsx
@@ -2,10 +2,10 @@
 require("setimmediate");
 // Shim ------------
 
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import { StoreProvider } from "./stores";
+import { StoreProvider, useStore } from "./stores";
 import { GlobalStyle } from "./styles";
 import { Keplr } from "@keplr-wallet/provider";
 import manifest from "./manifest.json";
@@ -16,6 +16,9 @@ import { ConfirmProvider } from "./hooks/confirm";
 import { RegisterPage } from "./pages/register";
 import { WelcomePage } from "./pages/register/pages/welcome";
 import { AppIntlProvider } from "./languages";
+import { BACKGROUND_PORT } from "@keplr-wallet/router";
+import { observer } from "mobx-react-lite";
+import { StartAutoLockMonitoringMsg } from "@keplr-wallet/background";
 
 configure({
   enforceActions: "always", // Make mobx to strict mode.
@@ -27,12 +30,39 @@ window.keplr = new Keplr(
   new InExtensionMessageRequester()
 );
 
+const AutoLockMonitor: FunctionComponent = observer(() => {
+  const { keyRingStore } = useStore();
+
+  useEffect(() => {
+    if (keyRingStore.status === "unlocked") {
+      const sendAutoLockMonitorMsg = async () => {
+        const msg = new StartAutoLockMonitoringMsg();
+        const requester = new InExtensionMessageRequester();
+        await requester.sendMessage(BACKGROUND_PORT, msg);
+      };
+
+      // Notify to auto lock service to start activation check whenever the keyring is unlocked.
+      sendAutoLockMonitorMsg();
+      const autoLockInterval = setInterval(() => {
+        sendAutoLockMonitorMsg();
+      }, 10000);
+
+      return () => {
+        clearInterval(autoLockInterval);
+      };
+    }
+  }, [keyRingStore.status]);
+
+  return null;
+});
+
 const App: FunctionComponent = () => {
   return (
     <StoreProvider>
       <ModalRootProvider>
         <ConfirmProvider>
           <GlobalStyle />
+          <AutoLockMonitor />
           <AppIntlProvider>
             <HashRouter>
               <Routes>
