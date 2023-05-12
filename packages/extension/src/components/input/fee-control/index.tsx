b6abaa595dba71013b53b4f9b6678eb7ff7e5513
Thunnini <yunjh1994@gmail.com>
2023-05-12 21:34:09 +0900

    Add warning/error UI state to fee control

--- packages/extension/src/components/input/fee-control/index.tsx
+++ packages/extension/src/components/input/fee-control/index.tsx
@@ -9,21 +9,34 @@ import {
 import styled from "styled-components";
 import { ColorPalette } from "../../../styles";
 import { Column, Columns } from "../../column";
-import { Subtitle3, Subtitle4 } from "../../typography";
-import { ArrowRightIcon, SettingIcon } from "../../icon";
+import { Caption1, Subtitle3, Subtitle4 } from "../../typography";
+import { ArrowRightIcon, LoadingIcon, SettingIcon } from "../../icon";
 import { Stack } from "../../stack";
 import { Modal } from "../../modal";
 import { TransactionFeeModal } from "./modal";
 import { useStore } from "../../../stores";
 import { autorun } from "mobx";
 import { PricePretty } from "@keplr-wallet/unit";
+import { Gutter } from "../../gutter";
+import Color from "color";
+import { Box } from "../../box";
+import { VerticalResizeTransition } from "../../transition";
 
 const Styles = {
-  Container: styled.div`
+  Container: styled.div<{
+    hasError: boolean;
+  }>`
     padding: 0.875rem 0.25rem 0.875rem 1rem;
     background-color: ${ColorPalette["gray-600"]};
 
-    border: 1.5px solid rgba(44, 75, 226, 0.5);
+    border: ${({ hasError }) =>
+      hasError
+        ? `1.5px solid ${Color(ColorPalette["yellow-400"])
+            .alpha(0.5)
+            .toString()}`
+        : `1.5px solid ${Color(ColorPalette["blue-400"])
+            .alpha(0.5)
+            .toString()}`};
     border-radius: 0.375rem;
 
     cursor: pointer;
@@ -138,78 +151,115 @@ export const FeeControl: FunctionComponent<{
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
-      <Styles.Container onClick={() => setIsModalOpen(true)}>
-        <Columns sum={1} alignY="center">
+      <React.Fragment>
+        <Styles.Container
+          hasError={
+            feeConfig.uiProperties.error != null ||
+            feeConfig.uiProperties.error != null
+          }
+          onClick={() => setIsModalOpen(true)}
+        >
           <Columns sum={1} alignY="center">
-            <Subtitle4>Transaction Fee</Subtitle4>
-            <SettingIcon width="1rem" height="1rem" />
-          </Columns>
+            <Columns sum={1} alignY="center">
+              <Subtitle4>Transaction Fee</Subtitle4>
+              <Gutter size="0.25rem" />
+              {feeConfig.uiProperties.loadingState ? (
+                <LoadingIcon width="1rem" height="1rem" />
+              ) : (
+                <SettingIcon width="1rem" height="1rem" />
+              )}
+            </Columns>
 
-          <Column weight={1} />
+            <Column weight={1} />
 
-          <Columns sum={1} gutter="0.25rem" alignY="center">
-            <Stack gutter="0.25rem" alignX="right">
-              <Subtitle3>
-                {feeConfig.fees
-                  .map((fee) => {
-                    return fee
-                      .maxDecimals(6)
-                      .inequalitySymbol(true)
-                      .trim(true)
-                      .shrink(true)
-                      .hideIBCMetadata(true)
-                      .toString();
-                  })
-                  .join(",")}
-              </Subtitle3>
-              <Subtitle3 style={{ color: ColorPalette["gray-300"] }}>
-                {(() => {
-                  let total: PricePretty | undefined;
-                  let hasUnknown = false;
-                  for (const fee of feeConfig.fees) {
-                    if (!fee.currency.coinGeckoId) {
-                      hasUnknown = true;
-                      break;
-                    } else {
-                      const price = priceStore.calculatePrice(fee);
-                      if (price) {
-                        if (!total) {
-                          total = price;
-                        } else {
-                          total = total.add(price);
+            <Columns sum={1} gutter="0.25rem" alignY="center">
+              <Stack gutter="0.25rem" alignX="right">
+                <Subtitle3>
+                  {feeConfig.fees
+                    .map((fee) => {
+                      return fee
+                        .maxDecimals(6)
+                        .inequalitySymbol(true)
+                        .trim(true)
+                        .shrink(true)
+                        .hideIBCMetadata(true)
+                        .toString();
+                    })
+                    .join(",")}
+                </Subtitle3>
+                <Subtitle3 style={{ color: ColorPalette["gray-300"] }}>
+                  {(() => {
+                    let total: PricePretty | undefined;
+                    let hasUnknown = false;
+                    for (const fee of feeConfig.fees) {
+                      if (!fee.currency.coinGeckoId) {
+                        hasUnknown = true;
+                        break;
+                      } else {
+                        const price = priceStore.calculatePrice(fee);
+                        if (price) {
+                          if (!total) {
+                            total = price;
+                          } else {
+                            total = total.add(price);
+                          }
                         }
                       }
                     }
-                  }
 
-                  if (hasUnknown || !total) {
-                    return "-";
-                  }
-                  return total.toString();
-                })()}
-              </Subtitle3>
-            </Stack>
+                    if (hasUnknown || !total) {
+                      return "-";
+                    }
+                    return total.toString();
+                  })()}
+                </Subtitle3>
+              </Stack>
 
-            <Styles.IconContainer>
-              <ArrowRightIcon />
-            </Styles.IconContainer>
+              <Styles.IconContainer>
+                <ArrowRightIcon />
+              </Styles.IconContainer>
+            </Columns>
           </Columns>
-        </Columns>
 
-        <Modal
-          isOpen={isModalOpen}
-          align="bottom"
-          close={() => setIsModalOpen(false)}
-        >
-          <TransactionFeeModal
+          <Modal
+            isOpen={isModalOpen}
+            align="bottom"
             close={() => setIsModalOpen(false)}
-            senderConfig={senderConfig}
-            feeConfig={feeConfig}
-            gasConfig={gasConfig}
-            gasSimulator={gasSimulator}
-          />
-        </Modal>
-      </Styles.Container>
+          >
+            <TransactionFeeModal
+              close={() => setIsModalOpen(false)}
+              senderConfig={senderConfig}
+              feeConfig={feeConfig}
+              gasConfig={gasConfig}
+              gasSimulator={gasSimulator}
+            />
+          </Modal>
+        </Styles.Container>
+
+        <VerticalResizeTransition transitionAlign="top">
+          {feeConfig.uiProperties.error || feeConfig.uiProperties.warning ? (
+            <Box marginTop="4px">
+              <Caption1 color={ColorPalette["yellow-400"]}>
+                {(() => {
+                  if (feeConfig.uiProperties.error) {
+                    return (
+                      feeConfig.uiProperties.error.message ||
+                      feeConfig.uiProperties.error.toString()
+                    );
+                  }
+
+                  if (feeConfig.uiProperties.warning) {
+                    return (
+                      feeConfig.uiProperties.warning.message ||
+                      feeConfig.uiProperties.warning.toString()
+                    );
+                  }
+                })()}
+              </Caption1>
+            </Box>
+          ) : null}
+        </VerticalResizeTransition>
+      </React.Fragment>
     );
   }
 );
