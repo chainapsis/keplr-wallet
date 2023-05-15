cf2ae6ad80765741fbd217181d718ae1439c2691
HeesungB <hezo25@naver.com>
2023-05-15 22:31:07 +0900

    Fix collapsible list

--- packages/extension/src/components/collapsible-list/collapsible-list.tsx
+++ packages/extension/src/components/collapsible-list/collapsible-list.tsx
@@ -2,12 +2,30 @@ import React, { FunctionComponent, useState } from "react";
 import { CollapsibleListProps } from "./types";
 import { Stack } from "../stack";
 import { Box } from "../box";
-import { Subtitle4 } from "../typography";
+import { Button2, Subtitle4 } from "../typography";
 import { ColorPalette } from "../../styles";
 import { ArrowDownIcon, ArrowUpIcon } from "../icon";
-import { Column, Columns } from "../column";
+import { Columns } from "../column";
 import { VerticalCollapseTransition } from "../transition/vertical-collapse";
 import { Gutter } from "../gutter";
+import { XAxis } from "../axis";
+import styled from "styled-components";
+
+const Styles = {
+  MoreViewContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    cursor: pointer;
+
+    color: ${ColorPalette["gray-100"]};
+
+    :hover {
+      color: ${ColorPalette["gray-200"]};
+    }
+  `,
+};
 
 export const CollapsibleList: FunctionComponent<CollapsibleListProps> = ({
   title,
@@ -18,7 +36,7 @@ export const CollapsibleList: FunctionComponent<CollapsibleListProps> = ({
     lenAlwaysShown = items.length;
   }
 
-  const [isExpanded, setIsExpanded] = useState(true);
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
   const alwaysShown = items.slice(0, lenAlwaysShown);
   const hidden = items.slice(lenAlwaysShown);
@@ -26,52 +44,57 @@ export const CollapsibleList: FunctionComponent<CollapsibleListProps> = ({
   return (
     <Stack>
       <Box
-        cursor={hidden.length > 0 ? "pointer" : undefined}
         marginBottom="0.5rem"
         onClick={(e) => {
           e.preventDefault();
-
-          if (hidden.length > 0) {
-            setIsExpanded(!isExpanded);
-          }
         }}
       >
         <Columns sum={1} alignY="center">
+          <Subtitle4 style={{ color: ColorPalette["gray-50"] }}>
+            {items.length}
+          </Subtitle4>
+
+          <Gutter size="0.25rem" />
+
           {title}
-
-          <Column weight={1} />
-
-          {hidden.length > 0 ? (
-            <React.Fragment>
-              <Subtitle4 style={{ color: ColorPalette["gray-300"] }}>
-                {items.length}
-              </Subtitle4>
-              <Box paddingLeft="0.25rem">
-                {isExpanded ? (
-                  <ArrowDownIcon
-                    width="1rem"
-                    height="1rem"
-                    color={ColorPalette["gray-300"]}
-                  />
-                ) : (
-                  <ArrowUpIcon
-                    width="1rem"
-                    height="1rem"
-                    color={ColorPalette["gray-300"]}
-                  />
-                )}
-              </Box>
-            </React.Fragment>
-          ) : null}
         </Columns>
       </Box>
 
       <Stack gutter="0.5rem">{alwaysShown}</Stack>
 
-      <VerticalCollapseTransition collapsed={isExpanded}>
+      <VerticalCollapseTransition collapsed={isCollapsed}>
         <Gutter size="0.5rem" />
         <Stack gutter="0.5rem">{hidden}</Stack>
       </VerticalCollapseTransition>
+
+      {hidden.length > alwaysShown.length ? (
+        <Styles.MoreViewContainer
+          onClick={(e) => {
+            e.preventDefault();
+
+            if (hidden.length > 0) {
+              setIsCollapsed(!isCollapsed);
+            }
+          }}
+        >
+          <Gutter size="0.75rem" />
+          <XAxis alignY="center">
+            <Button2>
+              {isCollapsed
+                ? `View ${hidden.length - alwaysShown.length} more tokens`
+                : "Collapse"}
+            </Button2>
+
+            <Gutter size="0.25rem" />
+
+            {isCollapsed ? (
+              <ArrowDownIcon width="1rem" height="1rem" />
+            ) : (
+              <ArrowUpIcon width="1rem" height="1rem" />
+            )}
+          </XAxis>
+        </Styles.MoreViewContainer>
+      ) : null}
     </Stack>
   );
 };
