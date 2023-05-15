e5b3883b977e9682683649d8e121cad108772e3c
HeesungB <hezo25@naver.com>
2023-05-15 19:10:27 +0900

    Fix menu bar sequence

--- packages/extension/src/pages/main/components/menu-bar/index.tsx
+++ packages/extension/src/pages/main/components/menu-bar/index.tsx
@@ -1,14 +1,15 @@
 import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import { ColorPalette } from "../../../../styles";
-import { CloseIcon } from "../../../../components/icon";
+import { CloseIcon, LinkIcon } from "../../../../components/icon";
 import { Box } from "../../../../components/box";
 import { Stack } from "../../../../components/stack";
 import { useNavigate } from "react-router";
 import { Gutter } from "../../../../components/gutter";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../../../stores";
-import { H3 } from "../../../../components/typography";
+import { Button2, H3 } from "../../../../components/typography";
+import { XAxis } from "../../../../components/axis";
 
 const Styles = {
   MenuItem: styled(H3)`
@@ -46,23 +47,8 @@ export const MenuBar: FunctionComponent<{
         <CloseIcon />
       </Box>
       <Gutter size="1.25rem" />
-      <Stack gutter="1.75rem">
-        <Styles.MenuItem
-          onClick={() => {
-            navigate("/setting");
-          }}
-        >
-          Settings
-        </Styles.MenuItem>
 
-        <Styles.MenuItem onClick={() => navigate("/setting/contacts/list")}>
-          Contacts
-        </Styles.MenuItem>
-
-        <Styles.MenuItem onClick={() => navigate("/setting/token/add")}>
-          Add Token
-        </Styles.MenuItem>
-
+      <Stack>
         <Styles.MenuItem
           onClick={(e) => {
             e.preventDefault();
@@ -80,6 +66,35 @@ export const MenuBar: FunctionComponent<{
         >
           Manage Chain List
         </Styles.MenuItem>
+
+        <Gutter size="1rem" />
+
+        <Box
+          width="6.5rem"
+          style={{ border: `1px solid ${ColorPalette["gray-400"]}` }}
+        />
+
+        <Gutter size="1rem" />
+
+        <Styles.MenuItem onClick={() => navigate("/setting/contacts/list")}>
+          Contacts
+        </Styles.MenuItem>
+
+        <Gutter size="1.75rem" />
+
+        <Styles.MenuItem onClick={() => navigate("/setting/token/add")}>
+          Add Token
+        </Styles.MenuItem>
+
+        <Gutter size="1.75rem" />
+
+        <Styles.MenuItem
+          onClick={() => {
+            navigate("/setting");
+          }}
+        >
+          Settings
+        </Styles.MenuItem>
       </Stack>
 
       <Styles.Flex1 />
@@ -93,6 +108,33 @@ export const MenuBar: FunctionComponent<{
       >
         Lock Wallet
       </Styles.MenuItem>
+
+      <Gutter size="0.75rem" />
+
+      <Box
+        cursor="pointer"
+        onClick={(e) => {
+          e.preventDefault();
+
+          browser.tabs.create({
+            url: "https://chains.keplr.app/",
+          });
+        }}
+      >
+        <XAxis alignY="center">
+          <Button2 color={ColorPalette["gray-300"]}>
+            Go to Keplr Chain Registry
+          </Button2>
+
+          <Gutter size="0.25rem" />
+
+          <LinkIcon
+            width="1.125rem"
+            height="1.125rem"
+            color={ColorPalette["gray-300"]}
+          />
+        </XAxis>
+      </Box>
     </Box>
   );
 });
