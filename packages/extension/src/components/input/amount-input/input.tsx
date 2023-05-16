24e15200fe379f1ba419e2ea2b210a81d6a42aaf
Thunnini <yunjh1994@gmail.com>
2023-05-16 19:03:22 +0900

    Add hover state to price button of amount input

--- packages/extension/src/components/input/amount-input/input.tsx
+++ packages/extension/src/components/input/amount-input/input.tsx
@@ -242,6 +242,10 @@ const BottomPriceButton: FunctionComponent<{
   return (
     <Box marginTop="0.375rem" marginLeft="0.375rem" alignX="left">
       <Box
+        color={ColorPalette["gray-300"]}
+        hover={{
+          color: ColorPalette["gray-200"],
+        }}
         onClick={(e) => {
           e.preventDefault();
 
@@ -258,7 +262,7 @@ const BottomPriceButton: FunctionComponent<{
             viewBox="0 0 20 20"
           >
             <path
-              fill={ColorPalette["gray-300"]}
+              fill="currentColor"
               fillRule="evenodd"
               d="M13.2 2.24a.75.75 0 00.04 1.06l2.1 1.95H6.75a.75.75 0 000 1.5h8.59l-2.1 1.95a.75.75 0 101.02 1.1l3.5-3.25a.75.75 0 000-1.1l-3.5-3.25a.75.75 0 00-1.06.04zm-6.4 8a.75.75 0 00-1.06-.04l-3.5 3.25a.75.75 0 000 1.1l3.5 3.25a.75.75 0 101.02-1.1l-2.1-1.95h8.59a.75.75 0 000-1.5H4.66l2.1-1.95a.75.75 0 00.04-1.06z"
               clipRule="evenodd"
@@ -266,7 +270,6 @@ const BottomPriceButton: FunctionComponent<{
           </svg>
           {/* 여기서 Gutter가 안먹힌다. 왜인지는 모른다. 그냥 marginLeft 썻음 */}
           <Body3
-            color={ColorPalette["gray-300"]}
             style={{
               marginLeft: "0.3rem",
             }}
