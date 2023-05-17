11eeb55c0ea9daeb2ce52e88fd59de1642adb4d5
blacktoast <dbstjd100@naver.com>
2023-05-17 12:33:15 +0900

    Build: version up RN

--- packages/mobile/ios/mobile/AppDelegate.m
+++ packages/mobile/ios/mobile/AppDelegate.m
@@ -65,7 +65,8 @@ static void InitializeFlipper(UIApplication *application) {
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+//  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
   return [CodePush bundleURL];
 #endif
