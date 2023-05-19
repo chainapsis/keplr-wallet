db5b0a35cfebe221f9e3b3a9512290f542d1e316
HeesungB <hezo25@naver.com>
2023-05-19 19:07:45 +0900

    Add viewingkey click logic

--- packages/extension/src/pages/main/components/token/index.tsx
+++ packages/extension/src/pages/main/components/token/index.tsx
@@ -31,6 +31,9 @@ import { XAxis } from "../../../../components/axis";
 import { Gutter } from "../../../../components/gutter";
 import Color from "color";
 import { Skeleton } from "../../../../components/skeleton";
+import { WrongViewingKeyError } from "@keplr-wallet/stores";
+import { AppCurrency } from "@keplr-wallet/types";
+import { useNavigate } from "react-router";
 
 const Styles = {
   Container: styled.div<{
@@ -102,7 +105,8 @@ interface TokenItemProps {
 
 export const TokenItem: FunctionComponent<TokenItemProps> = observer(
   ({ viewToken, onClick, disabled, forChange, isNotReady, altSentence }) => {
-    const { priceStore } = useStore();
+    const { accountStore, tokensStore, priceStore } = useStore();
+    const navigate = useNavigate();
 
     const pricePretty = priceStore.calculatePrice(viewToken.token);
 
@@ -143,13 +147,56 @@ export const TokenItem: FunctionComponent<TokenItemProps> = observer(
       }
     }, [viewToken.token.currency]);
 
+    const createViewingKey = (): Promise<string | undefined> => {
+      if (
+        "type" in viewToken.token.currency &&
+        viewToken.token.currency.type === "secret20"
+      ) {
+        const contractAddress = viewToken.token.currency.contractAddress;
+        return new Promise((resolve) => {
+          accountStore
+            .getAccount(viewToken.chainInfo.chainId)
+            .secret.createSecret20ViewingKey(
+              contractAddress,
+              "",
+              {},
+              {},
+              (_, viewingKey) => {
+                resolve(viewingKey);
+              }
+            );
+        });
+      } else {
+        return new Promise((resolve) => resolve(undefined));
+      }
+    };
+
     return (
       <Styles.Container
         forChange={forChange}
         isError={viewToken.error != null}
         disabled={disabled}
-        onClick={(e) => {
+        onClick={async (e) => {
           e.preventDefault();
+
+          if (
+            viewToken.error?.data &&
+            viewToken.error.data instanceof WrongViewingKeyError
+          ) {
+            const viewingKey = await createViewingKey();
+            if (viewingKey) {
+              await tokensStore.addToken(viewToken.chainInfo.chainId, {
+                ...viewToken.token.currency,
+                viewingKey,
+              } as AppCurrency);
+            }
+
+            navigate("/", {
+              replace: true,
+            });
+
+            return;
+          }
 
           if (onClick) {
             onClick();
@@ -182,7 +229,7 @@ export const TokenItem: FunctionComponent<TokenItemProps> = observer(
 
               {tag ? (
                 <React.Fragment>
-                  <Gutter size="0.5rem" />
+                  <Gutter size="0.25rem" />
                   <Box alignY="center" height="1px">
                     <Tag text={tag.text} tooltip={tag.tooltip} />
                   </Box>
@@ -249,17 +296,41 @@ export const TokenItem: FunctionComponent<TokenItemProps> = observer(
                 isNotReady={isNotReady}
                 dummyMinWidth="4.5rem"
               >
-                <Subtitle3 style={{ color: ColorPalette["gray-300"] }}>
-                  {(() => {
-                    if (altSentence) {
-                      return altSentence;
-                    }
+                {viewToken.error ? (
+                  <Box position="relative" alignX="right">
+                    <Subtitle3
+                      color={ColorPalette["gray-100"]}
+                      style={{
+                        textDecoration: "underline",
+                        position: "absolute",
+                        whiteSpace: "nowrap",
+                      }}
+                    >
+                      Set your viewing key
+                    </Subtitle3>
+                    <Subtitle3
+                      style={{
+                        textDecoration: "underline",
+                        whiteSpace: "nowrap",
+                        opacity: 0,
+                      }}
+                    >
+                      &nbps;
+                    </Subtitle3>
+                  </Box>
+                ) : (
+                  <Subtitle3 color={ColorPalette["gray-300"]}>
+                    {(() => {
+                      if (altSentence) {
+                        return altSentence;
+                      }
 
-                    return pricePretty
-                      ? pricePretty.inequalitySymbol(true).toString()
-                      : "-";
-                  })()}
-                </Subtitle3>
+                      return pricePretty
+                        ? pricePretty.inequalitySymbol(true).toString()
+                        : "-";
+                    })()}
+                  </Subtitle3>
+                )}
               </Skeleton>
             </Stack>
 
