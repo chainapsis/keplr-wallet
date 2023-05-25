4a03b2376edb70693a9522400ec90044010b567a
Thunnini <yunjh1994@gmail.com>
2023-05-25 18:54:01 +0900

    Some improvements for styling of menu bar

--- packages/extension/src/pages/main/components/menu-bar/index.tsx
+++ packages/extension/src/pages/main/components/menu-bar/index.tsx
@@ -10,6 +10,7 @@ import { observer } from "mobx-react-lite";
 import { useStore } from "../../../../stores";
 import { Button2, H3 } from "../../../../components/typography";
 import { XAxis } from "../../../../components/axis";
+import { Bleed } from "../../../../components/bleed";
 
 const Styles = {
   MenuItem: styled(H3)`
@@ -30,41 +31,87 @@ export const MenuBar: FunctionComponent<{
   const navigate = useNavigate();
 
   return (
-    <Box
-      height="100%"
-      minWidth="15rem"
-      maxWidth="20rem"
-      backgroundColor={ColorPalette["gray-600"]}
-      paddingTop="1.125rem"
-      paddingLeft="1.25rem"
-      paddingBottom="2rem"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <Box onClick={close} cursor="pointer">
-        <CloseIcon />
-      </Box>
-      <Gutter size="1.25rem" />
+    <Box height="100%" alignX="left">
+      <Box
+        height="100%"
+        backgroundColor={ColorPalette["gray-600"]}
+        paddingTop="1.125rem"
+        paddingX="1.75rem"
+        paddingBottom="1.25rem"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Bleed horizontal="0.15rem">
+          {/*
+            좀 이상한 구조지만 clickable area를 조절하고
+            아이콘이 약간 오른쪽으로 치우져보이는 느낌을 없애기 위해서
+            어쩔 수 없이 약간 복잡해짐
+           */}
+          <Box alignX="left">
+            <Box onClick={close} cursor="pointer">
+              <CloseIcon width="1.5rem" height="1.5rem" />
+            </Box>
+          </Box>
+        </Bleed>
+        <Gutter size="1.25rem" />
 
-      <Stack>
+        <Stack gutter="1.5rem">
+          <Styles.MenuItem
+            onClick={(e) => {
+              e.preventDefault();
+
+              if (keyRingStore.selectedKeyInfo) {
+                browser.tabs
+                  .create({
+                    url: `/register.html#?route=enable-chains&vaultId=${keyRingStore.selectedKeyInfo.id}&skipWelcome=true`,
+                  })
+                  .then(() => {
+                    window.close();
+                  });
+              }
+            }}
+          >
+            Manage Chain Visibility
+          </Styles.MenuItem>
+
+          <Gutter size="1rem" />
+
+          <Box
+            width="6.5rem"
+            style={{ border: `1px solid ${ColorPalette["gray-400"]}` }}
+          />
+
+          <Gutter size="1rem" />
+
+          <Styles.MenuItem onClick={() => navigate("/setting/contacts/list")}>
+            My Contacts
+          </Styles.MenuItem>
+
+          <Styles.MenuItem onClick={() => navigate("/setting/token/list")}>
+            Add Token
+          </Styles.MenuItem>
+
+          <Styles.MenuItem
+            onClick={() => {
+              navigate("/setting");
+            }}
+          >
+            Settings
+          </Styles.MenuItem>
+        </Stack>
+
+        <Styles.Flex1 />
+
         <Styles.MenuItem
           onClick={(e) => {
             e.preventDefault();
 
-            if (keyRingStore.selectedKeyInfo) {
-              browser.tabs
-                .create({
-                  url: `/register.html#?route=enable-chains&vaultId=${keyRingStore.selectedKeyInfo.id}&skipWelcome=true`,
-                })
-                .then(() => {
-                  window.close();
-                });
-            }
+            keyRingStore.lock();
           }}
         >
-          Manage Chain Visibility
+          Lock Wallet
         </Styles.MenuItem>
 
         <Gutter size="1rem" />
@@ -76,64 +123,30 @@ export const MenuBar: FunctionComponent<{
 
         <Gutter size="1rem" />
 
-        <Styles.MenuItem onClick={() => navigate("/setting/contacts/list")}>
-          My Contacts
-        </Styles.MenuItem>
+        <Box
+          cursor="pointer"
+          onClick={(e) => {
+            e.preventDefault();
 
-        <Gutter size="1.75rem" />
-
-        <Styles.MenuItem onClick={() => navigate("/setting/token/list")}>
-          Add Token
-        </Styles.MenuItem>
-
-        <Gutter size="1.75rem" />
-
-        <Styles.MenuItem
-          onClick={() => {
-            navigate("/setting");
+            browser.tabs.create({
+              url: "https://chains.keplr.app/",
+            });
           }}
         >
-          Settings
-        </Styles.MenuItem>
-      </Stack>
+          <XAxis alignY="center">
+            <Button2 color={ColorPalette["gray-300"]}>
+              Go to Keplr Chain Registry
+            </Button2>
 
-      <Styles.Flex1 />
+            <Gutter size="0.25rem" />
 
-      <Styles.MenuItem
-        onClick={(e) => {
-          e.preventDefault();
-
-          keyRingStore.lock();
-        }}
-      >
-        Lock Wallet
-      </Styles.MenuItem>
-
-      <Gutter size="0.75rem" />
-
-      <Box
-        cursor="pointer"
-        onClick={(e) => {
-          e.preventDefault();
-
-          browser.tabs.create({
-            url: "https://chains.keplr.app/",
-          });
-        }}
-      >
-        <XAxis alignY="center">
-          <Button2 color={ColorPalette["gray-300"]}>
-            Go to Keplr Chain Registry
-          </Button2>
-
-          <Gutter size="0.25rem" />
-
-          <LinkIcon
-            width="1.125rem"
-            height="1.125rem"
-            color={ColorPalette["gray-300"]}
-          />
-        </XAxis>
+            <LinkIcon
+              width="1.125rem"
+              height="1.125rem"
+              color={ColorPalette["gray-300"]}
+            />
+          </XAxis>
+        </Box>
       </Box>
     </Box>
   );
