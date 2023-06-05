76d695b9791ff091804661281e2e1dcf7fd35d1c
Thunnini <yunjh1994@gmail.com>
2023-06-05 16:48:40 +0900

    Restore `signEthereum` API in provider

--- packages/provider/src/core.ts
+++ packages/provider/src/core.ts
@@ -290,12 +290,23 @@ export class Keplr implements IKeplr, KeplrCoreTypes {
   }
 
   async signEthereum(
-    _chainId: string,
-    _signer: string,
-    _data: string | Uint8Array,
-    _type: EthSignType
+    chainId: string,
+    signer: string,
+    message: Uint8Array,
+    signType: EthSignType
   ): Promise<Uint8Array> {
-    throw new Error("TODO");
+    return await sendSimpleMessage(
+      this.requester,
+      BACKGROUND_PORT,
+      "keyring-ethereum",
+      "request-sign-ethereum",
+      {
+        chainId,
+        signer,
+        message,
+        signType,
+      }
+    );
   }
 
   async signICNSAdr36(
