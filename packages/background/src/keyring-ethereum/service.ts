f108ab1302efc4b4e0b77ee259896f3b3efb7418
Thunnini <yunjh1994@gmail.com>
2023-06-05 17:24:49 +0900

    Minor fix

--- packages/background/src/keyring-ethereum/service.ts
+++ packages/background/src/keyring-ethereum/service.ts
@@ -146,14 +146,14 @@ export class KeyRingEthereumService {
                 ]),
                 "keccak256"
               );
-              // return Buffer.concat([
-              //   Buffer.from(signature.r.replace("0x", ""), "hex"),
-              //   Buffer.from(signature.s.replace("0x", ""), "hex"),
-              //   // The metamask doesn't seem to consider the chain id in this case... (maybe bug on metamask?)
-              //   signature.recoveryParam
-              //     ? Buffer.from("1c", "hex")
-              //     : Buffer.from("1b", "hex"),
-              // ]);
+              return Buffer.concat([
+                Buffer.from(signature.r.replace("0x", ""), "hex"),
+                Buffer.from(signature.s.replace("0x", ""), "hex"),
+                // The metamask doesn't seem to consider the chain id in this case... (maybe bug on metamask?)
+                signature.recoveryParam
+                  ? Buffer.from("1c", "hex")
+                  : Buffer.from("1b", "hex"),
+              ]);
               return signature;
             }
             default:
