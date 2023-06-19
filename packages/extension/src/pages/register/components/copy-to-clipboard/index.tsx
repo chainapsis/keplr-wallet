22c7ce633683a5bcf89e6f325f9e1b6b2bf4b312
HeesungB <hezo25@naver.com>
2023-06-19 21:10:29 +0900

    Revert "Remove stroke on copy to clipboard svg"
    This reverts commit c7228075a91885ca6d90df160ae3366e6a77efd0.

--- packages/extension/src/pages/register/components/copy-to-clipboard/index.tsx
+++ packages/extension/src/pages/register/components/copy-to-clipboard/index.tsx
@@ -5,21 +5,6 @@ import { TextButton } from "../../../../components/button-text";
 import React, { FunctionComponent, useEffect, useRef, useState } from "react";
 import lottie from "lottie-web";
 import AnimCheck from "../../../../public/assets/lottie/register/check_circle-icon.json";
-import styled from "styled-components";
-
-const SVGNoneTextButton = styled(TextButton)`
-  svg {
-    fill: none;
-    stroke: none;
-  }
-
-  :hover {
-    svg {
-      fill: none;
-      stroke: none;
-    }
-  }
-`;
 
 export const CopyToClipboard: FunctionComponent<{ text: string }> = ({
   text,
@@ -45,7 +30,7 @@ export const CopyToClipboard: FunctionComponent<{ text: string }> = ({
   }, [hasCopied]);
 
   return (
-    <SVGNoneTextButton
+    <TextButton
       text={
         hasCopied ? (
           <Columns sum={1} gutter="0.25rem">
