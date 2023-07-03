df98d5cd606132bfe48768e8219c09bb39a4e1bc
HeesungB <hezo25@naver.com>
2023-07-03 14:48:42 +0900

    Add light theme globally

--- packages/extension/src/styles/global.ts
+++ packages/extension/src/styles/global.ts
@@ -10,20 +10,27 @@ export const GlobalStyle = createGlobalStyle`
   
   html {
     // TODO: Change the scheme according to theme after theme feature is implemented.
-    color-scheme: dark;
+    color-scheme: ${(props) =>
+      props.theme.mode === "light" ? "light" : "dark"};
   }
   
   html, body {
     font-family: 'Inter', sans-serif;
-    color: ${ColorPalette.white};
-    background-color: ${ColorPalette["gray-700"]};
-
+    
     &[data-lang="ko"] {
       font-family: 'NotoSansKR', sans-serif;
       
       word-break: keep-all;
       word-wrap: break-word;
     }
+    color: ${(props) =>
+      props.theme.mode === "light"
+        ? ColorPalette["gray-700"]
+        : ColorPalette.white};
+    background: ${(props) =>
+      props.theme.mode === "light"
+        ? ColorPalette["light-gradient"]
+        : ColorPalette["gray-700"]};
   }
   
   pre {
