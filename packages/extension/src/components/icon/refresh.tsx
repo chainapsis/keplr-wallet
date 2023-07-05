8b2409efee3e09c941aefaf3ce4e0b83666aeef6
Spustnik <1psimonova1@gmail.com>
2023-07-05 15:04:42 +0000

    Snip-20 Balance Update

---
+++ packages/extension/src/components/icon/refresh.tsx
@@ -0,0 +1,34 @@
+import React, { FunctionComponent } from "react";
+import { IconProps } from "./types";
+
+export const RefreshIcon: FunctionComponent<
+  IconProps & { animate: boolean }
+> = ({ width = "1rem", height = "1rem", color, animate = false }) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke={color || "currentColor"}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+      >
+        {animate && (
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            dur="1s"
+            repeatCount="indefinite"
+            values="0 12 12; 360 12 12"
+          />
+        )}
+      </path>
+    </svg>
+  );
+};
