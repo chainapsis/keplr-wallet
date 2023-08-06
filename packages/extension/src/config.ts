f14e8fb62e902cf7db62f7a2efba90bd33f36a76
delivan <wjdgurdl272@gmail.com>
2023-08-06 15:17:36 +0900

    Add Optimism, Base chain info in config

--- packages/extension/src/config.ts
+++ packages/extension/src/config.ts
@@ -12,18 +12,20 @@ export const EmbedChainInfos: ChainInfo[] = [
         coinMinimalDenom: "wei",
         coinDecimals: 18,
         coinImageUrl:
-          "https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/6ed5f/eth-diamond-black.webp",
+          "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
         coinGeckoId: "ethereum",
       },
     },
-    rpc: "",
-    rest: "",
-    chainId: "",
-    chainName: "Dummy",
+    rpc: "https://mainnet.optimism.io",
+    rest: "https://mainnet.optimism.io",
+    chainId: "60",
+    chainName: "Optimism",
     stakeCurrency: {
-      coinDenom: "DUMMY",
-      coinMinimalDenom: "dummy",
-      coinDecimals: 0,
+      coinDenom: "ETH",
+      coinMinimalDenom: "wei",
+      coinDecimals: 18,
+      coinImageUrl: "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
+      coinGeckoId: "ethereum",
     },
     bip44: {
       coinType: 60,
@@ -31,16 +33,74 @@ export const EmbedChainInfos: ChainInfo[] = [
     bech32Config: Bech32Address.defaultBech32Config("dummy"),
     currencies: [
       {
-        coinDenom: "DUMMY",
-        coinMinimalDenom: "dummy",
-        coinDecimals: 0,
+        coinDenom: "ETH",
+        coinMinimalDenom: "wei",
+        coinDecimals: 18,
+        coinImageUrl:
+          "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
+        coinGeckoId: "ethereum",
       },
     ],
     feeCurrencies: [
       {
-        coinDenom: "DUMMY",
-        coinMinimalDenom: "dummy",
-        coinDecimals: 0,
+        coinDenom: "ETH",
+        coinMinimalDenom: "wei",
+        coinDecimals: 18,
+        coinImageUrl:
+          "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
+        coinGeckoId: "ethereum",
+      },
+    ],
+    features: ["eth-address-gen", "eth-key-sign"],
+  },
+  {
+    evm: {
+      chainId: "8453",
+      chainName: "Base",
+      jsonRpcProvider: "https://mainnet.base.org",
+      nativeCurrency: {
+        coinDenom: "ETH",
+        coinMinimalDenom: "wei",
+        coinDecimals: 18,
+        coinImageUrl:
+          "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
+        coinGeckoId: "ethereum",
+      },
+    },
+    rpc: "https://mainnet.base.org",
+    rest: "https://mainnet.base.org",
+    chainId: "8453",
+    chainName: "Base",
+    stakeCurrency: {
+      coinDenom: "ETH",
+      coinMinimalDenom: "wei",
+      coinDecimals: 18,
+      coinImageUrl:
+        "https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/6ed5f/eth-diamond-black.webp",
+      coinGeckoId: "ethereum",
+    },
+    bip44: {
+      coinType: 60,
+    },
+    bech32Config: Bech32Address.defaultBech32Config("dummy"),
+    currencies: [
+      {
+        coinDenom: "ETH",
+        coinMinimalDenom: "wei",
+        coinDecimals: 18,
+        coinImageUrl:
+          "https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/6ed5f/eth-diamond-black.webp",
+        coinGeckoId: "ethereum",
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "ETH",
+        coinMinimalDenom: "wei",
+        coinDecimals: 18,
+        coinImageUrl:
+          "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
+        coinGeckoId: "ethereum",
       },
     ],
     features: ["eth-address-gen", "eth-key-sign"],
