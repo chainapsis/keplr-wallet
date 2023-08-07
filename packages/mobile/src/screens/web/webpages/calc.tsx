211c6446ca564da0a2c5896e1914395c68f32491
Thunnini <yunjh1994@gmail.com>
2023-08-07 16:52:09 +0900

    Minor fix

--- packages/mobile/src/screens/web/webpages/calc.tsx
+++ packages/mobile/src/screens/web/webpages/calc.tsx
@@ -5,7 +5,10 @@ export const CalcFiWebpageScreen: FunctionComponent = () => {
   return (
     <WebpageScreen
       name="Calc Finance"
-      source={{ uri: "https://app.calculated.fi/?chain=Osmosis" }}
+      source={{
+        uri:
+          "https://app.calculated.fi/create-strategy/dca-in/assets/?chain=Osmosis&utm_source=kplr",
+      }}
       originWhitelist={["https://app.calculated.fi"]}
     />
   );
