4ed6c5786d9bf0b7f80c9953c0b1141fd2382c90
Thunnini <yunjh1994@gmail.com>
2023-08-10 00:38:29 +0900

    hoist 처리 개선

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -37,6 +37,23 @@ const packages = (() => {
   return res;
 })();
 
+// react와 같은 library는 여러 버전이 동시에 존재하면 오류가 난다.
+// 근데 mobile package는 nohoist 옵션에 의해서 react와 같은 library를
+// 본인의 node_modules에 가지고 있다.
+// 하지만 다른 패키지들의 경우에는 react가 hoist되어서 root의 node_modules에 존재한다.
+// 잠재적으로 다른 버전의 react들이 존재할 수 있기 때문에
+// mobile에는 실제로는 본인의 node_modules의 library들이 아니라 root의 node_modules의 library들을 사용하게 한다.
+// XXX: 각 package들이 버전 관리를 잘못해서 각 package들 밑에 node_modules가 생기면 이 경우는 처리하지 못한다.
+//      근데 이 경우면 이미 extension에서도 문제가 생겼을 것이기 때문에 이러한 버전 관리는 알아서 잘 하도록 하자...
+const shouldNohoistLibs = [
+  'react-native',
+  'react',
+  'mobx',
+  'mobx-utils',
+  'mobx-react-lite',
+  'buffer',
+];
+
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
@@ -57,30 +74,9 @@ const config = {
   resolver: {
     blockList: exclusionList(
       (() => {
-        // react와 같은 library는 여러 버전이 동시에 존재하면 오류가 난다.
-        // 근데 mobile package는 nohoist 옵션에 의해서 react와 같은 library를
-        // 본인의 node_modules에 가지고 있다.
-        // 하지만 다른 패키지들의 경우에는 react가 hoist되어서 root의 node_modules에 존재한다.
-        // 잠재적으로 다른 버전의 react들이 존재할 수 있기 때문에
-        // mobile에는 실제로는 본인의 node_modules의 library들이 아니라 root의 node_modules의 library들을 사용하게 한다.
-        // XXX: 각 package들이 버전 관리를 잘못해서 각 package들 밑에 node_modules가 생기면 이 경우는 처리하지 못한다.
-        //      근데 이 경우면 이미 extension에서도 문제가 생겼을 것이기 때문에 이러한 버전 관리는 알아서 잘 하도록 하자...
-        //      또한 일단 react-native package는 이 package에서만 존재한다고 가정한다...
-        const shouldNohoistLibs = [
-          'react',
-          'mobx',
-          'mobx-utils',
-          'mobx-react-lite',
-          'buffer',
-        ];
-
         const res = [];
         for (const lib of shouldNohoistLibs) {
-          res.push(
-            new RegExp(
-              `^${rootPath}\\/packages\\/mobile\\/node_modules\\/${lib}\\/.*$`,
-            ),
-          );
+          res.push(new RegExp(`^${rootPath}\\/node_modules\\/${lib}\\/.*$`));
         }
 
         return res;
@@ -91,13 +87,20 @@ const config = {
         __dirname,
         './node_modules/expo-standard-web-crypto',
       ),
-      buffer: path.resolve(__dirname, '../../node_modules/buffer'),
+      buffer: path.resolve(__dirname, './node_modules/buffer'),
       stream: path.resolve(__dirname, './node_modules/stream-browserify'),
       string_decoder: path.resolve(__dirname, './node_modules/string_decoder'),
       path: path.resolve(__dirname, './node_modules/path-browserify'),
       http: path.resolve(__dirname, './node_modules/http-browserify'),
       https: path.resolve(__dirname, './node_modules/https-browserify'),
       os: path.resolve(__dirname, './node_modules/os-browserify'),
+      ...(() => {
+        const res = {};
+        for (const lib of shouldNohoistLibs) {
+          res[lib] = `${rootPath}/packages/mobile/node_modules/${lib}`;
+        }
+        return res;
+      })(),
     },
   },
 };
