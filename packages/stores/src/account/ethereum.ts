013059a7d3e2740260c10ce758d0774906b7fd3b
delivan <wjdgurdl272@gmail.com>
2023-08-11 23:55:10 +0900

    Add `makeSendTokenTx`, `sendEthereumTx` methods to `EthereumAccountImpl`

--- packages/stores/src/account/ethereum.ts
+++ packages/stores/src/account/ethereum.ts
@@ -1,6 +1,10 @@
 import { EthereumQueries, IQueriesStore } from "../query";
 import { AccountSetBase, AccountSetBaseSuper } from "./base";
 import { ChainGetter } from "../chain";
+import { AppCurrency, EthSignType } from "@keplr-wallet/types";
+import { DenomHelper } from "@keplr-wallet/common";
+import { parseEther } from "@ethersproject/units";
+import { UnsignedTransaction, serialize } from "@ethersproject/transactions";
 
 export interface EthereumAccount {
   ethereum: EthereumAccountImpl;
@@ -34,4 +38,57 @@ export class EthereumAccountImpl {
     protected readonly chainId: string,
     protected readonly queriesStore: IQueriesStore<EthereumQueries>
   ) {}
+
+  async makeSendTokenTx(
+    amount: string,
+    currency: AppCurrency,
+    recipient: string
+  ): Promise<UnsignedTransaction | undefined> {
+    const denomHelper = new DenomHelper(currency.coinMinimalDenom);
+    if (denomHelper.type === "native") {
+      const ethereumNonceQuery = this.queriesStore
+        .get(this.chainId)
+        .ethereum.queryEthereumNonce.getQueryEthereumNonce(
+          this.base.ethereumHexAddress
+        );
+      await ethereumNonceQuery.waitResponse();
+      const value = parseEther(amount).toNumber();
+      const to = recipient;
+      const gasLimit = 21000;
+      const nonce = ethereumNonceQuery.nonce;
+
+      const tx: UnsignedTransaction = {
+        value,
+        gasLimit,
+        gasPrice: 4100000000,
+        nonce,
+        to,
+        chainId: Number(this.chainId),
+      };
+
+      return tx;
+    }
+
+    return;
+  }
+
+  async sendEthereumTx(unsignedTx: UnsignedTransaction) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const keplr = (await this.base.getKeplr())!;
+    const signEthereum = keplr.signEthereum.bind(keplr);
+    const signature = await signEthereum(
+      this.chainId,
+      this.base.ethereumHexAddress,
+      JSON.stringify(unsignedTx),
+      EthSignType.TRANSACTION
+    );
+
+    const rawTransaction = serialize(unsignedTx, signature);
+
+    const sendTx = keplr.sendEthereumTx.bind(keplr);
+
+    const result = await sendTx(this.chainId, rawTransaction);
+
+    return result;
+  }
 }
