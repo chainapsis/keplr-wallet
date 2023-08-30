a414246aa60ad1c472fbf688a7ce957346a24ae2
Thunnini <yunjh1994@gmail.com>
2023-08-30 14:51:43 +0900

    Minor fixes

--- packages/mobile/src/navigation.tsx
+++ packages/mobile/src/navigation.tsx
@@ -13,29 +13,36 @@ import {RegisterScreen} from './screen/register';
 import {HomeScreen} from './screen/home';
 import {LockedScreen} from './screen/locked';
 
+const Stack = createNativeStackNavigator();
+
 //TODO 이후 상태가 not-loaded일때 스플레시 스크린화면 처리 필요
 export const AppNavigation: FunctionComponent = observer(() => {
   const {keyRingStore} = useStore();
-  const Stack = createNativeStackNavigator();
   const style = useStyle();
+
+  if (keyRingStore.status === 'not-loaded') {
+    return null;
+  }
 
   return (
     <NavigationContainer
       theme={style.theme === 'light' ? DefaultTheme : DarkTheme}>
       <Stack.Navigator
-        initialRouteName={
-          keyRingStore.status === 'locked'
-            ? 'Locked'
-            : keyRingStore.status === 'unlocked'
-            ? 'Home'
-            : keyRingStore.status === 'empty'
-            ? 'Register'
-            : 'Splash'
-        }>
+        initialRouteName={(() => {
+          switch (keyRingStore.status) {
+            case 'locked':
+              return 'Locked';
+            case 'unlocked':
+              return 'Home';
+            case 'empty':
+              return 'Register';
+            default:
+              throw new Error('Unknown status');
+          }
+        })()}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Locked" component={LockedScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Splash" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
