9963fa96ecf1a3fb7c011cb3d2edb6039b3d1959
blacktoast <dbstjd100@naver.com>
2023-09-01 11:23:31 +0900

    [config]  fix  experimental syntax 'decorators'

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+  ],
 };
