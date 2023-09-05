4ab2fcfd5f508bea531877b353770eb1bd1b49ca
blacktoast <dbstjd100@naver.com>
2023-09-05 13:42:14 +0900

    [feature] register stack navigation
    - add enable chain page

--- packages/mobile/src/navigation.tsx
+++ packages/mobile/src/navigation.tsx
@@ -12,14 +12,36 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RegisterScreen} from './screen/register';
 import {HomeScreen} from './screen/home';
 import {LockedScreen} from './screen/locked';
+import {RegisterEnableChainScreen} from './screen/register/enable-chain';
 
 export type RootStackParamList = {
   Home: undefined;
   Register: undefined;
+  'Register.Intro': undefined;
+  'Register.EnableChain': undefined;
   Locked: undefined;
 };
 
+export type RegisterStackParamList = {
+  Intro: undefined;
+  EnableChain: undefined;
+};
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export const RegisterNavigation: FunctionComponent = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Register.Intro"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Register.Intro" component={RegisterScreen} />
+      <Stack.Screen
+        name="Register.EnableChain"
+        component={RegisterEnableChainScreen}
+      />
+    </Stack.Navigator>
+  );
+};
 
 //TODO 이후 상태가 not-loaded일때 스플레시 스크린화면 처리 필요
 export const AppNavigation: FunctionComponent = observer(() => {
@@ -47,7 +69,7 @@ export const AppNavigation: FunctionComponent = observer(() => {
         })()}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Locked" component={LockedScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Register" component={RegisterNavigation} />
       </Stack.Navigator>
     </NavigationContainer>
   );
