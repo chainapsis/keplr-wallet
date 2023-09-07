deb28bcd0aa1961e6be179ec9c3fcc20845024bf
Thunnini <yunjh1994@gmail.com>
2023-09-07 12:30:02 +0900

    Minor ui improvements on the setting pages

--- packages/extension/src/pages/setting/index.tsx
+++ packages/extension/src/pages/setting/index.tsx
@@ -10,16 +10,34 @@ import {
 } from "../../components/icon";
 import { useNavigate } from "react-router";
 import { Box } from "../../components/box";
-import { useIntl } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 import { MainHeaderLayout } from "../main/layouts/header";
+import { H3 } from "../../components/typography";
+import { useTheme } from "styled-components";
+import { ColorPalette } from "../../styles";
+import { Gutter } from "../../components/gutter";
 
 export const SettingPage: FunctionComponent = observer(() => {
   const navigate = useNavigate();
   const intl = useIntl();
 
+  const theme = useTheme();
+
   return (
     <MainHeaderLayout>
       <Box padding="0.75rem" paddingTop="0">
+        <Box paddingY="1.15rem" alignX="center" alignY="center">
+          <H3
+            color={
+              theme.mode === "light"
+                ? ColorPalette["gray-700"]
+                : ColorPalette.white
+            }
+          >
+            <FormattedMessage id="page.setting.title" />
+          </H3>
+        </Box>
+        <Gutter size="0.5rem" />
         <Stack gutter="0.5rem">
           <PageButton
             title={intl.formatMessage({ id: "page.setting.general-title" })}
