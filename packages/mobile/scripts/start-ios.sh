366226e75de84a5caa8f831a847f7ced8cdd4c34
samsiegart <samsiegart@gmail.com>
2023-09-08 20:58:32 -0700

    fix ios for arm64

--- packages/mobile/scripts/start-ios.sh
+++ packages/mobile/scripts/start-ios.sh
@@ -3,7 +3,13 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-cd "$DIR/../ios"
+cd "$DIR/.."
+
+# Fix from https://github.com/facebook/react-native/issues/36758
+sed -i.bo 's/    node->getLayout()\.hadOverflow() |/    node->getLayout()\.hadOverflow() ||/' ./node_modules/react-native/ReactCommon/yoga/yoga/Yoga.cpp
+sed -i.bo 's/    node->getLayout()\.hadOverflow() |||/    node->getLayout()\.hadOverflow() ||/' ./node_modules/react-native/ReactCommon/yoga/yoga/Yoga.cpp
+
+cd "ios"
 
 yarn pod-install
 yarn build:provider
