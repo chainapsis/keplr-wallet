09ecc24cfd22c802ba7806e590e295a35956d54c
Thunnini <yunjh1994@gmail.com>
2023-09-09 19:16:15 +0900

    Minor change

--- packages/mobile/src/screens/web/components/webpage-screen/index.tsx
+++ packages/mobile/src/screens/web/components/webpage-screen/index.tsx
@@ -175,7 +175,7 @@ export const WebpageScreen: FunctionComponent<
     () =>
       new SuggestChainReceiverKeplr(
         chainStore,
-        "0.10.10",
+        "0.12.20",
         "core",
         new RNMessageRequesterExternal(() => {
           if (!webviewRef.current) {
