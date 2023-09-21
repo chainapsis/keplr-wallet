1ea79a77f77e438cdc70180548cdb3ae20cb471b
blacktoast <dbstjd100@naver.com>
2023-09-21 19:13:58 +0900

    [feat] edit navigation

--- packages/mobile/src/navigation.tsx
+++ packages/mobile/src/navigation.tsx
@@ -10,20 +10,14 @@ import {
 } from '@react-navigation/native';
 import {useStyle} from './styles';
 import React from 'react';
-import {
-  NativeStackHeaderProps,
-  createNativeStackNavigator,
-} from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RegisterScreen} from './screen/register';
 import {HomeScreen} from './screen/home';
 import {LockedScreen} from './screen/locked';
 import {RegisterEnableChainScreen} from './screen/register/enable-chain';
 import {createDrawerNavigator, useDrawerStatus} from '@react-navigation/drawer';
 import {DrawerContent} from './components/drawer';
-import {Pressable, StyleSheet, Text, View} from 'react-native';
-import {QRScanIcon} from './components/icon';
-import {MenuIcon} from './components/icon/menu';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {
   FocusedScreenProvider,
@@ -32,8 +26,11 @@ import {
 import {SettingIcon} from './components/icon/setting';
 import {BrowserIcon} from './components/icon/browser';
 import {WalletIcon} from './components/icon/wallet';
-import {getHeaderTitle} from '@react-navigation/elements';
-import {Box} from './components/box';
+
+import {
+  HomeScreenHeader,
+  defaultHeaderOptions,
+} from './components/pageHeader/header';
 
 export type RootStackParamList = {
   Home: undefined;
@@ -41,139 +38,13 @@ export type RootStackParamList = {
   'Register.Intro': undefined;
   'Register.EnableChain': undefined;
   'Setting.Intro': undefined;
+  'Setting.Second': undefined;
   Locked: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
-
-const HomeScreenHeaderLeft: FunctionComponent = () => {
-  const style = useStyle();
-
-  const navigation = useNavigation();
-
-  return (
-    <Pressable
-      onPress={() => {
-        navigation.dispatch(DrawerActions.toggleDrawer());
-      }}>
-      <MenuIcon size={28} color={style.flatten(['color-gray-10']).color} />
-    </Pressable>
-  );
-};
-
-const HomeScreenHeaderRight: FunctionComponent = () => {
-  const style = useStyle();
-
-  // const navigation = useNavigation();
-
-  return (
-    <Pressable
-      onPress={() => {
-        //TODO 이후 qr촬영 페이지로 넘겨야 함
-        // navigation.navigate('');
-      }}>
-      <QRScanIcon size={28} color={style.flatten(['color-gray-10']).color} />
-    </Pressable>
-  );
-};
-
-const HomeScreenHeader = observer(() => {
-  const {keyRingStore} = useStore();
-  const style = useStyle();
-  const insect = useSafeAreaInsets();
-
-  return (
-    <Box
-      alignY="center"
-      style={StyleSheet.flatten([
-        style.flatten([
-          'padding-bottom-18',
-          'padding-x-20',
-          'background-color-gray-700',
-          'border-width-bottom-1',
-          'border-color-gray-600',
-        ]),
-        {
-          height: insect.top + 64,
-          paddingTop: insect.top,
-        },
-      ])}>
-      <View
-        style={StyleSheet.flatten([
-          style.flatten(['flex-row', 'justify-between', 'width-full']),
-        ])}>
-        <HomeScreenHeaderLeft />
-        <Box>
-          <Text
-            numberOfLines={1}
-            ellipsizeMode="tail"
-            style={StyleSheet.flatten([
-              style.flatten([
-                'color-white',
-                'h4',
-                'width-160',
-                'overflow-scroll',
-              ]),
-            ])}>
-            {keyRingStore.selectedKeyInfo?.name || 'Keplr Account'}
-          </Text>
-        </Box>
-        <HomeScreenHeaderRight />
-      </View>
-    </Box>
-  );
-});
-
-// const DefaultScreenHeaderTitle: FunctionComponent<PropsWithChildren> = ({
-//   children,
-// }) => {
-//   const style = useStyle();
-//   return (
-//     <View style={style.flatten(['padding-bottom-20', 'text-center'])}>
-//       <Text style={style.flatten(['h4', 'color-white', 'text-center'])}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
-
-const DefaultScreenHeader: FunctionComponent<NativeStackHeaderProps> = ({
-  route,
-  options,
-}) => {
-  const title = getHeaderTitle(options, route.name);
-  const style = useStyle();
-  const insect = useSafeAreaInsets();
-  return (
-    <View
-      style={StyleSheet.flatten([
-        style.flatten([
-          'text-center',
-          'flex-column',
-          'justify-center',
-          'padding-bottom-18',
-          'background-color-background-default',
-          'border-width-bottom-1',
-          'border-color-gray-600',
-        ]),
-        {
-          height: insect.top + 64,
-          paddingTop: insect.top,
-        },
-        options.headerStyle,
-      ])}>
-      <Text
-        style={StyleSheet.flatten([
-          style.flatten(['h4', 'color-white', 'text-center']),
-          options.headerStyle,
-        ])}>
-        {title}
-      </Text>
-    </View>
-  );
-};
 
 export const RegisterNavigation: FunctionComponent = () => {
   return (
@@ -193,7 +64,6 @@ export const MainTabNavigationWithDrawer: FunctionComponent = () => {
   const style = useStyle();
 
   const focused = useFocusedScreen();
-
   return (
     <Drawer.Navigator
       screenOptions={{
@@ -205,7 +75,7 @@ export const MainTabNavigationWithDrawer: FunctionComponent = () => {
         swipeEnabled: focused.name === 'Home',
         headerShown: false,
       }}
-      drawerContent={DrawerContent}>
+      drawerContent={() => <DrawerContent />}>
       <Drawer.Screen name="MainTab" component={MainTabNavigation} />
     </Drawer.Navigator>
   );
@@ -272,17 +142,22 @@ export const MainTabNavigation: FunctionComponent = () => {
 };
 
 const SettingNavigation = () => {
-  const style = useStyle();
   return (
     <Stack.Navigator initialRouteName="Setting.Intro">
       <Stack.Screen
         name="Setting.Intro"
         options={{
           title: 'Setting',
-          header: DefaultScreenHeader,
-          headerStyle: {...style.get('h3')} as any,
+          ...defaultHeaderOptions,
         }}
         component={LockedScreen}
+      />
+      <Stack.Screen
+        name="Setting.Second"
+        options={{
+          ...defaultHeaderOptions,
+        }}
+        component={RegisterScreen}
       />
     </Stack.Navigator>
   );
@@ -320,7 +195,7 @@ export const AppNavigation: FunctionComponent = observer(() => {
           />
           <Stack.Screen
             options={{
-              header: DefaultScreenHeader,
+              ...defaultHeaderOptions,
             }}
             name="Locked"
             component={LockedScreen}
@@ -328,7 +203,7 @@ export const AppNavigation: FunctionComponent = observer(() => {
           <Stack.Screen
             name="Register"
             options={{
-              header: DefaultScreenHeader,
+              ...defaultHeaderOptions,
             }}
             component={RegisterNavigation}
           />
