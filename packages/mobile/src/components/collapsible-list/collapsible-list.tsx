4664bc7344a2afd16d916afae82767b82d11be29
blacktoast <dbstjd100@naver.com>
2023-09-25 18:33:36 +0900

    [feat] add collapse-list, edit check box

---
+++ packages/mobile/src/components/collapsible-list/collapsible-list.tsx
@@ -0,0 +1,80 @@
+import React, {FunctionComponent, useState} from 'react';
+import {CollapsibleListProps} from './types';
+import {Stack} from '../stack';
+import {Box} from '../box';
+import {useStyle} from '../../styles';
+import {Columns} from '../column';
+import {Gutter} from '../gutter';
+import {Text, View} from 'react-native';
+import {ArrowDownIcon} from '../icon/arrow-down';
+import {ArrowUpIcon} from '../icon/arrow-up';
+import {TextButton} from '../text-button';
+
+export const CollapsibleList: FunctionComponent<CollapsibleListProps> = ({
+  title,
+  items,
+  lenAlwaysShown,
+}) => {
+  if (!lenAlwaysShown || lenAlwaysShown < 0) {
+    lenAlwaysShown = items.length;
+  }
+
+  const style = useStyle();
+
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const alwaysShown = items.slice(0, lenAlwaysShown);
+  const hidden = items.slice(lenAlwaysShown);
+
+  return (
+    <Stack>
+      <Box
+        marginBottom={8}
+        paddingX={6}
+        onClick={e => {
+          e.preventDefault();
+        }}>
+        <Columns sum={1} alignY="center">
+          <Text style={style.flatten(['color-gray-50'])}>{items.length}</Text>
+          <Gutter size={4} />
+          {title}
+        </Columns>
+      </Box>
+
+      <Stack gutter={8}>{alwaysShown}</Stack>
+
+      {!isCollapsed ? (
+        <View>
+          <Gutter size={8} />
+          <Stack gutter={8}>{hidden}</Stack>
+        </View>
+      ) : null}
+      {hidden.length > 0 ? (
+        <Box>
+          <Gutter size={12} />
+          <TextButton
+            text={
+              isCollapsed ? `View ${hidden.length} more tokens` : 'Collapse'
+            }
+            rightIcon={
+              isCollapsed ? (
+                <ArrowDownIcon
+                  size={16}
+                  color={style.get('color-gray-300').color}
+                />
+              ) : (
+                <ArrowUpIcon
+                  size={16}
+                  color={style.get('color-gray-300').color}
+                />
+              )
+            }
+            onPress={() => {
+              setIsCollapsed(!isCollapsed);
+            }}
+          />
+        </Box>
+      ) : null}
+    </Stack>
+  );
+};
