65e928c10f6e2531814c84e66c48254ef7c99c4b
heesung bae <heesungbae@heesungui-MacBookPro-2.local>
2023-10-11 14:02:24 +0900

    Fix select asset page style to flat list

--- packages/mobile/src/screen/send/select-asset/index.tsx
+++ packages/mobile/src/screen/send/select-asset/index.tsx
@@ -3,15 +3,14 @@ import React, {FunctionComponent, useMemo, useState} from 'react';
 import {useStyle} from '../../../styles';
 import {useStore} from '../../../stores';
 import {Dec} from '@keplr-wallet/unit';
-import {PageWithScrollView} from '../../../components/page';
 import {TokenItem} from '../../../components/token-view';
-import {Stack} from '../../../components/stack';
 import {SearchIcon} from '../../../components/icon';
 import {TextInput} from '../../../components/input';
 import {Gutter} from '../../../components/gutter';
 import {Column, Columns} from '../../../components/column';
-import {Pressable, Text} from 'react-native';
+import {FlatList, Pressable, Text} from 'react-native';
 import {Checkbox} from '../../../components/checkbox';
+import {Box} from '../../../components/box';
 
 export const SendScreen: FunctionComponent = observer(() => {
   const style = useStyle();
@@ -50,55 +49,53 @@ export const SendScreen: FunctionComponent = observer(() => {
   }, [search, tokens]);
 
   return (
-    <PageWithScrollView
-      backgroundMode={'default'}
-      style={style.flatten(['margin-12'])}>
-      <Gutter size={10} />
+    <Box padding={12}>
+      <FlatList
+        ListHeaderComponent={
+          <React.Fragment>
+            <TextInput
+              left={color => <SearchIcon size={20} color={color} />}
+              value={search}
+              placeholder="Search by a chain or asset name"
+              onChange={e => {
+                e.preventDefault();
 
-      <TextInput
-        left={color => <SearchIcon size={20} color={color} />}
-        placeholder="Search by a chain or asset name"
-        onChange={e => {
-          e.preventDefault();
-
-          setSearch(e.nativeEvent.text);
-        }}
-      />
-
-      <Gutter size={10} />
-
-      <Pressable onPress={() => setHideIBCToken(!hideIBCToken)}>
-        <Columns sum={1} gutter={4}>
-          <Column weight={1} />
-
-          <Text
-            style={style.flatten([
-              hideIBCToken ? 'color-gray-200' : 'color-gray-300',
-              'body2',
-            ])}>
-            Hide IBC token
-          </Text>
-
-          <Checkbox
-            size="small"
-            checked={hideIBCToken}
-            onPress={() => setHideIBCToken(!hideIBCToken)}
-          />
-        </Columns>
-      </Pressable>
-
-      <Gutter size={12} />
-
-      <Stack gutter={8}>
-        {filteredTokens.map(viewToken => {
-          return (
-            <TokenItem
-              key={`${viewToken.chainInfo.chainId}-${viewToken.token.currency.coinMinimalDenom}`}
-              viewToken={viewToken}
+                setSearch(e.nativeEvent.text);
+              }}
             />
-          );
-        })}
-      </Stack>
-    </PageWithScrollView>
+
+            <Gutter size={10} />
+
+            <Pressable onPress={() => setHideIBCToken(!hideIBCToken)}>
+              <Columns sum={1} gutter={4}>
+                <Column weight={1} />
+
+                <Text
+                  style={style.flatten([
+                    hideIBCToken ? 'color-gray-200' : 'color-gray-300',
+                    'body2',
+                  ])}>
+                  Hide IBC token
+                </Text>
+
+                <Checkbox
+                  size="small"
+                  checked={hideIBCToken}
+                  onPress={() => setHideIBCToken(!hideIBCToken)}
+                />
+              </Columns>
+            </Pressable>
+
+            <Gutter size={12} />
+          </React.Fragment>
+        }
+        data={filteredTokens}
+        renderItem={({item}) => <TokenItem viewToken={item} />}
+        keyExtractor={item =>
+          `${item.chainInfo.chainId}-${item.token.currency.coinMinimalDenom}`
+        }
+        ItemSeparatorComponent={() => <Gutter size={8} />}
+      />
+    </Box>
   );
 });
