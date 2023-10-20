892c9837d0acfbd0acfd227af425b1927acf5244
blacktoast <dbstjd100@naver.com>
2023-10-20 19:25:59 +0900

    [feat] add pageButton and setting default page

--- packages/mobile/src/screen/setting/index.tsx
+++ packages/mobile/src/screen/setting/index.tsx
@@ -1,20 +1,102 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {FunctionComponent} from 'react';
-import {Button, Text, View} from 'react-native';
 import {StackNavProp} from '../../navigation';
 import {PageWithScrollView} from '../../components/page';
+import {PageButton} from './components/page-button';
+import {ArrowRightIcon} from '../../components/icon/arrow-right';
+import {useStyle} from '../../styles';
+import {useIntl} from 'react-intl';
+import {Box} from '../../components/box';
+import {Gutter} from '../../components/gutter';
+import {Stack} from '../../components/stack';
+import {RocketLaunchIcon, SettingIcon} from '../../components/icon';
+import {KeyIcon} from '../../components/icon/key';
 
 export const SettingScreen: FunctionComponent = () => {
-  const nav = useNavigation<StackNavProp>();
+  const navigate = useNavigation<StackNavProp>();
+  const style = useStyle();
+  const intl = useIntl();
   return (
     <PageWithScrollView backgroundMode={'default'}>
-      <View>
-        <Text>setting</Text>
-        <Button
-          title="General"
-          onPress={() => nav.navigate('Setting.General')}
-        />
-      </View>
+      <Box padding={12} paddingTop={0}>
+        <Gutter size={8} />
+        <Stack gutter={8}>
+          <PageButton
+            title={intl.formatMessage({id: 'page.setting.general-title'})}
+            paragraph={intl.formatMessage({
+              id: 'page.setting.general-paragraph',
+            })}
+            titleIcon={
+              <SettingIcon
+                size={16}
+                color={style.get('color-text-high').color}
+              />
+            }
+            endIcon={
+              <ArrowRightIcon
+                size={24}
+                color={style.get('color-text-low').color}
+              />
+            }
+            onClick={() => navigate.navigate('Setting.General')}
+          />
+
+          <PageButton
+            title={intl.formatMessage({id: 'page.setting.advanced-title'})}
+            paragraph={intl.formatMessage({
+              id: 'page.setting.advanced-paragraph',
+            })}
+            titleIcon={
+              <RocketLaunchIcon
+                size={16}
+                color={style.get('color-text-high').color}
+              />
+            }
+            endIcon={
+              <ArrowRightIcon
+                size={24}
+                color={style.get('color-text-low').color}
+              />
+            }
+            onClick={() => navigate.navigate('Setting.General')}
+          />
+
+          <PageButton
+            title={intl.formatMessage({
+              id: 'page.setting.security-privacy-title',
+            })}
+            paragraph={intl.formatMessage({
+              id: 'page.setting.security-privacy-paragraph',
+            })}
+            titleIcon={
+              <KeyIcon size={16} color={style.get('color-text-high').color} />
+            }
+            endIcon={
+              <ArrowRightIcon
+                size={24}
+                color={style.get('color-text-low').color}
+              />
+            }
+            onClick={() => navigate.navigate('Setting.General')}
+          />
+
+          <PageButton
+            title={intl.formatMessage({
+              id: 'page.setting.manage-token-list-title',
+            })}
+            paragraph={intl.formatMessage({
+              id: 'page.setting.manage-token-list-paragraph',
+            })}
+            endIcon={
+              <ArrowRightIcon
+                size={24}
+                color={style.get('color-text-low').color}
+              />
+            }
+            onClick={() => navigate.navigate('Setting.General')}
+          />
+        </Stack>
+      </Box>
     </PageWithScrollView>
   );
 };
