91baad2c5fccf18b5041c4196182afeeb8cb88f7
blacktoast <dbstjd100@naver.com>
2023-10-31 16:26:55 +0900

    [feat] add intl

--- packages/mobile/src/screen/home/available.tsx
+++ packages/mobile/src/screen/home/available.tsx
@@ -21,7 +21,7 @@ import {LookingForChains} from './components/looking-for-chains';
 import {Gutter} from '../../components/gutter';
 import FastImage from 'react-native-fast-image';
 import {StackActions, useNavigation} from '@react-navigation/native';
-import {FormattedMessage} from 'react-intl';
+import {FormattedMessage, useIntl} from 'react-intl';
 import {Toggle} from '../../components/toggle';
 import {InformationModal} from '../../components/modal/infoModal';
 
@@ -40,6 +40,7 @@ export const AvailableTabView: FunctionComponent<{
   // const navigate = useNavigate();
   const tokenFoundModalRef = useRef<BottomSheetModal>(null);
   const infoModalRef = useRef<BottomSheetModal>(null);
+  const intl = useIntl();
 
   const allBalances = hugeQueriesStore.getAllBalances(true);
   const allBalancesNonZero = useMemo(() => {
@@ -278,10 +279,12 @@ export const AvailableTabView: FunctionComponent<{
       </Modal>
       <Modal ref={infoModalRef} enableDynamicSizing={true} snapPoints={['90%']}>
         <InformationModal
-          title="Available Balance"
-          paragraph="The amount of your assets that are available for use or transfer
-        immediately, except for those that are currently staked or locked in
-        LP pools."
+          title={intl.formatMessage({
+            id: 'page.main.available.available-balance-title',
+          })}
+          paragraph={intl.formatMessage({
+            id: 'page.main.available.available-balance-tooltip',
+          })}
         />
       </Modal>
     </React.Fragment>
