15dee019e01d892edd9648a71351222baaefb394
blacktoast <dbstjd100@naver.com>
2023-11-17 19:03:14 +0900

    [feat] add validator list item

--- packages/mobile/src/screen/staking/components/validator-item/index.tsx
+++ packages/mobile/src/screen/staking/components/validator-item/index.tsx
@@ -3,18 +3,21 @@ import {Box} from '../../../../components/box';
 import {useStyle} from '../../../../styles';
 import {RectButton} from '../../../../components/rect-button';
 import {Column, Columns} from '../../../../components/column';
-import {StyleSheet, Text, TextStyle} from 'react-native';
+import {Text} from 'react-native';
 import {Gutter} from '../../../../components/gutter';
 import FastImage from 'react-native-fast-image';
-import {CoinPretty} from '@keplr-wallet/unit';
+import {CoinPretty, Dec, RatePretty} from '@keplr-wallet/unit';
 import {Stack} from '../../../../components/stack';
 import {ArrowRightIcon} from '../../../../components/icon/arrow-right';
 import {StakingIcon} from '../../../../components/icon/stacking';
+import {observer} from 'mobx-react-lite';
+import {useStore} from '../../../../stores';
+import {Staking} from '@keplr-wallet/stores';
+import {FilterOption} from '../../vlidator-list';
 
 export interface ViewValidator {
   coin?: CoinPretty;
   name?: string;
-  imageUrl: string | undefined;
   subString?: string;
   isDelegation?: boolean;
   validatorAddress: string;
@@ -23,19 +26,19 @@ export interface ViewValidator {
 export const ValidatorItem: FunctionComponent<{
   viewValidator: ViewValidator;
   isNotReady?: boolean;
-  subStringStyle?: TextStyle;
-  coinTextStyle?: TextStyle;
-  coinMaxDecimal?: number;
-  warning?: boolean;
+  chainId: string;
   afterSelect: () => void;
-}> = ({
-  viewValidator,
-  coinTextStyle,
-  coinMaxDecimal,
-  subStringStyle,
-  afterSelect,
-}) => {
+}> = observer(({chainId, viewValidator, afterSelect}) => {
+  const {queriesStore} = useStore();
+  const queries = queriesStore.get(chainId);
   const style = useStyle();
+  const bondedValidators = queries.cosmos.queryValidators.getQueryStatus(
+    Staking.BondStatus.Bonded,
+  );
+
+  const thumbnail = bondedValidators.getValidatorThumbnail(
+    viewValidator.validatorAddress,
+  );
 
   return (
     <RectButton
@@ -50,7 +53,7 @@ export const ValidatorItem: FunctionComponent<{
         <Columns sum={1} alignY="center" gutter={8}>
           <Box>
             <ValidatorImage
-              imageUrl={viewValidator.imageUrl}
+              imageUrl={thumbnail}
               name={viewValidator.name}
               isDelegation={viewValidator.isDelegation}
             />
@@ -67,13 +70,9 @@ export const ValidatorItem: FunctionComponent<{
           <Column weight={1} />
           <Stack alignX="right" gutter={4}>
             {viewValidator.coin ? (
-              <Text
-                style={StyleSheet.flatten([
-                  style.flatten(['subtitle1', 'color-text-high']),
-                  coinTextStyle,
-                ])}>
+              <Text style={style.flatten(['subtitle1', 'color-text-high'])}>
                 {viewValidator.coin
-                  .maxDecimals(coinMaxDecimal || 6)
+                  .maxDecimals(6)
                   .trim(true)
                   .shrink(true)
                   .toString()}
@@ -82,11 +81,7 @@ export const ValidatorItem: FunctionComponent<{
 
             {viewValidator.subString ? (
               <Columns sum={1}>
-                <Text
-                  style={StyleSheet.flatten([
-                    style.flatten(['subtitle2', 'color-text-low']),
-                    subStringStyle,
-                  ])}>
+                <Text style={style.flatten(['subtitle2', 'color-text-low'])}>
                   {viewValidator.subString}
                 </Text>
               </Columns>
@@ -98,7 +93,135 @@ export const ValidatorItem: FunctionComponent<{
       </Box>
     </RectButton>
   );
-};
+});
+
+export const ValidatorListItem: FunctionComponent<{
+  validatorAddress: string;
+  chainId: string;
+  filterOption: FilterOption;
+  afterSelect: () => void;
+}> = observer(({chainId, validatorAddress, filterOption, afterSelect}) => {
+  const {queriesStore, chainStore, accountStore} = useStore();
+  const queries = queriesStore.get(chainId);
+  const bondedValidators = queries.cosmos.queryValidators.getQueryStatus(
+    Staking.BondStatus.Bonded,
+  );
+  const bondedToken = queries.cosmos.queryPool.bondedTokens;
+  const delegationsValidator =
+    queries.cosmos.queryDelegations.getQueryBech32Address(
+      accountStore.getAccount(chainId).bech32Address,
+    ).delegations;
+
+  const chainInfo = chainStore.getChain(chainId);
+  const viewValidator: ViewValidator | undefined = (() => {
+    const val = bondedValidators.getValidator(validatorAddress);
+    if (!val) {
+      return undefined;
+    }
+
+    const coin = new CoinPretty(
+      chainInfo.stakeCurrency || chainInfo.feeCurrencies[0],
+      new Dec(val.delegator_shares),
+    );
+    if (filterOption === 'Voting') {
+      return {
+        name: val.description.moniker,
+        validatorAddress: val.operator_address,
+        coin,
+        subString:
+          bondedToken?.toCoin().amount === '0'
+            ? '0%'
+            : new RatePretty(
+                coin.toDec().quo(bondedToken?.toDec() || new Dec(1)),
+              )
+                .maxDecimals(2)
+                .toString(),
+        isDelegation: !!delegationsValidator.find(
+          del => del.delegation.validator_address === val.operator_address,
+        ),
+      };
+    }
+
+    if (filterOption === 'Commission') {
+      return {
+        name: val.description.moniker,
+        validatorAddress: val.operator_address,
+        subString: new RatePretty(val.commission.commission_rates.rate)
+          .maxDecimals(2)
+          .toString(),
+        isDelegation: !!delegationsValidator.find(
+          del => del.delegation.delegator_address === val.operator_address,
+        ),
+      };
+    }
+  })();
+  const style = useStyle();
+  const thumbnail = bondedValidators.getValidatorThumbnail(validatorAddress);
+
+  return (
+    <React.Fragment>
+      {viewValidator ? (
+        <RectButton
+          underlayColor={style.get('color-gray-550').color}
+          rippleColor={style.get('color-gray-550').color}
+          style={style.flatten([
+            'border-radius-6',
+            'background-color-gray-600',
+          ])}
+          activeOpacity={0.5}
+          onPress={async () => {
+            afterSelect();
+          }}>
+          <Box paddingLeft={16} paddingRight={8} paddingY={16} borderRadius={6}>
+            <Columns sum={1} alignY="center" gutter={8}>
+              <Box>
+                <ValidatorImage
+                  imageUrl={thumbnail}
+                  name={viewValidator.name}
+                  isDelegation={viewValidator.isDelegation}
+                />
+              </Box>
+              <Gutter size={12} />
+              <Column weight={6}>
+                <Text
+                  numberOfLines={1}
+                  style={style.flatten(['subtitle2', 'color-text-high'])}>
+                  {viewValidator.name}
+                </Text>
+                <Gutter size={4} />
+              </Column>
+              <Column weight={1} />
+              <Stack alignX="right" gutter={4}>
+                {viewValidator.coin ? (
+                  <Text style={style.flatten(['body2', 'color-text-low'])}>
+                    {viewValidator.coin
+                      .maxDecimals(10)
+                      .trim(true)
+                      .shrink(true)
+                      .toString()}
+                  </Text>
+                ) : null}
+
+                {viewValidator.subString ? (
+                  <Columns sum={1}>
+                    <Text style={style.flatten(['body2', 'color-text-low'])}>
+                      {viewValidator.subString}
+                    </Text>
+                  </Columns>
+                ) : null}
+              </Stack>
+              <Gutter size={4} />
+              <ArrowRightIcon
+                size={24}
+                color={style.get('color-gray-400').color}
+              />
+            </Columns>
+          </Box>
+        </RectButton>
+      ) : null}
+    </React.Fragment>
+  );
+});
 
 const ValidatorImage = ({
   imageUrl,
--- packages/mobile/src/screen/staking/vlidator-list/index.tsx
+++ packages/mobile/src/screen/staking/vlidator-list/index.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, useState} from 'react';
+import React, {FunctionComponent, useMemo, useState} from 'react';
 
 import {observer} from 'mobx-react-lite';
 import {FlatList} from 'react-native';
@@ -9,166 +9,49 @@ import {Staking} from '@keplr-wallet/stores';
 import {ValidatorInfo} from '../type';
 import {Box} from '../../../components/box';
 import {DebounceSearchTextInput} from '../../../components/input/search-text-input';
-import {ValidatorItem, ViewValidator} from '../components/validator-item';
-import {CoinPretty, Dec, RatePretty} from '@keplr-wallet/unit';
+import {ValidatorListItem} from '../components/validator-item';
 import {useStyle} from '../../../styles';
 import {Gutter} from '../../../components/gutter';
 import {ArrowDownUpIcon} from '../../../components/icon';
 import {TextButton} from '../../../components/text-button';
 
-type FilterOption = 'Commission' | 'Voting';
+export type FilterOption = 'Commission' | 'Voting';
 export const ValidatorListScreen: FunctionComponent = observer(() => {
-  const {queriesStore, chainStore, accountStore} = useStore();
-  const [thumbnailLen, setThumbnailLen] = useState(20);
+  const {queriesStore} = useStore();
   const style = useStyle();
   const [filterOption, setFilterOption] = useState<FilterOption>('Voting');
   const [search, setSearch] = useState('');
-
   const route = useRoute<RouteProp<StakeNavigation, 'Stake.ValidateList'>>();
   const {chainId} = route.params;
-  const chainInfo = chainStore.getChain(chainId);
   const queries = queriesStore.get(chainId);
   const bondedValidators = queries.cosmos.queryValidators.getQueryStatus(
     Staking.BondStatus.Bonded,
   );
-  const bondedToken = queries.cosmos.queryPool.bondedTokens;
-
-  const delegationsValidator =
-    queries.cosmos.queryDelegations.getQueryBech32Address(
-      accountStore.getAccount(chainId).bech32Address,
-    ).delegations;
-
-  console.log(delegationsValidator.length);
 
   const activeValidators: ValidatorInfo[] = bondedValidators.validators
     .sort((a, b) => Number(b.tokens) - Number(a.tokens))
     .map((validator, i) => ({...validator, rank: i + 1}));
 
-  const filteredValidators: {
-    viewValidator: ViewValidator;
-    validatorInfo: ValidatorInfo;
-  }[] = (() => {
+  const filteredValidators = useMemo(() => {
     const _search = search.trim().toLowerCase();
     const _filteredValidators = activeValidators.filter(val => {
       return val.description.moniker?.toLocaleLowerCase().includes(_search);
     });
 
     if (filterOption === 'Voting') {
-      return _filteredValidators.map(val => {
-        const coin = new CoinPretty(
-          chainInfo.stakeCurrency || chainInfo.feeCurrencies[0],
-          new Dec(val.delegator_shares),
-        );
-        return {
-          viewValidator: {
-            name: val.description.moniker,
-            validatorAddress: val.operator_address,
-            imageUrl: bondedValidators.getValidatorThumbnail(
-              val.operator_address,
-            ),
-            coin,
-            subString:
-              bondedToken?.toCoin().amount === '0'
-                ? '0%'
-                : new RatePretty(
-                    coin.toDec().quo(bondedToken?.toDec() || new Dec(1)),
-                  )
-                    .maxDecimals(2)
-                    .toString(),
-            isDelegation: !!delegationsValidator.find(
-              del => del.delegation.validator_address === val.operator_address,
-            ),
-          },
-          validatorInfo: val,
-        };
-      });
+      return _filteredValidators;
     }
     if (filterOption === 'Commission') {
-      return _filteredValidators
-        .sort(
-          (a, b) =>
-            Number(a.commission.commission_rates.rate) -
-            Number(b.commission.commission_rates.rate),
-        )
-        .map(val => {
-          return {
-            viewValidator: {
-              name: val.description.moniker,
-              validatorAddress: val.operator_address,
-              imageUrl: bondedValidators.getValidatorThumbnail(
-                val.operator_address,
-              ),
-              subString: new RatePretty(val.commission.commission_rates.rate)
-                .maxDecimals(2)
-                .toString(),
-              isDelegation: !!delegationsValidator.find(
-                del =>
-                  del.delegation.delegator_address === val.operator_address,
-              ),
-            },
-            validatorInfo: val,
-          };
-        });
+      return _filteredValidators.sort(
+        (a, b) =>
+          Number(a.commission.commission_rates.rate) -
+          Number(b.commission.commission_rates.rate),
+      );
     }
     return [];
-  })();
-  // const filteredValidators: {
-  //   viewValidator: ViewValidator;
-  //   validatorInfo: ValidatorInfo;
-  // }[] = useMemo(() => {
-  //   const _search = search.trim().toLowerCase();
-  //   const _filteredValidators = activeValidators.filter(val => {
-  //     return val.description.moniker?.toLocaleLowerCase().includes(_search);
-  //   });
-  //   if (filterOption === 'Voting') {
-  //     return _filteredValidators.slice(0, thumbnailLen).map(val => {
-  //       const coin = new CoinPretty(
-  //         chainInfo.stakeCurrency || chainInfo.feeCurrencies[0],
-  //         new Dec(val.delegator_shares),
-  //       );
-  //       return {
-  //         viewValidator: {
-  //           name: val.description.moniker,
-  //           validatorAddress: val.operator_address,
-  //           // imageUrl: '',
-  //           imageUrl: bondedValidators.getValidatorThumbnail(
-  //             val.operator_address,
-  //           ),
-  //           coin,
-  //           subString:
-  //             bondedToken?.toCoin().amount === '0'
-  //               ? '0%'
-  //               : new RatePretty(
-  //                   coin.toDec().quo(bondedToken?.toDec() || new Dec(1)),
-  //                 )
-  //                   .maxDecimals(2)
-  //                   .toString(),
-  //         },
-  //         validatorInfo: val,
-  //       };
-  //     });
-  //   }
-  //   // if (filterOption === 'Commission') {
-  //   //   return activeValidators.sort(
-  //   //     (a, b) =>
-  //   //       Number(b.commission.commission_rates.rate) -
-  //   //       Number(a.commission.commission_rates.rate),
-  //   //   );
-  //   // }
-  //   return [];
-  // }, [
-  //   activeValidators,
-  //   bondedToken,
-  //   chainInfo.feeCurrencies,
-  //   chainInfo.stakeCurrency,
-  //   filterOption,
-  //   search,
-  // ]);
-
-  //NOTE flatList render item안에는 observe 할 수 없어서 외부에서 썸네일 리스트를 만들고 사용
+  }, [activeValidators, filterOption, search]);
 
   return (
-    // <Box></Box>
     <FlatList
       ListHeaderComponent={
         <Box marginTop={12}>
@@ -200,20 +83,15 @@ export const ValidatorListScreen: FunctionComponent = observer(() => {
           </Box>
         </Box>
       }
-      keyExtractor={item => item.validatorInfo.consensus_pubkey.key}
+      keyExtractor={item => item.consensus_pubkey.key}
       data={filteredValidators}
-      maxToRenderPerBatch={20}
-      onEndReached={() => {
-        setThumbnailLen(thumbnailLen + 20);
-      }}
       ItemSeparatorComponent={() => <Gutter size={8} />}
       renderItem={({item: validator}) => {
         return (
-          <ValidatorItem
-            viewValidator={validator.viewValidator}
-            coinMaxDecimal={9}
-            coinTextStyle={style.flatten(['body2', 'color-text-low'])}
-            subStringStyle={style.flatten(['body2', 'color-text-low'])}
+          <ValidatorListItem
+            filterOption={filterOption}
+            chainId={chainId}
+            validatorAddress={validator.operator_address}
             afterSelect={() => {}}
           />
         );
