b151600cac57af32b85f775407316604be4b8b0c
blacktoast <dbstjd100@naver.com>
2023-11-20 17:30:25 +0900

    [style] edit yellow 500 color

--- packages/mobile/src/styles/index.tsx
+++ packages/mobile/src/styles/index.tsx
@@ -75,7 +75,7 @@ export const ColorPalette = {
   'yellow-200': '#EDD18A',
   'yellow-300': '#EBBF50',
   'yellow-400': '#F0B622',
-  'yellow-500': '#FF6BB8',
+  'yellow-500': '#D29C11',
   'yellow-600': '#A67B0C',
   'yellow-700': '#705512',
   'yellow-800': '#2F2611',
