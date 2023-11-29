97c095c1f87de85fd91dd3e0337cfc970dc65445
blacktoast <dbstjd100@naver.com>
2023-11-29 15:35:58 +0900

    [feat] add manage chain routing in setting

--- packages/mobile/src/screen/setting/components/page-button/index.tsx
+++ packages/mobile/src/screen/setting/components/page-button/index.tsx
@@ -1,9 +1,10 @@
-import React, {FunctionComponent, useState} from 'react';
+import React, {FunctionComponent} from 'react';
 import {Column, Columns} from '../../../../components/column';
 import {Box} from '../../../../components/box';
 import {Stack} from '../../../../components/stack';
-import {Pressable, StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import {useStyle} from '../../../../styles';
+import {RectButton} from '../../../../components/rect-button';
 
 export interface PageButtonProps {
   title: string | React.ReactNode;
@@ -21,56 +22,82 @@ export const PageButton: FunctionComponent<PageButtonProps> = ({
   onClick,
 }) => {
   const style = useStyle();
-  const [isPressing, setIsPressing] = useState(false);
+
+  if (onClick) {
+    return (
+      <RectButton
+        rippleColor={style.get('color-gray-550').color}
+        underlayColor={style.get('color-gray-550').color}
+        style={style.flatten([
+          'background-color-card-default',
+          'border-radius-6',
+        ])}
+        onPress={onClick}>
+        <Box alignX="center" alignY="center" padding={16} minHeight={74}>
+          <Columns sum={1} alignY="center">
+            <Column weight={1}>
+              <Stack gutter={6}>
+                <Columns sum={1} gutter={6} alignY="center">
+                  {titleIcon ? <Box>{titleIcon}</Box> : null}
+                  <Text
+                    style={style.flatten([
+                      'color-text-high',
+                      'subtitle2',
+                      'padding-right-20',
+                    ])}>
+                    {title}
+                  </Text>
+                </Columns>
+                {paragraph ? (
+                  <Text
+                    style={StyleSheet.flatten([
+                      style.flatten(['color-text-low', 'padding-right-20']),
+                    ])}>
+                    {paragraph}
+                  </Text>
+                ) : null}
+              </Stack>
+            </Column>
+            {endIcon ? <Box>{endIcon}</Box> : null}
+          </Columns>
+        </Box>
+      </RectButton>
+    );
+  }
   return (
-    <Pressable
-      onPressIn={() => setIsPressing(true)}
-      onPressOut={() => setIsPressing(false)}
-      onPress={
-        onClick &&
-        (e => {
-          e.preventDefault();
-          onClick();
-        })
-      }>
-      <Box
-        alignX="center"
-        alignY="center"
-        padding={16}
-        borderRadius={6}
-        minHeight={74}
-        backgroundColor={
-          isPressing
-            ? style.get('background-color-gray-550').backgroundColor
-            : style.get('background-color-gray-600').backgroundColor
-        }>
-        <Columns sum={1} alignY="center">
-          <Column weight={1}>
-            <Stack gutter={6}>
-              <Columns sum={1} gutter={6} alignY="center">
-                {titleIcon ? <Box>{titleIcon}</Box> : null}
-                <Text
-                  style={style.flatten([
-                    'color-text-high',
-                    'subtitle2',
-                    'padding-right-20',
-                  ])}>
-                  {title}
-                </Text>
-              </Columns>
-              {paragraph ? (
-                <Text
-                  style={StyleSheet.flatten([
-                    style.flatten(['color-text-low', 'padding-right-20']),
-                  ])}>
-                  {paragraph}
-                </Text>
-              ) : null}
-            </Stack>
-          </Column>
-          {endIcon ? <Box>{endIcon}</Box> : null}
-        </Columns>
-      </Box>
-    </Pressable>
+    <Box
+      alignX="center"
+      alignY="center"
+      borderRadius={6}
+      padding={16}
+      minHeight={74}
+      backgroundColor={style.get('color-card-default').color}>
+      <Columns sum={1} alignY="center">
+        <Column weight={1}>
+          <Stack gutter={6}>
+            <Columns sum={1} gutter={6} alignY="center">
+              {titleIcon ? <Box>{titleIcon}</Box> : null}
+              <Text
+                style={style.flatten([
+                  'color-text-high',
+                  'subtitle2',
+                  'padding-right-20',
+                ])}>
+                {title}
+              </Text>
+            </Columns>
+            {paragraph ? (
+              <Text
+                style={StyleSheet.flatten([
+                  style.flatten(['color-text-low', 'padding-right-20']),
+                ])}>
+                {paragraph}
+              </Text>
+            ) : null}
+          </Stack>
+        </Column>
+        {endIcon ? <Box>{endIcon}</Box> : null}
+      </Columns>
+    </Box>
   );
 };
