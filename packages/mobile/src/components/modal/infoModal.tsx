712a48cb3c5afed2f96d56f6d52820cc1b58359a
blacktoast <dbstjd100@naver.com>
2023-12-06 11:53:49 +0900

    [feat] add bottom button in info modal

--- packages/mobile/src/components/modal/infoModal.tsx
+++ packages/mobile/src/components/modal/infoModal.tsx
@@ -6,38 +6,48 @@ import {Columns} from '../../components/column';
 import {InformationOutlinedIcon} from '../../components/icon/information-outlined';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {registerCardModal} from './card';
+import {Gutter} from '../gutter';
 export interface InformationModalProps {
   title: string;
   paragraph: string;
+  bottomButton?: React.ReactElement;
 }
 export const InformationModal = registerCardModal(
-  ({title, paragraph}: InformationModalProps) => {
+  ({title, paragraph, bottomButton}: InformationModalProps) => {
     const style = useStyle();
     const insects = useSafeAreaInsets();
     return (
-      <Box paddingX={12} paddingBottom={60 + insects.bottom}>
+      <Box paddingX={12} paddingBottom={insects.bottom}>
         <Box>
-          <Box paddingBottom={12} paddingX={8}>
-            <Columns sum={1} gutter={10} alignY="center">
-              <InformationOutlinedIcon
-                size={20}
-                color={style.get('color-text-low').color}
-              />
+          <Box>
+            <Box paddingBottom={12} paddingX={8}>
+              <Columns sum={1} gutter={10} alignY="center">
+                <InformationOutlinedIcon
+                  size={20}
+                  color={style.get('color-text-low').color}
+                />
 
-              <Text
-                style={style.flatten(['h4', 'color-text-high', 'padding-8'])}>
-                {title}
-              </Text>
-            </Columns>
+                <Text
+                  style={style.flatten(['h4', 'color-text-high', 'padding-8'])}>
+                  {title}
+                </Text>
+              </Columns>
+            </Box>
+            <Text
+              style={style.flatten([
+                'body2',
+                'color-text-middle',
+                'padding-x-16',
+              ])}>
+              {paragraph}
+            </Text>
+            {bottomButton ? (
+              <React.Fragment>
+                <Gutter size={20} />
+                {bottomButton}
+              </React.Fragment>
+            ) : null}
           </Box>
-          <Text
-            style={style.flatten([
-              'body2',
-              'color-text-middle',
-              'padding-x-16',
-            ])}>
-            {paragraph}
-          </Text>
         </Box>
       </Box>
     );
