5437561ddea2e591e456328ee103bd3e2af2bfc0
blacktoast <dbstjd100@naver.com>
2023-12-07 16:23:25 +0900

    [feat] add register header title comp

--- packages/mobile/src/components/pageHeader/header-register.tsx
+++ packages/mobile/src/components/pageHeader/header-register.tsx
@@ -1,10 +1,63 @@
-import React, {FunctionComponent} from 'react';
-import {useStyle} from '../../styles';
+import React, {
+  FunctionComponent,
+  PropsWithChildren,
+  useLayoutEffect,
+} from 'react';
+import {ColorPalette, useStyle} from '../../styles';
 import {StatusBar, Pressable, Text, SafeAreaView} from 'react-native';
 import {Gutter} from '../gutter';
 import {HeaderBackButtonIcon} from './icon/back';
-import {useNavigation} from '@react-navigation/native';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {Box} from '../box';
+
+export const RegisterHeaderTitle: FunctionComponent<PropsWithChildren> = ({
+  children,
+}) => {
+  const style = useStyle();
+  const route = useRoute<
+    RouteProp<
+      Record<
+        string,
+        {
+          paragraph?: string;
+          hideBackButton?: boolean;
+        }
+      >,
+      string
+    >
+  >();
+  const navigation = useNavigation();
+  const paragraph = route.params?.paragraph;
+  const hideBackButton = route.params?.hideBackButton;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: hideBackButton ? null : () => <HeaderBackButton />,
+    });
+  }, [hideBackButton, navigation]);
+  return (
+    <Box alignX="center" alignY="center" marginBottom={paragraph ? 2 : 0}>
+      <Text style={style.flatten(['h3', 'color-text-high'])}>{children}</Text>
+      {paragraph ? (
+        <React.Fragment>
+          <Gutter size={4} />
+          <Text style={style.flatten(['body2', 'color-text-low'])}>
+            {paragraph}
+          </Text>
+        </React.Fragment>
+      ) : null}
+    </Box>
+  );
+};
+export const registerHeaderTitleFunc = () => <RegisterHeaderTitle />;
+export const registerHeaderOptions = {
+  headerTitle: RegisterHeaderTitle,
+  headerTitleAlign: 'center' as 'center' | 'left',
+  headerStyle: {
+    backgroundColor: ColorPalette['gray-700'],
+  },
+  headerShadowVisible: false,
+};
 
 export const RegisterHeader: FunctionComponent<{
   title: string;
--- packages/mobile/src/screen/register/components/register-container.tsx
+++ packages/mobile/src/screen/register/components/register-container.tsx
@@ -1,30 +1,35 @@
 import React, {FunctionComponent, PropsWithChildren} from 'react';
 import {useStyle} from '../../../styles';
 import {Box} from '../../../components/box';
-import {RegisterHeader} from '../../../components/pageHeader/header-register';
 import {KeyboardAvoidingView, Platform} from 'react-native';
+import {RouteProp, useRoute} from '@react-navigation/native';
 
 export const RegisterContainer: FunctionComponent<
   PropsWithChildren<{
-    title: string;
-    paragraph?: string;
     bottom?: React.ReactNode;
-    hideBackButton?: boolean;
   }>
-> = ({title, paragraph, children, bottom, hideBackButton}) => {
+> = ({children, bottom}) => {
   const style = useStyle();
+  const route = useRoute<
+    RouteProp<
+      Record<
+        string,
+        {
+          paragraph?: string;
+        }
+      >,
+      string
+    >
+  >();
+  const paragraph = route.params?.paragraph;
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={{flex: 1}}>
-      <RegisterHeader
-        title={title}
-        paragraph={paragraph}
-        hideBackButton={hideBackButton}
-      />
-
-      <Box style={{flex: 1}}>{children}</Box>
+      <Box style={{flex: 1}} marginTop={paragraph ? 18 : 0}>
+        {children}
+      </Box>
 
       <Box padding={20} backgroundColor={style.get('color-gray-700').color}>
         {bottom}
