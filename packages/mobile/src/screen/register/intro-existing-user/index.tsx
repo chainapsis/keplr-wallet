bb72a4d8ea6e732d8d82d4ee7a2d8d0526b9a3ae
Thunnini <yunjh1994@gmail.com>
2023-12-12 23:58:26 +0900

    `RegisterIntroExistingUserScene`와 `RegisterIntroNewUserScreen`를 작은 사이즈에서는 스크롤 되도록 수정

--- packages/mobile/src/screen/register/intro-existing-user/index.tsx
+++ packages/mobile/src/screen/register/intro-existing-user/index.tsx
@@ -4,84 +4,93 @@ import {Text} from 'react-native';
 import {FormattedMessage, useIntl} from 'react-intl';
 import {Gutter} from '../../../components/gutter';
 import {Button} from '../../../components/button';
-import {OptionContainer, RegisterContainer} from '../components';
+import {OptionContainer} from '../components';
 import {AppleIcon, GoogleIcon} from '../../../components/icon';
-import {Box} from '../../../components/box';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavProp} from '../../../navigation';
+import {ContentHeightAwareScrollView} from '../../../components/scroll-view';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const RegisterIntroExistingUserScene: FunctionComponent = () => {
   const intl = useIntl();
   const style = useStyle();
   const navigation = useNavigation<StackNavProp>();
 
+  const safeAreaInsets = useSafeAreaInsets();
+
   return (
-    <RegisterContainer>
-      <Box alignX="center" padding={20}>
-        <Text style={style.flatten(['color-text-low', 'body1'])}>
-          <FormattedMessage id="pages.register.intro-existing-user.paragraph" />
-        </Text>
+    <ContentHeightAwareScrollView
+      style={{
+        height: '100%',
+      }}
+      contentContainerStyle={{
+        alignItems: 'center',
+        padding: 20,
+        paddingBottom: safeAreaInsets.bottom,
+      }}>
+      <Text style={style.flatten(['color-text-low', 'body1'])}>
+        <FormattedMessage id="pages.register.intro-existing-user.paragraph" />
+      </Text>
+
+      <Gutter size={12} />
+
+      <OptionContainer
+        title={intl.formatMessage({
+          id: 'pages.register.intro-existing-user.recovery-title',
+        })}
+        paragraph={intl.formatMessage({
+          id: 'pages.register.intro-existing-user.recovery-paragraph',
+        })}>
+        <Gutter size={20} />
+
+        <Button
+          text={intl.formatMessage({
+            id: 'pages.register.intro-existing-user.recovery-button',
+          })}
+          color="secondary"
+          size="large"
+          onPress={() => {
+            navigation.navigate('Register.RecoverMnemonic');
+          }}
+        />
+
+        <Gutter size={20} />
+
+        {/* TODO: INTL 적용하기 */}
+        <Button text="Import from Keplr Extension" size="large" />
+      </OptionContainer>
+
+      <Gutter size={16} />
+
+      <OptionContainer
+        title={intl.formatMessage({
+          id: 'pages.register.intro-existing-user.social-recovery-title',
+        })}
+        paragraph={intl.formatMessage({
+          id: 'pages.register.intro-existing-user.social-recovery-paragraph',
+        })}>
+        <Gutter size={20} />
+
+        <Button
+          text={intl.formatMessage({
+            id: 'pages.register.intro-new-user.sign-up-apple-button',
+          })}
+          size="large"
+          color="secondary"
+          leftIcon={<AppleIcon />}
+        />
 
         <Gutter size={12} />
 
-        <OptionContainer
-          title={intl.formatMessage({
-            id: 'pages.register.intro-existing-user.recovery-title',
+        <Button
+          text={intl.formatMessage({
+            id: 'pages.register.intro-new-user.sign-up-google-button',
           })}
-          paragraph={intl.formatMessage({
-            id: 'pages.register.intro-existing-user.recovery-paragraph',
-          })}>
-          <Gutter size={20} />
-
-          <Button
-            text={intl.formatMessage({
-              id: 'pages.register.intro-existing-user.recovery-button',
-            })}
-            color="secondary"
-            size="large"
-            onPress={() => {
-              navigation.navigate('Register.RecoverMnemonic');
-            }}
-          />
-
-          <Gutter size={20} />
-
-          {/* TODO: INTL 적용하기 */}
-          <Button text="Import from Keplr Extension" size="large" />
-        </OptionContainer>
-
-        <Gutter size={16} />
-
-        <OptionContainer
-          title={intl.formatMessage({
-            id: 'pages.register.intro-existing-user.social-recovery-title',
-          })}
-          paragraph={intl.formatMessage({
-            id: 'pages.register.intro-existing-user.social-recovery-paragraph',
-          })}>
-          <Gutter size={20} />
-
-          <Button
-            text={intl.formatMessage({
-              id: 'pages.register.intro-new-user.sign-up-apple-button',
-            })}
-            size="large"
-            color="secondary"
-            leftIcon={<AppleIcon />}
-          />
-
-          <Gutter size={12} />
-
-          <Button
-            text={intl.formatMessage({
-              id: 'pages.register.intro-new-user.sign-up-google-button',
-            })}
-            size="large"
-            color="secondary"
-            leftIcon={<GoogleIcon />}
-          />
-        </OptionContainer>
-      </Box>
-    </RegisterContainer>
+          size="large"
+          color="secondary"
+          leftIcon={<GoogleIcon />}
+        />
+      </OptionContainer>
+    </ContentHeightAwareScrollView>
   );
 };
--- packages/mobile/src/screen/register/intro-new-user/index.tsx
+++ packages/mobile/src/screen/register/intro-new-user/index.tsx
@@ -7,127 +7,136 @@ import {XAxis} from '../../../components/axis';
 import {Button} from '../../../components/button';
 import {TextButton} from '../../../components/text-button';
 import {StackActions, useNavigation} from '@react-navigation/native';
-import {OptionContainer, RegisterContainer} from '../components';
+import {OptionContainer} from '../components';
 import {
   AppleIcon,
   GoogleIcon,
   BoltIcon,
   ShieldIcon,
 } from '../../../components/icon';
-import {Box} from '../../../components/box';
 import {StackNavProp} from '../../../navigation';
+import {ContentHeightAwareScrollView} from '../../../components/scroll-view';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const RegisterIntroNewUserScreen: FunctionComponent = () => {
   const intl = useIntl();
   const style = useStyle();
   const navigation = useNavigation<StackNavProp>();
 
+  const safeAreaInsets = useSafeAreaInsets();
+
   return (
-    <RegisterContainer>
-      <Box alignX="center" padding={20}>
-        <Text style={style.flatten(['color-text-low', 'body1'])}>
-          <FormattedMessage id="pages.register.intro-new-user.paragraph" />
-        </Text>
+    <ContentHeightAwareScrollView
+      style={{
+        height: '100%',
+      }}
+      contentContainerStyle={{
+        alignItems: 'center',
+        padding: 20,
+        paddingBottom: safeAreaInsets.bottom,
+      }}>
+      <Text style={style.flatten(['color-text-low', 'body1'])}>
+        <FormattedMessage id="pages.register.intro-new-user.paragraph" />
+      </Text>
+
+      <Gutter size={12} />
+
+      <OptionContainer
+        title={intl.formatMessage({
+          id: 'pages.register.intro-new-user.recovery-path-title',
+        })}
+        paragraph={intl.formatMessage({
+          id: 'pages.register.intro-new-user.recovery-path-paragraph',
+        })}>
+        <Gutter size={20} />
+
+        <XAxis alignY="center">
+          <ShieldIcon />
+          <ShieldIcon />
+          <ShieldIcon />
+
+          <Gutter size={4} />
+
+          <Text style={style.flatten(['text-caption1', 'color-text-low'])}>
+            <FormattedMessage id="pages.register.intro-new-user.high-security-text" />
+          </Text>
+        </XAxis>
+
+        <Gutter size={20} />
+
+        <Button
+          text={intl.formatMessage({
+            id: 'pages.register.intro-new-user.new-recovery-path-button',
+          })}
+          size="large"
+          onPress={() => {
+            navigation.navigate('Register.NewMnemonic', {
+              paragraph: 'Step 1/3',
+            });
+          }}
+        />
+
+        <Gutter size={20} />
+
+        <TextButton
+          containerStyle={{height: 32}}
+          text={intl.formatMessage({
+            id: 'pages.register.intro-new-user.import-recovery-path-button',
+          })}
+          size="large"
+          onPress={() => {
+            navigation.dispatch(
+              StackActions.replace('Register.Intro.ExistingUser'),
+            );
+          }}
+        />
+      </OptionContainer>
+
+      <Gutter size={16} />
+
+      <OptionContainer
+        title={intl.formatMessage({
+          id: 'pages.register.intro-new-user.sign-up-social-title',
+        })}
+        paragraph={intl.formatMessage({
+          id: 'pages.register.intro-new-user.sign-up-social-paragraph',
+        })}>
+        <Gutter size={20} />
+
+        <XAxis alignY="center">
+          <BoltIcon />
+          <BoltIcon />
+          <BoltIcon />
+
+          <Gutter size={4} />
+
+          <Text style={style.flatten(['text-caption1', 'color-text-low'])}>
+            <FormattedMessage id="pages.register.intro-new-user.more-convenience-text" />
+          </Text>
+        </XAxis>
+
+        <Gutter size={20} />
+
+        <Button
+          text={intl.formatMessage({
+            id: 'pages.register.intro-new-user.sign-up-apple-button',
+          })}
+          size="large"
+          color="secondary"
+          leftIcon={<AppleIcon />}
+        />
 
         <Gutter size={12} />
 
-        <OptionContainer
-          title={intl.formatMessage({
-            id: 'pages.register.intro-new-user.recovery-path-title',
+        <Button
+          text={intl.formatMessage({
+            id: 'pages.register.intro-new-user.sign-up-google-button',
           })}
-          paragraph={intl.formatMessage({
-            id: 'pages.register.intro-new-user.recovery-path-paragraph',
-          })}>
-          <Gutter size={20} />
-
-          <XAxis alignY="center">
-            <ShieldIcon />
-            <ShieldIcon />
-            <ShieldIcon />
-
-            <Gutter size={4} />
-
-            <Text style={style.flatten(['text-caption1', 'color-text-low'])}>
-              <FormattedMessage id="pages.register.intro-new-user.high-security-text" />
-            </Text>
-          </XAxis>
-
-          <Gutter size={20} />
-
-          <Button
-            text={intl.formatMessage({
-              id: 'pages.register.intro-new-user.new-recovery-path-button',
-            })}
-            size="large"
-            onPress={() => {
-              navigation.navigate('Register.NewMnemonic', {
-                paragraph: 'Step 1/3',
-              });
-            }}
-          />
-
-          <Gutter size={20} />
-
-          <TextButton
-            containerStyle={{height: 32}}
-            text={intl.formatMessage({
-              id: 'pages.register.intro-new-user.import-recovery-path-button',
-            })}
-            size="large"
-            onPress={() => {
-              navigation.dispatch(
-                StackActions.replace('Register.Intro.ExistingUser'),
-              );
-            }}
-          />
-        </OptionContainer>
-
-        <Gutter size={16} />
-
-        <OptionContainer
-          title={intl.formatMessage({
-            id: 'pages.register.intro-new-user.sign-up-social-title',
-          })}
-          paragraph={intl.formatMessage({
-            id: 'pages.register.intro-new-user.sign-up-social-paragraph',
-          })}>
-          <Gutter size={20} />
-
-          <XAxis alignY="center">
-            <BoltIcon />
-            <BoltIcon />
-            <BoltIcon />
-
-            <Gutter size={4} />
-
-            <Text style={style.flatten(['text-caption1', 'color-text-low'])}>
-              <FormattedMessage id="pages.register.intro-new-user.more-convenience-text" />
-            </Text>
-          </XAxis>
-
-          <Gutter size={20} />
-
-          <Button
-            text={intl.formatMessage({
-              id: 'pages.register.intro-new-user.sign-up-apple-button',
-            })}
-            size="large"
-            color="secondary"
-            leftIcon={<AppleIcon />}
-          />
-
-          <Gutter size={12} />
-
-          <Button
-            text={intl.formatMessage({
-              id: 'pages.register.intro-new-user.sign-up-google-button',
-            })}
-            size="large"
-            color="secondary"
-            leftIcon={<GoogleIcon />}
-          />
-        </OptionContainer>
-      </Box>
-    </RegisterContainer>
+          size="large"
+          color="secondary"
+          leftIcon={<GoogleIcon />}
+        />
+      </OptionContainer>
+    </ContentHeightAwareScrollView>
   );
 };
