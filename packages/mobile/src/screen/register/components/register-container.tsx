afc9991aab69e7550ca884adc81066a0d8d2fa8d
blacktoast <dbstjd100@naver.com>
2023-12-13 14:34:06 +0900

    [style] change step sting location in register container

--- packages/mobile/src/screen/register/components/register-container.tsx
+++ packages/mobile/src/screen/register/components/register-container.tsx
@@ -1,32 +1,28 @@
 import React, {FunctionComponent, PropsWithChildren} from 'react';
 import {useStyle} from '../../../styles';
 import {Box} from '../../../components/box';
-import {KeyboardAvoidingView, Platform} from 'react-native';
-import {RouteProp, useRoute} from '@react-navigation/native';
+import {KeyboardAvoidingView, Platform, Text} from 'react-native';
 
 export const RegisterContainer: FunctionComponent<
   PropsWithChildren<{
     bottom?: React.ReactNode;
+    paragraph?: string;
   }>
-> = ({children, bottom}) => {
+> = ({children, bottom, paragraph}) => {
   const style = useStyle();
-  const route = useRoute<
-    RouteProp<
-      Record<
-        string,
-        {
-          paragraph?: string;
-        }
-      >,
-      string
-    >
-  >();
-  const paragraph = route.params?.paragraph;
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={{flex: 1}}>
+      {paragraph ? (
+        <React.Fragment>
+          <Text
+            style={style.flatten(['body2', 'text-center', 'color-text-low'])}>
+            {paragraph}
+          </Text>
+        </React.Fragment>
+      ) : null}
       <Box style={{flex: 1}} marginTop={paragraph ? 18 : 0}>
         {children}
       </Box>
