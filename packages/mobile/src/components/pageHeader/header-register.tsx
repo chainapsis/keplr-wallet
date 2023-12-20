38514ceb57f9439032974f9e1c78dd4ae953233f
blacktoast <dbstjd100@naver.com>
2023-12-20 12:57:14 +0900

    [refactor] refactoring register header

--- packages/mobile/src/components/pageHeader/header-register.tsx
+++ packages/mobile/src/components/pageHeader/header-register.tsx
@@ -1,18 +1,33 @@
-import React, {
-  FunctionComponent,
-  PropsWithChildren,
-  useLayoutEffect,
-} from 'react';
+import React, {FunctionComponent, PropsWithChildren} from 'react';
 import {ColorPalette, useStyle} from '../../styles';
 import {Pressable, Text} from 'react-native';
 import {HeaderBackButtonIcon} from './icon/back';
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
-import {Box} from '../box';
 
 export const RegisterHeaderTitle: FunctionComponent<PropsWithChildren> = ({
   children,
 }) => {
   const style = useStyle();
+  return (
+    <Text style={style.flatten(['h3', 'color-text-high', 'text-center'])}>
+      {children}
+    </Text>
+  );
+};
+export const registerHeaderOptions = {
+  headerTitle: RegisterHeaderTitle,
+  headerTitleAlign: 'center' as 'center' | 'left',
+  headerBackVisible: false,
+  headerStyle: {
+    backgroundColor: ColorPalette['gray-700'],
+  },
+  headerShadowVisible: false,
+  headerLeft: (props: any) => <HeaderBackButton {...props} />,
+};
+
+export const HeaderBackButton: FunctionComponent = () => {
+  const style = useStyle();
+  const navigation = useNavigation();
   const route = useRoute<
     RouteProp<
       Record<
@@ -25,47 +40,20 @@ export const RegisterHeaderTitle: FunctionComponent<PropsWithChildren> = ({
       string
     >
   >();
-  const navigation = useNavigation();
   const hideBackButton = route.params?.hideBackButton;
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerLeft: hideBackButton ? null : () => <HeaderBackButton />,
-    });
-  }, [hideBackButton, navigation]);
   return (
-    <Box
-      alignX="center"
-      alignY="center"
-      //NOTE 240을 준 이유는 왼쪽에 아이콘이 생길 경우 자체적인 header 길이가 제목을 짜를때가 있음
-      //해서 그냥 find 튜닝으로 안짤리는 최소 값을 지정함
-      minWidth={240}>
-      <Text style={style.flatten(['h3', 'color-text-high'])}>{children}</Text>
-    </Box>
-  );
-};
-export const registerHeaderTitleFunc = () => <RegisterHeaderTitle />;
-export const registerHeaderOptions = {
-  headerTitle: RegisterHeaderTitle,
-  headerTitleAlign: 'center' as 'center' | 'left',
-  headerStyle: {
-    backgroundColor: ColorPalette['gray-700'],
-  },
-  headerShadowVisible: false,
-};
-
-export const HeaderBackButton: FunctionComponent = () => {
-  const style = useStyle();
-  const navigation = useNavigation();
-
-  return (
-    <Pressable
-      onPress={() => navigation.goBack()}
-      style={{position: 'absolute', left: 20}}>
-      <HeaderBackButtonIcon
-        size={28}
-        color={style.get('color-gray-300').color}
-      />
-    </Pressable>
+    <React.Fragment>
+      {!hideBackButton ? (
+        <Pressable
+          onPress={() => navigation.goBack()}
+          style={style.flatten(['padding-left-20'])}>
+          <HeaderBackButtonIcon
+            size={28}
+            color={style.get('color-gray-300').color}
+          />
+        </Pressable>
+      ) : null}
+    </React.Fragment>
   );
 };
