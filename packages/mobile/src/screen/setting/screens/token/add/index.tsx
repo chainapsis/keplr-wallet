d9b33cd676341b4f8d6863a73a267fc020d4dcc5
blacktoast <dbstjd100@naver.com>
2024-01-05 19:14:06 +0900

    [style] add bottom padding

--- packages/mobile/src/screen/setting/screens/token/add/index.tsx
+++ packages/mobile/src/screen/setting/screens/token/add/index.tsx
@@ -23,7 +23,7 @@ import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {RootStackParamList, StackNavProp} from '../../../../../navigation';
 import {PageWithScrollView} from '../../../../../components/page';
 import {ContractAddressBookModal} from '../../../components/contract-address-book-modal';
-import {Text} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import {Controller, useForm} from 'react-hook-form';
 import {Button} from '../../../../../components/button';
 import {Gutter} from '../../../../../components/gutter';
@@ -214,7 +214,12 @@ export const SettingTokenAddScreen: FunctionComponent = observer(() => {
   return (
     <PageWithScrollView
       backgroundMode={'default'}
-      contentContainerStyle={style.flatten(['flex-grow-1'])}>
+      contentContainerStyle={StyleSheet.flatten([
+        style.flatten(['flex-grow-1']),
+        {
+          paddingBottom: 23,
+        },
+      ])}>
       <Box paddingX={12} paddingTop={12} height={'100%'}>
         <Stack gutter={16}>
           <Box width={208}>
