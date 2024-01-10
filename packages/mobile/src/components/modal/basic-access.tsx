e2b948f301c8c7b237ec9c6794fbcc2eeb2c49f2
blacktoast <dbstjd100@naver.com>
2024-01-10 18:31:21 +0900

    [fix] sync permssion store

--- packages/mobile/src/components/modal/basic-access.tsx
+++ packages/mobile/src/components/modal/basic-access.tsx
@@ -19,7 +19,7 @@ export const BasicAccessModal = registerCardModal(
   }>(() => {
     const intl = useIntl();
     const style = useStyle();
-    const {permissionStore} = useStore();
+    const {permissionStore, permissionManagerStore} = useStore();
 
     const waitingPermission =
       permissionStore.waitingPermissionDatas.length > 0
@@ -96,7 +96,9 @@ export const BasicAccessModal = registerCardModal(
               if (waitingPermission) {
                 await permissionStore.approvePermissionWithProceedNext(
                   waitingPermission.id,
-                  () => {},
+                  async () => {
+                    await permissionManagerStore.syncPermissionsFromBackground();
+                  },
                 );
               }
             }}
