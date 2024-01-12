71a32e017188fe5d18500d98f3b83ae3903e4183
Thunnini <yunjh1994@gmail.com>
2024-01-13 00:20:10 +0900

    흠냐링

--- packages/mobile/ios/keplrmobile/AppDelegate.mm
+++ packages/mobile/ios/keplrmobile/AppDelegate.mm
@@ -13,13 +13,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [Bugsnag start];
-
   self.moduleName = @"keplrmobile";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+  [Bugsnag start];
   [AppCenterReactNative register];
   [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
 
