857573146d857fb0ca4c727b0a242725d5df7101
Thunnini <yunjh1994@gmail.com>
2024-01-12 16:15:29 +0900

    Fix a mistake

--- packages/stores-core/src/core/permission-manager.ts
+++ packages/stores-core/src/core/permission-manager.ts
@@ -57,6 +57,14 @@ export class PermissionManagerStore {
     return trimmed;
   }
 
+  async syncPermissionsFromBackground() {
+    const msg = new GetAllPermissionDataPerOriginMsg();
+    const result = await this.requester.sendMessage(BACKGROUND_PORT, msg);
+    runInAction(() => {
+      this._permissionData = result;
+    });
+  }
+
   async clearAllPermissions() {
     runInAction(() => {
       this._permissionData = {};
