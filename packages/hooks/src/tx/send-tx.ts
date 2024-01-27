eb03cf2d270445dce5c7d8152ea24ff89265c986
HeesungB <hezo25@naver.com>
2024-01-27 12:46:45 +0900

    Revert "add useSendMixedIBCTransferConfig hook"
    This reverts commit e6247b2c7e9250ea0ed58924bdc0633a3d459707.

--- packages/hooks/src/tx/send-tx.ts
+++ packages/hooks/src/tx/send-tx.ts
@@ -1,4 +1,4 @@
-import { ChainGetter, IQueriesStore } from "@keplr-wallet/stores";
+import { ChainGetter } from "@keplr-wallet/stores";
 import {
   useFeeConfig,
   useGasConfig,
@@ -8,8 +8,6 @@ import {
 } from "./index";
 import { useAmountConfig } from "./amount";
 import { QueriesStore } from "./internal";
-import { useIBCAmountConfig, useIBCChannelConfig } from "../ibc";
-import { useIBCRecipientConfig } from "../ibc/reciepient";
 
 export const useSendTxConfig = (
   chainGetter: ChainGetter,
@@ -58,71 +56,5 @@ export const useSendTxConfig = (
     gasConfig,
     feeConfig,
     recipientConfig,
-  };
-};
-
-export const useSendMixedIBCTransferConfig = (
-  chainGetter: ChainGetter,
-  queriesStore: IQueriesStore,
-  chainId: string,
-  sender: string,
-  initialGas: number,
-  isIBCTransfer: boolean,
-  options: {
-    allowHexAddressOnEthermint?: boolean;
-    icns?: {
-      chainId: string;
-      resolverContractAddress: string;
-    };
-    computeTerraClassicTax?: boolean;
-  } = {}
-) => {
-  const channelConfig = useIBCChannelConfig(!isIBCTransfer);
-
-  const senderConfig = useSenderConfig(chainGetter, chainId, sender);
-
-  const amountConfig = useIBCAmountConfig(
-    chainGetter,
-    queriesStore,
-    chainId,
-    senderConfig,
-    channelConfig,
-    isIBCTransfer
-  );
-
-  const memoConfig = useMemoConfig(chainGetter, chainId);
-  const gasConfig = useGasConfig(chainGetter, chainId, initialGas);
-  const feeConfig = useFeeConfig(
-    chainGetter,
-    queriesStore,
-    chainId,
-    senderConfig,
-    amountConfig,
-    gasConfig,
-    {
-      computeTerraClassicTax: isIBCTransfer
-        ? false
-        : options.computeTerraClassicTax,
-    }
-  );
-
-  amountConfig.setFeeConfig(feeConfig);
-
-  const recipientConfig = useIBCRecipientConfig(
-    chainGetter,
-    chainId,
-    channelConfig,
-    options,
-    isIBCTransfer
-  );
-
-  return {
-    amountConfig,
-    memoConfig,
-    gasConfig,
-    feeConfig,
-    recipientConfig,
-    channelConfig,
-    senderConfig,
   };
 };
