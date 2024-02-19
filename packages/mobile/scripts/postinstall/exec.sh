90f31fbb1a1c6681c97b818a8e61df3caee0a8ab
Thunnini <yunjh1994@gmail.com>
2024-02-19 14:57:51 +0900

    Try bump "react-native"

--- packages/mobile/scripts/postinstall/exec.sh
+++ packages/mobile/scripts/postinstall/exec.sh
@@ -13,9 +13,9 @@ cp ${DIR}/ledger/BleTransport.js ${DIR}/../../node_modules/@ledgerhq/react-nativ
 # https://github.com/software-mansion/react-native-reanimated/issues/4783#issuecomment-1732498548
 # codepush에서 restart 문제를 해결하기 위해서 대충 이렇게 처리한다.
 # 적절해보이는 방법은 아니지만... 이게 아니면 해결법을 찾을 수가 없다...
-cp ${DIR}/restart-fix/JSCRuntime.cpp ${DIR}/../../node_modules/react-native/ReactCommon/jsc/JSCRuntime.cpp
+# cp ${DIR}/restart-fix/JSCRuntime.cpp ${DIR}/../../node_modules/react-native/ReactCommon/jsc/JSCRuntime.cpp
 
 cp ${DIR}/wc/index.js ${DIR}/../../node_modules/@walletconnect/react-native-compat/index.js
 
-cp ${DIR}/expo-image/ExpoImageViewWrapper.kt ${DIR}/../../node_modules/expo-image/android/src/main/java/expo/modules/image/ExpoImageViewWrapper.kt
-cp ${DIR}/expo-image/ImageViewWrapperTarget.kt ${DIR}/../../node_modules/expo-image/android/src/main/java/expo/modules/image/ImageViewWrapperTarget.kt
+# cp ${DIR}/expo-image/ExpoImageViewWrapper.kt ${DIR}/../../node_modules/expo-image/android/src/main/java/expo/modules/image/ExpoImageViewWrapper.kt
+# cp ${DIR}/expo-image/ImageViewWrapperTarget.kt ${DIR}/../../node_modules/expo-image/android/src/main/java/expo/modules/image/ImageViewWrapperTarget.kt
