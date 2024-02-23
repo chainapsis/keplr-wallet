9a44d20fb7a1fe8697a05962fafac9e9f89fc94a
Thunnini <yunjh1994@gmail.com>
2024-02-23 16:00:26 +0900

    Add chihuahua

--- packages/mobile/src/config.ts
+++ packages/mobile/src/config.ts
@@ -2394,6 +2394,108 @@ export const EmbedChainInfos: ChainInfo[] = [
     ],
     features: ['eth-address-gen', 'eth-key-sign'],
   },
+  {
+    chainId: 'chihuahua-1',
+    chainName: 'Chihuahua',
+    chainSymbolImageUrl:
+      'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/chihuahua/chain.png',
+    rpc: 'https://rpc-chihuahua.keplr.app',
+    rest: 'https://lcd-chihuahua.keplr.app',
+    stakeCurrency: {
+      coinDenom: 'HUAHUA',
+      coinMinimalDenom: 'uhuahua',
+      coinDecimals: 6,
+      coinGeckoId: 'chihuahua-token',
+      coinImageUrl:
+        'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/chihuahua/chain.png',
+    },
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: {
+      bech32PrefixAccAddr: 'chihuahua',
+      bech32PrefixAccPub: 'chihuahuapub',
+      bech32PrefixValAddr: 'chihuahuavaloper',
+      bech32PrefixValPub: 'chihuahuavaloperpub',
+      bech32PrefixConsAddr: 'chihuahuavalcons',
+      bech32PrefixConsPub: 'chihuahuavalconspub',
+    },
+    currencies: [
+      {
+        coinDenom: 'HUAHUA',
+        coinMinimalDenom: 'uhuahua',
+        coinDecimals: 6,
+        coinGeckoId: 'chihuahua-token',
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/chihuahua/chain.png',
+      },
+      {
+        coinDenom: 'BADDOG',
+        coinMinimalDenom:
+          'factory/chihuahua1x4q2vkrz4dfgd9hcw0p5m2f2nuv2uqmt9xr8k2/achihuahuawifhat',
+        coinDecimals: 6,
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/chihuahua/factory/chihuahua1x4q2vkrz4dfgd9hcw0p5m2f2nuv2uqmt9xr8k2/achihuahuawifhat.png',
+      },
+      {
+        coinDenom: 'WOOF',
+        coinMinimalDenom:
+          'factory/chihuahua13jawsn574rf3f0u5rhu7e8n6sayx5gkw3eddhp/uwoof',
+        coinDecimals: 6,
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/chihuahua/factory/chihuahua13jawsn574rf3f0u5rhu7e8n6sayx5gkw3eddhp/woof.png',
+      },
+      {
+        coinDenom: 'TACOS',
+        coinMinimalDenom:
+          'factory/chihuahua13jawsn574rf3f0u5rhu7e8n6sayx5gkw3eddhp/utacos',
+        coinDecimals: 6,
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/chihuahua/factory/chihuahua13jawsn574rf3f0u5rhu7e8n6sayx5gkw3eddhp/tacos.png',
+      },
+      {
+        coinDenom: 'WEED',
+        coinMinimalDenom:
+          'factory/chihuahua13jawsn574rf3f0u5rhu7e8n6sayx5gkw3eddhp/uweed',
+        coinDecimals: 6,
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/chihuahua/factory/chihuahua13jawsn574rf3f0u5rhu7e8n6sayx5gkw3eddhp/weed.png',
+      },
+      {
+        coinDenom: 'BDOG',
+        coinMinimalDenom:
+          'factory/chihuahua13jawsn574rf3f0u5rhu7e8n6sayx5gkw3eddhp/ubdog',
+        coinDecimals: 6,
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/chihuahua/factory/chihuahua13jawsn574rf3f0u5rhu7e8n6sayx5gkw3eddhp/bdog.png',
+      },
+      {
+        coinDenom: 'CORSO',
+        coinMinimalDenom:
+          'factory/chihuahua13jawsn574rf3f0u5rhu7e8n6sayx5gkw3eddhp/ucorso',
+        coinDecimals: 6,
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/chihuahua/factory/chihuahua13jawsn574rf3f0u5rhu7e8n6sayx5gkw3eddhp/corso.png',
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: 'HUAHUA',
+        coinMinimalDenom: 'uhuahua',
+        coinDecimals: 6,
+        coinGeckoId: 'chihuahua-token',
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/chihuahua/chain.png',
+        gasPriceStep: {
+          low: 500,
+          average: 1250,
+          high: 2000,
+        },
+      },
+    ],
+    features: ['cosmwasm'],
+    walletUrlForStaking: 'https://wallet.keplr.app/chains/chihuahua',
+  },
 ];
 
 // TODO: 원격으로 변경할 수 있도록 나중에 네트워크에서 이 정보를 가져오는 방식으로 변경될 수 있음
@@ -2528,6 +2630,10 @@ export const ChainIdentifierToTxExplorerMap: Record<
   dymension_1100: {
     name: 'Mintscan',
     txUrl: 'https://www.mintscan.io/dymension/tx/{txHash}',
+  },
+  chihuahua: {
+    name: 'Mintscan',
+    txUrl: 'https://www.mintscan.io/chihuahua/tx/{txHash}',
   },
 };
 
