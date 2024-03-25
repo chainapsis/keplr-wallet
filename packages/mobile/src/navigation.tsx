f056747ef03cff7e57d4e58dc1dab502054fcb05
HeesungB <hezo25@naver.com>
2024-03-25 17:52:37 +0900

    Remove unused

--- packages/mobile/src/navigation.tsx
+++ packages/mobile/src/navigation.tsx
@@ -1,5 +1,5 @@
 import {observer} from 'mobx-react-lite';
-import {FunctionComponent, useCallback, useEffect} from 'react';
+import {FunctionComponent, useEffect} from 'react';
 import {useStore} from './stores';
 import {
   DarkTheme,
