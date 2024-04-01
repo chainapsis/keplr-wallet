d216d27dc94ab53fbce986aefc862b0bd27d10c3
delivan <wjdgurdl272@gmail.com>
2024-04-02 00:23:01 +0900

    Implement `signEthereum` wallet-connect example

--- packages/wc-client-example/src/app.tsx
+++ packages/wc-client-example/src/app.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react";
 import { observer } from "mobx-react-lite";
 import { useStore } from "./stores";
+import { EthSignType } from "@keplr-wallet/types";
 
 const IBCChannel = "channel-141";
 const CounterpartyIBCChannel = "channel-0";
@@ -92,6 +93,39 @@ export const App: FunctionComponent = observer(() => {
         }}
       >
         Sign Abitrary
+      </button>
+      <button
+        onClick={() => {
+          const evmosChainInfo = chainStore.getChain("evmos_9001-2");
+          const account = accountStore.getAccount(evmosChainInfo.chainId);
+
+          const ethereumTx = {
+            type: 2,
+            chainId: 9001,
+            nonce: 95,
+            gasLimit: "0xae3f",
+            maxFeePerGas: "0x5efeb1f00",
+            maxPriorityFeePerGas: "0x59682f00",
+            to: "0xD4949664cD82660AaE99bEdc034a0deA8A0bd517",
+            value: "0x0",
+            data: "0xa9059cbb0000000000000000000000007f7ec812297f74c80fc8bcaf11ac881dc88eb216000000000000000000000000000000000000000000000000002386f26fc10000",
+          };
+
+          account.getKeplr().then((keplr) => {
+            keplr
+              ?.signEthereum(
+                evmosChainInfo.chainId,
+                account.ethereumHexAddress,
+                JSON.stringify(ethereumTx),
+                EthSignType.TRANSACTION
+              )
+              .then((signature) => {
+                console.log("signature", signature);
+              });
+          });
+        }}
+      >
+        Sign Ethereum
       </button>
     </div>
   );
