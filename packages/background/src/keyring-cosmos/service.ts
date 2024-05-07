07cf4f65fda22a6d135d138af282f9a0e2250b07
Thunnini <yunjh1994@gmail.com>
2024-05-07 15:53:58 +0900

    Minor change

--- packages/background/src/keyring-cosmos/service.ts
+++ packages/background/src/keyring-cosmos/service.ts
@@ -1229,7 +1229,9 @@ Salt: ${salt}`;
     if (
       !chainId.startsWith("evmos_") &&
       !chainId.startsWith("injective") &&
-      !chainId.startsWith("dymension_")
+      !chainId.startsWith("dymension_") &&
+      !chainId.startsWith("nim_") &&
+      !chainId.startsWith("dimension_")
     ) {
       throw new KeplrError(
         "keyring",
