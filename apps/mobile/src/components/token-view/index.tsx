7d66fba9622993bde513a99b9411762b6c59cd63
Thunnini <yunjh1994@gmail.com>
2024-05-08 17:49:14 +0900

    Revert "Minor fix"
    This reverts commit 3958e2bd042505b7109cfa497207f5f5978888e6.

--- apps/mobile/src/components/token-view/index.tsx
+++ apps/mobile/src/components/token-view/index.tsx
@@ -1,9 +1,10 @@
 import React, {FunctionComponent, useMemo} from 'react';
 import {observer} from 'mobx-react-lite';
-import {Image, StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import {useStyle} from '../../styles';
 import {Column, Columns} from '../column';
 import {useStore} from '../../stores';
+import * as ExpoImage from 'expo-image';
 import {Gutter} from '../gutter';
 import {XAxis} from '../axis';
 import {Tag} from '../tag';
@@ -64,19 +65,17 @@ export const TokenItem: FunctionComponent<TokenItemProps> = observer(
         rippleColor={style.get('color-card-pressing-default').color}
         underlayColor={style.get('color-card-pressing-default').color}>
         <Columns sum={1} alignY="center">
-          <Image
+          <ExpoImage.Image
             style={{
               ...style.flatten(['width-32', 'height-32']),
               borderRadius: 99999,
             }}
             source={
               viewToken.token.currency.coinImageUrl
-                ? {
-                    uri: viewToken.token.currency.coinImageUrl,
-                  }
+                ? viewToken.token.currency.coinImageUrl
                 : require('../../public/assets/img/chain-icon-alt.png')
             }
-            resizeMode="contain"
+            contentFit="contain"
           />
 
           <Gutter size={12} />
