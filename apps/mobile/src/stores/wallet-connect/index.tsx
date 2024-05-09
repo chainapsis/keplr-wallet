863eb6af6ed1751f0ef3d9948a74f627a4104e98
HeesungB <hezo25@naver.com>
2024-05-09 17:16:50 +0900

    Add suggest token/chain on wallet connect

--- apps/mobile/src/stores/wallet-connect/index.tsx
+++ apps/mobile/src/stores/wallet-connect/index.tsx
@@ -106,7 +106,8 @@ export class WalletConnectStore {
   }
 
   protected async init(): Promise<void> {
-    const projectId = process.env['WC_PROJECT_ID'];
+    const projectId =
+      process.env['WC_PROJECT_ID'] || 'a9fd98e67c49a9930d59cbb301be4583';
     if (!projectId) {
       return;
     }
@@ -814,6 +815,30 @@ export class WalletConnectStore {
           });
           break;
         }
+        case 'keplr_experimentalSuggestChain': {
+          await keplr.experimentalSuggestChain(params.chainInfo);
+          await signClient.respond({
+            topic,
+            response: {
+              id,
+              jsonrpc: '2.0',
+              result: {},
+            },
+          });
+          break;
+        }
+        case 'keplr_suggestToken': {
+          await keplr.suggestToken(params.chainId, params.contractAddress);
+          await signClient.respond({
+            topic,
+            response: {
+              id,
+              jsonrpc: '2.0',
+              result: {},
+            },
+          });
+          break;
+        }
         default:
           throw new Error('Unknown request method');
       }
@@ -960,9 +985,7 @@ export class WalletConnectStore {
     await this.kvStore.set(`topic-to-from-deep-link:${ackTopic}`, fromDeepLink);
   }
 
-  protected async getTopicByRandomId(
-    randomId: string,
-  ): Promise<string | undefined> {
+  async getTopicByRandomId(randomId: string): Promise<string | undefined> {
     return await this.kvStore.get(`id-to-topic:${randomId}`);
   }
 
--- packages/wc-client-example/src/app.tsx
+++ packages/wc-client-example/src/app.tsx
@@ -127,6 +127,91 @@ export const App: FunctionComponent = observer(() => {
       >
         Sign Ethereum
       </button>
+
+      <button
+        onClick={() => {
+          accountStore
+            .getAccount(chainStore.chainInfos[0].chainId)
+            .getKeplr()
+            .then((keplr) => {
+              keplr?.experimentalSuggestChain({
+                rpc: "https://rpc.testnet.osmosis.zone",
+                rest: "https://lcd.testnet.osmosis.zone",
+                chainId: "osmo-test-5",
+                chainName: "Osmosis Testnet",
+                chainSymbolImageUrl:
+                  "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/osmosis/chain.png",
+                bip44: {
+                  coinType: 118,
+                },
+                bech32Config: {
+                  bech32PrefixAccAddr: "osmo",
+                  bech32PrefixAccPub: "osmopub",
+                  bech32PrefixValAddr: "osmovaloper",
+                  bech32PrefixValPub: "osmovaloperpub",
+                  bech32PrefixConsAddr: "osmovalcons",
+                  bech32PrefixConsPub: "osmovalconspub",
+                },
+                stakeCurrency: {
+                  coinDenom: "OSMO",
+                  coinMinimalDenom: "uosmo",
+                  coinDecimals: 6,
+                  coinImageUrl:
+                    "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/osmosis/uosmo.png",
+                },
+                currencies: [
+                  {
+                    coinDenom: "OSMO",
+                    coinMinimalDenom: "uosmo",
+                    coinDecimals: 6,
+                    coinImageUrl:
+                      "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/osmosis/uosmo.png",
+                  },
+                  {
+                    coinDenom: "ION",
+                    coinMinimalDenom: "uion",
+                    coinDecimals: 6,
+                    coinImageUrl:
+                      "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/osmosis/uion.png",
+                  },
+                ],
+                feeCurrencies: [
+                  {
+                    coinDenom: "OSMO",
+                    coinMinimalDenom: "uosmo",
+                    coinDecimals: 6,
+                    coinImageUrl:
+                      "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/osmosis/uosmo.png",
+                    gasPriceStep: {
+                      low: 0.0025,
+                      average: 0.025,
+                      high: 0.04,
+                    },
+                  },
+                ],
+                features: [],
+              });
+            });
+        }}
+      >
+        Suggest Chain
+      </button>
+
+      <button
+        onClick={() => {
+          accountStore
+            .getAccount(chainStore.chainInfos[0].chainId)
+            .getKeplr()
+            .then((keplr) => {
+              keplr?.suggestToken(
+                "juno-1",
+                "juno10vgf2u03ufcf25tspgn05l7j3tfg0j63ljgpffy98t697m5r5hmqaw95ux"
+              );
+            });
+        }}
+      >
+        Suggest Token
+      </button>
     </div>
   );
 });
