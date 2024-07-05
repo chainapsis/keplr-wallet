31d9792aab3130c37917b3dccfabed7d5ac801d6
delivan <wjdgurdl272@gmail.com>
2024-07-05 18:58:33 +0900

    Fix keystone signing issue for evm only chain

--- apps/extension/src/pages/sign/utils/handle-eth-sign.ts
+++ apps/extension/src/pages/sign/utils/handle-eth-sign.ts
@@ -100,6 +100,22 @@ export const handleEthereumPreSignByKeystone = async (
     interactionData.data.message,
     interactionData.data.signType
   ).toString("hex");
+  const evmChainId = (() => {
+    try {
+      return EthermintChainIdHelper.parse(interactionData.data.chainId)
+        .ethChainId;
+    } catch (e) {
+      const chainIdLikeCAIP2 = interactionData.data.chainId.split(":");
+      const evmChainId = parseInt(chainIdLikeCAIP2[1]);
+      const isEVMOnlyChain =
+        chainIdLikeCAIP2.length === 2 && chainIdLikeCAIP2[0] === "eip155";
+      if (isEVMOnlyChain && !isNaN(evmChainId)) {
+        return evmChainId;
+      }
+
+      throw e;
+    }
+  })();
   const ur = keystoneSDK.eth.generateSignRequest({
     requestId,
     signData,
@@ -109,8 +125,7 @@ export const handleEthereumPreSignByKeystone = async (
     ),
     path,
     xfp: interactionData.data.keyInsensitive["xfp"] as string,
-    chainId: EthermintChainIdHelper.parse(interactionData.data.chainId)
-      .ethChainId,
+    chainId: evmChainId,
     address,
   });
   await options.displayQRCode({
