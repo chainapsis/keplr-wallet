da63e4971ddf7ad60adef317ca59906f204f8508
Thunnini <yunjh1994@gmail.com>
2024-07-22 19:20:47 +0900

    Minor fix

--- packages/background/src/side-panel/service.ts
+++ packages/background/src/side-panel/service.ts
@@ -1,5 +1,5 @@
 import { action, autorun, makeObservable, observable, runInAction } from "mobx";
-import { KVStore } from "@keplr-wallet/common";
+import { isServiceWorker, KVStore } from "@keplr-wallet/common";
 
 export class SidePanelService {
   // TODO: 기본값을 false로 수정. 일단은 테스트를 위해서 기본값을 true로 설정.
@@ -45,6 +45,11 @@ export class SidePanelService {
   }
 
   isSidePanelSupported(): boolean {
+    // manifest v3가 아니면 side panel은 작동하지 않음
+    if (!isServiceWorker()) {
+      return false;
+    }
+
     // TODO: 웹브라우저에 대한 체크 추가. (chrome.sidePanel이 존재해도 웹브라우저에 따라서 안되는 경우가 있는 듯 하다)
     return (
       typeof chrome !== "undefined" && typeof chrome.sidePanel !== "undefined"
