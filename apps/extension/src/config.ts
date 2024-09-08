a26173d3d2231ba2448d8ed37376f621ffc84216
Thunnini <yunjh1994@gmail.com>
2024-09-09 00:51:25 +0900

    스타크넷 메인넷 추가

--- apps/extension/src/config.ts
+++ apps/extension/src/config.ts
@@ -2446,6 +2446,38 @@ export const EmbedChainInfos: (ChainInfo | ModularChainInfo)[] = [
         "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
     },
   },
+  {
+    chainId: "starknet:SN_MAIN",
+    chainName: "Starknet",
+    starknet: {
+      chainId: "starknet:SN_MAIN",
+      rpc: "https://starknet-mainnet.public.blastapi.io/rpc/v0_7",
+      currencies: [
+        {
+          type: "erc20",
+          contractAddress:
+            "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+          coinDenom: "ETH",
+          coinMinimalDenom:
+            "erc20:0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+          coinDecimals: 18,
+        },
+        {
+          type: "erc20",
+          contractAddress:
+            "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+          coinDenom: "STRK",
+          coinMinimalDenom:
+            "erc20:0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+          coinDecimals: 18,
+        },
+      ],
+      ethContractAddress:
+        "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+      strkContractAddress:
+        "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+    },
+  },
 ];
 
 // The origins that are able to pass any permission that external webpages can have.
