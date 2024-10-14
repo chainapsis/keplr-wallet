2d45b9a2f0f4bb1a17999837abda161d7ec86e40
Thunnini <yunjh1994@gmail.com>
2024-10-14 20:20:40 +0900

    스타크넷에 토큰 몇개 추가

--- apps/extension/src/config.ts
+++ apps/extension/src/config.ts
@@ -2485,6 +2485,61 @@ export const EmbedChainInfos: (ChainInfo | ModularChainInfo)[] = [
           coinImageUrl:
             "https://keplr-ext-update-note-images.s3.amazonaws.com/token/starknet.png",
         },
+        {
+          type: "erc20",
+          contractAddress:
+            "0x3fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac",
+          coinDenom: "WBTC",
+          coinMinimalDenom:
+            "erc20:0x3fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac",
+          coinDecimals: 8,
+          coinImageUrl:
+            "https://keplr-ext-update-note-images.s3.amazonaws.com/token/wbtc.png",
+        },
+        {
+          type: "erc20",
+          contractAddress:
+            "0x42b8f0484674ca266ac5d08e4ac6a3fe65bd3129795def2dca5c34ecc5f96d2",
+          coinDenom: "wstETH",
+          coinMinimalDenom:
+            "erc20:0x42b8f0484674ca266ac5d08e4ac6a3fe65bd3129795def2dca5c34ecc5f96d2",
+          coinDecimals: 18,
+          coinImageUrl:
+            "https://keplr-ext-update-note-images.s3.amazonaws.com/token/wstETH.png",
+        },
+        {
+          type: "erc20",
+          contractAddress:
+            "0x68f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8",
+          coinDenom: "USDT",
+          coinMinimalDenom:
+            "erc20:0x68f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8",
+          coinDecimals: 6,
+          coinImageUrl:
+            "https://keplr-ext-update-note-images.s3.amazonaws.com/token/usdt.png",
+        },
+        {
+          type: "erc20",
+          contractAddress:
+            "0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
+          coinDenom: "USDC",
+          coinMinimalDenom:
+            "erc20:0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
+          coinDecimals: 6,
+          coinImageUrl:
+            "https://keplr-ext-update-note-images.s3.amazonaws.com/token/usdc.png",
+        },
+        {
+          type: "erc20",
+          contractAddress:
+            "0x5574eb6b8789a91466f902c380d978e472db68170ff82a5b650b95a58ddf4ad",
+          coinDenom: "DAI",
+          coinMinimalDenom:
+            "erc20:0x5574eb6b8789a91466f902c380d978e472db68170ff82a5b650b95a58ddf4ad",
+          coinDecimals: 18,
+          coinImageUrl:
+            "https://keplr-ext-update-note-images.s3.amazonaws.com/token/dai.png",
+        },
       ],
       ethContractAddress:
         "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
