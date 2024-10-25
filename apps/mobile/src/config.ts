bc763165c39a72e33ccf0ee96d1d56b6a95e4a05
Thunnini <yunjh1994@gmail.com>
2024-10-25 12:46:32 +0400

    Add nolus

--- apps/mobile/src/config.ts
+++ apps/mobile/src/config.ts
@@ -2194,6 +2194,59 @@ export const EmbedChainInfos: ChainInfo[] = [
     },
     features: ['cosmwasm'],
   },
+  {
+    rpc: 'https://rpc-pirin.keplr.app',
+    rest: 'https://lcd-pirin.keplr.app',
+    chainId: 'pirin-1',
+    chainName: 'Nolus',
+    chainSymbolImageUrl:
+      'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/pirin/nolus.png',
+    stakeCurrency: {
+      coinDenom: 'NLS',
+      coinMinimalDenom: 'unls',
+      coinDecimals: 6,
+      coinGeckoId: 'nolus',
+      coinImageUrl:
+        'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/pirin/nolus.png',
+    },
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: {
+      bech32PrefixAccAddr: 'nolus',
+      bech32PrefixAccPub: 'noluspub',
+      bech32PrefixValAddr: 'nolusvaloper',
+      bech32PrefixValPub: 'nolusvaloperpub',
+      bech32PrefixConsAddr: 'nolusvalcons',
+      bech32PrefixConsPub: 'nolusvalconspub',
+    },
+    currencies: [
+      {
+        coinDenom: 'NLS',
+        coinMinimalDenom: 'unls',
+        coinDecimals: 6,
+        coinGeckoId: 'nolus',
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/pirin/nolus.png',
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: 'NLS',
+        coinMinimalDenom: 'unls',
+        coinDecimals: 6,
+        coinGeckoId: 'nolus',
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/pirin/nolus.png',
+        gasPriceStep: {
+          low: 0.01,
+          average: 0.025,
+          high: 0.05,
+        },
+      },
+    ],
+    features: ['cosmwasm'],
+  },
 ];
 
 // TODO: 원격으로 변경할 수 있도록 나중에 네트워크에서 이 정보를 가져오는 방식으로 변경될 수 있음
@@ -2336,6 +2389,10 @@ export const ChainIdentifierToTxExplorerMap: Record<
   dimension_37: {
     name: 'Mintscan',
     txUrl: 'https://www.mintscan.io/xpla/tx/{txHash}',
+  },
+  pirin: {
+    name: 'Nolus Explorer',
+    txUrl: 'https://explorer.nolus.io/pirin/tx/{txHash}',
   },
 };
 
