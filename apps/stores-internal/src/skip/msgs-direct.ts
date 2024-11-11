4a47d4c1d40fcb78bd3fbe17ba935df8d62555ba
Thunnini <yunjh1994@gmail.com>
2024-11-11 16:33:11 +0900

    skip API swap에서 split_routes를 사용하도록 수정 & swap venue에 injective-hallswap 추가 & ibc swap history 개선

--- apps/stores-internal/src/skip/msgs-direct.ts
+++ apps/stores-internal/src/skip/msgs-direct.ts
@@ -209,6 +209,7 @@ export class ObservableQueryMsgsDirectInner extends ObservableQuery<MsgsDirectRe
                 ]
               : [],
           swap_venues: this.swapVenues,
+          smart_swap_options: { split_routes: true },
           allow_unsafe: true,
         }),
         signal: abortController.signal,
