80af452e3e8ff1128bd0a2befd82d2d9e391d77e
delivan <wjdgurdl272@gmail.com>
2024-12-12 22:33:21 +0900

    Add react-window `FixedSizeList` to improve rendering performance

--- apps/stores-internal/src/skip/assets.ts
+++ apps/stores-internal/src/skip/assets.ts
@@ -113,7 +113,11 @@ export class ObservableQueryAssetsInner extends ObservableQuery<AssetsResponse> 
               asset.is_evm && asset.token_contract != null
                 ? `erc20:${asset.origin_denom}`
                 : asset.denom;
-            if (chainInfo.findCurrencyWithoutReaction(coinMinimalDenom)) {
+            const currencyFound =
+              chainInfo.findCurrencyWithoutReaction(coinMinimalDenom);
+            // decimals이 18 이하인 경우만을 고려해서 짜여진 코드가 많아서 임시로 18 이하인 경우만 고려한다.
+            // TODO: Dec, Int 같은 곳에서 18 이상인 경우도 고려하도록 수정
+            if (currencyFound && currencyFound.coinDecimals <= 18) {
               res.push({
                 denom: coinMinimalDenom,
                 chainId: chainId,
