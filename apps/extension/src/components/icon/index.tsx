2d0483a9dcd98973b13cb84e918298c849e1793f
blacktoast <dbstjd100@naver.com>
2025-02-07 17:48:04 +0900

    [feat] add message-icon

--- apps/extension/src/components/icon/index.tsx
+++ apps/extension/src/components/icon/index.tsx
@@ -57,4 +57,5 @@ export * from "./msg-adr36";
 export * from "./msg-receive";
 export * from "./msg-swap";
 export * from "./msg-cancel-undelegate";
+export * from "./message-icon";
 export * from "./folder-minus";
