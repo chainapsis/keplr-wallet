4638666a0e96dc59f6f46ccf5d340cbf885b064d
Rita <editaahn@yahoo.com>
2025-02-10 20:50:05 +0900

    Implement EarnOverviewTutorialModal with carousel and navigation

---
+++ apps/extension/src/pages/earn/overview/tutorial-modal/index.tsx
@@ -0,0 +1,96 @@
+import { FunctionComponent, useState } from "react";
+import React from "react";
+import { ColorPalette } from "../../../../styles";
+import { Box } from "../../../../components/box";
+import { ManageEarnContent } from "./manage-earn-content";
+import { EarnClaimContent } from "./earn-claim-content";
+import { AppCurrency } from "@keplr-wallet/types";
+import { useTheme } from "styled-components";
+import { XAxis, YAxis } from "../../../../components/axis";
+import { CancelIcon } from "../../../../components/button";
+
+export const EarnOverviewTutorialModal: FunctionComponent<{
+  holdingCurrency: AppCurrency;
+  rewardDenom: string;
+  onClose: () => void;
+}> = ({ holdingCurrency, rewardDenom, onClose }) => {
+  const theme = useTheme();
+
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
+  const handleNext = () => {
+    setCarouselIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handlePrev = () => {
+    setCarouselIndex((prevIndex) => prevIndex - 1);
+  };
+
+  const Contents = [
+    <ManageEarnContent
+      denom={holdingCurrency.coinDenom}
+      coinImageUrl={holdingCurrency.coinImageUrl ?? ""}
+      key="manage-earn"
+      onNext={handleNext}
+    />,
+    <EarnClaimContent
+      tokenName={rewardDenom}
+      moveToNext={onClose}
+      moveToPrev={handlePrev}
+      key="earn-claim"
+    />,
+  ];
+
+  return (
+    <YAxis alignX="center">
+      <Box
+        borderRadius="1.25rem"
+        backgroundColor={
+          theme.mode === "light" ? ColorPalette.white : ColorPalette["gray-600"]
+        }
+        padding="1.25rem"
+        width="95%"
+        maxWidth="18.75rem"
+      >
+        <Box style={{ marginLeft: "auto" }}>
+          <CancelIcon
+            width="1.25rem"
+            height="1.25rem"
+            color={
+              theme.mode === "light"
+                ? ColorPalette["gray-200"]
+                : ColorPalette["gray-300"]
+            }
+          />
+        </Box>
+        <YAxis alignX="center">
+          <XAxis>
+            {Contents.map((_, index) => (
+              <DotIndicator
+                key={index}
+                isActive={index === carouselIndex}
+                isLast={index === Contents.length - 1}
+              />
+            ))}
+          </XAxis>
+        </YAxis>
+        <Box>{Contents[carouselIndex]}</Box>
+      </Box>
+    </YAxis>
+  );
+};
+
+const DotIndicator: FunctionComponent<{
+  isActive: boolean;
+  isLast: boolean;
+}> = ({ isActive, isLast }) => (
+  <Box
+    width="5px"
+    height="5px"
+    borderRadius="50%"
+    backgroundColor={
+      isActive ? ColorPalette["gray-100"] : ColorPalette["gray-400"]
+    }
+    marginRight={isLast ? "0" : "0.5rem"}
+  />
+);
