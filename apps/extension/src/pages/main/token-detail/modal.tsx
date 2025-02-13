bdb51fe5eb96ba89ea7f8d632983d5ebf04ab5fb
Rita <editaahn@yahoo.com>
2025-02-13 14:52:20 +0900

    Add Manage Earn button for USDN token detail

--- apps/extension/src/pages/main/token-detail/modal.tsx
+++ apps/extension/src/pages/main/token-detail/modal.tsx
@@ -31,6 +31,9 @@ import { DenomHelper } from "@keplr-wallet/common";
 import { ChainIdHelper } from "@keplr-wallet/cosmos";
 import { EarnApyBanner } from "./banners/earn-apy-banner";
 import { validateIsUsdcFromNoble } from "../../earn/utils";
+import { Button } from "../../../components/button";
+import { FormattedMessage } from "react-intl";
+import { IBCCurrency } from "@keplr-wallet/types";
 
 const Styles = {
   Container: styled.div`
@@ -477,6 +480,24 @@ export const TokenDetailModal: FunctionComponent<{
               )}
             </Subtitle3>
           </YAxis>
+
+          {currency.coinMinimalDenom === "uusdn" &&
+          (chainId === "noble-1" ||
+            (currency as IBCCurrency).originChainId === "noble-1") ? (
+            <Box padding="1.25rem 0.75rem 1.25rem 0.75rem">
+              <Button
+                text={
+                  <FormattedMessage id="page.token-detail.manage-earn-button" />
+                }
+                color="secondary"
+                size="medium"
+                onClick={() => {
+                  navigate(`/earn/overview?chainId=${chainId}`);
+                }}
+              />
+            </Box>
+          ) : null}
+
           <Gutter size="1.25rem" />
           <YAxis alignX="center">
             <XAxis>
