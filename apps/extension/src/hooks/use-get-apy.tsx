70eca83f602c746365b6818bae28bc148b0e0922
Rita <editaahn@yahoo.com>
2025-02-19 13:13:20 +0900

    Fix useGetEarnApy hook with MobX autorun

--- apps/extension/src/hooks/use-get-apy.tsx
+++ apps/extension/src/hooks/use-get-apy.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from "react";
 import { useStore } from "../stores";
 import { Dec } from "@keplr-wallet/unit";
+import { autorun } from "mobx";
 
 export function useGetEarnApy(chainId: string) {
   const { queriesStore, chainStore } = useStore();
+  const [apy, setApy] = useState<string>("-%");
 
   const queryAPY = queriesStore.simpleQuery.queryGet<{
     earnApy: number;
@@ -11,17 +14,25 @@ export function useGetEarnApy(chainId: string) {
     `/earn-apy/${chainStore.getChain(chainId).chainIdentifier}`
   );
 
-  const aprAvailable =
-    queryAPY.response &&
-    "earnApy" in queryAPY.response.data &&
-    typeof queryAPY.response.data.earnApy === "number" &&
-    queryAPY.response.data.earnApy > 0;
+  useEffect(() => {
+    const disposer = autorun(() => {
+      const apyAvailable =
+        queryAPY.response &&
+        "earnApy" in queryAPY.response.data &&
+        typeof queryAPY.response.data.earnApy === "number" &&
+        queryAPY.response.data.earnApy > 0;
 
-  const apy = aprAvailable
-    ? `${new Dec(queryAPY.response.data.earnApy ?? "0")
-        .mul(new Dec(100))
-        .toString(2)}%`
-    : "-%";
+      if (apyAvailable) {
+        setApy(
+          `${new Dec(queryAPY.response.data.earnApy ?? "0")
+            .mul(new Dec(100))
+            .toString(2)}%`
+        );
+      }
+    });
+
+    return () => disposer();
+  }, [queryAPY.response]);
 
   return { apy };
 }
