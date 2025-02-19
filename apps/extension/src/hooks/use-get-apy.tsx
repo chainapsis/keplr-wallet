547b85e851e059efa850ec2550648c7ff23aad20
Rita <editaahn@yahoo.com>
2025-02-19 22:49:25 +0900

    Revert "Fix useGetEarnApy hook with MobX autorun"
    This reverts commit 70eca83f602c746365b6818bae28bc148b0e0922.

--- apps/extension/src/hooks/use-get-apy.tsx
+++ apps/extension/src/hooks/use-get-apy.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from "react";
 import { useStore } from "../stores";
 import { Dec } from "@keplr-wallet/unit";
-import { autorun } from "mobx";
 
 export function useGetEarnApy(chainId: string) {
   const { queriesStore, chainStore } = useStore();
-  const [apy, setApy] = useState<string>("-%");
 
   const queryAPY = queriesStore.simpleQuery.queryGet<{
     earnApy: number;
@@ -14,25 +11,17 @@ export function useGetEarnApy(chainId: string) {
     `/earn-apy/${chainStore.getChain(chainId).chainIdentifier}`
   );
 
-  useEffect(() => {
-    const disposer = autorun(() => {
-      const apyAvailable =
-        queryAPY.response &&
-        "earnApy" in queryAPY.response.data &&
-        typeof queryAPY.response.data.earnApy === "number" &&
-        queryAPY.response.data.earnApy > 0;
+  const aprAvailable =
+    queryAPY.response &&
+    "earnApy" in queryAPY.response.data &&
+    typeof queryAPY.response.data.earnApy === "number" &&
+    queryAPY.response.data.earnApy > 0;
 
-      if (apyAvailable) {
-        setApy(
-          `${new Dec(queryAPY.response.data.earnApy ?? "0")
-            .mul(new Dec(100))
-            .toString(2)}%`
-        );
-      }
-    });
-
-    return () => disposer();
-  }, [queryAPY.response]);
+  const apy = aprAvailable
+    ? `${new Dec(queryAPY.response.data.earnApy ?? "0")
+        .mul(new Dec(100))
+        .toString(2)}%`
+    : "-%";
 
   return { apy };
 }
