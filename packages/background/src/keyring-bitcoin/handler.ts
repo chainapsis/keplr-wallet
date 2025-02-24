0919c7e597908f3e71d5a56607a5ff16d9622adf
rowan <crewe1746@naver.com>
2025-02-24 11:10:04 +0900

    Add stub handlers for messages related to `keyring-bitcoin`

--- packages/background/src/keyring-bitcoin/handler.ts
+++ packages/background/src/keyring-bitcoin/handler.ts
@@ -8,7 +8,14 @@ import {
 import { Env } from "@keplr-wallet/router";
 import { KeyRingBitcoinService } from "./service";
 import { PermissionInteractiveService } from "src/permission-interactive";
-import { RequestSignBitcoinMessageMsg } from "./messages";
+import {
+  GetBitcoinKeyMsg,
+  GetBitcoinKeysForEachVaultSettledMsg,
+  GetBitcoinKeysSettledMsg,
+  RequestSignBitcoinMessageMsg,
+  RequestSignBitcoinPsbtMsg,
+  RequestSignBitcoinPsbtsMsg,
+} from "./messages";
 
 export const getHandler: (
   service: KeyRingBitcoinService,
@@ -19,8 +26,34 @@ export const getHandler: (
 ) => {
   return (env: Env, msg: Message<unknown>) => {
     switch (msg.constructor) {
+      case GetBitcoinKeyMsg:
+        return handleGetBitcoinKeyMsg(_service, _permissionInteractionService)(
+          env,
+          msg as GetBitcoinKeyMsg
+        );
+      case GetBitcoinKeysSettledMsg:
+        return handleGetBitcoinKeysSettledMsg(
+          _service,
+          _permissionInteractionService
+        )(env, msg as GetBitcoinKeysSettledMsg);
+      case GetBitcoinKeysForEachVaultSettledMsg:
+        return handleGetBitcoinKeysForEachVaultSettledMsg(
+          _service,
+          _permissionInteractionService
+        )(env, msg as GetBitcoinKeysForEachVaultSettledMsg);
+      case RequestSignBitcoinPsbtMsg:
+        return handleRequestSignBitcoinPsbtMsg(
+          _service,
+          _permissionInteractionService
+        )(env, msg as RequestSignBitcoinPsbtMsg);
+      case RequestSignBitcoinPsbtsMsg:
+        return handleRequestSignBitcoinPsbtsMsg(
+          _service,
+          _permissionInteractionService
+        )(env, msg as RequestSignBitcoinPsbtsMsg);
+
       case RequestSignBitcoinMessageMsg:
-        return handleRequestSignBitcoinMessage(
+        return handleRequestSignBitcoinMessageMsg(
           _service,
           _permissionInteractionService
         )(env, msg as RequestSignBitcoinMessageMsg);
@@ -30,7 +63,67 @@ export const getHandler: (
   };
 };
 
-const handleRequestSignBitcoinMessage: (
+const handleGetBitcoinKeyMsg: (
+  service: KeyRingBitcoinService,
+  permissionInteractionService: PermissionInteractiveService
+) => InternalHandler<GetBitcoinKeyMsg> = (
+  _service,
+  _permissionInteractionService
+) => {
+  return async (_env, _msg) => {
+    throw new KeplrError("keyring", 221, "Not implemented");
+  };
+};
+
+const handleGetBitcoinKeysSettledMsg: (
+  service: KeyRingBitcoinService,
+  permissionInteractionService: PermissionInteractiveService
+) => InternalHandler<GetBitcoinKeysSettledMsg> = (
+  _service,
+  _permissionInteractionService
+) => {
+  return async (_env, _msg) => {
+    throw new KeplrError("keyring", 221, "Not implemented");
+  };
+};
+
+const handleGetBitcoinKeysForEachVaultSettledMsg: (
+  service: KeyRingBitcoinService,
+  permissionInteractionService: PermissionInteractiveService
+) => InternalHandler<GetBitcoinKeysForEachVaultSettledMsg> = (
+  _service,
+  _permissionInteractionService
+) => {
+  return async (_env, _msg) => {
+    throw new KeplrError("keyring", 221, "Not implemented");
+  };
+};
+
+const handleRequestSignBitcoinPsbtMsg: (
+  service: KeyRingBitcoinService,
+  permissionInteractionService: PermissionInteractiveService
+) => InternalHandler<RequestSignBitcoinPsbtMsg> = (
+  _service,
+  _permissionInteractionService
+) => {
+  return async (_env, _msg) => {
+    throw new KeplrError("keyring", 221, "Not implemented");
+  };
+};
+
+const handleRequestSignBitcoinPsbtsMsg: (
+  service: KeyRingBitcoinService,
+  permissionInteractionService: PermissionInteractiveService
+) => InternalHandler<RequestSignBitcoinPsbtsMsg> = (
+  _service,
+  _permissionInteractionService
+) => {
+  return async (_env, _msg) => {
+    throw new KeplrError("keyring", 221, "Not implemented");
+  };
+};
+
+const handleRequestSignBitcoinMessageMsg: (
   service: KeyRingBitcoinService,
   permissionInteractionService: PermissionInteractiveService
 ) => InternalHandler<RequestSignBitcoinMessageMsg> = (
