331600fdaf56c81134dab343170014cd33defcab
Calvin Lee <jinwoo0451@naver.com>
2025-02-25 15:37:50 +0900

    Change makeWithdrawRewardsTx with makeClaimYieldTx

--- packages/stores/src/account/noble.ts
+++ packages/stores/src/account/noble.ts
@@ -5,10 +5,8 @@ import { Currency } from "@keplr-wallet/types";
 import { DeepReadonly } from "utility-types";
 import { CosmosAccount } from "./cosmos";
 import { Dec, DecUtils } from "@keplr-wallet/unit";
-import {
-  MsgSwap,
-  MsgWithdrawRewards,
-} from "@keplr-wallet/proto-types/noble/swap/v1/tx";
+import { MsgSwap } from "@keplr-wallet/proto-types/noble/swap/v1/tx";
+import { MsgClaimYield } from "@keplr-wallet/proto-types/noble/dollar/v1/tx";
 
 export interface NobleAccount {
   noble: NobleAccountImpl;
@@ -121,9 +119,9 @@ export class NobleAccountImpl {
     );
   }
 
-  makeWithdrawRewardsTx(
+  makeClaimYieldTx(
     // This arg can be used to override the type of sending tx if needed.
-    type: string = "noble-withdraw-rewards",
+    type: string = "noble-claim-yield",
     preOnTxEvents?:
       | ((tx: any) => void)
       | {
@@ -133,7 +131,7 @@ export class NobleAccountImpl {
   ) {
     const msg = {
       // TODO: 이거 뭔지 확인해야함. (noble에서 처음에 실수해서 아직 뭐가 맞는지 정확히 모름)
-      type: "swap/WithdrawRewards",
+      type: "dollar/ClaimYield",
       value: {
         signer: this.base.bech32Address,
       },
@@ -146,9 +144,9 @@ export class NobleAccountImpl {
         // aminoMsgs: [msg],
         protoMsgs: [
           {
-            typeUrl: "/noble.swap.v1.MsgWithdrawRewards",
-            value: MsgWithdrawRewards.encode(
-              MsgWithdrawRewards.fromPartial({
+            typeUrl: "/noble.dollar.v1.MsgClaimYield",
+            value: MsgClaimYield.encode(
+              MsgClaimYield.fromPartial({
                 signer: msg.value.signer,
               })
             ).finish(),
