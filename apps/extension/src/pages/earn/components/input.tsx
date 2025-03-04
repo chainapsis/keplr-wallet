9a925968384db8813d6b940b2f50e58ea7a210ac
Thunnini <yunjh1994@gmail.com>
2025-03-04 21:37:20 +0900

    `Input에서` suffix 쪽 구현 개선

--- apps/extension/src/pages/earn/components/input.tsx
+++ apps/extension/src/pages/earn/components/input.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { ColorPalette } from "../../../styles";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
 import Color from "color";
 
 export const Input: FunctionComponent<
@@ -9,26 +9,137 @@ export const Input: FunctionComponent<
     suffix?: string;
   }
 > = ({ warning, suffix, value, ...props }) => {
+  if (!value) {
+    suffix = undefined;
+  }
+  if (suffix) {
+    suffix = " " + suffix;
+  }
+
+  const widthCheckRef = useRef<HTMLSpanElement | null>(null);
+  const [textWidth, setTextWidth] = useState<number | null>(null);
+  useEffect(() => {
+    if (widthCheckRef.current) {
+      const resizeObserver = new ResizeObserver((entries) => {
+        if (entries.length > 0) {
+          const entry = entries[0];
+          const boxSize = Array.isArray(entry.borderBoxSize)
+            ? entry.borderBoxSize[0]
+            : entry.borderBoxSize;
+
+          const width = boxSize.inlineSize;
+          setTextWidth(width);
+        }
+      });
+      resizeObserver.observe(widthCheckRef.current);
+      return () => resizeObserver.disconnect();
+    }
+  }, []);
+  const inputWidthCheckRef = useRef<HTMLInputElement | null>(null);
+  const [textInputWidth, setTextInputWidth] = useState<number | null>(null);
+  useEffect(() => {
+    if (inputWidthCheckRef.current) {
+      const resizeObserver = new ResizeObserver((entries) => {
+        if (entries.length > 0) {
+          const entry = entries[0];
+          const boxSize = Array.isArray(entry.contentBoxSize)
+            ? entry.contentBoxSize[0]
+            : entry.contentBoxSize;
+
+          const width = boxSize.inlineSize;
+          setTextInputWidth(width);
+        }
+      });
+      resizeObserver.observe(inputWidthCheckRef.current);
+      return () => resizeObserver.disconnect();
+    }
+  }, []);
+  const suffixWidthCheckRef = useRef<HTMLDivElement | null>(null);
+  const [suffixTextWidth, setSuffixTextWidth] = useState<number | null>(null);
+  useEffect(() => {
+    if (suffixWidthCheckRef.current) {
+      const resizeObserver = new ResizeObserver((entries) => {
+        if (entries.length > 0) {
+          const entry = entries[0];
+          const boxSize = Array.isArray(entry.borderBoxSize)
+            ? entry.borderBoxSize[0]
+            : entry.borderBoxSize;
+
+          const width = boxSize.inlineSize;
+          setSuffixTextWidth(width);
+        }
+      });
+      resizeObserver.observe(suffixWidthCheckRef.current);
+      return () => resizeObserver.disconnect();
+    }
+  }, []);
+
   return (
     <InputContainer>
-      <StyledInput warning={warning} value={value ?? ""} {...props} />
-      {suffix && value && <Suffix isWarning={!!warning}>{suffix}</Suffix>}
+      <div
+        style={{
+          position: "relative",
+          opacity: 0,
+          visibility: "hidden",
+        }}
+      >
+        <TextInputWidthChecker ref={widthCheckRef}>
+          {value}
+        </TextInputWidthChecker>
+      </div>
+      <div
+        style={{
+          position: "relative",
+          display: "flex",
+        }}
+      >
+        <StyledInput ref={inputWidthCheckRef} warning={warning} {...props} />
+        <div
+          style={(() => {
+            if (suffixTextWidth == null || !suffix) {
+              return {
+                width: 0,
+                opacity: 0,
+              };
+            }
+            return {
+              width: suffixTextWidth + "px",
+              // 옆의 마진이 왜 필요한지 모른다...
+              // 어쨋든 실행시켜서 살펴보면 이 요소는 의도보다 오른쪽으로 치우져진다
+              // 사실 24px도 정확한 값은 아닌데... 원인을 못 찾았으므로 일단 대강 처리
+              marginRight: "24px",
+            };
+          })()}
+        />
+        <Suffix
+          ref={suffixWidthCheckRef}
+          isWarning={!!warning}
+          textWidth={(() => {
+            if (textWidth != null && textInputWidth != null) {
+              return Math.min(textWidth, textInputWidth);
+            }
+            if (textWidth != null) {
+              return textWidth;
+            }
+            if (textInputWidth != null) {
+              return textInputWidth;
+            }
+
+            return 0;
+          })()}
+        >
+          {suffix}
+        </Suffix>
+      </div>
     </InputContainer>
   );
 };
 
 const StyledInput = styled.input<{ warning?: boolean }>`
+  width: 100%;
   font-weight: 700;
   font-size: 1.75rem;
   line-height: 2.25rem;
-
-  width: ${(props) =>
-    props.value?.toString().length
-      ? `${
-          props.value.toString().length +
-          (props.value.toString().includes(".") ? 0.2 : 0.5)
-        }ch`
-      : "100%"};
 
   background: none;
 
@@ -61,7 +172,19 @@ const StyledInput = styled.input<{ warning?: boolean }>`
   }
 `;
 
+const TextInputWidthChecker = styled.span`
+  position: fixed;
+  visibility: hidden;
+  opacity: 0;
+  font-weight: 700;
+  font-size: 1.75rem;
+  line-height: 2.25rem;
+  white-space: pre;
+`;
+
 const InputContainer = styled.div`
+  position: relative;
+
   overflow: scroll;
   width: 100%;
 
@@ -80,7 +203,13 @@ const InputContainer = styled.div`
         : Color(ColorPalette["gray-300"]).alpha(0.5).toString()};
 `;
 
-const Suffix = styled.span<{ isWarning?: boolean }>`
+const Suffix = styled.span<{ isWarning?: boolean; textWidth?: number }>`
+  position: absolute;
+  white-space: pre;
+  top: 50%;
+  transform: translateY(-50%);
+  left: ${({ textWidth }) => (textWidth ? `${textWidth + 2}px` : 0)};
+
   font-weight: 700;
   font-size: 1.75rem;
   line-height: 2.25rem;
