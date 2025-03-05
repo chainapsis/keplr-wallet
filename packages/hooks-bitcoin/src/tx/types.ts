7dd2de0726644f28658d8a23d3bed652de0ce5fc
rowan <crewe1746@naver.com>
2025-03-06 08:34:16 +0900

    Add bitcoin `SenderConfig` and `useSenderConfig` hook

---
+++ packages/hooks-bitcoin/src/tx/types.ts
@@ -0,0 +1,23 @@
+export interface ITxChainSetter {
+  chainId: string;
+  setChain(chainId: string): void;
+}
+
+export interface UIProperties {
+  // There is an error that cannot proceed the tx.
+  readonly error?: Error;
+  // Able to handle tx but prefer to show warning
+  readonly warning?: Error;
+  // Prefer that the loading UI is displayed.
+  // In the case of "loading-block", the UI should handle it so that the user cannot proceed until loading is completed.
+  readonly loadingState?: "loading" | "loading-block";
+}
+
+export interface ISenderConfig extends ITxChainSetter {
+  value: string;
+  setValue(value: string): void;
+
+  sender: string;
+
+  uiProperties: UIProperties;
+}
