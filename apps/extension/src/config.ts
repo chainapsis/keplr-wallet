0914faf7198f6dfa47eaed61e9a1e88e21b2b659
Thunnini <yunjh1994@gmail.com>
2025-03-24 13:51:11 +0900

    Add nillion

--- apps/extension/src/config.ts
+++ apps/extension/src/config.ts
@@ -3172,6 +3172,58 @@ export const EmbedChainInfos: (ChainInfo | ModularChainInfo)[] = [
     ],
     features: [],
   },
+  {
+    rpc: "https://rpc-nillion.keplr.app",
+    rest: "https://lcd-nillion.keplr.app",
+    chainId: "nillion-1",
+    chainName: "Nillion",
+    chainSymbolImageUrl:
+      "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/nillion/nil.png",
+    stakeCurrency: {
+      coinDenom: "NIL",
+      coinMinimalDenom: "unil",
+      coinDecimals: 6,
+      coinGeckoId: "nillion",
+      coinImageUrl:
+        "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/nillion/nil.png",
+    },
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: {
+      bech32PrefixAccAddr: "nillion",
+      bech32PrefixAccPub: "nillionpub",
+      bech32PrefixValAddr: "nillionvaloper",
+      bech32PrefixValPub: "nillionvaloperpub",
+      bech32PrefixConsAddr: "nillionvalcons",
+      bech32PrefixConsPub: "nillionvalconspub",
+    },
+    currencies: [
+      {
+        coinDenom: "NIL",
+        coinMinimalDenom: "unil",
+        coinDecimals: 6,
+        coinGeckoId: "nillion",
+        coinImageUrl:
+          "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/nillion/nil.png",
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "NIL",
+        coinMinimalDenom: "unil",
+        coinDecimals: 6,
+        coinImageUrl:
+          "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/nillion/nil.png",
+        gasPriceStep: {
+          low: 0.001,
+          average: 0.001,
+          high: 0.01,
+        },
+      },
+    ],
+    features: [],
+  },
 ];
 
 // The origins that are able to pass any permission that external webpages can have.
