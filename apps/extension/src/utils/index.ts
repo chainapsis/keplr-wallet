5bc2145b0e920a5195a4501aa7f1190b7b9e28f4
blacktoast <dbstjd100@naver.com>
2025-04-11 17:30:18 +0900

    Revert "[refactor] add findFeeCurrency util function"
    This reverts commit 33b759debe4a2bdf9db833bbafca99bf0a0a6e1e.

--- apps/extension/src/utils/index.ts
+++ apps/extension/src/utils/index.ts
@@ -4,4 +4,3 @@ export * from "./amount-to-ambiguous-string";
 export * from "./side-panel";
 export * from "./set-interaction-data-href";
 export * from "./hex-to-rgba";
-export * from "./fine-fee-currency";
