50ed1eb1017e22e6f511cc8d2389a91e4708f627
kws <kws1207@kaist.ac.kr>
2025-04-28 11:56:37 +0900

    Move states from icon to icon button

--- apps/extension/src/pages/unlock/index.tsx
+++ apps/extension/src/pages/unlock/index.tsx
@@ -563,28 +563,6 @@ const VisibilityIconButton: FunctionComponent<{
   isVisible: boolean;
   onClick: () => void;
 }> = ({ isVisible, onClick }) => {
-  return (
-    <div
-      style={{
-        width: "1.75rem",
-        height: "1.75rem",
-        cursor: "pointer",
-      }}
-      onClick={onClick}
-    >
-      {isVisible ? (
-        <VisibleIcon width="1.75rem" height="1.75rem" />
-      ) : (
-        <InvisibleIcon width="1.75rem" height="1.75rem" />
-      )}
-    </div>
-  );
-};
-
-const InvisibleIcon: FunctionComponent<{
-  width: string;
-  height: string;
-}> = ({ width, height }) => {
   const theme = useTheme();
   const [isHover, setIsHover] = useState(false);
 
@@ -597,12 +575,42 @@ const InvisibleIcon: FunctionComponent<{
       ? ColorPalette["gray-400"]
       : ColorPalette["gray-300"];
 
+  const strokeColor =
+    theme.mode === "light" ? ColorPalette["white"] : ColorPalette["gray-700"];
+
   return (
     <div
-      style={{ width, height }}
+      style={{
+        width: "1.75rem",
+        height: "1.75rem",
+        cursor: "pointer",
+      }}
+      onClick={onClick}
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
     >
+      {isVisible ? (
+        <VisibleIcon width="1.75rem" height="1.75rem" color={fillColor} />
+      ) : (
+        <InvisibleIcon
+          width="1.75rem"
+          height="1.75rem"
+          fillColor={fillColor}
+          strokeColor={strokeColor}
+        />
+      )}
+    </div>
+  );
+};
+
+const InvisibleIcon: FunctionComponent<{
+  width: string;
+  height: string;
+  fillColor: string;
+  strokeColor: string;
+}> = ({ width, height, fillColor, strokeColor }) => {
+  return (
+    <div style={{ width, height }}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="28"
@@ -618,7 +626,7 @@ const InvisibleIcon: FunctionComponent<{
         />
         <path
           d="M7.0835 6L22.2502 21.1667"
-          stroke={theme.mode === "light" ? "#FEFEFE" : "#09090A"}
+          stroke={strokeColor}
           strokeWidth="3.5"
           strokeLinecap="round"
           strokeLinejoin="round"
@@ -638,25 +646,10 @@ const InvisibleIcon: FunctionComponent<{
 const VisibleIcon: FunctionComponent<{
   width: string;
   height: string;
-}> = ({ width, height }) => {
-  const theme = useTheme();
-  const [isHover, setIsHover] = useState(false);
-
-  const fillColor =
-    theme.mode === "light"
-      ? isHover
-        ? ColorPalette["gray-100"]
-        : ColorPalette["gray-200"]
-      : isHover
-      ? ColorPalette["gray-400"]
-      : ColorPalette["gray-300"];
-
+  color: string;
+}> = ({ width, height, color }) => {
   return (
-    <div
-      style={{ width, height }}
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
-    >
+    <div style={{ width, height }}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="28"
@@ -666,13 +659,13 @@ const VisibleIcon: FunctionComponent<{
       >
         <path
           d="M14.0835 10.959C13.3873 10.959 12.7196 11.2355 12.2273 11.7278C11.7351 12.2201 11.4585 12.8878 11.4585 13.584C11.4585 14.2802 11.7351 14.9479 12.2273 15.4401C12.7196 15.9324 13.3873 16.209 14.0835 16.209C14.7797 16.209 15.4474 15.9324 15.9397 15.4401C16.4319 14.9479 16.7085 14.2802 16.7085 13.584C16.7085 12.8878 16.4319 12.2201 15.9397 11.7278C15.4474 11.2355 14.7797 10.959 14.0835 10.959Z"
-          fill={fillColor}
+          fill={color}
         />
         <path
           fillRule="evenodd"
           clipRule="evenodd"
           d="M14.0833 6C11.029 6 8.28267 7.22617 6.304 8.74517C5.31233 9.50583 4.49917 10.3505 3.92867 11.1718C3.36867 11.9792 3 12.8262 3 13.5833C3 14.3405 3.36983 15.1875 3.92867 15.9937C4.50033 16.8162 5.31233 17.6608 6.304 18.4203C8.28267 19.9417 11.029 21.1667 14.0833 21.1667C17.1377 21.1667 19.884 19.9405 21.8627 18.4215C22.8543 17.6608 23.6675 16.8162 24.2368 15.9948C24.7968 15.1875 25.1667 14.3405 25.1667 13.5833C25.1667 12.8262 24.7968 11.9792 24.2368 11.173C23.6675 10.3505 22.8543 9.50583 21.8627 8.74633C19.884 7.225 17.1377 6 14.0833 6ZM9.70833 13.5833C9.70833 12.423 10.1693 11.3102 10.9897 10.4897C11.8102 9.66927 12.923 9.20833 14.0833 9.20833C15.2437 9.20833 16.3565 9.66927 17.1769 10.4897C17.9974 11.3102 18.4583 12.423 18.4583 13.5833C18.4583 14.7437 17.9974 15.8565 17.1769 16.6769C16.3565 17.4974 15.2437 17.9583 14.0833 17.9583C12.923 17.9583 11.8102 17.4974 10.9897 16.6769C10.1693 15.8565 9.70833 14.7437 9.70833 13.5833Z"
-          fill={fillColor}
+          fill={color}
         />
       </svg>
     </div>
