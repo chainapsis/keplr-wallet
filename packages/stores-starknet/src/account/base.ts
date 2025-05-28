70f5dff2e876513373c6253ccbb7305367e24d17
rowan <crewe1746@naver.com>
2025-05-28 17:50:51 +0900

    Update `stores-starknet` to use only v3 tx type

--- packages/stores-starknet/src/account/base.ts
+++ packages/stores-starknet/src/account/base.ts
@@ -9,9 +9,18 @@ import {
   num,
   DeployAccountSignerDetails,
   InvocationsSignerDetails,
+  ETransactionVersion,
+  OutsideExecutionVersion,
+  UniversalDetails,
 } from "starknet";
 import { StoreAccount } from "./internal";
 import { Dec, DecUtils, Int } from "@keplr-wallet/unit";
+
+export type Fee = {
+  gas: string;
+  maxGasPrice: string;
+  gasTokenAddress?: string;
+};
 
 export class StarknetAccountBase {
   @observable
@@ -51,7 +60,7 @@ export class StarknetAccountBase {
     classHash: string,
     constructorCalldata: RawArgs,
     addressSalt: string,
-    feeType: "ETH" | "STRK"
+    gasTokenAddress?: string
   ) {
     const modularChainInfo = this.chainGetter.getModularChain(this.chainId);
     if (!("starknet" in modularChainInfo)) {
@@ -71,9 +80,7 @@ export class StarknetAccountBase {
         constructorCalldata,
         addressSalt,
       },
-      {
-        version: feeType === "ETH" ? "0x1" : "0x3",
-      }
+      await this._buildUniversalDetails(sender, gasTokenAddress)
     );
   }
 
@@ -82,7 +89,7 @@ export class StarknetAccountBase {
     classHash: string,
     constructorCalldata: RawArgs,
     addressSalt: string,
-    feeType: "ETH" | "STRK",
+    feeTokenAddress?: string,
     {
       onFulfilled,
       onBroadcastFailed,
@@ -110,9 +117,7 @@ export class StarknetAccountBase {
           constructorCalldata,
           addressSalt,
         },
-        {
-          version: feeType === "ETH" ? "0x1" : "0x3",
-        }
+        await this._buildUniversalDetails(sender, feeTokenAddress)
       );
 
       onFulfilled?.(res);
@@ -126,16 +131,7 @@ export class StarknetAccountBase {
     classHash: string,
     constructorCalldata: RawArgs,
     addressSalt: string,
-    fee:
-      | {
-          type: "ETH";
-          maxFee: string;
-        }
-      | {
-          type: "STRK";
-          gas: string;
-          maxGasPrice: string;
-        },
+    fee: Fee,
     preSigned?: {
       transaction: DeployAccountSignerDetails;
       signature: string[];
@@ -172,7 +168,7 @@ export class StarknetAccountBase {
   async estimateInvokeFee(
     sender: string,
     calls: Call[],
-    feeType: "ETH" | "STRK"
+    gasTokenAddress?: string
   ) {
     const modularChainInfo = this.chainGetter.getModularChain(this.chainId);
     if (!("starknet" in modularChainInfo)) {
@@ -186,9 +182,10 @@ export class StarknetAccountBase {
       this.getKeplr
     );
 
-    return await walletAccount.estimateInvokeFee(calls, {
-      version: feeType === "ETH" ? "0x1" : "0x3",
-    });
+    return await walletAccount.estimateInvokeFee(
+      calls,
+      await this._buildUniversalDetails(sender, gasTokenAddress)
+    );
   }
 
   async estimateInvokeFeeForSendTokenTx(
@@ -203,7 +200,7 @@ export class StarknetAccountBase {
       sender: string;
       recipient: string;
     },
-    feeType: "ETH" | "STRK"
+    gasTokenAddress?: string
   ) {
     const actualAmount = (() => {
       let dec = new Dec(amount);
@@ -224,22 +221,13 @@ export class StarknetAccountBase {
       },
     ];
 
-    return await this.estimateInvokeFee(sender, calls, feeType);
+    return await this.estimateInvokeFee(sender, calls, gasTokenAddress);
   }
 
   async execute(
     sender: string,
     calls: Call[],
-    fee:
-      | {
-          type: "ETH";
-          maxFee: string;
-        }
-      | {
-          type: "STRK";
-          gas: string;
-          maxGasPrice: string;
-        },
+    fee: Fee,
     signTx?: (
       chainId: string,
       calls: Call[],
@@ -270,16 +258,7 @@ export class StarknetAccountBase {
     amount: string,
     currency: ERC20Currency,
     recipient: string,
-    fee:
-      | {
-          type: "ETH";
-          maxFee: string;
-        }
-      | {
-          type: "STRK";
-          gas: string;
-          maxGasPrice: string;
-        }
+    fee: Fee
   ) {
     const actualAmount = (() => {
       let dec = new Dec(amount);
@@ -315,5 +294,105 @@ export class StarknetAccountBase {
     );
 
     return new Int(num.toBigInt(await walletAccount.getNonce()));
+  }
+
+  async getOutsideExecutionSupported(sender: string) {
+    const modularChainInfo = this.chainGetter.getModularChain(this.chainId);
+    if (!("starknet" in modularChainInfo)) {
+      throw new Error(`${this.chainId} is not starknet chain`);
+    }
+
+    const walletAccount = new StoreAccount(
+      modularChainInfo.starknet.rpc,
+      sender,
+      this.chainId,
+      this.getKeplr
+    );
+
+    const snip9Version = await walletAccount.getSnip9Version();
+    return snip9Version !== OutsideExecutionVersion.UNSUPPORTED;
+  }
+
+  private async _buildUniversalDetails(
+    sender: string,
+    gasTokenAddress?: string
+  ): Promise<UniversalDetails> {
+    const details: UniversalDetails = {
+      version: ETransactionVersion.V3,
+    };
+
+    if (gasTokenAddress) {
+      // TODO: if account is not deployed, it will throw error.
+      // Thus, we need to check if the account is deployed
+      // or find is there any way to check the contract to deploy is supported by paymaster.
+      const isSupported = await this.getOutsideExecutionSupported(sender);
+      if (!isSupported) {
+        // TODO: handle this case
+        // throw new Error("Outside execution is not supported");
+      }
+
+      const modularChainInfo = this.chainGetter.getModularChain(this.chainId);
+      if (!("starknet" in modularChainInfo)) {
+        throw new Error(`${this.chainId} is not starknet chain`);
+      }
+
+      const walletAccount = new StoreAccount(
+        modularChainInfo.starknet.rpc,
+        sender,
+        this.chainId,
+        this.getKeplr
+      );
+
+      const isAvailable = await walletAccount.paymaster.isAvailable();
+      if (!isAvailable) {
+        throw new Error("Paymaster is not available");
+      }
+
+      // TODO: observable query로 빼기
+      const supportedTokens =
+        await walletAccount.paymaster.getSupportedTokens();
+
+      let found = false;
+      let gasTokenAddressWithout0x = gasTokenAddress.replace("0x", "");
+
+      for (const token of supportedTokens) {
+        // 0x 떼고 비교, 길이가 맞지 않으면 짧은 쪽에 zero padding 추가
+        let tokenAddressWithout0x = token.token_address.replace("0x", "");
+        if (tokenAddressWithout0x.length < gasTokenAddressWithout0x.length) {
+          tokenAddressWithout0x = tokenAddressWithout0x.padStart(
+            gasTokenAddressWithout0x.length,
+            "0"
+          );
+        } else if (
+          tokenAddressWithout0x.length > gasTokenAddressWithout0x.length
+        ) {
+          gasTokenAddressWithout0x = gasTokenAddressWithout0x.padStart(
+            tokenAddressWithout0x.length,
+            "0"
+          );
+        }
+
+        if (
+          tokenAddressWithout0x.toLowerCase() ===
+          gasTokenAddressWithout0x.toLowerCase()
+        ) {
+          found = true;
+          break;
+        }
+      }
+
+      if (!found) {
+        throw new Error("Gas token is not supported by paymaster");
+      }
+
+      details.paymaster = {
+        feeMode: {
+          mode: "default",
+          gasToken: gasTokenAddress,
+        },
+      };
+    }
+
+    return details;
   }
 }
