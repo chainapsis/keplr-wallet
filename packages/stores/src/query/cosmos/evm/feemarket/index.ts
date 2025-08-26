b69ce683b3638d503ac8b764e11413e8847caeea
BigtoC <taitocdt@gmail.com>
2025-08-26 16:35:40 +0800

    feat: Add a new query method for EVM fee market queries

---
+++ packages/stores/src/query/cosmos/evm/feemarket/index.ts
@@ -0,0 +1,26 @@
+import { ObservableChainQuery } from "../../../chain-query";
+import { BaseFee } from "./types";
+import { QuerySharedContext } from "../../../../common";
+import { ChainGetter } from "../../../../chain";
+import { makeObservable } from "mobx";
+import { Dec } from "@keplr-wallet/unit";
+
+export class ObservableQueryEvmFeeMarketBaseFee extends ObservableChainQuery<BaseFee> {
+  constructor(
+    sharedContext: QuerySharedContext,
+    chainId: string,
+    chainGetter: ChainGetter
+  ) {
+    super(sharedContext, chainId, chainGetter, "/feemarket/v1/base_fee");
+
+    makeObservable(this);
+  }
+
+  get baseFee(): Dec | null {
+    if (!this.response || !this.response.data.base_fee) {
+      return null;
+    }
+
+    return new Dec(this.response.data.base_fee);
+  }
+}
