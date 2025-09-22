cf6b609b88f0010208e2cc44a5a2cc1c12ded768
Thunnini <yunjh1994@gmail.com>
2025-09-22 15:50:42 +0900

    Revert "USDC <-> USDN swap에 대한 경고 삭제"
    This reverts commit 41b44e2451e58345d0e70435d9250d96128f2cfd.

--- apps/extension/src/pages/ibc-swap/index.tsx
+++ apps/extension/src/pages/ibc-swap/index.tsx
@@ -29,11 +29,11 @@ import { useNotification } from "../../hooks/notification";
 import { FormattedMessage, useIntl } from "react-intl";
 import { SwapFeeBps, TermsOfUseUrl } from "../../config.ui";
 import { BottomTabsHeightRem } from "../../bottom-tabs";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { useTxConfigsQueryString } from "../../hooks/use-tx-config-query-string";
 import { MainHeaderLayout } from "../main/layouts/header";
 import { XAxis } from "../../components/axis";
-import { Caption2, H4 } from "../../components/typography";
+import { Caption2, H4, Subtitle4 } from "../../components/typography";
 import { SlippageModal } from "./components/slippage-modal";
 import styled, { useTheme } from "styled-components";
 import { GuideBox } from "../../components/guide-box";
@@ -62,6 +62,10 @@ import {
 import { EthTxStatus } from "@keplr-wallet/types";
 import { InsufficientFeeError } from "@keplr-wallet/hooks";
 import { useSwapAnalytics } from "./hooks/use-swap-analytics";
+import {
+  validateIsUsdcFromNoble,
+  validateIsUsdnFromNoble,
+} from "../earn/utils";
 
 const TextButtonStyles = {
   Container: styled.div`
@@ -814,6 +818,39 @@ export const IBCSwapPage: FunctionComponent = observer(() => {
 
   const isSwap = ibcSwapConfigs.amountConfig.type === "swap";
 
+  const showUSDNWarning = (() => {
+    if (
+      validateIsUsdcFromNoble(
+        ibcSwapConfigs.amountConfig.currency,
+        ibcSwapConfigs.amountConfig.chainId
+      )
+    ) {
+      if (
+        validateIsUsdnFromNoble(
+          ibcSwapConfigs.amountConfig.outCurrency,
+          ibcSwapConfigs.amountConfig.outChainId
+        )
+      ) {
+        return true;
+      }
+    }
+    if (
+      validateIsUsdnFromNoble(
+        ibcSwapConfigs.amountConfig.currency,
+        ibcSwapConfigs.amountConfig.chainId
+      )
+    ) {
+      if (
+        validateIsUsdcFromNoble(
+          ibcSwapConfigs.amountConfig.outCurrency,
+          ibcSwapConfigs.amountConfig.outChainId
+        )
+      ) {
+        return true;
+      }
+    }
+    return false;
+  })();
   const showCelestiaWarning = (() => {
     if (uiConfigStore.ibcSwapConfig.celestiaDisabled) {
       return (
@@ -1966,6 +2003,7 @@ export const IBCSwapPage: FunctionComponent = observer(() => {
         />
 
         <WarningGuideBox
+          showUSDNWarning={showUSDNWarning}
           showCelestiaWarning={showCelestiaWarning}
           amountConfig={ibcSwapConfigs.amountConfig}
           feeConfig={ibcSwapConfigs.feeConfig}
@@ -2029,7 +2067,9 @@ export const IBCSwapPage: FunctionComponent = observer(() => {
 
         <Button
           type="submit"
-          disabled={interactionBlocked || showCelestiaWarning}
+          disabled={
+            interactionBlocked || showUSDNWarning || showCelestiaWarning
+          }
           text={intl.formatMessage({
             id: "page.ibc-swap.button.next",
           })}
@@ -2083,6 +2123,7 @@ const WarningGuideBox: FunctionComponent<{
   forceWarning?: Error;
   title?: string;
 
+  showUSDNWarning?: boolean;
   showCelestiaWarning?: boolean;
 }> = observer(
   ({
@@ -2092,9 +2133,11 @@ const WarningGuideBox: FunctionComponent<{
     forceError,
     forceWarning,
     title,
+    showUSDNWarning,
     showCelestiaWarning,
   }) => {
     const intl = useIntl();
+    const theme = useTheme();
 
     const error: string | undefined = (() => {
       if (feeConfig.uiProperties.error) {
@@ -2209,6 +2252,13 @@ const WarningGuideBox: FunctionComponent<{
       return err;
     })();
 
+    if (showUSDNWarning) {
+      title = "Swap Smarter";
+      errorText =
+        "To avoid high slippage, use Deposit or Withdraw on the Earn page.";
+      collapsed = false;
+    }
+
     if (showCelestiaWarning) {
       title = "Temporarily Unavailable";
       errorText =
@@ -2224,10 +2274,35 @@ const WarningGuideBox: FunctionComponent<{
         </VerticalCollapseTransition>
         <VerticalCollapseTransition collapsed={collapsed}>
           <GuideBox
-            color={"warning"}
+            color={showUSDNWarning ? "default" : "warning"}
             title={title || errorText}
             paragraph={title ? errorText : undefined}
             hideInformationIcon={!title}
+            backgroundColor={(() => {
+              if (showUSDNWarning && theme.mode === "light") {
+                return ColorPalette["gray-10"];
+              }
+            })()}
+            bottom={(() => {
+              if (showUSDNWarning) {
+                return (
+                  <Link to="/earn/overview">
+                    <Subtitle4
+                      style={{
+                        textDecorationLine: "underline",
+                      }}
+                      color={
+                        theme.mode === "light"
+                          ? ColorPalette["gray-600"]
+                          : ColorPalette["gray-100"]
+                      }
+                    >
+                      Go to Earn page
+                    </Subtitle4>
+                  </Link>
+                );
+              }
+            })()}
           />
         </VerticalCollapseTransition>
       </React.Fragment>
