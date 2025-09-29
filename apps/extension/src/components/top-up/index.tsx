04f7a2a199df65bc3ef840dc873185b35f042b3d
kws <kws1207@kaist.ac.kr>
2025-09-29 18:29:38 +0900

    Display FeeCoverageBox to internal tx confirm page

--- apps/extension/src/components/top-up/index.tsx
+++ apps/extension/src/components/top-up/index.tsx
@@ -1,7 +1,8 @@
 import { IFeeConfig } from "@keplr-wallet/hooks";
 import React from "react";
-import { Subtitle4 } from "../typography";
+import { Subtitle3, Subtitle4 } from "../typography";
 import { ColorPalette } from "../../styles";
+import { useStore } from "../../stores";
 
 export const FeeCoverageDescription = ({
   feeConfig,
@@ -9,7 +10,14 @@ export const FeeCoverageDescription = ({
   feeConfig: IFeeConfig;
 }) => {
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "0.25rem" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "0.25rem",
+        lineHeight: "155%",
+      }}
+    >
       <Subtitle4
         color={ColorPalette["gray-300"]}
         style={{ textAlign: "center" }}
@@ -21,6 +29,83 @@ export const FeeCoverageDescription = ({
         </span>{" "}
         for you :)
       </Subtitle4>
+    </div>
+  );
+};
+
+export const FeeCoverageBox = ({ feeConfig }: { feeConfig: IFeeConfig }) => {
+  const { priceStore } = useStore();
+  const price = (() => {
+    if (!feeConfig.fees[0]) {
+      return undefined;
+    }
+    return priceStore.calculatePrice(feeConfig.fees[0]);
+  })();
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "0.75rem",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          padding: "1rem",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.75rem",
+          borderRadius: "0.375rem",
+          background: ColorPalette["gray-600"],
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <Subtitle3 color={ColorPalette["gray-200"]}>Tx Fee</Subtitle3>
+
+          <Subtitle3 style={{ textDecoration: "line-through" }}>
+            <span style={{ color: ColorPalette["gray-50"] }}>
+              {feeConfig.fees[0]?.toString()}{" "}
+            </span>
+            <span style={{ color: ColorPalette["gray-300"] }}>
+              {price?.toString()}
+            </span>
+          </Subtitle3>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <Subtitle3 color={ColorPalette["gray-200"]}>
+            Covered by Keplr
+          </Subtitle3>
+          <Subtitle3 color={ColorPalette["gray-50"]}>
+            0 {feeConfig.fees[0]?.denom}
+          </Subtitle3>
+        </div>
+      </div>
+      <Subtitle3
+        color={ColorPalette["gray-300"]}
+        style={{ textAlign: "center" }}
+      >
+        Keep this window open during the transaction
+      </Subtitle3>
     </div>
   );
 };
