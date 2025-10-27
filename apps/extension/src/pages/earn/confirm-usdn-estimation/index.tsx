2d987ee592529fac90aeafa639075c38b65ec873
Thunnini <yunjh1994@gmail.com>
2025-10-27 15:32:17 +0900

    lint

--- apps/extension/src/pages/earn/confirm-usdn-estimation/index.tsx
+++ apps/extension/src/pages/earn/confirm-usdn-estimation/index.tsx
@@ -383,7 +383,8 @@ export const EarnConfirmUsdnEstimationPage: FunctionComponent = observer(() => {
   );
 });
 
-const LEARN_MORE_URL = "https://help.keplr.app/extension/5R3bMyjtr2FwnBvJQuJwJu/earn-more-with-keplr/5R3bMyjtqZ3BsEKHQy2tYf";
+const LEARN_MORE_URL =
+  "https://help.keplr.app/extension/5R3bMyjtr2FwnBvJQuJwJu/earn-more-with-keplr/5R3bMyjtqZ3BsEKHQy2tYf";
 
 const Styles = {
   Link: styled.span`
