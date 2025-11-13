a990f034586164b54ea30e8455098dc23a5463fd
JungHwan Tony Yun <yunjh1994@gmail.com>
2025-11-13 20:52:18 +0900

    Merge pull request #1650 from chainapsis/roy/keplr-1361
    Add Neutron governance proposal and vote queries to stores package

--- packages/stores/src/query/cosmwasm/index.ts
+++ packages/stores/src/query/cosmwasm/index.ts
@@ -3,3 +3,5 @@ export * as CosmWasm from "./types";
 export * from "./queries";
 export * from "./neutron/staking-rewards";
 export * from "./neutron/staking-rewards-config";
+export * from "./neutron/governance-proposals";
+export * from "./neutron/governance-vote";
