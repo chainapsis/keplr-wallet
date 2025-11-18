4bf30fe83f0d81c4bb08b3c85b1957d3e823a9be
rowan <crewe1746@naver.com>
2025-11-18 15:35:45 +0900

    minor fix

--- apps/stores-internal/src/swap/queries.ts
+++ apps/stores-internal/src/swap/queries.ts
@@ -4,6 +4,7 @@ import { ObservableQuerySwappable } from "./swappable";
 import { InternalChainStore } from "../internal";
 import { ObservableQueryTargetAssets } from "./target-assets";
 import { ObservableQueryRelatedAssets } from "./related-assets";
+import { ObservableQueryValidateTargetAssets } from "./validate-target-assets";
 import { ObservableQueryRouteV2 } from "./route";
 import { ObservableQueryChainsV2 } from "./chains";
 import { ObservableQueryTransferPaths } from "./transfer-paths";
@@ -14,6 +15,7 @@ export class SwapQueries {
   public readonly querySwappable: DeepReadonly<ObservableQuerySwappable>;
   public readonly queryTargetAssets: DeepReadonly<ObservableQueryTargetAssets>;
   public readonly queryRelatedAssets: DeepReadonly<ObservableQueryRelatedAssets>;
+  public readonly queryValidateTargetAssets: DeepReadonly<ObservableQueryValidateTargetAssets>;
   public readonly queryRoute: DeepReadonly<ObservableQueryRouteV2>;
 
   public readonly queryTransferPaths: DeepReadonly<ObservableQueryTransferPaths>;
@@ -39,6 +41,11 @@ export class SwapQueries {
       baseURL
     );
     this.queryRelatedAssets = new ObservableQueryRelatedAssets(
+      sharedContext,
+      chainStore,
+      baseURL
+    );
+    this.queryValidateTargetAssets = new ObservableQueryValidateTargetAssets(
       sharedContext,
       chainStore,
       baseURL
