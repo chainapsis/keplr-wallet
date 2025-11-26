3baaa52c5ab0f2b0b2fdbf52e44894310c84d61a
rowan <crewe1746@naver.com>
2025-11-26 15:50:39 +0900

    define messages for direct tx execution

--- packages/background/src/direct-tx-executor/init.ts
+++ packages/background/src/direct-tx-executor/init.ts
@@ -2,10 +2,20 @@ import { Router } from "@keplr-wallet/router";
 import { ROUTE } from "./constants";
 import { getHandler } from "./handler";
 import { DirectTxExecutorService } from "./service";
-import { GetDirectTxExecutorDataMsg } from "./messages";
+import {
+  RecordAndExecuteDirectTxsMsg,
+  GetDirectTxsExecutionDataMsg,
+  ExecuteDirectTxMsg,
+  CancelDirectTxsExecutionMsg,
+  GetDirectTxsExecutionResultMsg,
+} from "./messages";
 
 export function init(router: Router, service: DirectTxExecutorService): void {
-  router.registerMessage(GetDirectTxExecutorDataMsg);
+  router.registerMessage(RecordAndExecuteDirectTxsMsg);
+  router.registerMessage(ExecuteDirectTxMsg);
+  router.registerMessage(GetDirectTxsExecutionDataMsg);
+  router.registerMessage(GetDirectTxsExecutionResultMsg);
+  router.registerMessage(CancelDirectTxsExecutionMsg);
 
   router.addHandler(ROUTE, getHandler(service));
 }
