2f0be5808aa48a83cdf2f8440e8bd2786db84416
rowan <crewe1746@naver.com>
2025-12-09 10:45:07 +0900

    add missing isSpecial flag

--- apps/extension/src/pages/sign/ethereum/views/sign-eip712-view.tsx
+++ apps/extension/src/pages/sign/ethereum/views/sign-eip712-view.tsx
@@ -160,7 +160,7 @@ export const EthereumSignEIP712View: FunctionComponent<{
     },
     {
       text: intl.formatMessage({ id: "button.approve" }),
-      color: "primary",
+      isSpecial: true,
       size: "large",
       left: !isLoading && <ApproveIcon />,
       isLoading,
